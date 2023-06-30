590c24ead70465a65e5095d4cb876841e1f43a0b
Alan Treadway <alan.treadway@ag-grid.com>
2023-06-30 11:15:34 +0100

    Add ag-charts-community-examples tweaks.

---
+++ packages/ag-charts-community-examples/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'ag-charts-community-examples',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/ag-charts-community-examples',
+};
