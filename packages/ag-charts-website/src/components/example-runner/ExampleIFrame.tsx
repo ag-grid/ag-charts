48fd27886f0af8830fe4c27ec00184bb5c252c23
Tak Tran <contact@tutaktran.com>
2023-07-06 14:52:55 +0100

    Only show example iframe if it is visible on the screen

---
+++ packages/ag-charts-website/src/components/example-runner/ExampleIFrame.tsx
@@ -0,0 +1,27 @@
+import { useRef, type FunctionComponent } from 'react';
+import styles from './ExampleIFrame.module.scss';
+import { useIntersectionObserver } from '../../utils/hooks/useIntersectionObserver';
+
+interface Props {
+    url: string;
+}
+
+export const ExampleIFrame: FunctionComponent<Props> = ({ url }) => {
+    const iFrameRef = useRef<HTMLIFrameElement>(null);
+
+    // Only show example iFrame if it is visible on the screen
+    useIntersectionObserver({
+        elementRef: iFrameRef,
+        onChange: ({ isIntersecting }) => {
+            if (isIntersecting && iFrameRef.current) {
+                iFrameRef.current.src = url;
+            }
+        },
+    });
+
+    return (
+        <div className={styles.container}>
+            <iframe ref={iFrameRef} className={styles.iframe} />;
+        </div>
+    );
+};
