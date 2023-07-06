84193a679c102040915ed501567067401a97df9d
Tak Tran <contact@tutaktran.com>
2023-07-06 15:43:09 +0100

    Add hmr file in nx root to server allow list

--- packages/ag-charts-website/astro.config.mjs
+++ packages/ag-charts-website/astro.config.mjs
@@ -4,12 +4,22 @@ import markdoc from '@astrojs/markdoc';
 import svgr from 'vite-plugin-svgr';
 import agFileWatcher from './src/astro/integrations/agFileWatcher';
 import agHotModuleReload from './src/astro/plugins/agHotModuleReload';
+import { getDevFileList } from './src/utils/pages';
 
 // https://astro.build/config
 export default defineConfig({
     outDir: '../../dist/packages/ag-charts-website',
     vite: {
         plugins: [svgr(), agHotModuleReload()],
+        server: {
+            fs: {
+                allow: [
+                    '.',
+                    // Nx root node modules - for hmr (hot module reloading)
+                    '../../node_modules/astro/dist/runtime/client',
+                ].concat(getDevFileList()),
+            },
+        },
     },
     integrations: [react(), markdoc(), agFileWatcher()],
 });
