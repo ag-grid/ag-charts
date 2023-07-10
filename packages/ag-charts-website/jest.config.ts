d0c8ac0fce2f89c64212b2293be6198322a8d2b2
Tak Tran <contact@tutaktran.com>
2023-07-10 11:38:14 +0100

    Set up tests for ag-charts-website

---
+++ packages/ag-charts-website/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+    displayName: 'ag-charts-website',
+    preset: '../../jest.preset.js',
+    transform: {
+        '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    testPathIgnorePatterns: ['examples-generator/transformation-scripts', 'snippet/snippetTransformer'],
+};
