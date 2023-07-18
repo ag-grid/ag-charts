9e3b371a78812bd2533bec34584e8c07fb179151
Mana <mana.jalali.15@ucl.ac.uk>
2023-07-18 15:24:00 +0100

    AG-4337 - Fix highlight label zIndex and legend data

--- packages/ag-charts-enterprise/src/waterfall/waterfallSeries.ts
+++ packages/ag-charts-enterprise/src/waterfall/waterfallSeries.ts
@@ -745,16 +745,27 @@ export class WaterfallBarSeries extends _ModuleSupport.CartesianSeries<
 
         const legendData: _ModuleSupport.CategoryLegendDatum[] = [];
 
-        function getLegendItemText(item: SeriesItemType, name?: string): string | undefined {
+        function getLegendItemText(item: SeriesItemType, index: number, name?: string): string | undefined {
             if (name !== undefined) {
                 return name;
             }
+<<<<<<< HEAD
             return `${item.charAt(0).toUpperCase()}${item.substring(1)}`;
+=======
+            if (item === 'subtotal') {
+                return 'Subtotal';
+            }
+            if (item === 'total') {
+                return 'Total';
+            }
+            return `${yName ?? yKey}-${index}`;
+>>>>>>> 3e2ea7c2 (AG-4337 - Fix highlight label zIndex and legend data)
         }
 
+        let index = 0;
         seriesItemTypes.forEach((item) => {
             const { fill, stroke, fillOpacity, strokeOpacity, name } = this.getItemConfig(item);
-            const legendItemText = getLegendItemText(item, name);
+            const legendItemText = getLegendItemText(item, index, name);
             legendData.push({
                 legendType: 'category',
                 id,
@@ -771,6 +782,7 @@ export class WaterfallBarSeries extends _ModuleSupport.CartesianSeries<
                     strokeOpacity: strokeOpacity,
                 },
             });
+            index++;
         });
 
         return legendData;
