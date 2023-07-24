eef3c608a6b5bdde8697bb4cdfad45a2ba194774
Mana <mana.jalali.15@ucl.ac.uk>
2023-07-24 16:50:57 +0100

    AG-8682- disable labels by default

--- packages/ag-charts-enterprise/src/waterfall/waterfallThemes.ts
+++ packages/ag-charts-enterprise/src/waterfall/waterfallThemes.ts
@@ -11,14 +11,23 @@ export const WATERFALL_SERIES_THEME = {
         __overrides__: _Theme.OVERRIDE_SERIES_LABEL_DEFAULTS,
     },
     positiveItem: {
+        label: {
+            enabled: false,
+        },
         fill: '#233e6f',
         stroke: '#233e6f',
     },
     negativeItem: {
+        label: {
+            enabled: false,
+        },
         fill: '#9FB7E1',
         stroke: '#9FB7E1',
     },
     totalItem: {
+        label: {
+            enabled: false,
+        },
         fill: '#79706E',
         stroke: '#79706E',
     },
