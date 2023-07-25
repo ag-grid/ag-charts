94d73539cf7b6cdddab05d3ff7ddd9a2250f50c1
Mana <mana.jalali.15@ucl.ac.uk>
2023-07-25 11:47:49 +0100

    AG-8682 - remove `series.label` defaults from theme and options

--- packages/ag-charts-enterprise/src/waterfall/waterfallThemes.ts
+++ packages/ag-charts-enterprise/src/waterfall/waterfallThemes.ts
@@ -2,14 +2,6 @@ import { _Theme } from 'ag-charts-community';
 
 export const WATERFALL_SERIES_THEME = {
     __extends__: _Theme.EXTENDS_SERIES_DEFAULTS,
-    label: {
-        enabled: false,
-        placement: 'end',
-        fontSize: 12,
-        fontFamily: _Theme.DEFAULT_FONT_FAMILY,
-        color: 'rgb(70, 70, 70)',
-        __overrides__: _Theme.OVERRIDE_SERIES_LABEL_DEFAULTS,
-    },
     positiveItem: {
         label: {
             enabled: false,
