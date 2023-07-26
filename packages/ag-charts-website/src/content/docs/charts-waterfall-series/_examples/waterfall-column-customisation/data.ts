0931aab430f81b690f26b327c95938f542ef35bb
Mana <mana.jalali.15@ucl.ac.uk>
2023-07-26 17:29:02 +0100

    AG-8936 - Waterfall series docs

---
+++ packages/ag-charts-website/src/content/docs/charts-waterfall-series/_examples/waterfall-column-customisation/data.ts
@@ -0,0 +1,60 @@
+export function getData() {
+    return [
+        {
+            date: 'July 3',
+            amount: 900,
+        },
+        {
+            date: 'July 4',
+            amount: 345,
+        },
+        {
+            date: 'July 5',
+            amount: 393,
+        },
+        {
+            date: 'July 6',
+            amount: -108,
+        },
+        {
+            date: 'July 7',
+            amount: -154,
+        },
+        {
+            date: 'Subtotal 1',
+            datumType: 'subtotal',
+        },
+        {
+            date: 'July 10',
+            amount: 135,
+        },
+        {
+            date: 'July 11',
+            amount: 178,
+        },
+        {
+            date: 'July 12',
+            amount: 286,
+        },
+        {
+            date: 'July 13',
+            amount: -119,
+        },
+        {
+            date: 'July 14',
+            amount: -361,
+        },
+        {
+            date: 'Subtotal 2',
+            datumType: 'subtotal',
+        },
+        {
+            date: 'July 17',
+            amount: -203,
+        },
+        {
+            date: 'Total',
+            datumType: 'total',
+        },
+    ];
+}
