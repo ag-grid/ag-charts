ab0741ae01af31d60192f5af362f51228c9f52f1
Alexander Shutau <shutovby@gmail.com>
2023-07-28 14:29:43 +0100

    AG-8973 Nightingale doc page

---
+++ packages/ag-charts-website/src/content/docs/charts-nightingale-series/_examples/group-nightingale/data.ts
@@ -0,0 +1,52 @@
+export function getData() {
+    return [
+        {
+            quarter: `Q1'22`,
+            'Mountain air': 4.35,
+            'Polar winds': 2.14,
+            'Donut holes': 3.91,
+        },
+        {
+            quarter: `Q2'22`,
+            'Mountain air': 4.28,
+            'Polar winds': 3.13,
+            'Donut holes': 3.04,
+        },
+        {
+            quarter: `Q3'22`,
+            'Mountain air': 4.14,
+            'Polar winds': 3.34,
+            'Donut holes': 3.18,
+        },
+        {
+            quarter: `Q4'22`,
+            'Mountain air': 3.48,
+            'Polar winds': 3.56,
+            'Donut holes': 3.61,
+        },
+        {
+            quarter: `Q1'23`,
+            'Mountain air': 3.35,
+            'Polar winds': 3.14,
+            'Donut holes': 3.91,
+        },
+        {
+            quarter: `Q2'23`,
+            'Mountain air': 3.28,
+            'Polar winds': 3.13,
+            'Donut holes': 3.54,
+        },
+        {
+            quarter: `Q3'23`,
+            'Mountain air': 3.14,
+            'Polar winds': 2.84,
+            'Donut holes': 3.18,
+        },
+        {
+            quarter: `Q4'23`,
+            'Mountain air': 2.48,
+            'Polar winds': 2.46,
+            'Donut holes': 3.21,
+        },
+    ];
+}
---
+++ packages/ag-charts-website/src/content/docs/charts-nightingale-series/_examples/stack-nightingale/main.ts
@@ -0,0 +1,47 @@
+import { AgEnterpriseCharts, AgChartOptions } from 'ag-charts-enterprise';
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: `Night & Gale Inc revenue by product category`,
+    },
+    subtitle: {
+        text: 'in million U.S. dollars',
+    },
+    series: [
+        {
+            type: 'nightingale',
+            angleKey: 'quarter',
+            radiusKey: 'Mountain air',
+            stacked: true,
+        },
+        {
+            type: 'nightingale',
+            angleKey: 'quarter',
+            radiusKey: 'Polar winds',
+            stacked: true,
+        },
+        {
+            type: 'nightingale',
+            angleKey: 'quarter',
+            radiusKey: 'Donut holes',
+            stacked: true,
+        },
+    ],
+    axes: [
+        {
+            type: 'angle-category',
+            shape: 'circle',
+            groupPaddingInner: 0.2,
+            groupPaddingOuter: 0.2,
+        },
+        {
+            type: 'radius-number',
+            shape: 'circle',
+        },
+    ],
+};
+
+const chart = AgEnterpriseCharts.create(options);
