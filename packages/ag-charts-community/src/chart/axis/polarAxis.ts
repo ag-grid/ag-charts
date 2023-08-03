8d8a0a238aeeea3b62132e5d545059060a742345
Alexander Shutau <shutovby@gmail.com>
2023-08-03 09:21:54 +0100

    AG-8997 Axis inner radius validation

--- packages/ag-charts-community/src/chart/axis/polarAxis.ts
+++ packages/ag-charts-community/src/chart/axis/polarAxis.ts
@@ -1,12 +1,14 @@
 import { Axis } from '../../axis';
 import type { BBox } from '../../scene/bbox';
 import type { Scale } from '../../scale/scale';
+import { Validate, NUMBER } from '../../util/validation';
 
 export abstract class PolarAxis<S extends Scale<any, any, any> = Scale<any, any, any>> extends Axis<S> {
     gridAngles: number[] | undefined;
 
     shape: 'polygon' | 'circle' = 'polygon';
 
+    @Validate(NUMBER(0, 1))
     innerRadiusRatio: number = 0;
 
     protected defaultTickMinSpacing = 20;
