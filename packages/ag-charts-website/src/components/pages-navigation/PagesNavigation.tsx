0780dfeb4aca6899d71562328026801dc9976346
Alan Treadway <alan.treadway@ag-grid.com>
2023-08-07 15:07:19 +0100

    Tweak sidebar ordering to make dev navigation easier.

--- packages/ag-charts-website/src/components/pages-navigation/PagesNavigation.tsx
+++ packages/ag-charts-website/src/components/pages-navigation/PagesNavigation.tsx
@@ -6,10 +6,15 @@ import { SITE_BASE_URL } from '../../constants';
 export function PagesNavigation({ pages, framework }: { pages: DocsPage[]; framework: string }) {
     const urlPrefix = `${SITE_BASE_URL}${framework}`;
 
+    // TODO: Do something better than just alpha sorting - but this should improve productivity
+    // in the meantime.
+    const sortedPages = [...pages];
+    sortedPages.sort((a, b) => a.data.title.localeCompare(b.data.title));
+
     return (
         <aside className={styles.nav}>
             <ul className={classnames('list-style-none', styles.navInner)}>
-                {pages.map(({ slug, data }: any) => {
+                {sortedPages.map(({ slug, data }: any) => {
                     const url = `${urlPrefix}/${slug}`;
                     return (
                         <li key={url} className={styles.navGroup}>
