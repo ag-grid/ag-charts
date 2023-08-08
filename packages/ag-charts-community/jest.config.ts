9f92b574a1ac21cce311bc7c946f51fc4d15655f
Alan Treadway <alan.treadway@ag-grid.com>
2023-08-08 14:53:24 +0100

    AG-9054 - Cleanup redundant code.

--- packages/ag-charts-community/jest.config.ts
+++ packages/ag-charts-community/jest.config.ts
@@ -1,5 +1,4 @@
 /* eslint-disable */
-import { registerFont } from 'canvas';
 import { readFileSync } from 'fs';
 import * as glob from 'glob';
 
@@ -40,8 +39,6 @@ const commonConfig = {
         '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
     },
 };
-
-registerFont('packages/ag-charts-community/src/chart/test/Inter-Regular.otf', { family: 'Verdana' });
 
 const reporters: any[] = ['default'];
 if (process.env.CI != null || process.env.NX_TASK_TARGET_CONFIGURATION === 'ci') {
--- packages/ag-charts-community/src/lib/ag-charts-community.spec.ts
+++
@@ -1,7 +0,0 @@
-import { agChartsCommunity } from './ag-charts-community';
-
-describe('agChartsCommunity', () => {
-    it('should work', () => {
-        expect(agChartsCommunity()).toEqual('ag-charts-community');
-    });
-});
