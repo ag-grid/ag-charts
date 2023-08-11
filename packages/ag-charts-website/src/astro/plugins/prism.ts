4f94c781a17cbee1c9f0de34429334c44286cb61
Tak Tran <contact@tutaktran.com>
2023-08-11 12:25:45 +0100

    AG-9075 - Add prism syntax highlighting
    Extend default astro prism plugin to add the `code` class for design system styles to come through

---
+++ packages/ag-charts-website/src/astro/plugins/prism.ts
@@ -0,0 +1,28 @@
+// @ts-expect-error Cannot find module 'astro/runtime/server/index.js' or its corresponding type declarations.
+import { unescapeHTML } from 'astro/runtime/server/index.js';
+
+import { runHighlighterWithAstro } from '@astrojs/prism/dist/highlighter';
+import { Markdoc, type AstroMarkdocConfig } from '@astrojs/markdoc/config';
+
+/**
+ * Extend default markdoc prism highlighter to include AG Charts `code` styles
+ *
+ * @see https://github.com/withastro/astro/blob/fa3e83984343d3b9c37af7af876ad03ac95bf1c6/packages/integrations/markdoc/src/extensions/prism.ts
+ */
+export default function prism(): AstroMarkdocConfig {
+    return {
+        nodes: {
+            fence: {
+                attributes: Markdoc.nodes.fence.attributes!,
+                transform({ attributes: { language, content } }) {
+                    const { html, classLanguage } = runHighlighterWithAstro(language, content);
+
+                    // Use `unescapeHTML` to return `HTMLString` for Astro renderer to inline as HTML
+                    return unescapeHTML(
+                        `<pre class="${classLanguage} code"><code class="${classLanguage}">${html}</code></pre>`
+                    );
+                },
+            },
+        },
+    };
+}
