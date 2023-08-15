5f2e03e77c3acf84217c850ca79fd700d0055974
Tak Tran <contact@tutaktran.com>
2023-08-15 16:29:56 +0100

    AG-3390 - Fix regex for git commands

--- packages/ag-charts-website/src/pages/debug/meta.json.ts
+++ packages/ag-charts-website/src/pages/debug/meta.json.ts
@@ -1,10 +1,11 @@
 import { execSync } from 'child_process';
 
 export async function get() {
+    const removeNewlineRegex = /\n/gm;
     const buildDate = new Date();
-    const hash = execSync('git rev-parse HEAD').toString().replace(/\s/gm, '');
-    const shortHash = execSync('git rev-parse --short HEAD').toString().replace(/\s/gm, '');
-    const gitDate = execSync('git --no-pager log -1 --format="%ai"').toString().replace(/\s/gm, '');
+    const hash = execSync('git rev-parse HEAD').toString().replace(removeNewlineRegex, '');
+    const shortHash = execSync('git rev-parse --short HEAD').toString().replace(removeNewlineRegex, '');
+    const gitDate = execSync('git --no-pager log -1 --format="%ai"').toString().replace(removeNewlineRegex, '');
 
     const response = {
         buildDate,
