2473cfc0dd310a533157949178b635588e033cee
Alan Treadway <alan.treadway@ag-grid.com>
2023-08-18 14:58:33 +0100

    AG-8930 - Fix build outputs for NPM packing.

---
+++ rollup.base.cjs
@@ -0,0 +1,45 @@
+module.exports = function buildConfig(name, { output, ...config }, { umd = {} } = {}) {
+    if (!Array.isArray(output)) {
+        output = [output];
+    }
+
+    const rename = (name, format) => {
+        // Workaround @nx/rollup:rollup package.json update assuming entry point is named 'index'.
+        // name = name.replace('[name]', 'index');
+
+        // if (format === 'esm') {
+        //     name = name.replace('.js', '.mjs');
+        // }
+
+        return name;
+    };
+
+    const result = {
+        ...config,
+        output: [
+            ...output.map(({ entryFileNames, chunkFileNames, format, ...opts }) => ({
+                ...opts,
+                format,
+                name,
+                entryFileNames: rename(entryFileNames, format),
+                chunkFileNames: rename(chunkFileNames, format),
+                sourcemap: true,
+            })),
+        ],
+    };
+
+    const { entryFileNames, chunkFileNames, format, ...opts } = result.output[0];
+    if (format === 'cjs') {
+        result.output.push({
+            ...opts,
+            name,
+            entryFileNames: 'index.umd.js',
+            chunkFileNames: 'index.umd.js',
+            sourcemap: true,
+            format: 'umd',
+            ...umd,
+        });
+    }
+
+    return result;
+};
