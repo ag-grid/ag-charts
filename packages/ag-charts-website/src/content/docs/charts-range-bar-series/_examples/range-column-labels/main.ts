4feefee33432e58a65a718f929e0856407a3b223
Mana <mana.jalali.15@ucl.ac.uk>
2023-08-22 16:28:29 +0100

    AG-9040 - Range Bar series docs

---
+++ packages/ag-charts-website/src/content/docs/charts-range-bar-series/_examples/range-column-labels/main.ts
@@ -0,0 +1,26 @@
+import { AgChartOptions, AgEnterpriseCharts, AgRangeBarSeriesLabelFormatterParams } from 'ag-charts-enterprise';
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'Range Column',
+    },
+    series: [
+        {
+            type: 'range-column',
+            xKey: 'date',
+            xName: 'Date',
+            yLowKey: 'low',
+            yHighKey: 'high',
+            label: {
+                formatter: ({ itemId, value }: AgRangeBarSeriesLabelFormatterParams) => {
+                    return `${itemId === 'low' ? 'L' : 'H'}: ${value.toFixed(0)}`;
+                }
+            }
+        } as any,
+    ],
+};
+
+AgEnterpriseCharts.create(options);
