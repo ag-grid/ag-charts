bc5a1e9e9567709240fb20e1d113b8a97d2ea91f
Mana <mana.jalali.15@ucl.ac.uk>
2023-08-23 15:34:19 +0100

    AG-9040 - Make separate docs pages for Range Bar and Column

---
+++ packages/ag-charts-website/src/content/docs/charts-range-bar-series/_examples/range-bar-labels/main.ts
@@ -0,0 +1,26 @@
+import { AgChartOptions, AgEnterpriseCharts, AgRangeBarSeriesLabelFormatterParams } from 'ag-charts-enterprise';
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'Range Bar',
+    },
+    series: [
+        {
+            type: 'range-bar',
+            xKey: 'date',
+            xName: 'Date',
+            yLowKey: 'low',
+            yHighKey: 'high',
+            label: {
+                formatter: ({ itemId, value }: AgRangeBarSeriesLabelFormatterParams) => {
+                    return `${itemId === 'low' ? 'L' : 'H'}: ${value.toFixed(0)}`;
+                }
+            }
+        },
+    ],
+};
+
+AgEnterpriseCharts.create(options);
