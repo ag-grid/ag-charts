deebac583fd6d758f7d2207626096154b6f8a106
Tak Tran <contact@tutaktran.com>
2023-08-31 16:35:00 +0100

    AG-9089 - Refactor docs header link icon into component

---
+++ packages/ag-charts-website/src/components/link-icon/LinkIcon.tsx
@@ -0,0 +1,15 @@
+import type { FunctionComponent } from 'react';
+import { Icon } from '@components/icon/Icon';
+import './LinkIcon.module.scss';
+
+interface Props {
+    href: string;
+}
+
+export const LinkIcon: FunctionComponent<Props> = ({ href }) => {
+    return (
+        <a href={href} className="docs-header-icon">
+            <Icon name="link" />
+        </a>
+    );
+};
