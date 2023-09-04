b89a9d2daacf210cbd264277491c0be16cbe4dda
rmc-software <rmc.software.consultancy@gmail.com>
2023-09-04 21:36:35 +0100

    AG-3390 - removed 'charts-' prefix from urls

--- packages/ag-charts-website/src/content/docs/range-area-series/_examples/basic-range-area/main.ts
+++ packages/ag-charts-website/src/content/docs/range-area-series/_examples/basic-range-area/main.ts
@@ -7,9 +7,6 @@ const options: AgChartOptions = {
     title: {
         text: 'Average High and Low Temperatures',
     },
-    subtitle: {
-        text: 'London 2022-2023'
-    },
     series: [
         {
             type: 'range-area',
