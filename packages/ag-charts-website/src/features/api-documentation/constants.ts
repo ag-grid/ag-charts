898553589552959be7e91e5a918593436556647f
Tak Tran <contact@tutaktran.com>
2023-09-08 16:20:06 +0100

    AG-9206 - Filter out axes and series inner options so the list is shorter

--- packages/ag-charts-website/src/features/api-documentation/constants.ts
+++ packages/ag-charts-website/src/features/api-documentation/constants.ts
@@ -1,4 +1,9 @@
 /**
+ * Property key used to differentiate between different union items
+ */
+export const UNION_DISCRIMINATOR_PROP = 'type';
+
+/**
  * Top level options reference items to limit the number of children shown
  */
 export const TOP_LEVEL_OPTIONS_TO_LIMIT_CHILDREN = ['axes', 'series'];
--- packages/ag-charts-website/src/features/api-documentation/utils/useJsObjectSelection.ts
+++ packages/ag-charts-website/src/features/api-documentation/utils/useJsObjectSelection.ts
@@ -6,6 +6,16 @@ import { smoothScrollIntoView } from '@utils/smoothScrollIntoView';
 import { getTopLevelSelection, getTopSelection } from './modelPath';
 import { TOP_LEVEL_OPTIONS_TO_LIMIT_CHILDREN } from '../constants';
 
+function selectionHasChanged({
+    selection,
+    newSelection,
+}: {
+    selection: JsObjectSelection;
+    newSelection: JsObjectSelection;
+}): boolean {
+    return JSON.stringify(newSelection.path) !== JSON.stringify(selection.path);
+}
+
 export function useJsObjectSelection({ model }: { model: JsonModel }) {
     const [selection, setSelection] = useState<JsObjectSelection>(getTopSelection({ model, hideChildren: true }));
 
@@ -16,33 +26,41 @@ export function useJsObjectSelection({ model }: { model: JsonModel }) {
 
             if (isTopLevelSelection) {
                 const { propName } = newSelection as JsObjectSelectionProperty;
-                const onlyShowToDepth = TOP_LEVEL_OPTIONS_TO_LIMIT_CHILDREN.includes(propName) ? 1 : undefined;
+                const shouldLimitChildren = TOP_LEVEL_OPTIONS_TO_LIMIT_CHILDREN.includes(propName);
+                const shouldLimitChildrenDepth = shouldLimitChildren ? 1 : undefined;
+                const onlyShowToDepth =
+                    newSelection.onlyShowToDepth === undefined
+                        ? shouldLimitChildrenDepth
+                        : newSelection.onlyShowToDepth;
                 setSelection({ ...newSelection, onlyShowToDepth });
                 smoothScrollIntoView('#top');
             } else {
-                const [newTopLevelPathItem] = newSelection.path;
-                const [currentTopLevelPathItem] = selection.path;
-                const isCurrentTopLevelSelection = currentTopLevelPathItem === newTopLevelPathItem;
-                if (!isCurrentTopLevelSelection) {
-                    const newTopLevelSelection = getTopLevelSelection({ pathItem: newTopLevelPathItem, model });
-                    if (newTopLevelSelection) {
-                        setSelection(newTopLevelSelection);
-                    } else {
-                        // eslint-disable-next-line no-console
-                        console.warn('No top level selection found:', {
-                            newTopLevelPathItem,
-                            newSelection,
-                        });
+                try {
+                    const [newTopLevelPathItem] = newSelection.path;
+                    if (selectionHasChanged({ selection, newSelection })) {
+                        const newTopLevelSelection = getTopLevelSelection({ selection: newSelection, model });
+                        if (newTopLevelSelection) {
+                            setSelection(newTopLevelSelection);
+                        } else {
+                            // eslint-disable-next-line no-console
+                            console.warn('No top level selection found:', {
+                                newTopLevelPathItem,
+                                newSelection,
+                            });
+                        }
                     }
-                }
 
-                const id = getSelectionReferenceId(newSelection);
-                // Scroll to top to reset scroll position
-                smoothScrollIntoView('#top');
-                // Wait for one render cycle before scrolling to position
-                setTimeout(() => {
-                    smoothScrollIntoView(`#${id}`);
-                }, 0);
+                    const id = getSelectionReferenceId(newSelection);
+                    // Scroll to top to reset scroll position
+                    smoothScrollIntoView('#top');
+                    // Wait for one render cycle before scrolling to position
+                    setTimeout(() => {
+                        smoothScrollIntoView(`#${id}`);
+                    }, 0);
+                } catch (error) {
+                    // eslint-disable-next-line no-console
+                    console.warn(error, { selection, newSelection });
+                }
             }
         },
         [selection]
