5cd3bf42170594039c713b902ab5c0cb4e64f10b
iMoses <imoses.g@gmail.com>
2023-09-08 14:13:20 +0100

    Basic Box Plot docs + Fix docs bug with Omit on optional properties (#185)

---
+++ packages/ag-charts-website/src/content/docs/box-series/_examples/regular-box-plot/main.ts
@@ -0,0 +1,28 @@
+import { AgChartOptions, AgEnterpriseCharts } from 'ag-charts-enterprise';
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    title: {
+        text: 'UK Government Budget',
+    },
+    subtitle: {
+        text: 'All values in £ billions',
+    },
+    data: getData(),
+    series: [
+        {
+            type: 'box-plot',
+            xKey: 'label',
+            xName: 'State',
+            yName: 'Billions of GBP',
+            minKey: 'min',
+            q1Key: 'q1',
+            medianKey: 'median',
+            q3Key: 'q3',
+            maxKey: 'max',
+        },
+    ],
+};
+
+AgEnterpriseCharts.create(options);
