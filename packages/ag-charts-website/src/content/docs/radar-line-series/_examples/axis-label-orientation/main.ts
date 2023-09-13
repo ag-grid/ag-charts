4c9648dad852c6d1f7fba40b48d0e366d263cb87
rmc-software <rmc.software.consultancy@gmail.com>
2023-09-13 18:01:13 +0100

    AG-8771 - radar line series doc updates

---
+++ packages/ag-charts-website/src/content/docs/radar-line-series/_examples/axis-label-orientation/main.ts
@@ -0,0 +1,30 @@
+import { AgEnterpriseCharts, AgPolarChartOptions } from 'ag-charts-enterprise';
+import { getData } from './data';
+
+const options: AgPolarChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'School Grades',
+    },
+    series: [
+        {
+            type: 'radar-line',
+            angleKey: 'subject',
+            radiusKey: 'grade',
+        },
+    ],
+    axes: [
+        {
+            type: 'angle-category',
+            label: {
+                orientation: 'parallel',
+            },
+        },
+        {
+            type: 'radius-number',
+        },
+    ],
+};
+
+AgEnterpriseCharts.create(options);
