75e0e10bfb13a84d55d334771aefb79962798e8e
rmc-software <rmc.software.consultancy@gmail.com>
2023-09-14 18:48:18 +0100

    AG-8771 - radar area series doc updates

---
+++ packages/ag-charts-website/src/content/docs/radar-area-series/_examples/axis-shape/main.ts
@@ -0,0 +1,29 @@
+import { AgEnterpriseCharts, AgPolarChartOptions } from 'ag-charts-enterprise';
+import { getData } from './data';
+
+const options: AgPolarChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'School Grades',
+    },
+    series: [
+        {
+            type: 'radar-area',
+            angleKey: 'subject',
+            radiusKey: 'grade',
+        },
+    ],
+    axes: [
+        {
+            type: 'angle-category',
+            shape: 'circle',
+        },
+        {
+            type: 'radius-number',
+            shape: 'circle',
+        },
+    ]
+};
+
+AgEnterpriseCharts.create(options);
