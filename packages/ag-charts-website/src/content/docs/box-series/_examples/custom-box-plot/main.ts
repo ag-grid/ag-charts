639bf3f1e16e2dd4086bd52d814c2970b6228f49
iMoses <imoses.g@gmail.com>
2023-09-18 18:46:13 +0300

    AG-9023: First iteration of Box Plot Docs (#225)
    * First iteration of Box Plot Docs
    
    * try to fix build
    
    * try to fix build
    
    * AG-9023 - nx format.
    
    * try to fix build
    
    ---------
    
    Co-authored-by: Alan Treadway <alan.treadway@ag-grid.com>

--- packages/ag-charts-website/src/content/docs/box-series/_examples/custom-box-plot/main.ts
+++ packages/ag-charts-website/src/content/docs/box-series/_examples/custom-box-plot/main.ts
@@ -4,43 +4,32 @@ import { getData } from './data';
 const options: AgChartOptions = {
     container: document.getElementById('myChart'),
     title: {
-        text: 'UK Government Budget',
+        text: 'IBM HR Analytics',
     },
     subtitle: {
-        text: 'All values in £ billions',
+        text: 'Employee Salaries Distribution by Role',
     },
     data: getData(),
     series: [
         {
+            // common props
             type: 'box-plot',
-            xKey: 'label',
-            xName: 'State',
-            yName: 'Billions of GBP',
+            yName: 'Job Role',
+            xKey: 'role',
             minKey: 'min',
             q1Key: 'q1',
             medianKey: 'median',
             q3Key: 'q3',
             maxKey: 'max',
-            // customized configuration
-            fill: 'green',
-            fillOpacity: 0.5,
-            stroke: 'purple',
-            strokeWidth: 6,
-            strokeOpacity: 0.5,
+            // styling props
+            strokeWidth: 1,
             cap: {
-                lengthRatio: 0.3,
+                lengthRatio: 0.8,
             },
             whisker: {
                 stroke: 'red',
                 strokeWidth: 3,
-                strokeOpacity: 0.8,
-                lineDash: [5, 2],
-            },
-            formatter(params) {
-                if (params.datum.min < 3.5) {
-                    return { fill: 'yellow', strokeWidth: 1 };
-                }
-                return {};
+                lineDash: [2, 1],
             },
         },
     ],
