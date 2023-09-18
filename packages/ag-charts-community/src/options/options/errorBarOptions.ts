f53437b9f4f345ed9742bf3f5a6ab336f2a06023
Oli Legat <olivier.legat@ag-grid.com>
2023-09-18 11:50:54 +0100

    AG-9022 Document error bar configuration

--- packages/ag-charts-community/src/options/options/errorBarOptions.ts
+++ packages/ag-charts-community/src/options/options/errorBarOptions.ts
@@ -1,10 +1,10 @@
 export interface AgErrorBarOptions {
-    /** TODO(olegat) writeme */
+    /** The key to use to retrieve lower bound error values from the y axis data. */
     yLowerKey: string;
-    /** TODO(olegat) writeme */
+    /** Human-readable description of the lower bound error value for the y axis. This is the value to use in tooltips or labels. */
     yLowerName?: string;
-    /** TODO(olegat) writeme */
+    /** The key to use to retrieve upper bound error values from the y axis data. */
     yUpperKey: string;
-    /** TODO(olegat) writeme */
+    /** Human-readable description of the upper bound error value for the y axis. This is the value to use in tooltips or labels. */
     yUpperName?: string;
 }
