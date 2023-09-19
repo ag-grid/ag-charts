6f8c871b38f448dddfa4d7365cb14433d93f1b5f
Alexander Shutau <shutovby@gmail.com>
2023-09-19 17:30:06 +0100

    AG-9252 Update Polar dataset key

--- packages/ag-charts-website/src/content/docs/radar-line-series/_examples/axis-shape/data.ts
+++ packages/ag-charts-website/src/content/docs/radar-line-series/_examples/axis-shape/data.ts
@@ -2,27 +2,27 @@ export function getData() {
     return [
         {
             department: 'Sales',            
-            employeeRetention: 60,
+            quality: 60,
             efficiency: 75,            
         },
         {
             department: 'Engineering',            
-            employeeRetention: 95,
+            quality: 95,
             efficiency: 90,            
         },
         {
             department: 'HR',            
-            employeeRetention: 85,
+            quality: 85,
             efficiency: 60,            
         },
         {
             department: 'Marketing',            
-            employeeRetention: 70,
+            quality: 70,
             efficiency: 60,            
         },
         {
             department: 'Finance',            
-            employeeRetention: 85,
+            quality: 85,
             efficiency: 80,            
         },
     ];
