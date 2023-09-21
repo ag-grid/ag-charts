8105359005bf0248c978afd45dbba9b1cb819b4b
Mana <mana.jalali.15@ucl.ac.uk>
2023-09-21 17:30:23 +0100

    AG-6686 - Increase legend spacing, reduce category axis padding

--- packages/ag-charts-community/src/chart/themes/chartTheme.ts
+++ packages/ag-charts-community/src/chart/themes/chartTheme.ts
@@ -233,7 +233,7 @@ export class ChartTheme {
             },
             legend: {
                 position: BOTTOM,
-                spacing: 20,
+                spacing: 30,
                 listeners: {},
                 item: {
                     paddingX: 16,
@@ -298,6 +298,8 @@ export class ChartTheme {
         category: {
             ...ChartTheme.getAxisDefaults(),
             groupPaddingInner: 0.1,
+            paddingInner: 0.2,
+            paddingOuter: 0.1,
             label: {
                 ...ChartTheme.getAxisDefaults().label,
                 autoRotate: true,
