9751132330ea0aacfe6d0a8c72ac78839e1ba50c
Oli Legat <olivier.legat@ag-grid.com>
2023-09-25 12:23:13 +0100

    AG-9016 Add scatter series with error bars example

---
+++ packages/ag-charts-website/src/content/docs/error-bars/_examples/scatter-series-error-bars/main.ts
@@ -0,0 +1,25 @@
+import { AgChartOptions, AgEnterpriseCharts } from 'ag-charts-enterprise';
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'Volume-Pressure Relationship with Confidence Intervals',
+    },
+    series: [
+        {
+            type: 'scatter',
+            xKey: 'volume',
+            yKey: 'pressure',
+            errorBar:  {
+                xLowerKey: 'volumeLower',
+                xUpperKey: 'volumeUpper',
+                yLowerKey: 'pressureLower',
+                yUpperKey: 'pressureUpper',
+            },
+        },
+    ],
+};
+
+AgEnterpriseCharts.create(options);
