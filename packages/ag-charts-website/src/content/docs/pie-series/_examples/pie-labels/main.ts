c7ebee3fa25f5dba6161aa87e3dd0fa10d811e82
Mark Durrant <mark.j.durrant@gmail.com>
2023-09-27 12:36:53 +0100

    AG-9399 - Add developer experience section to homepage

--- packages/ag-charts-website/src/content/docs/pie-series/_examples/pie-labels/main.ts
+++ packages/ag-charts-website/src/content/docs/pie-series/_examples/pie-labels/main.ts
@@ -1,21 +1,22 @@
 import { AgChartOptions, AgChart } from 'ag-charts-community';
 import { getData } from './data';
-
 const options: AgChartOptions = {
-    container: document.getElementById('myChart'),
-    data: getData(),
-    series: [
-        {
-            type: 'pie',
-            angleKey: 'value',
-            calloutLabelKey: 'label',
-            sectorLabelKey: 'value',
-            sectorLabel: {
-                color: 'white',
-                fontWeight: 'bold',
-            },
-        },
-    ],
+  container: document.getElementById('myChart'),
+  data: [
+      { label: 'Android', value: 56.9 },
+      { label: 'iOS', value: 22.5 },
+      { label: 'BlackBerry', value: 6.8 },
+      { label: 'Symbian', value: 8.5 },
+      { label: 'Windows', value: 2.6 },
+  ],
+  series: [
+      {
+          type: 'pie',
+          angleKey: 'value',
+          calloutLabelKey: 'label',
+          sectorLabelKey: 'value',
+      },
+  ],
 };
 
 AgChart.create(options);
