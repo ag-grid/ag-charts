0f170888285ab52cdb939fbf03dd8111323c4cb2
Tak Tran <contact@tutaktran.com>
2023-09-27 14:21:14 +0100

    AG-9399 - Add homepage specific example for the hero chart

---
+++ packages/ag-charts-website/src/content/docs/homepage/_examples/hero-chart/main.ts
@@ -0,0 +1,47 @@
+import { AgChart, AgChartOptions } from 'ag-charts-community';
+import { getData } from "./data";
+
+const options: AgChartOptions = {
+  container: document.getElementById('myChart'),
+  title: {
+    text: "Apple's Revenue by Product Category",
+  },
+  subtitle: {
+    text: 'in billion U.S. dollars',
+  },
+  data: getData(),
+  series: [
+    {
+      type: 'bar',
+      xKey: 'quarter',
+      yKey: 'iphone',
+      yName: 'iPhone',
+    },
+    {
+      type: 'bar',
+      xKey: 'quarter',
+      yKey: 'mac',
+      yName: 'Mac',
+    },
+    {
+      type: 'bar',
+      xKey: 'quarter',
+      yKey: 'ipad',
+      yName: 'iPad',
+    },
+    {
+      type: 'bar',
+      xKey: 'quarter',
+      yKey: 'wearables',
+      yName: 'Wearables',
+    },
+    {
+      type: 'bar',
+      xKey: 'quarter',
+      yKey: 'services',
+      yName: 'Services',
+    },
+  ],
+}
+
+AgChart.create(options)
