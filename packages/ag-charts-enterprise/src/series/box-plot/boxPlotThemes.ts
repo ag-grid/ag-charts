e8e4d186e058d558b5d76ef443aea90c84d51fa7
Mana <mana.jalali.15@ucl.ac.uk>
2023-09-27 15:43:08 +0100

    AG-6686 - Clean up bar examples

--- packages/ag-charts-enterprise/src/series/box-plot/boxPlotThemes.ts
+++ packages/ag-charts-enterprise/src/series/box-plot/boxPlotThemes.ts
@@ -2,5 +2,5 @@ import { _Theme } from 'ag-charts-community';
 
 export const BOX_PLOT_SERIES_THEME = {
     __extends__: _Theme.EXTENDS_SERIES_DEFAULTS,
-    strokeWidth: 1,
+    strokeWidth: 2,
 };
