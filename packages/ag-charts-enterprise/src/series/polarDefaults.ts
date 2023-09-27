9e7593c9cefeade34f106f4eb4ae3070e936dbd6
Alexander Shutau <shutovby@gmail.com>
2023-09-27 11:40:25 +0100

    AG-9394 Increase Radar label padding

--- packages/ag-charts-enterprise/src/series/polarDefaults.ts
+++ packages/ag-charts-enterprise/src/series/polarDefaults.ts
@@ -5,6 +5,9 @@ export const POLAR_DEFAULTS = {
     axes: [
         {
             type: POLAR_AXIS_TYPES.ANGLE_CATEGORY,
+            label: {
+                padding: 10,
+            },
         },
         {
             type: POLAR_AXIS_TYPES.RADIUS_NUMBER,
