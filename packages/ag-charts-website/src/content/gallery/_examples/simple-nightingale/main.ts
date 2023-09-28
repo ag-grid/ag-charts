07146ebd52f0a9bde9a263f6d4fa3cfb39d3a7df
Alexander Shutau <shutovby@gmail.com>
2023-09-28 12:03:42 +0100

    AG-9394 Polar series gallery examples

---
+++ packages/ag-charts-website/src/content/gallery/_examples/simple-nightingale/main.ts
@@ -0,0 +1,35 @@
+import { AgEnterpriseCharts, AgChartOptions } from 'ag-charts-enterprise';
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'Revenue by Product Category',
+    },
+    subtitle: {
+        text: 'Millions USD',
+    },
+    series: [
+        {
+            type: 'nightingale',
+            angleKey: 'quarter',
+            radiusKey: 'software',
+            radiusName: 'Software',
+        },
+        {
+            type: 'nightingale',
+            angleKey: 'quarter',
+            radiusKey: 'hardware',
+            radiusName: 'Hardware',
+        },
+        {
+            type: 'nightingale',
+            angleKey: 'quarter',
+            radiusKey: 'services',
+            radiusName: 'Services',
+        },
+    ],
+};
+
+AgEnterpriseCharts.create(options);
---
+++ packages/ag-charts-website/src/content/gallery/_examples/simple-radar-area/main.ts
@@ -0,0 +1,26 @@
+import { AgEnterpriseCharts, AgChartOptions } from 'ag-charts-enterprise';
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'KPIs by Department',
+    },
+    series: [
+        {
+            type: 'radar-area',
+            angleKey: 'department',
+            radiusKey: 'quality',
+            radiusName: 'Quality',
+        },
+        {
+            type: 'radar-area',
+            angleKey: 'department',
+            radiusKey: 'efficiency',
+            radiusName: 'Efficiency',
+        },
+    ],
+};
+
+AgEnterpriseCharts.create(options);
