e9207292e0116cd11d31feff9cbdf222877b7c8e
Mana <mana.jalali.15@ucl.ac.uk>
2023-09-28 11:31:42 +0100

    AG-9343 - Add box plot examples

---
+++ packages/ag-charts-website/src/content/gallery/_examples/box-plot-outliers/main.ts
@@ -0,0 +1,41 @@
+import { AgChartOptions, AgEnterpriseCharts } from 'ag-charts-enterprise';
+import { getBoxPlotData, getOutliersData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    title: {
+        text: 'HR Analytics',
+    },
+    subtitle: {
+        text: 'Salary Distribution by Role',
+    },
+    series: [
+        {
+            data: getBoxPlotData(),
+            type: 'box-plot',
+            yName: 'Employee Salaries',
+            xKey: 'role',
+            xName: 'Role',
+            minKey: 'min',
+            minName: 'Min',
+            q1Key: 'q1',
+            q1Name: 'Q1',
+            medianKey: 'median',
+            medianName: 'Median',
+            q3Key: 'q3',
+            q3Name: 'Q3',
+            maxKey: 'max',
+            maxName: 'Max',
+        },
+        {
+            data: getOutliersData(),
+            type: 'scatter',
+            xKey: 'role',
+            xName: 'Role',
+            yKey: 'salary',
+            yName: 'Data Outliers',
+        },
+    ],
+};
+
+AgEnterpriseCharts.create(options);
