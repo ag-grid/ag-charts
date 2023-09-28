e1b489c05a15b3b9cb90564fa276d0eeaf33ade8
Alexander Shutau <shutovby@gmail.com>
2023-09-28 14:38:38 +0100

    AG-8260 Fix Angle Axis tick interval

--- packages/ag-charts-enterprise/src/axes/angle-number/linearAngleScale.ts
+++ packages/ag-charts-enterprise/src/axes/angle-number/linearAngleScale.ts
@@ -5,13 +5,32 @@ const { range } = _Util;
 
 export class LinearAngleScale extends LinearScale {
     override ticks() {
-        const count = 8;
         if (!this.domain || this.domain.length < 2 || this.domain.some((d) => !isFinite(d))) {
             return [];
         }
         this.refresh();
         const [d0, d1] = this.getDomain();
-        const step = (d1 - d0) / count;
-        return range(d0, d1, step);
+
+        const { interval } = this;
+
+        if (interval) {
+            const step = Math.abs(interval);
+            if (!this.isDenseInterval({ start: d0, stop: d1, interval: step })) {
+                return range(d0, d1, step);
+            }
+        }
+        
+        const angleStep = Math.PI / 4;
+        const { range: scaleRange } = this;
+        let tickStep = this.invert(scaleRange[0] + angleStep) - d0;
+        if (tickStep > 1e-15) {
+            tickStep = parseFloat(tickStep.toPrecision(15));
+        }
+        const ticks = range(d0, d1, tickStep);
+        if (ticks[ticks.length - 1] > d1) {
+            ticks.pop();
+            ticks.push(d1);
+        }
+        return ticks;
     }
 }
