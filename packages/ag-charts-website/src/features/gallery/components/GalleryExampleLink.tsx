059be88c1f8b7bfb9b844347c2f0f0679a77f1d8
Mana <mana.jalali.15@ucl.ac.uk>
2023-10-02 15:03:26 +0100

    AG-9343 - Make gallery example link thumbnails match dark styling when dark theme selected

--- packages/ag-charts-website/src/features/gallery/components/GalleryExampleLink.tsx
+++ packages/ag-charts-website/src/features/gallery/components/GalleryExampleLink.tsx
@@ -1,10 +1,10 @@
+import { useStore } from '@nanostores/react';
+import { $theme } from '@stores/themeStore';
 import classnames from 'classnames';
 import type { FunctionComponent } from 'react';
 
 import { getPageUrl, getPlainExampleImageUrl } from '../utils/urlPaths';
 import styles from './GalleryExampleLink.module.scss';
-import { useStore } from '@nanostores/react';
-import { $theme } from '@stores/themeStore';
 
 interface Props {
     label: string;
@@ -16,16 +16,29 @@ export const GalleryExampleLink: FunctionComponent<Props> = ({ label, exampleNam
     const theme = useStore($theme);
     const imageUrl = getPlainExampleImageUrl({
         exampleName,
-        theme
+        theme,
     });
+
+    const isDarkTheme = theme.indexOf('dark') > -1;
+    const darkLinkStyle = {
+        backgroundColor: 'rgb(34, 38, 41)',
+        'border-top': '1px solid rgb(88, 88, 88)',
+    };
+    const darkSpanStyle = {
+        color: 'rgb(200, 200, 200)',
+    };
+
+    const linkStyle = isDarkTheme ? darkLinkStyle : {};
+    const spanStyle = isDarkTheme ? darkSpanStyle : {};
 
     return (
         <a
             className={classnames(styles.link, className, 'font-size-responsive', 'font-size-small', 'text-secondary')}
             href={getPageUrl(exampleName)}
+            style={linkStyle}
         >
             <img src={imageUrl} alt={label} />
-            <span>{label + " " + theme}</span>
+            <span style={spanStyle}>{label}</span>
         </a>
     );
 };
