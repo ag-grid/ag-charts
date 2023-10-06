a3922eb60092af4e91c9082e57dd8353505c06d5
Oli Legat <olivier.legat@ag-grid.com>
2023-10-06 13:26:09 +0100

    AG-9014 Add more errorbar tests and update snapshots

--- packages/ag-charts-enterprise/src/features/error-bar/errorBar.test.ts
+++ packages/ag-charts-enterprise/src/features/error-bar/errorBar.test.ts
@@ -57,6 +57,23 @@ const SERIES_AUSTRALIA = {
     errorBar: { yLowerKey: 'temperatureLower', yUpperKey: 'temperatureUpper' },
 };
 
+const EXTENDING_BARS = [
+    // Same data as Canada, but with Jan & Jul changed to test errorBar.ts's
+    // ability to extend the domain for error values.
+    { month: 'Jan', temperature: 12.5, temperatureLower: 2.0, temperatureUpper: 15.0 },
+    { month: 'Feb', temperature: 13.0, temperatureLower: 11.5, temperatureUpper: 15.5 },
+    { month: 'Mar', temperature: 15.5, temperatureLower: 13.0, temperatureUpper: 18.0 },
+    { month: 'Apr', temperature: 18.0, temperatureLower: 16.5, temperatureUpper: 19.5 },
+    { month: 'May', temperature: 21.5, temperatureLower: 19.0, temperatureUpper: 24.0 },
+    { month: 'Jun', temperature: 24.0, temperatureLower: 22.5, temperatureUpper: 26.0 },
+    { month: 'Jul', temperature: 26.5, temperatureLower: 24.0, temperatureUpper: 40.0 },
+    { month: 'Aug', temperature: 25.0, temperatureLower: 22.5, temperatureUpper: 28.0 },
+    { month: 'Sep', temperature: 23.5, temperatureLower: 21.0, temperatureUpper: 27.0 },
+    { month: 'Oct', temperature: 20.0, temperatureLower: 17.5, temperatureUpper: 22.5 },
+    { month: 'Nov', temperature: 16.5, temperatureLower: 14.0, temperatureUpper: 19.0 },
+    { month: 'Dec', temperature: 13.0, temperatureLower: 11.5, temperatureUpper: 15.5 },
+];
+
 const SERIES_BOYLESLAW = {
     type: 'scatter',
     data: [
@@ -143,6 +160,30 @@ describe('ErrorBars', () => {
                 { type: 'bar', direction: 'horizontal', ...SERIES_CANADA },
                 { type: 'bar', direction: 'horizontal', ...SERIES_AUSTRALIA },
             ],
+        });
+        await compare();
+    });
+
+    it('should extend Y axis on line series as expected', async () => {
+        chart = AgEnterpriseCharts.create({
+            ...opts,
+            series: [{ ...SERIES_CANADA, data: EXTENDING_BARS }],
+        });
+        await compare();
+    });
+
+    it('should extend Y axis on vertical bar series as expected', async () => {
+        chart = AgEnterpriseCharts.create({
+            ...opts,
+            series: [{ type: 'bar', ...SERIES_CANADA, data: EXTENDING_BARS }],
+        });
+        await compare();
+    });
+
+    it('should extend X axis on horizontal bar series as expected', async () => {
+        chart = AgEnterpriseCharts.create({
+            ...opts,
+            series: [{ type: 'bar', direction: 'horizontal', ...SERIES_CANADA, data: EXTENDING_BARS }],
         });
         await compare();
     });
