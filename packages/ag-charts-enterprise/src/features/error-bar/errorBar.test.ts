15c5f54e65de20551243a85183593dbb17955ab9
Oli Legat <olivier.legat@ag-grid.com>
2023-10-11 17:59:54 +0100

    AG-9272 Add tests for errorbar styling

--- packages/ag-charts-enterprise/src/features/error-bar/errorBar.test.ts
+++ packages/ag-charts-enterprise/src/features/error-bar/errorBar.test.ts
@@ -221,6 +221,53 @@ describe('ErrorBars', () => {
         await compare();
     });
 
+    it('should apply styling to whiskers and cap as expected', async () => {
+        chart = AgEnterpriseCharts.create({
+            ...opts,
+            series: [
+                {
+                    ...SERIES_CANADA,
+                    type: 'bar',
+                    data: FEWER_MONTHS,
+                    errorBar: {
+                        ...SERIES_CANADA.errorBar,
+                        stroke: 'rgb(0,0,255)',
+                        strokeWidth: 10,
+                        strokeOpacity: 0.5,
+                    },
+                },
+            ],
+        });
+        await compare();
+    });
+
+    it('should override cap styling as expected', async () => {
+        chart = AgEnterpriseCharts.create({
+            ...opts,
+            series: [
+                {
+                    ...SERIES_CANADA,
+                    type: 'bar',
+                    data: FEWER_MONTHS,
+                    errorBar: {
+                        ...SERIES_CANADA.errorBar,
+                        visible: false,
+                        stroke: 'rgb(0,0,255)',
+                        strokeWidth: 10,
+                        strokeOpacity: 0.25,
+                        cap: {
+                            visible: true,
+                            stroke: 'rgb(0,255,0)',
+                            strokeWidth: 20,
+                            strokeOpacity: 0.75,
+                        },
+                    },
+                },
+            ],
+        });
+        await compare();
+    });
+
     it('should default to marker size for cap length on line series', async () => {
         chart = AgEnterpriseCharts.create({
             ...opts,
