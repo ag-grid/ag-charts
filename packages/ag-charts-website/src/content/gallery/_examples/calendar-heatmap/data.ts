0fcfb926938062a7d7c79d2f90d90fe0250dacda
Mana <mana.jalali.15@ucl.ac.uk>
2023-10-17 15:37:13 +0100

    AG-9571 - Add heatmap series calendar example

---
+++ packages/ag-charts-website/src/content/gallery/_examples/calendar-heatmap/data.ts
@@ -0,0 +1,244 @@
+export function getData() {
+    return [
+        {
+            week: 1,
+            month: 'January',
+            steps: 63474,
+        },
+        {
+            week: 2,
+            month: 'January',
+            steps: 67329,
+        },
+        {
+            week: 3,
+            month: 'January',
+            steps: 68771,
+        },
+        {
+            week: 4,
+            month: 'January',
+            steps: 66018,
+        },
+        {
+            week: 1,
+            month: 'February',
+            steps: 68810,
+        },
+        {
+            week: 2,
+            month: 'February',
+            steps: 68765,
+        },
+        {
+            week: 3,
+            month: 'February',
+            steps: 69524,
+        },
+        {
+            week: 4,
+            month: 'February',
+            steps: 68052,
+        },
+        {
+            week: 1,
+            month: 'March',
+            steps: 69966,
+        },
+        {
+            week: 2,
+            month: 'March',
+            steps: 73819,
+        },
+        {
+            week: 3,
+            month: 'March',
+            steps: 70983,
+        },
+        {
+            week: 4,
+            month: 'March',
+            steps: 61076,
+        },
+        {
+            week: 1,
+            month: 'April',
+            steps: 65943,
+        },
+        {
+            week: 2,
+            month: 'April',
+            steps: 64573,
+        },
+        {
+            week: 3,
+            month: 'April',
+            steps: 61743,
+        },
+        {
+            week: 4,
+            month: 'April',
+            steps: 80800,
+        },
+        {
+            week: 1,
+            month: 'May',
+            steps: 63447,
+        },
+        {
+            week: 2,
+            month: 'May',
+            steps: 63509,
+        },
+        {
+            week: 3,
+            month: 'May',
+            steps: 65146,
+        },
+        {
+            week: 4,
+            month: 'May',
+            steps: 67222,
+        },
+        {
+            week: 1,
+            month: 'June',
+            steps: 69847,
+        },
+        {
+            week: 2,
+            month: 'June',
+            steps: 64697,
+        },
+        {
+            week: 3,
+            month: 'June',
+            steps: 62611,
+        },
+        {
+            week: 4,
+            month: 'June',
+            steps: 63919,
+        },
+        {
+            week: 1,
+            month: 'July',
+            steps: 65154,
+        },
+        {
+            week: 2,
+            month: 'July',
+            steps: 72564,
+        },
+        {
+            week: 3,
+            month: 'July',
+            steps: 70465,
+        },
+        {
+            week: 4,
+            month: 'July',
+            steps: 69305,
+        },
+        {
+            week: 1,
+            month: 'August',
+            steps: 66295,
+        },
+        {
+            week: 2,
+            month: 'August',
+            steps: 53583,
+        },
+        {
+            week: 3,
+            month: 'August',
+            steps: 60751,
+        },
+        {
+            week: 4,
+            month: 'August',
+            steps: 71064,
+        },
+        {
+            week: 1,
+            month: 'September',
+            steps: 76364,
+        },
+        {
+            week: 2,
+            month: 'September',
+            steps: 81668,
+        },
+        {
+            week: 3,
+            month: 'September',
+            steps: 68517,
+        },
+        {
+            week: 4,
+            month: 'September',
+            steps: 76266,
+        },
+        {
+            week: 1,
+            month: 'October',
+            steps: 73123,
+        },
+        {
+            week: 2,
+            month: 'October',
+            steps: 64771,
+        },
+        {
+            week: 3,
+            month: 'October',
+            steps: 67226,
+        },
+        {
+            week: 4,
+            month: 'October',
+            steps: 69147,
+        },
+        {
+            week: 1,
+            month: 'November',
+            steps: 62846,
+        },
+        {
+            week: 2,
+            month: 'November',
+            steps: 77315,
+        },
+        {
+            week: 3,
+            month: 'November',
+            steps: 58193,
+        },
+        {
+            week: 4,
+            month: 'November',
+            steps: 58707,
+        },
+        {
+            week: 1,
+            month: 'December',
+            steps: 71182,
+        },
+        {
+            week: 2,
+            month: 'December',
+            steps: 62424,
+        },
+        {
+            week: 3,
+            month: 'December',
+            steps: 62153,
+        },
+        {
+            week: 4,
+            month: 'December',
+            steps: 63686,
+        },
+    ];
+}
---
+++ packages/ag-charts-website/src/content/gallery/_examples/calendar-heatmap/main.ts
@@ -0,0 +1,31 @@
+import { AgEnterpriseCharts, AgChartOptions } from 'ag-charts-enterprise';
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'Daily Step Count'
+    },
+    series: [
+        {
+            type: 'heatmap',
+
+            xKey: 'month',
+            xName: 'Month',
+
+            yKey: 'week',
+            yName: 'Week',
+
+            colorKey: 'steps',
+            colorName: 'Steps',
+
+            strokeWidth: 4,
+        },
+    ],
+    gradientLegend: {
+        enabled: false,
+    },
+};
+
+AgEnterpriseCharts.create(options);
