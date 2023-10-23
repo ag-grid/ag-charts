d13931d69af85c71ed571e592dc9f249e7f676e8
AG-DavidG <121935568+AG-DavidG@users.noreply.github.com>
2023-10-23 10:26:41 +0000

    Updated Area Series docs

---
+++ packages/ag-charts-website/src/content/docs/area-series/_examples/simple-area/main.ts
@@ -0,0 +1,32 @@
+import { AgChart, AgChartOptions } from "ag-charts-community";
+import { getData } from "./data";
+
+const options: AgChartOptions = {
+  container: document.getElementById('myChart'),
+  title: {
+    text: 'Sales by Month',
+  },
+  data: getData(),
+  series: [
+    {
+      type: 'area',
+      xKey: 'month',
+      yKey: 'subscriptions',
+      yName: 'Subscriptions',
+    },
+    {
+      type: 'area',
+      xKey: 'month',
+      yKey: 'services',
+      yName: 'Services',
+    },
+        {
+      type: 'area',
+      xKey: 'month',
+      yKey: 'products',
+      yName: 'Products',
+    },
+  ],
+}
+
+AgChart.create(options)
