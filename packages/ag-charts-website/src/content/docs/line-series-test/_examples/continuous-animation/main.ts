633efc577cd988b6fc4ac1c2d7ed1b27502f1b05
Alan Treadway <alan.treadway@ag-grid.com>
2023-10-25 12:31:51 +0100

    Add line and area animation examples for regression testing.

---
+++ packages/ag-charts-website/src/content/docs/line-series-test/_examples/continuous-animation/main.ts
@@ -0,0 +1,184 @@
+import { AgChart, AgChartOptions, AgEnterpriseCharts, time } from 'ag-charts-enterprise';
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    animation: {
+        enabled: true,
+    },
+    legend: {
+        enabled: false,
+    },
+    data: getData(),
+    series: [
+        {
+            type: 'line',
+            xKey: 'date',
+            yKey: 'petrol',
+            stroke: '#01c185',
+            marker: {
+                stroke: '#01c185',
+                fill: '#01c185',
+            },
+        },
+        {
+            type: 'line',
+            xKey: 'date',
+            yKey: 'diesel',
+            stroke: '#000000',
+            marker: {
+                stroke: '#000000',
+                fill: '#000000',
+            },
+        },
+    ],
+    axes: [
+        {
+            position: 'bottom',
+            type: 'time',
+        },
+        {
+            position: 'left',
+            type: 'number',
+            label: {
+                autoRotate: false,
+            },
+        },
+    ],
+};
+
+const chart = AgEnterpriseCharts.create(options);
+
+function genDataPoint(ref: Date | { date: Date, petrol: number, diesel: number }, offsetDays: number) {
+    const { date, petrol = 120, diesel = 125 } = ref instanceof Date ? { date: ref } : ref;
+
+    return {
+        date: new Date(date.getTime() + (offsetDays * 3600 * 24 * 1000)),
+        petrol: petrol + Math.random() * 4 - 2,
+        diesel: diesel + Math.random() * 4 - 2,
+    };
+}
+
+function times<T>(cb: () => T, count: number) {
+    const result = [];
+    for (; count > 0; count--) {
+        result.push(cb());
+    }
+    return result;
+}
+
+function actionReset() {
+    options.data = getData();
+    AgChart.update(chart, options);
+}
+
+function actionRemovePoints() {
+    options.data = [...options.data];
+    options.data.splice(options.data.length / 2 - 5, 10);
+    AgChart.update(chart, options);
+}
+
+function actionRemoveFirstPoint() {
+    options.data = [...options.data.slice(1)];
+    AgChart.update(chart, options);
+}
+
+function actionRemoveLastPoint() {
+    options.data = [...options.data.slice(0, -1)];
+    AgChart.update(chart, options);
+}
+
+function actionRemoveHalf() {
+    const { length } = options.data;
+    options.data = options.data.slice(Math.floor(length * 1/4), Math.floor(length * 3/4));
+
+    AgChart.update(chart, options);
+}
+
+function actionAddPoints() {
+    options.data = [...options.data];
+    const { length } = options.data;
+    for (const idx of [length / 4, length / 2, length * 3 / 4]) {
+        const dataIdx = Math.floor(idx);
+        const [datum, nextDatum] = options.data.slice(dataIdx, dataIdx + 2);
+
+        const date = new Date((datum.date.getTime() + nextDatum.date.getTime()) / 2);
+        options.data.splice(dataIdx + 1, 0, genDataPoint({ ...datum, date}, 0));
+    }
+    AgChart.update(chart, options);
+}
+
+function actionAddPointsBefore() {
+    options.data = [...options.data];
+
+    const ref = options.data[0];
+    options.data.splice(0, 0, genDataPoint(ref, -14), genDataPoint(ref, -7));
+    AgChart.update(chart, options);
+}
+
+function actionAddPointsAfter(count = 2) {
+    options.data = [...options.data];
+
+    const [ ref ] = options.data.slice(-1);
+    for (let idx = 0; idx < count; idx++) {
+        options.data.push(genDataPoint(ref, (idx + 1) * 7));
+    }
+    AgChart.update(chart, options);
+}
+
+function actionAddDouble() {
+    const { length } = options.data;
+
+    const count = Math.ceil(length / 4);
+    let start = genDataPoint(options.data[0], -7 * (count + 1));
+    let [end] = options.data.slice(-1);
+    options.data = [
+        ...times(() => start = genDataPoint(start, 7), count),
+        ...options.data,
+        ...times(() => end = genDataPoint(end, 7), count),
+    ];
+    AgChart.update(chart, options);
+}
+
+function actionUpdatePoints() {
+    options.data = options.data.map((d: any) => ({
+        ...d,
+        petrol: d.petrol + Math.random() * 4 - 2,
+        diesel: d.diesel + Math.random() * 4 - 2,
+    }));
+    AgChart.update(chart, options);
+}
+
+function actionUpdatePointUndefined() {
+    options.data = options.data.map((d: any) => ({
+        ...d,
+        petrol: Math.random() > 0.9 ? undefined : d.petrol,
+        diesel: Math.random() > 0.9 ? undefined : d.diesel,
+    }));
+    AgChart.update(chart, options);
+}
+
+function actionShiftLeft() {
+    const [ ref ] = options.data.slice(-1);
+    options.data = [...options.data.slice(1), genDataPoint(ref, 7)];
+
+    AgChart.update(chart, options);
+}
+
+function actionShiftRight() {
+    const [ ref ] = options.data.slice(0);
+    options.data = [genDataPoint(ref, -7), ...options.data.slice(0, -1)];
+
+    AgChart.update(chart, options);
+}
+
+let tick: NodeJS.Timer;
+function actionTickStart() {
+    if (tick) clearInterval(tick);
+
+    tick = setInterval(() => actionAddPointsAfter(1), 1000)
+}
+
+function actionTickStop() {
+    if (tick) clearInterval(tick);
+}
