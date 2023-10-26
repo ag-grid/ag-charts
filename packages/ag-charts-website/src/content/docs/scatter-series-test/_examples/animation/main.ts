39827ad3fe4c17d835b7cacd3fda5d8d1f102c79
Alan Treadway <alan.treadway@ag-grid.com>
2023-10-26 12:52:18 +0100

    Ressurect animation examples

---
+++ packages/ag-charts-website/src/content/docs/scatter-series-test/_examples/animation/main.ts
@@ -0,0 +1,81 @@
+import { AgChart, AgChartOptions, AgEnterpriseCharts } from 'ag-charts-enterprise';
+import {
+    getFemaleData,
+    getMaleData,
+    getRandomisedFemaleData,
+    getRandomisedMaleData,
+    getRemovedFemaleData,
+    getRemovedMaleData,
+} from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    animation: {
+        enabled: true,
+    },
+    series: [
+        {
+            type: 'bubble',
+            title: 'Male',
+            data: getMaleData(),
+            xKey: 'height',
+            xName: 'Height',
+            yKey: 'weight',
+            yName: 'Weight',
+            sizeKey: 'age',
+            sizeName: 'Age',
+            labelKey: 'name',
+            marker: {
+                shape: 'square',
+                size: 6,
+                maxSize: 30,
+                fill: 'rgba(227,111,106,0.71)',
+                stroke: '#9f4e4a',
+            },
+            label: {
+                enabled: true,
+            },
+        },
+        {
+            type: 'bubble',
+            title: 'Female',
+            data: getFemaleData(),
+            xKey: 'height',
+            xName: 'Height',
+            yKey: 'weight',
+            yName: 'Weight',
+            sizeKey: 'age',
+            sizeName: 'Age',
+            labelKey: 'name',
+            marker: {
+                size: 6,
+                maxSize: 30,
+                fill: 'rgba(123,145,222,0.71)',
+                stroke: '#56659b',
+            },
+            label: {
+                enabled: true,
+            },
+        },
+    ],
+};
+
+const chart = AgEnterpriseCharts.create(options);
+
+function reset() {
+    options.series[0].data = getMaleData();
+    options.series[1].data = getFemaleData();
+    AgChart.update(chart, options as any);
+}
+
+function randomise() {
+    options.series[0].data = getRandomisedMaleData();
+    options.series[1].data = getRandomisedFemaleData();
+    AgChart.update(chart, options as any);
+}
+
+function remove() {
+    options.series[0].data = getRemovedMaleData();
+    options.series[1].data = getRemovedFemaleData();
+    AgChart.update(chart, options as any);
+}
