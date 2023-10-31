0742c101c2a4ae0a3c7ef062e3cfc8909b29f94a
Tak Tran <contact@tutaktran.com>
2023-10-31 12:16:51 +0000

    AG-9655 - Make `SITE_URL` accessible on client side
    Use `PUBLIC_` prefix to expose it

--- packages/ag-charts-website/astro.config.mjs
+++ packages/ag-charts-website/astro.config.mjs
@@ -1,23 +1,22 @@
 import markdoc from '@astrojs/markdoc';
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 import agHotModuleReload from './src/astro/plugins/agHotModuleReload';
 import { getDevFileList } from './src/utils/pages';
 
-const SITE_URL = process?.env?.SITE_URL;
-
 const DEFAULT_SITE_BASE_URL = '/';
-const SITE_BASE_URL = process?.env?.SITE_BASE_URL || DEFAULT_SITE_BASE_URL;
+const { PUBLIC_SITE_URL, SITE_BASE_URL = DEFAULT_SITE_BASE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
 const OUTPUT_DIR = '../../dist/packages/ag-charts-website';
 
-console.log('Astro configuration', JSON.stringify({ SITE_URL, SITE_BASE_URL, OUTPUT_DIR }, null, 2));
+console.log('Astro configuration', JSON.stringify({ PUBLIC_SITE_URL, SITE_BASE_URL, OUTPUT_DIR }, null, 2));
 
 // https://astro.build/config
 export default defineConfig({
-    site: SITE_URL,
+    site: PUBLIC_SITE_URL,
     base: SITE_BASE_URL,
     outDir: OUTPUT_DIR,
     vite: {
--- packages/ag-charts-website/src/utils/env.ts
+++ packages/ag-charts-website/src/utils/env.ts
@@ -1,2 +1,2 @@
 export const getIsDev = () => import.meta.env?.DEV;
-export const getIsStaging = () => import.meta.env?.SITE_URL === 'https://testing.ag-grid.com';
+export const getIsStaging = () => import.meta.env?.PUBLIC_SITE_URL === 'https://testing.ag-grid.com';
