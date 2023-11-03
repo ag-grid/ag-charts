29dd2a0c21dbc491990445a3cfedd656a61ea2a5
Laurence Roberts <laurence.roberts@ag-grid.com>
2023-11-03 10:07:51 +0000

    AG-9254 Add extra interactions to animation data updates example

--- packages/ag-charts-website/src/content/docs/animation/_examples/data-updates/data.ts
+++ packages/ag-charts-website/src/content/docs/animation/_examples/data-updates/data.ts
@@ -1,20 +1,43 @@
 const NUM_DATA_POINTS = 30;
+let _seed = 1234;
 
-export function getData(offset = 0) {
-    seed = 1234;
-    const data: Array<{ year: number; spending: number }> = [];
-    for (let i = 0; i < NUM_DATA_POINTS + offset; i++) {
+// Create a set of data with predicatable "randomness"
+export function getData(
+    start = [100, 100, 100, 100],
+    variance = 2,
+    offset = 0,
+    length = NUM_DATA_POINTS,
+    seed = _seed
+) {
+    // Vary the datum by a random proportion of the variance +ve or -ve
+    const vary = (n: number) => Math.max(0, n + variance * random() * 2 - variance);
+
+    _seed = seed;
+    const data: Array<{ year: number; one: number; two: number; three: number; four: number; five: number }> = [
+        {
+            year: new Date().getFullYear() - NUM_DATA_POINTS,
+            one: start[0],
+            two: start[1],
+            three: start[2],
+            four: start[3],
+            five: start[4],
+        },
+    ];
+    for (let i = 1; i < NUM_DATA_POINTS + offset; i++) {
         data.push({
             year: new Date().getFullYear() - NUM_DATA_POINTS + i,
-            spending: i === 0 ? random() * 100 * 4 - 2 : data[i - 1].spending + random() * 4 - 2,
+            one: vary(data[i - 1].one),
+            two: vary(data[i - 1].two),
+            three: vary(data[i - 1].three),
+            four: vary(data[i - 1].four),
+            five: vary(data[i - 1].five),
         });
     }
-    return data.slice(offset);
+    return data.slice(offset - (length - NUM_DATA_POINTS));
 }
 
 // Simple seeded randomisation for consistent data - https://stackoverflow.com/a/19303725
-let seed = 1234;
-function random() {
-    const x = Math.sin(seed++) * 10000;
+export function random() {
+    const x = Math.sin(_seed++) * 10000;
     return x - Math.floor(x);
 }
