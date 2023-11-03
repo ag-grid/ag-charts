ab4b5e8add7c3ae32de9a0d16f6d2555234f97da
Alan Treadway <alan.treadway@ag-grid.com>
2023-11-03 13:04:02 +0000

    Add debug log gap logging.

--- packages/ag-charts-community/src/util/debug.ts
+++ packages/ag-charts-community/src/util/debug.ts
@@ -4,6 +4,18 @@ import { windowValue } from './window';
 
 type DebugLogger = (...logContent: any[]) => void;
 
+const LONG_TIME_PERIOD_THRESHOLD = 2000;
+
+let timeOfLastLog = Date.now();
+const logTimeGap = () => {
+    const timeSinceLastLog = Date.now() - timeOfLastLog;
+    if (timeSinceLastLog > LONG_TIME_PERIOD_THRESHOLD) {
+        const prettyDuration = (Math.floor(timeSinceLastLog / 100) / 10).toFixed(1);
+        Logger.log(`**** ${prettyDuration}s since last log message ****`);
+    }
+    timeOfLastLog = Date.now();
+};
+
 export const Debug = {
     create(...debugSelectors: Array<boolean | string>): DebugLogger {
         return (...logContent: any[]) => {
@@ -11,6 +23,7 @@ export const Debug = {
                 if (typeof logContent[0] === 'function') {
                     logContent = toArray(logContent[0]());
                 }
+                logTimeGap();
                 Logger.log(...logContent);
             }
         };
