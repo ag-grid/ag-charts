9767f2f220e64c94eddbb66d5c28a12309a8c8fa
Oli Legat <olivier.legat@ag-grid.com>
2023-11-06 10:55:45 +0000

    AG-9022 Add 'error-bars-test' docs page

---
+++ packages/ag-charts-website/src/content/docs/error-bars-test/_examples/boyles/data.ts
@@ -0,0 +1,150 @@
+export function getHelium() {
+    return [
+        {
+            volume: 0.5,
+            volumeLower: 0.45,
+            volumeUpper: 0.55,
+            pressure: 9.5,
+            pressureLower: 8.7,
+            pressureUpper: 10.3,
+        },
+        {
+            volume: 1.0,
+            volumeLower: 0.9,
+            volumeUpper: 1.1,
+            pressure: 8.1,
+            pressureLower: 7.4,
+            pressureUpper: 8.9,
+        },
+        {
+            volume: 1.5,
+            volumeLower: 1.35,
+            volumeUpper: 1.65,
+            pressure: 6.8,
+            pressureLower: 6.2,
+            pressureUpper: 7.5,
+        },
+        {
+            volume: 2.0,
+            volumeLower: 1.8,
+            volumeUpper: 2.2,
+            pressure: 5.5,
+            pressureLower: 5.0,
+            pressureUpper: 5.9,
+        },
+        {
+            volume: 2.5,
+            volumeLower: 2.25,
+            volumeUpper: 2.75,
+            pressure: 4.2,
+            pressureLower: 3.8,
+            pressureUpper: 4.7,
+        },
+        {
+            volume: 3.0,
+            volumeLower: 2.7,
+            volumeUpper: 3.3,
+            pressure: 3.1,
+            pressureLower: 2.8,
+            pressureUpper: 3.5,
+        },
+        {
+            volume: 3.5,
+            volumeLower: 3.15,
+            volumeUpper: 3.85,
+            pressure: 2.0,
+            pressureLower: 1.8,
+            pressureUpper: 2.3,
+        },
+        {
+            volume: 4.0,
+            volumeLower: 3.6,
+            volumeUpper: 4.4,
+            pressure: 1.2,
+            pressureLower: 1.1,
+            pressureUpper: 1.4,
+        }
+    ];
+}
+
+export function getOxygen() {
+    return [
+        {
+            volume: 0.5,
+            volumeLower: 0.45,
+            volumeUpper: 0.55,
+            pressure: 8.0,
+            pressureLower: 7.7,
+            pressureUpper: 8.3,
+        },
+        {
+            volume: 1.0,
+            volumeLower: 0.9,
+            volumeUpper: 1.1,
+            pressure: 6.0,
+            pressureLower: 5.7,
+            pressureUpper: 6.3,
+        },
+        {
+            volume: 1.5,
+            volumeLower: 1.35,
+            volumeUpper: 1.65,
+            pressure: 4.0,
+            pressureLower: 3.7,
+            pressureUpper: 4.3,
+        },
+        {
+            volume: 2.0,
+            volumeLower: 1.8,
+            volumeUpper: 2.2,
+            pressure: 2.0,
+            pressureLower: 1.7,
+            pressureUpper: 2.3,
+        },
+        {
+            volume: 2.5,
+            volumeLower: 2.25,
+            volumeUpper: 2.75,
+            pressure: 0.0,
+            pressureLower: -0.3,
+            pressureUpper: 0.3,
+        },
+    ];
+}
+
+export function getArgon() {
+    return [
+        {
+            volume: 0.5,
+            volumeLower: 0.45,
+            volumeUpper: 0.55,
+            pressure: 12.0,
+            pressureLower: 11.7,
+            pressureUpper: 12.3,
+        },
+        {
+            volume: 1.0,
+            volumeLower: 0.9,
+            volumeUpper: 1.1,
+            pressure: 8.0,
+            pressureLower: 7.7,
+            pressureUpper: 8.3,
+        },
+        {
+            volume: 1.5,
+            volumeLower: 1.35,
+            volumeUpper: 1.65,
+            pressure: 4.0,
+            pressureLower: 3.7,
+            pressureUpper: 4.3,
+        },
+        {
+            volume: 2.0,
+            volumeLower: 1.8,
+            volumeUpper: 2.2,
+            pressure: 0.0,
+            pressureLower: -0.3,
+            pressureUpper: 0.3,
+        },
+    ];
+}
---
+++ packages/ag-charts-website/src/content/docs/error-bars-test/_examples/temperatures/data.ts
@@ -0,0 +1,154 @@
+export function getData() {
+    return [
+        {
+            month: 'Jan',
+            temperature: 12.5,
+            temperatureLower: 10.0,
+            temperatureUpper: 15.0
+        },
+        {
+            month: 'Feb',
+            temperature: 13.0,
+            temperatureLower: 11.5,
+            temperatureUpper: 15.5
+        },
+        {
+            month: 'Mar',
+            temperature: 15.5,
+            temperatureLower: 13.0,
+            temperatureUpper: 18.0
+        },
+        {
+            month: 'Apr',
+            temperature: 18.0,
+            temperatureLower: 16.5,
+            temperatureUpper: 19.5
+        },
+        {
+            month: 'May',
+            temperature: 21.5,
+            temperatureLower: 19.0,
+            temperatureUpper: 24.0
+        },
+        {
+            month: 'Jun',
+            temperature: 24.0,
+            temperatureLower: 22.5,
+            temperatureUpper: 26.0
+        },
+        {
+            month: 'Jul',
+            temperature: 26.5,
+            temperatureLower: 24.0,
+            temperatureUpper: 29.0
+        },
+        {
+            month: 'Aug',
+            temperature: 25.0,
+            temperatureLower: 22.5,
+            temperatureUpper: 28.0
+        },
+        {
+            month: 'Sep',
+            temperature: 23.5,
+            temperatureLower: 21.0,
+            temperatureUpper: 27.0
+        },
+        {
+            month: 'Oct',
+            temperature: 20.0,
+            temperatureLower: 17.5,
+            temperatureUpper: 22.5
+        },
+        {
+            month: 'Nov',
+            temperature: 16.5,
+            temperatureLower: 14.0,
+            temperatureUpper: 19.0
+        },
+        {
+            month: 'Dec',
+            temperature: 13.0,
+            temperatureLower: 11.5,
+            temperatureUpper: 15.5
+        },
+    ] ;
+}
+
+
+export function getData2() {
+    return [
+        {
+            month: 'Jan',
+            temperature: 8.0,
+            temperatureLower: 6.5,
+            temperatureUpper: 10.0
+        },
+        {
+            month: 'Feb',
+            temperature: 8.5,
+            temperatureLower: 7.0,
+            temperatureUpper: 10.5
+        },
+        {
+            month: 'Mar',
+            temperature: 10.0,
+            temperatureLower: 8.5,
+            temperatureUpper: 12.0
+        },
+        {
+            month: 'Apr',
+            temperature: 12.0,
+            temperatureLower: 10.5,
+            temperatureUpper: 13.5
+        },
+        {
+            month: 'May',
+            temperature: 14.5,
+            temperatureLower: 13.0,
+            temperatureUpper: 16.0
+        },
+        {
+            month: 'Jun',
+            temperature: 16.5,
+            temperatureLower: 15.0,
+            temperatureUpper: 18.0
+        },
+        {
+            month: 'Jul',
+            temperature: 18.0,
+            temperatureLower: 16.5,
+            temperatureUpper: 19.5
+        },
+        {
+            month: 'Aug',
+            temperature: 17.0,
+            temperatureLower: 15.5,
+            temperatureUpper: 18.5
+        },
+        {
+            month: 'Sep',
+            temperature: 15.5,
+            temperatureLower: 14.0,
+            temperatureUpper: 17.0
+        },
+        {
+            month: 'Oct',
+            temperature: 12.5,
+            temperatureLower: 11.0,
+            temperatureUpper: 14.0
+        },
+        {
+            month: 'Nov',
+            temperature: 10.0,
+            temperatureLower: 8.5,
+            temperatureUpper: 11.5
+        },
+        {
+            month: 'Dec',
+            temperature: 8.5,
+            temperatureLower: 7.0,
+            temperatureUpper: 10.0
+        },
+    ];
+}
