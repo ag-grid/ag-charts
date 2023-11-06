073be12db8ad9bdf5b6de577fe38209ad653933e
Tak Tran <contact@tutaktran.com>
2023-11-06 11:00:36 +0000

    AG-9715 - Move tab data generation to astro build time
    Allows it to load before client side code is run
    
    Also move `DOCS_TAB_ITEM_ID_PREFIX` constant to top level, so that TabsWithData is generic

---
+++ packages/ag-charts-website/src/components/tabs/TabsWithData.tsx
@@ -0,0 +1,82 @@
+import { useLocation } from '@utils/navigation';
+import classnames from 'classnames';
+import type { FunctionComponent } from 'react';
+import { useEffect, useState } from 'react';
+
+import { TabNavItem } from './TabNavItem';
+import styles from './Tabs.module.scss';
+import type { TabData } from './types';
+
+interface Props {
+    tabsData: TabData[];
+    tabItemIdPrefix?: string;
+}
+
+const getTabId = ({ id, prefix }: { id: string; prefix?: string }) => `${prefix}${id}`;
+
+function useSelectTabFromLocationHash({
+    tabsData,
+    setSelected,
+    tabItemIdPrefix,
+}: {
+    tabsData: TabData[];
+    setSelected: (tab: TabData) => void;
+    tabItemIdPrefix?: string;
+}) {
+    const location = useLocation();
+
+    useEffect(() => {
+        const hash = location?.hash;
+        if (!hash) {
+            return;
+        }
+
+        const tab = tabsData.find((t) => hash.startsWith(`#${tabItemIdPrefix}${t.id}`));
+        if (tab) {
+            setSelected(tab);
+        }
+    }, [location, tabsData]);
+}
+
+/**
+ * Tabs where the children are in a HTML string format
+ *
+ * Children content is parsed to determine what the tab content is
+ */
+export const TabsWithData: FunctionComponent<Props> = ({ tabsData, tabItemIdPrefix }) => {
+    const [selected, setSelected] = useState<TabData>(tabsData[0]);
+
+    useSelectTabFromLocationHash({ tabsData, setSelected, tabItemIdPrefix });
+
+    return (
+        <div className={classnames('tabs-outer', styles.tabsOuter)}>
+            <header className={'tabs-header'}>
+                <ul className="tabs-nav-list" role="tablist">
+                    {tabsData.map(({ id, label }) => {
+                        return (
+                            <TabNavItem
+                                key={label}
+                                tabId={getTabId({ id: id!, prefix: tabItemIdPrefix })}
+                                label={label}
+                                selected={label === selected?.label}
+                                onSelect={(selectedLabel) => {
+                                    const tab = tabsData.find(({ label }) => label === selectedLabel);
+
+                                    if (tab) {
+                                        setSelected(tab);
+                                    }
+                                }}
+                            />
+                        );
+                    })}
+                </ul>
+            </header>
+            <div
+                className="tabs-content"
+                role="tabpanel"
+                aria-labelledby={`${selected} tab`}
+                dangerouslySetInnerHTML={{ __html: selected?.content ?? '' }}
+            ></div>
+        </div>
+    );
+};
--- packages/ag-charts-website/src/components/tabs/constants.ts
+++ packages/ag-charts-website/src/components/tabs/constants.ts
@@ -2,5 +2,3 @@
 export const TAB_ID_PROP = 'tab-id';
 export const TAB_LABEL_PROP = 'tab-label';
 export const TABS_LINKS_PROP = 'tabs-links';
-
-export const TAB_ITEM_ID_PREFIX = `reference-`;
