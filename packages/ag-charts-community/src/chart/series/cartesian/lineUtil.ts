815039be6c1e88603292d535b6920e7695973fef
Alan Treadway <alan.treadway@ag-grid.com>
2023-11-07 18:09:48 +0000

    AG-9310 - Fix AreaSeries category axis handling.

--- packages/ag-charts-community/src/chart/series/cartesian/lineUtil.ts
+++ packages/ag-charts-community/src/chart/series/cartesian/lineUtil.ts
@@ -159,14 +159,42 @@ export function pairCategoryData(
     diff: ProcessedOutputDiff,
     opts: {
         backfillSplitMode?: BackfillSplitMode;
+        multiDatum?: boolean;
+        datumIndexMultiplier?: number;
     } = {}
 ) {
-    const { backfillSplitMode = 'intersect' } = opts;
+    const { backfillSplitMode = 'intersect', multiDatum = false, datumIndexMultiplier = 1 } = opts;
     const result: PathPoint[] = [];
-    const resultMap: PathPointMap = {
+    const resultMapSingle: PathPointMap<false> = {
         added: {},
         moved: {},
         removed: {},
+    };
+    const resultMapMulti: PathPointMap<true> = {
+        added: {},
+        moved: {},
+        removed: {},
+    };
+
+    const addToResultMap = (xValue: string, type: keyof PathPointMap, result: PathPoint) => {
+        if (multiDatum) {
+            resultMapMulti[type][xValue] ??= [];
+            resultMapMulti[type][xValue].push(result);
+        } else {
+            resultMapSingle[type][xValue] = result;
+        }
+    };
+
+    const getFirstWithoutTo = (xValue: string, type: keyof PathPointMap) => {
+        if (multiDatum) {
+            for (const result of resultMapMulti[type][xValue]) {
+                if (result.to == null) {
+                    return result;
+                }
+            }
+            return undefined;
+        }
+        return resultMapSingle[type][xValue];
     };
 
     // Process oldData first to maintain old order if possible.
@@ -178,14 +206,14 @@ export function pairCategoryData(
                 moveTo: next.point.moveTo ?? false,
                 from: next.point,
             };
-            resultMap.removed[next.xValue] = resultPoint;
+            addToResultMap(next.xValue, 'removed', resultPoint);
         } else {
             resultPoint = {
                 change: 'move',
                 moveTo: next.point.moveTo ?? false,
                 from: next.point,
             };
-            resultMap.moved[next.xValue] = resultPoint;
+            addToResultMap(next.xValue, 'moved', resultPoint);
         }
         result.push(resultPoint);
     }
@@ -199,12 +227,14 @@ export function pairCategoryData(
                 moveTo: next.point.moveTo ?? false,
                 to: next.point,
             };
-            resultMap.added[next.xValue] = resultPoint;
-            result.splice(diff.addedIndices[addedIdx], 0, resultPoint);
+            addToResultMap(next.xValue, 'added', resultPoint);
+            result.splice(diff.addedIndices[addedIdx] * datumIndexMultiplier, 0, resultPoint);
         } else {
-            const moved = resultMap.moved[next.xValue];
-            moved.to = next.point;
-            moved.moveTo = calculateMoveTo(!!moved.moveTo, next.point.moveTo);
+            const moved = getFirstWithoutTo(next.xValue, 'moved');
+            if (moved) {
+                moved.to = next.point;
+                moved.moveTo = calculateMoveTo(!!moved.moveTo, next.point.moveTo);
+            }
         }
     }
 
@@ -223,7 +253,10 @@ export function pairCategoryData(
 
     backfillPathPointData(result, backfillSplitMode);
 
-    return { result, resultMap };
+    if (multiDatum) {
+        return { result, resultMap: resultMapMulti };
+    }
+    return { result, resultMap: resultMapSingle };
 }
 
 export function determinePathStatus(newData: LineContextLike, oldData: LineContextLike) {
