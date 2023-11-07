68a7f5148a20c846ab6178e848609a1cfd174ea6
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-11-07 23:32:24 +0000

    AG-9571 - New scatter example with labels

---
+++ packages/ag-charts-website/src/content/gallery/_examples/scatter-with-labels/data.ts
@@ -0,0 +1,173 @@
+export function getData() {
+    return [
+      {
+          "name": "Olivia",
+          "popularity": 52,
+          "trend": 5,
+          "gender": "Girl"
+      },
+      {
+          "name": "Amelia",
+          "popularity": 65,
+          "trend": 3,
+          "gender": "Girl"
+      },
+      {
+          "name": "Isla",
+          "popularity": 88,
+          "trend": 2,
+          "gender": "Girl"
+      },
+      {
+          "name": "Mia",
+          "popularity": 62,
+          "trend": 2,
+          "gender": "Girl"
+      },
+      {
+          "name": "Sophia",
+          "popularity": 43,
+          "trend": 6,
+          "gender": "Girl"
+      },
+      {
+          "name": "Ella",
+          "popularity": 11,
+          "trend": 8,
+          "gender": "Girl"
+      },
+      {
+          "name": "Charlotte",
+          "popularity": 41,
+          "trend": 7,
+          "gender": "Girl"
+      },
+      {
+          "name": "Grace",
+          "popularity": 69,
+          "trend": 8,
+          "gender": "Girl"
+      },
+      {
+          "name": "Lily",
+          "popularity": 22,
+          "trend": 9,
+          "gender": "Girl"
+      },
+      {
+          "name": "Sophie",
+          "popularity": 56,
+          "trend": 9,
+          "gender": "Girl"
+      },
+      {
+          "name": "Emily",
+          "popularity": 80,
+          "trend": 1,
+          "gender": "Girl"
+      },
+      {
+          "name": "Evie",
+          "popularity": 43,
+          "trend": 4,
+          "gender": "Girl"
+      },
+      {
+          "name": "Scarlett",
+          "popularity": 17,
+          "trend": 4,
+          "gender": "Girl"
+      },
+      {
+          "name": "Oliver",
+          "popularity": 66,
+          "trend": 6,
+          "gender": "Boy"
+      },
+      {
+          "name": "George",
+          "popularity": 76,
+          "trend": 6,
+          "gender": "Boy"
+      },
+      {
+          "name": "Noah",
+          "popularity": 63,
+          "trend": 5,
+          "gender": "Boy"
+      },
+      {
+          "name": "Arthur",
+          "popularity": 85,
+          "trend": 9,
+          "gender": "Boy"
+      },
+      {
+          "name": "Harry",
+          "popularity": 8,
+          "trend": 1,
+          "gender": "Boy"
+      },
+      {
+          "name": "Leo",
+          "popularity": 9,
+          "trend": 2,
+          "gender": "Boy"
+      },
+      {
+          "name": "Muhammad",
+          "popularity": 74,
+          "trend": 5,
+          "gender": "Boy"
+      },
+      {
+          "name": "Jack",
+          "popularity": 96,
+          "trend": 7,
+          "gender": "Boy"
+      },
+      {
+          "name": "Charlie",
+          "popularity": 46,
+          "trend": 2,
+          "gender": "Boy"
+      },
+      {
+          "name": "Oscar",
+          "popularity": 91,
+          "trend": 3,
+          "gender": "Boy"
+      },
+      {
+          "name": "Henry",
+          "popularity": 11,
+          "trend": 5,
+          "gender": "Boy"
+      },
+      {
+          "name": "Thomas",
+          "popularity": 51,
+          "trend": 4,
+          "gender": "Boy"
+      },
+      {
+          "name": "Ethan",
+          "popularity": 73,
+          "trend": 8,
+          "gender": "Boy"
+      },
+      {
+          "name": "Alfie",
+          "popularity": 89,
+          "trend": 5,
+          "gender": "Boy"
+      },
+      {
+          "name": "Jacob",
+          "popularity": 26,
+          "trend": 3,
+          "gender": "Boy"
+      }
+  ]
+  }
+  
