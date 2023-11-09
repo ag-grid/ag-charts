ab93cf09d8fd07e0212e2e6efdc406926e572e54
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-11-09 18:29:46 +0000

    AG-9571 - New Grouped Stacked Bar gallery example

---
+++ packages/ag-charts-website/src/content/gallery/_examples/grouped-stacked-bar/main.ts
@@ -0,0 +1,105 @@
+import { AgEnterpriseCharts, AgChartOptions } from 'ag-charts-enterprise'
+import { getData } from "./data"
+
+const options: AgChartOptions =  {
+  container: document.getElementById('myChart'),
+  title: {
+    text: 'Dolphins & Mirrors',
+  },
+  subtitle: {
+    text: 'Interactions of Dolphins With Marked Mirrors',
+  },
+  data: getData(),
+  series: [
+    {
+      type: 'bar',
+      xKey: 'dolphin',
+      yKey: 'interactionDurationTM',
+      yName: 'Interaction Duration - Transparent Mirror',
+      legendItemName: 'Interaction Duration - Transparent Mirror',
+      stackGroup: 'ID',
+      errorBar: {
+        yLowerKey: 'interactionDurationTMLower',
+        yUpperKey: 'interactionDurationTMUpper',
+      },
+      strokeWidth: 2,
+      stroke: 'transparent',
+    },
+    {
+      type: 'bar',
+      xKey: 'dolphin',
+      yKey: 'interactionDurationYM',
+      yName: 'Interaction Duration - Yellow Mirror',
+      legendItemName: 'Interaction Duration - Yellow Mirror',
+      stackGroup: 'ID',
+      errorBar: {
+        yLowerKey: 'interactionDurationYMLower',
+        yUpperKey: 'interactionDurationYMUpper',
+      },
+      strokeWidth: 2,
+      stroke: 'transparent',
+    },
+    {
+      type: 'bar',
+      xKey: 'dolphin',
+      yKey: 'numberOfLooksTM',
+      yName: 'Number of Looks - Transparent Mirror',
+      legendItemName: 'Number of Looks - Transparent Mirror',
+      stackGroup: 'NOL',
+      strokeWidth: 2,
+      stroke: 'transparent',
+    },
+    {
+      type: 'bar',
+      xKey: 'dolphin',
+      yKey: 'numberOfLooksYM',
+      yName: 'Number of Looks - Yellow Mirror',
+      legendItemName: 'Number of Looks - Yellow Mirror',
+      stackGroup: 'NOL',
+      strokeWidth: 2,
+      stroke: 'transparent',
+    },
+  ],
+  axes: [
+    {
+      position: 'top',
+      type: 'category',
+      keys: ['dolphin'],
+      title: {
+        text: 'Dolphin',
+      },
+      paddingInner: 0.5,
+      paddingOuter: 0.2,
+      crossLines: [
+        {
+          type: 'range',
+          range: ['Peter', 'Peter'],
+          strokeWidth: 0,
+        },
+        {
+          type: 'range',
+          range: ['Mercutio', 'Mercutio'],
+          strokeWidth: 0,
+        },
+      ],
+    },
+    {
+      position: 'left',
+      type: 'number',
+      keys: ['interactionDurationTM', 'interactionDurationYM'],
+      title: {
+        text: 'Duration of Interaction (seconds)',
+      },
+    },
+    {
+      position: 'right',
+      type: 'number',
+      title: {
+        text: 'Numer of Looks',
+      },
+      keys: ['numberOfLooksTM', 'numberOfLooksYM'],
+    },
+  ],
+};
+
+AgEnterpriseCharts.create(options)
