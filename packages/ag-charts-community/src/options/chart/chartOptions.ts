55df423f2cb0719e426d5d716a22e9168c29065e
iMoses (Ido Moshe) <imoses.g@gmail.com>
2023-11-09 10:45:11 +0000

    fix another comment we left out

--- packages/ag-charts-community/src/options/chart/chartOptions.ts
+++ packages/ag-charts-community/src/options/chart/chartOptions.ts
@@ -105,13 +105,9 @@ export interface AgBaseThemeableChartOptions {
     /** The height of the chart in pixels. */
     height?: PixelSize;
     /**
-     * By default, the chart will resize automatically to fill the container element. Set this to `false` to disable
-     * this behaviour. If `width` or `height` are specified, auto-sizing will be active for the other unspecified
-     * dimension.
+     * By default, the chart will resize automatically to fill the container element. Set this to `false` to disable this behaviour. If `width` or `height` are specified, auto-sizing will be active for the other unspecified dimension.
      *
-     * __Important:__ if this config is set to `true`, make sure to give the chart's `container` element an explicit
-     * size, otherwise you will run into a chicken and egg situation where the container expects to size itself
-     * according to the content and the chart expects to size itself according to the container.
+     * __Important:__ if this config is set to `true`, make sure to give the chart's `container` element an explicit size, otherwise you will run into a chicken and egg situation where the container expects to size itself according to the content and the chart expects to size itself according to the container.
      */
     autoSize?: boolean;
     /** Configuration for the padding shown around the chart. */
