377e43113d261ee201bd9a15338ad8b19b256a7f
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-11-10 19:22:01 +0000

    AG-9571 - Update gallery pie with variable radius

--- packages/ag-charts-website/src/content/gallery/_examples/pie-with-variable-radius/main.ts
+++ packages/ag-charts-website/src/content/gallery/_examples/pie-with-variable-radius/main.ts
@@ -2,8 +2,6 @@ import { AgChartOptions, AgEnterpriseCharts } from 'ag-charts-enterprise';
 
 import { getData } from './data';
 
-const numFormatter = new Intl.NumberFormat('en-US');
-const usdFormatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 });
 const usdShortOptions: any = { style: 'currency', currency: 'USD', notation: 'compact' };
 const usdShortFormatter = new Intl.NumberFormat('en-US', usdShortOptions);
 
@@ -13,7 +11,7 @@ const options: AgChartOptions = {
         text: 'The GDP of Baltic States',
     },
     subtitle: {
-        text: 'Population (Angle) & GDP per Capita (Radius)',
+        text: 'Population & GDP per Capita',
     },
     series: [
         {
@@ -23,25 +21,9 @@ const options: AgChartOptions = {
             sectorLabelKey: 'gdpPerCapita',
             angleKey: 'population',
             radiusKey: 'gdpPerCapita',
-            calloutLabel: {
-                minAngle: 0,
-            },
             sectorLabel: {
                 formatter: ({ datum }) => {
                     return usdShortFormatter.format(datum['population'] * datum['gdpPerCapita']);
-                },
-            },
-            tooltip: {
-                renderer: ({ datum, color }) => {
-                    return [
-                        `<div style="background-color: ${color}; padding: 4px 8px; border-top-left-radius: 5px; border-top-right-radius: 5px; font-weight: bold; color: white;">${datum['country']}</div>`,
-                        `<div style="padding: 4px 8px"><strong>Population:</strong> ${numFormatter.format(
-                            datum['population']
-                        )}</div>`,
-                        `<div style="padding: 4px 8px"><strong>GDP per Capita:</strong> ${usdFormatter.format(
-                            datum['gdpPerCapita']
-                        )}</div>`,
-                    ].join('\n');
                 },
             },
         },
