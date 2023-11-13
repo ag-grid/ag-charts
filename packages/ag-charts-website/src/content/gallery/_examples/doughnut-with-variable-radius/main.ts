b02e6d29d5067d385f78aff16cab56c0bfeebaa3
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-11-13 15:56:06 +0000

    AG-9571 - New Gallery doughnut with variable radius example

---
+++ packages/ag-charts-website/src/content/gallery/_examples/doughnut-with-variable-radius/main.ts
@@ -0,0 +1,65 @@
+import { AgChartOptions, AgEnterpriseCharts, AgPieSeriesTooltipRendererParams } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+const data = getData();
+const numFormatter = new Intl.NumberFormat('en-US');
+const tooltip = {
+    renderer: ({ datum, angleKey }: AgPieSeriesTooltipRendererParams) => ({
+        content: `${numFormatter.format(datum[angleKey] / 1000000)}M`,
+    }),
+};
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    title: {
+        text: 'Oxford Street Selfridges',
+    },
+    subtitle: {
+        text: 'Total Product Value by Department',
+    },
+    series: [
+        {
+            data: data['categories'],
+            type: 'pie',
+            calloutLabelKey: 'category',
+            calloutLabel: {
+                offset: 10,
+            },
+            angleKey: 'value',
+            radiusKey: 'value',
+            outerRadiusRatio: 0.8,
+            innerRadiusRatio: 0.6,
+            fillOpacity: 0.4,
+            tooltip,
+        },
+        {
+            data: data['departments'],
+            type: 'pie',
+            sectorLabelKey: 'department',
+            angleKey: 'value',
+            outerRadiusRatio: 0.6,
+            innerRadiusRatio: 0.4,
+            fillOpacity: 0.6,
+            tooltip,
+        },
+        {
+            data: data['stores'],
+            type: 'pie',
+            sectorLabelKey: 'store',
+            angleKey: 'total',
+            outerRadiusRatio: 0.4,
+            innerRadiusRatio: 0,
+            tooltip: {
+                renderer: ({ datum, angleKey }) => ({
+                    content: `Total: ${numFormatter.format(datum[angleKey] / 1000000000)}B`,
+                }),
+            },
+        },
+    ],
+    legend: {
+        enabled: false,
+    },
+};
+
+AgEnterpriseCharts.create(options);
