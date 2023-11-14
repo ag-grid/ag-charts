2974005b7556110efe4e669b8fe29d0f41d6bc11
Oli Legat <olivier.legat@ag-grid.com>
2023-11-14 12:11:57 +0000

    AG-9302 Add 'Multiple Bullets' section

---
+++ packages/ag-charts-website/src/content/docs/bullet-series/_examples/5-bullets/main.ts
@@ -0,0 +1,90 @@
+import { AgCharts } from 'ag-charts-enterprise';
+
+const BAD = '#FFB6C1'; /* Light Pink */
+const OKAY = '#FFFACD'; /* Light Yellow */
+const GOOD = '#B6FBB6'; /* Light Green */
+
+AgCharts.create({
+    container: document.getElementById('myChart1'),
+    title: { text: 'Return Of Investment (ROI) %' },
+    data: [{ target: 15, actual: 11 }],
+    series: [
+        {
+            type: 'bullet',
+            valueKey: 'actual',
+            valueName: 'Actual ROI',
+            targetKey: 'target',
+            targetName: 'Target ROI',
+            scale: { max: 20 },
+            colorRanges: [{ color: BAD, stop: 8 }, { color: OKAY, stop: 12 }, { color: GOOD }],
+        },
+    ],
+});
+
+AgCharts.create({
+    container: document.getElementById('myChart2'),
+    title: { text: 'Debt-to-Equity' },
+    data: [{ target: 0.5, actual: 0.7 }],
+    series: [
+        {
+            type: 'bullet',
+            valueKey: 'actual',
+            valueName: 'Actual Ratio',
+            targetKey: 'target',
+            targetName: 'Target Ratio',
+            scale: { max: 1 },
+            colorRanges: [{ color: GOOD, stop: 0.6 }, { color: OKAY, stop: 0.8 }, { color: BAD }],
+        },
+    ],
+});
+
+AgCharts.create({
+    container: document.getElementById('myChart3'),
+    title: { text: 'Profit Margin %' },
+    data: [{ target: 20, actual: 16.2 }],
+    series: [
+        {
+            type: 'bullet',
+            valueKey: 'actual',
+            valueName: 'Actual Margin',
+            targetKey: 'target',
+            targetName: 'Target Margin',
+            scale: { max: 25 },
+            colorRanges: [{ color: BAD, stop: 12 }, { color: OKAY, stop: 18 }, { color: GOOD }],
+        },
+    ],
+});
+
+AgCharts.create({
+    container: document.getElementById('myChart4'),
+    title: { text: 'Cash Flow (US$)' },
+    data: [{ target: 500000, actual: 468100 }],
+    series: [
+        {
+            type: 'bullet',
+            valueKey: 'actual',
+            valueName: 'Actual Cash Flow',
+            targetKey: 'target',
+            targetName: 'Target Cash Flow',
+            scale: { max: 600000 },
+            colorRanges: [{ color: BAD, stop: 350000 }, { color: OKAY, stop: 450000 }, { color: GOOD }],
+        },
+    ],
+});
+
+AgCharts.create({
+    container: document.getElementById('myChart5'),
+    title: { text: 'Earnings Before Interest, Taxes, Depreciation, and Amortization (EBITDA) Margin' },
+    data: [{ target: 25, actual: 21.7 }],
+    series: [
+        {
+            type: 'bullet',
+            valueKey: 'actual',
+            valueName: 'Actual EBITDA',
+            targetKey: 'target',
+            targetName: 'Target EBITDA',
+            scale: { max: 30 },
+            colorRanges: [{ color: BAD, stop: 15 }, { color: OKAY, stop: 22 }, { color: GOOD }],
+        },
+    ],
+});
