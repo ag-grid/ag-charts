5ec435f4b5d213a4f10ab3c78e61c3c5c90d992e
Oli Legat <olivier.legat@ag-grid.com>
2023-11-14 13:05:19 +0000

    AG-9302 Add 'Horizontal Bullet' section

---
+++ packages/ag-charts-website/src/content/docs/bullet-series/_examples/horizontal-bullet/main.ts
@@ -0,0 +1,26 @@
+import { AgChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    title: { text: 'Income' },
+    subtitle: { text: 'USD' },
+    data: [{ income: 12500, objective: 10000 }],
+    series: [
+        {
+            type: 'bullet',
+            direction: 'horizontal',
+            valueKey: 'income',
+            valueName: 'Actual income',
+            targetKey: 'objective',
+            targetName: 'Target income',
+            scale: { max: 15000 },
+            colorRanges: [
+                { color: '#FFB6C1' /* Light Pink */, stop: 8000 },
+                { color: '#FFFACD' /* Light Yellow */, stop: 13000 },
+                { color: '#B6FBB6' /* Light Green */ },
+            ],
+        },
+    ],
+};
+
+AgCharts.create(options);
