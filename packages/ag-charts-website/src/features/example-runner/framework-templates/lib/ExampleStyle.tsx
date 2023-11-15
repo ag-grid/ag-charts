a680cda792eb22daa0464065778baab16d87c858
Kyler Phillips <kylerphillips@hotmail.co.uk>
2023-11-15 09:09:29 +0000

    Attempt to style buttons better

--- packages/ag-charts-website/src/features/example-runner/framework-templates/lib/ExampleStyle.tsx
+++ packages/ag-charts-website/src/features/example-runner/framework-templates/lib/ExampleStyle.tsx
@@ -26,6 +26,25 @@ export const ExampleStyle = ({ rootId }: { rootId?: string }) => (
             padding: 1rem;
             overflow: auto;
         }
+
+        html button:not(#myGrid button, #myChart button, button[class*='ag-'], .ag-chart-context-menu__menu) {
+            appearance: none;
+            background-color: var(--background-100);
+            border: 2px solid rgba(187, 187, 187, 0.3);
+            border-radius: 6px;
+            height: 36px;
+            color: #969696;
+            cursor: pointer;
+            display: inline-block;
+            font-size: 14px;
+            font-weight: 500;
+            letter-spacing: .01em;
+            padding: 0.375em 1em 0.5em;
+            white-space: nowrap;
+            margin-right: 6px;
+            margin-bottom: 8px;
+            transition: background-color .25s ease-in-out;
+        }
     `}
     </style>
 );
