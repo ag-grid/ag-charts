4228e4d99dd07a0aac9089034812fc4973a1fd8d
Alan Treadway <alan.treadway@ag-grid.com>
2023-11-16 13:47:44 +0000

    AG-9818 - Rework rollup config to support UMD-only build.

--- packages/ag-charts-enterprise/rollup.config.cjs
+++ packages/ag-charts-enterprise/rollup.config.cjs
@@ -1,11 +1,5 @@
 const base = require('../../rollup.base.cjs');
 
 module.exports = (config) => {
-    return base('agChartsEnterprise', config, {
-        umd: {
-            globals: {
-                'ag-charts-community': 'agCharts',
-            },
-        },
-    });
+    return base('agChartsEnterprise', config, { onlyUmd: false });
 };
