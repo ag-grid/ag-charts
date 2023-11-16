beebbcba44e7e26377d33cf8fec79c4d3dd64306
iMoses <imoses.g@gmail.com>
2023-11-16 12:44:53 +0200

    AG-9223: Fix docs infinite loop bug (#541)
    * Revert "AG-9223 - Fix trailing slash infinite loop (#534)"
    
    This reverts commit a5b073be43e8bebb92be2fb15c248573328c5c6d.
    
    * fix types & stop infinite refresh loop
    
    * fix tsconfig
    
    * revert frameworkStore.ts changes so it can be pushed as a separate PR
    
    * AG-9223: Fix docs infinite loop bug
    
    * set all `persistentAtom` to `listen: false`
    
    * fix types

--- packages/ag-charts-website/src/stores/darkmodeStore.ts
+++ packages/ag-charts-website/src/stores/darkmodeStore.ts
@@ -10,20 +10,22 @@ const isOSDarkmode = (
     window.matchMedia('(prefers-color-scheme: dark)').matches
 ).toString();
 
-export const $darkmode = persistentAtom(`${LOCALSTORAGE_PREFIX}:darkmode`, isOSDarkmode);
+export const $darkmode = persistentAtom(`${LOCALSTORAGE_PREFIX}:darkmode`, isOSDarkmode, { listen: false });
 
 export const setDarkmode = (darkmode: boolean) => {
-    $darkmode.set(darkmode);
+    $darkmode.set(String(darkmode));
 
     const htmlEl = document.querySelector('html');
 
-    // Using .no-transitions class so that there are no animations between light/dark modes
-    htmlEl.classList.add('no-transitions');
-    htmlEl.dataset.darkMode = darkmode ? 'true' : 'false';
-    htmlEl.offsetHeight; // Trigger a reflow, flushing the CSS changes
-    htmlEl.classList.remove('no-transitions');
+    if (htmlEl) {
+        // Using .no-transitions class so that there are no animations between light/dark modes
+        htmlEl.classList.add('no-transitions');
+        htmlEl.dataset.darkMode = darkmode ? 'true' : 'false';
+        htmlEl.offsetHeight; // Trigger a reflow, flushing the CSS changes
+        htmlEl.classList.remove('no-transitions');
+    }
 };
 
 export const getDarkmode = () => {
-    return $darkmode.get() === 'true' || $darkmode.get() === true;
+    return $darkmode.get() === 'true';
 };
