37d2ffcf05f761a8f9a4d0ea75c5df78a82fc1ac
iMoses (Ido Moshe) <imoses.g@gmail.com>
2023-11-16 10:33:37 +0000

    fix types

--- packages/ag-charts-website/src/stores/darkmodeStore.ts
+++ packages/ag-charts-website/src/stores/darkmodeStore.ts
@@ -13,17 +13,19 @@ const isOSDarkmode = (
 export const $darkmode = persistentAtom(`${LOCALSTORAGE_PREFIX}:darkmode`, isOSDarkmode, { listen: false });
 
 export const setDarkmode = (darkmode: boolean) => {
-    $darkmode.set(darkmode);
+    $darkmode.set(String(darkmode));
 
     const htmlEl = document.querySelector('html');
 
-    // Using .no-transitions class so that there are no animations between light/dark modes
-    htmlEl.classList.add('no-transitions');
-    htmlEl.dataset.darkMode = darkmode ? 'true' : 'false';
-    htmlEl.offsetHeight; // Trigger a reflow, flushing the CSS changes
-    htmlEl.classList.remove('no-transitions');
+    if (htmlEl) {
+        // Using .no-transitions class so that there are no animations between light/dark modes
+        htmlEl.classList.add('no-transitions');
+        htmlEl.dataset.darkMode = darkmode ? 'true' : 'false';
+        htmlEl.offsetHeight; // Trigger a reflow, flushing the CSS changes
+        htmlEl.classList.remove('no-transitions');
+    }
 };
 
 export const getDarkmode = () => {
-    return $darkmode.get() === 'true' || $darkmode.get() === true;
+    return $darkmode.get() === 'true';
 };
