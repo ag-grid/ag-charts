4495c7c8df6a32320f397b0a4c072c68e8ffaa3f
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-11-17 17:05:34 +0000

    AG-9571 - New horizontal range bar with labels example

---
+++ packages/ag-charts-website/src/content/gallery/_examples/horizontal-range-bar-with-labels/main.ts
@@ -0,0 +1,70 @@
+import { type AgChartOptions, AgEnterpriseCharts } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    title: {
+        text: 'Belsize Road Tesco Annual Sales',
+    },
+    subtitle: {
+        text: 'Total Sales In 2022 And 2023 For Different Product Categories',
+    },
+    footnote: {
+        text: 'Sales in Pound Sterling (£)',
+    },
+    seriesArea: {
+        padding: {
+            right: 20,
+        },
+    },
+    data: getData(),
+    series: [
+        {
+            type: 'range-bar',
+            direction: 'horizontal',
+            xKey: 'category',
+            xName: 'Category',
+            yName: 'Annual Sales',
+            yLowKey: 'sales2022',
+            yLowName: 'Sales 2022',
+            yHighKey: 'sales2023',
+            yHighName: 'Sales 2023',
+            label: {
+                placement: 'outside',
+                color: 'rgb(118,118,118)',
+                formatter: ({ itemId, datum, yHighKey, yLowKey }) => {
+                    const increase = datum[0][yHighKey] - datum[0][yLowKey];
+                    return itemId === 'high' ? `↑£${increase / 1000}K` : '';
+                },
+            },
+            formatter: ({ datum, yHighKey, yLowKey }) => ({
+                fillOpacity: (datum[0][yHighKey] - datum[0][yLowKey]) / 100000,
+            }),
+            strokeWidth: 1,
+            lineDash: [3, 5],
+        },
+    ],
+    axes: [
+        {
+            type: 'category',
+            position: 'left',
+            paddingInner: 0.4,
+            gridLine: {
+                enabled: true,
+            },
+        },
+        {
+            type: 'number',
+            position: 'top',
+            gridLine: {
+                enabled: false,
+            },
+            label: {
+                formatter: ({ value }) => `${(+value).toLocaleString()}`,
+            },
+        },
+    ],
+};
+
+AgEnterpriseCharts.create(options);
