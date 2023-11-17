b695bf38c4340737a980dabeb58bfe4fe8b79470
seanlandsman <sean@thelandsmans.com>
2023-11-17 13:39:00 +0000

    AG-9827 Implement Prod Docs Deploy for Charts

---
+++ tools/release/archiveCurrentRelease.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+function checkFileExists {
+    file=$1
+    if ! [[ -f "$file" ]]
+    then
+        echo "File [$file] doesn't exist - exiting script.";
+        exit 1;
+    fi
+}
+
+SSH_LOCATION=$SSH_FILE
+
+if [ -z "$SSH_LOCATION" ]
+then
+      echo "\$SSH_LOCATION is not set"
+      exit 1;
+fi
+
+checkFileExists $SSH_LOCATION
+
+TIMESTAMP=`date +%Y%m%d`
+ARCHIVE_FILENAME="agCharts_$TIMESTAMP.zip"
+
+echo "$ARCHIVE_FILENAME"
+echo "$CHARTS_PUBLIC_HTML_PATH"
+
+# archive the current charts folder if the archive doesn't already exist (to take account of multiple deployments on the same day)
+#ssh -i $SSH_LOCATION -p $SSH_PORT $HOST  "if [ -f "$WORKING_DIR_ROOT/$ARCHIVE_FILENAME" ]; then echo \"$WORKING_DIR_ROOT/$ARCHIVE_FILENAME exists - skipping archive\"; else zip -r --exclude=*archive* $WORKING_DIR_ROOT/$ARCHIVE_FILENAME $CHARTS_PUBLIC_HTML_PATH; fi"
+
+
