8ae05e4be3f01c2570baf2ec61ed244abe625bb2
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-11-17 16:59:11 +0000

    AG-9571 - Update simple range bar example

--- packages/ag-charts-website/src/content/gallery/_examples/simple-range-bar/main.ts
+++ packages/ag-charts-website/src/content/gallery/_examples/simple-range-bar/main.ts
@@ -6,15 +6,67 @@ const options: AgChartOptions = {
     container: document.getElementById('myChart'),
     data: getData(),
     title: {
-        text: 'Range Column',
+        text: 'S&P 500 Index',
+    },
+    subtitle: {
+        text: 'Daily High and Low Prices',
+    },
+    footnote: {
+        text: '1 Aug 2023 - 1 Nov 2023',
     },
     series: [
         {
             type: 'range-bar',
-            xKey: 'date',
+            xKey: 'Date',
             xName: 'Date',
-            yLowKey: 'low',
-            yHighKey: 'high',
+            yLowKey: 'Low',
+            yHighKey: 'High',
+            strokeWidth: 1,
+        },
+    ],
+    axes: [
+        {
+            type: 'time',
+            position: 'bottom',
+            nice: false,
+            tick: {
+                values: [
+                    new Date(2023, 7, 1),
+                    new Date(2023, 7, 15),
+                    new Date(2023, 8, 1),
+                    new Date(2023, 8, 15),
+                    new Date(2023, 9, 1),
+                    new Date(2023, 9, 15),
+                    new Date(2023, 10, 1),
+                ],
+            },
+            line: {
+                width: 0,
+            },
+            gridLine: {
+                style: [
+                    {
+                        stroke: 'rgb(216,216,216)',
+                        lineDash: [2, 2],
+                    },
+                ],
+            },
+        },
+        {
+            type: 'number',
+            position: 'right',
+            nice: false,
+            label: {
+                formatter: ({ value }) => Number(value).toLocaleString(),
+            },
+            gridLine: {
+                style: [
+                    {
+                        stroke: 'rgb(216,216,216)',
+                        lineDash: [2, 2],
+                    },
+                ],
+            },
         },
     ],
 };
