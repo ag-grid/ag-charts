02db5dbb6956c6aa2569267c30dae039d4360ec0
Alan Treadway <alan.treadway@ag-grid.com>
2023-11-17 13:56:48 +0000

    AG-9818 - Fix UMD exports and use.

--- packages/ag-charts-enterprise/rollup.config.cjs
+++ packages/ag-charts-enterprise/rollup.config.cjs
@@ -1,5 +1,5 @@
 const base = require('../../rollup.base.cjs');
 
 module.exports = (config) => {
-    return base('agChartsEnterprise', config, { umdOutput: 'none' });
+    return base('agCharts', config, { umdOutput: 'none' });
 };
--- packages/ag-charts-enterprise/rollup.umd.config.cjs
+++ packages/ag-charts-enterprise/rollup.umd.config.cjs
@@ -1,5 +1,5 @@
 const base = require('../../rollup.base.cjs');
 
 module.exports = (config) => {
-    return base('agChartsEnterprise', config, { umdOutput: 'only', filename: 'ag-charts-enterprise.js' });
+    return base('agCharts', config, { umdOutput: 'only', filename: 'ag-charts-enterprise.js' });
 };
