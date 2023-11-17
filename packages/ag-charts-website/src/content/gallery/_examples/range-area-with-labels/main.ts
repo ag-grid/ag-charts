967cec7042f7402e7d186ad066878f89455e7e69
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-11-17 17:07:26 +0000

    AG-9571 - New range area with labels example

---
+++ packages/ag-charts-website/src/content/gallery/_examples/range-area-with-labels/main.ts
@@ -0,0 +1,71 @@
+import { AgChartOptions, AgEnterpriseCharts } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'House Price Index In London',
+    },
+    subtitle: {
+        text: 'Price Indices of Flats and Terraced Houses from 2020 to 2023',
+        spacing: 45,
+    },
+    footnote: {
+        text: 'Source: UK Gov Land Registry',
+    },
+    series: [
+        {
+            type: 'range-area',
+            xKey: 'Date',
+            yLowKey: 'Flats and maisonettes',
+            yHighKey: 'Terraced houses',
+            fillOpacity: 0.2,
+            label: {
+                formatter: ({ value }) => `${value === 113.4 ? value : ''}`,
+            },
+        },
+        {
+            type: 'range-area',
+            xKey: 'Date',
+            yLowKey: 'Terraced houses',
+            yHighKey: 'Semi-detached houses',
+            fillOpacity: 0.2,
+            label: {
+                formatter: ({ value }) => `${value === 149.9 ? value : ''}`,
+            },
+        },
+    ],
+    axes: [
+        {
+            type: 'number',
+            position: 'right',
+            gridLine: {
+                enabled: false,
+            },
+            nice: false,
+            min: 100,
+            max: 160,
+        },
+        {
+            type: 'time',
+            position: 'bottom',
+            nice: false,
+            label: {
+                format: `%b %y`,
+            },
+            tick: {
+                size: 14,
+            },
+            gridLine: {
+                enabed: true,
+            },
+        },
+    ],
+    legend: {
+        enabled: false,
+    },
+};
+
+AgEnterpriseCharts.create(options);
