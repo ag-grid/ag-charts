ba686bc84cf18e289764b4711ba3275286f8d0f4
Alan Treadway <alan.treadway@ag-grid.com>
2023-11-17 14:46:40 +0000

    AG-9818 - Add generated example caching to improve website build time by 8%.

--- packages/ag-charts-website/src/features/examples-generator/examplesGenerator.ts
+++ packages/ag-charts-website/src/features/examples-generator/examplesGenerator.ts
@@ -1,6 +1,7 @@
 import type { InternalFramework } from '@ag-grid-types';
 import fs from 'node:fs/promises';
 
+import { getIsDev } from '../../utils/env';
 import { ANGULAR_GENERATED_MAIN_FILE_NAME, SOURCE_ENTRY_FILE_NAME } from './constants';
 import chartVanillaSrcParser from './transformation-scripts/chart-vanilla-src-parser';
 import type { GeneratedContents } from './types.d';
@@ -9,17 +10,66 @@ import { frameworkFilesGenerator } from './utils/frameworkFilesGenerator';
 import { getOtherScriptFiles } from './utils/getOtherScriptFiles';
 import { getStyleFiles } from './utils/getStyleFiles';
 
+class ContentCache<K extends object, V> {
+    private cacheKeyMap = new Map<string, K>();
+    private cache = new WeakMap<K, V>();
+    private readonly keyToCacheKey: (k: K) => string;
+
+    constructor(...keys: (keyof K)[]) {
+        const parts = new Array(keys.length);
+        this.keyToCacheKey = (k: K) => {
+            for (let i = 0; i < keys.length; i++) {
+                parts[i] = k[keys[i]];
+            }
+            return parts.join('_');
+        };
+    }
+
+    get(key: K) {
+        const keyStr = this.keyToCacheKey(key);
+        const cacheKey = this.cacheKeyMap.get(keyStr);
+
+        if (cacheKey && this.cache.has(cacheKey)) {
+            return this.cache.get(cacheKey);
+        }
+
+        return undefined;
+    }
+
+    set(key: K, val: V) {
+        const keyStr = this.keyToCacheKey(key);
+
+        this.cacheKeyMap.set(keyStr, key);
+        this.cache.set(key, val);
+    }
+
+    invalidate() {
+        for (const key of this.cacheKeyMap.values()) {
+            this.cache.delete(key);
+        }
+        this.cacheKeyMap.clear();
+    }
+}
+
+const fileListCache = new ContentCache<FileListParams, string[]>('internalFramework', 'folderUrl');
+
+type FileListParams = {
+    internalFramework: InternalFramework;
+    folderUrl: URL;
+};
+
 /**
  * Get the file list of the generated contents
  * (without generating the contents)
  */
-export const getGeneratedContentsFileList = async ({
-    internalFramework,
-    folderUrl,
-}: {
-    internalFramework: InternalFramework;
-    folderUrl: URL;
-}): Promise<string[]> => {
+export const getGeneratedContentsFileList = async (params: FileListParams): Promise<string[]> => {
+    const { internalFramework, folderUrl } = params;
+
+    if (!getIsDev()) {
+        const cachedResult = fileListCache.get(params);
+        if (cachedResult) return cachedResult;
+    }
+
     const entryFileName = getEntryFileName(internalFramework)!;
     const sourceFileList = await fs.readdir(folderUrl);
 
@@ -43,21 +93,33 @@ export const getGeneratedContentsFileList = async ({
         .concat(Object.keys(scriptFiles))
         .concat(Object.keys(styleFiles));
 
+    fileListCache.set(params, generatedFileList);
+
     return generatedFileList;
+};
+
+const contentCache = new ContentCache<GeneratedContentParams, GeneratedContents>(
+    'internalFramework',
+    'folderUrl',
+    'ignoreDarkMode'
+);
+
+type GeneratedContentParams = {
+    internalFramework: InternalFramework;
+    folderUrl: URL;
+    ignoreDarkMode?: boolean;
 };
 
 /**
  * Get generated contents for an example
  */
-export const getGeneratedContents = async ({
-    internalFramework,
-    folderUrl,
-    ignoreDarkMode,
-}: {
-    internalFramework: InternalFramework;
-    folderUrl: URL;
-    ignoreDarkMode?: boolean;
-}): Promise<GeneratedContents | undefined> => {
+export const getGeneratedContents = async (params: GeneratedContentParams): Promise<GeneratedContents | undefined> => {
+    if (!getIsDev()) {
+        const cachedResult = contentCache.get(params);
+        if (cachedResult) return cachedResult;
+    }
+
+    const { internalFramework, folderUrl, ignoreDarkMode } = params;
     const sourceFileList = await fs.readdir(folderUrl);
 
     const entryFile = await getFileContents({ folderUrl, fileName: SOURCE_ENTRY_FILE_NAME }).catch(() => {}); // Fail silently
@@ -100,7 +162,7 @@ export const getGeneratedContents = async ({
         ignoreDarkMode,
     });
 
-    return {
+    const result: GeneratedContents = {
         isEnterprise,
         scriptFiles: scriptFiles!,
         styleFiles: Object.keys(styleFiles),
@@ -110,4 +172,7 @@ export const getGeneratedContents = async ({
         entryFileName,
         mainFileName,
     };
+
+    contentCache.set(params, result);
+    return result;
 };
