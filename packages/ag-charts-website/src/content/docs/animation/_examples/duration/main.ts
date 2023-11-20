575c2d57aa2e95e66d3e7b59bbe2974e111762b0
Laurence Roberts <laurence.roberts@ag-grid.com>
2023-11-20 10:17:26 +0000

    AG-9254 Fix animation duration example on frameworks

--- packages/ag-charts-website/src/content/docs/animation/_examples/duration/main.ts
+++ packages/ag-charts-website/src/content/docs/animation/_examples/duration/main.ts
@@ -20,7 +20,6 @@ const options: AgChartOptions = {
     data: getData(),
     animation: {
         enabled: true,
-        duration: 500,
     },
     series: [
         {
@@ -81,15 +80,166 @@ const options: AgChartOptions = {
     ],
 };
 
+const lineOptions: AgChartOptions = {
+    series: [
+        {
+            type: 'line',
+            xKey: 'station',
+            yKey: 'early',
+            yName: 'Early',
+            tooltip,
+        },
+        {
+            type: 'line',
+            xKey: 'station',
+            yKey: 'morningPeak',
+            yName: 'Morning peak',
+            tooltip,
+        },
+        {
+            type: 'line',
+            xKey: 'station',
+            yKey: 'interPeak',
+            yName: 'Between peak',
+            tooltip,
+        },
+        {
+            type: 'line',
+            xKey: 'station',
+            yKey: 'afternoonPeak',
+            yName: 'Afternoon peak',
+            tooltip,
+        },
+        {
+            type: 'line',
+            xKey: 'station',
+            yKey: 'evening',
+            yName: 'Evening',
+            tooltip,
+        },
+    ],
+    axes: [
+        {
+            type: 'category',
+            position: 'bottom',
+        },
+        {
+            type: 'number',
+            position: 'left',
+            label: {
+                formatter: (params) => {
+                    return params.value / 1000 + 'k';
+                },
+            },
+        },
+    ],
+};
+
+const areaOptions: AgChartOptions = {
+    series: [
+        {
+            type: 'area',
+            xKey: 'station',
+            yKey: 'early',
+            stacked: true,
+            yName: 'Early',
+            tooltip,
+        },
+        {
+            type: 'area',
+            xKey: 'station',
+            yKey: 'morningPeak',
+            yName: 'Morning peak',
+            stacked: true,
+            tooltip,
+        },
+        {
+            type: 'area',
+            xKey: 'station',
+            yKey: 'interPeak',
+            yName: 'Between peak',
+            stacked: true,
+            tooltip,
+        },
+        {
+            type: 'area',
+            xKey: 'station',
+            yKey: 'afternoonPeak',
+            yName: 'Afternoon peak',
+            stacked: true,
+            tooltip,
+        },
+        {
+            type: 'area',
+            xKey: 'station',
+            yKey: 'evening',
+            yName: 'Evening',
+            stacked: true,
+            tooltip,
+        },
+    ],
+    axes: [
+        {
+            type: 'category',
+            position: 'bottom',
+        },
+        {
+            type: 'number',
+            position: 'left',
+            label: {
+                formatter: (params) => {
+                    return params.value / 1000 + 'k';
+                },
+            },
+        },
+    ],
+};
+
+const pieOptions: AgChartOptions = {
+    series: [
+        {
+            type: 'pie',
+            title: {
+                text: 'Morning Peak',
+            },
+            calloutLabelKey: 'station',
+            legendItemKey: 'station',
+            angleKey: 'morningPeak',
+            outerRadiusRatio: 0.6,
+            innerRadiusRatio: 0.3,
+        },
+        {
+            type: 'pie',
+            title: {
+                text: 'Afternoon Peak',
+            },
+            calloutLabelKey: 'station',
+            legendItemKey: 'station',
+            angleKey: 'afternoonPeak',
+            innerRadiusRatio: 0.7,
+            showInLegend: false,
+        },
+    ],
+};
+
 let chart = AgCharts.create(options);
 
+function changeSeriesBar() {
+    AgCharts.updateDelta(chart, options);
+}
+
+function changeSeriesLine() {
+    AgCharts.updateDelta(chart, lineOptions);
+}
+
+function changeSeriesArea() {
+    AgCharts.updateDelta(chart, areaOptions);
+}
+
+function changeSeriesPie() {
+    AgCharts.updateDelta(chart, pieOptions);
+}
+
 function changeDuration(duration: number) {
-    chart.destroy();
-    chart = AgCharts.create({
-        ...options,
-        animation: {
-            ...options.animation,
-            duration,
-        },
-    });
+    AgCharts.updateDelta(chart, { animation: { duration } });
 }
