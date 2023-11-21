1bfa61fed05e37b8f41f837cac5d308bd388fba1
seanlandsman <sean@thelandsmans.com>
2023-11-21 12:00:21 +0000

    AG-9829 Add pricing page for charts

--- packages/ag-charts-website/src/features/license-pricing/LicensePricing.tsx
+++ packages/ag-charts-website/src/features/license-pricing/LicensePricing.tsx
@@ -1,4 +1,5 @@
 import { GRID_URL } from '@constants';
+import { useDarkmode } from '@utils/hooks/useDarkmode';
 import classnames from 'classnames';
 import React from 'react';
 
@@ -7,15 +8,17 @@ import styles from './license-pricing.module.scss';
 const IS_SSR = typeof window === 'undefined';
 
 export const LicensePricing = () => {
+    const darkMode = useDarkmode();
+
     return (
         <>
             {!IS_SSR && (
-                <div className={classnames('page-margin', styles.container)}>
+                <div className={classnames(styles.container)}>
                     <iframe
                         style={{ height: '100%', width: '100%' }}
                         frameBorder="0"
                         scrolling="no"
-                        src={`${GRID_URL}/license-pricing-bare/`}
+                        src={`${GRID_URL}/license-pricing-bare?darkMode=${darkMode}`}
                     ></iframe>
                 </div>
             )}
