bf2ed0a83906db0d9db4a4677db1dc4824144569
Mark Durrant <mark.j.durrant@gmail.com>
2023-11-21 16:40:00 +0000

    AG-3390 - Fix included themes example

--- packages/ag-charts-website/src/content/docs/themes/_examples/stock-themes/main.ts
+++ packages/ag-charts-website/src/content/docs/themes/_examples/stock-themes/main.ts
@@ -18,14 +18,40 @@ const options: AgChartOptions = {
     ],
 };
 
-var chart = AgCharts.create(options);
+const chart = AgCharts.create(options);
 
-function applyTheme(theme: AgChartThemeName) {
-    options.theme = theme;
+function setThemeDefault() {
+    options.theme = options.theme.includes('-dark') ? 'ag-default-dark' : 'ag-default';
 
     AgCharts.update(chart, options);
 }
 
-document.getElementById('theme-select')?.addEventListener('input', (event) => {
-    applyTheme((event.target as HTMLInputElement).value as AgChartThemeName);
-});
+function setThemeSheets() {
+    options.theme = options.theme.includes('-dark') ? 'ag-sheets-dark' : 'ag-sheets';
+
+    AgCharts.update(chart, options);
+}
+
+function setThemePolychroma() {
+    options.theme = options.theme.includes('-dark') ? 'ag-polychroma-dark' : 'ag-polychroma';
+
+    AgCharts.update(chart, options);
+}
+
+function setThemeVivid() {
+    options.theme = options.theme.includes('-dark') ? 'ag-vivid-dark' : 'ag-vivid';
+
+    AgCharts.update(chart, options);
+}
+
+function setThemeMaterial() {
+    options.theme = options.theme.includes('-dark') ? 'ag-material-dark' : 'ag-material';
+
+    AgCharts.update(chart, options);
+}
+
+function toggleDarkTheme() {
+    options.theme = options.theme.includes('-dark') ? options.theme.replace('-dark', '') : options.theme + '-dark';
+
+    AgCharts.update(chart, options);
+}
---
+++ packages/ag-charts-website/src/content/docs/themes/_examples/stock-themes/provided/modules/reactFunctionalTs/index.tsx
@@ -0,0 +1,92 @@
+'use strict';
+
+import React, { useState, useCallback, useRef } from 'react';
+import { createRoot } from 'react-dom/client';
+import { AgChartsReact } from 'ag-charts-react';
+import { AgChartOptions, AgChartThemeName, AgCharts } from 'ag-charts-community';
+import { getData } from './data';
+
+const ChartExample = () => {
+    const chartRef = useRef<AgChartsReact>(null);
+    const [options, setOptions] = useState<AgChartOptions>({
+        theme: 'ag-default-dark',
+        title: {
+            text: 'Chart Theme Example',
+        },
+        data: getData(),
+        series: [
+            {
+                type: 'pie',
+                angleKey: 'value',
+                calloutLabelKey: 'label',
+            },
+        ],
+    }); 
+
+    const setThemeDefault = useCallback(() => {
+        const clone = { ...options };
+
+        clone.theme = options.theme.includes('-dark') ? 'ag-default-dark' : 'ag-default';
+
+        setOptions(clone);
+    }, [options]);
+
+    const setThemeSheets = useCallback(() => {
+        const clone = { ...options };
+
+        clone.theme = options.theme.includes('-dark') ? 'ag-sheets-dark' : 'ag-sheets';
+
+        setOptions(clone);
+    }, [options]);
+
+    const setThemePolychroma = useCallback(() => {
+        const clone = { ...options };
+
+        clone.theme = options.theme.includes('-dark') ? 'ag-polychroma-dark' : 'ag-polychroma';
+
+        setOptions(clone);
+    }, [options]);
+
+    const setThemeVivid = useCallback(() => {
+        const clone = { ...options };
+
+        clone.theme = options.theme.includes('-dark') ? 'ag-vivid-dark' : 'ag-vivid';
+
+        setOptions(clone);
+    }, [options]);
+
+    const setThemeMaterial = useCallback(() => {
+        const clone = { ...options };
+
+        clone.theme = options.theme.includes('-dark') ? 'ag-material-dark' : 'ag-material';
+
+        setOptions(clone);
+    }, [options]);
+
+    const toggleDarkTheme = useCallback(() => {
+        const clone = { ...options };
+
+        clone.theme = options.theme.includes('-dark') ? clone.theme.replace('-dark', '') : clone.theme + '-dark';
+
+        setOptions(clone);
+    }, [options]);
+
+    return (
+        <div className="wrapper">
+            <div className="toolPanel">
+                <button onClick={setThemeDefault}>Default Theme</button>
+                <button onClick={setThemeSheets}>Sheets Theme</button>
+                <button onClick={setThemePolychroma}>Polychroma Theme</button>
+                <button onClick={setThemeVivid}>Vivid Theme</button>
+                <button onClick={setThemeMaterial}>Material Theme</button>
+            </div>
+            <div className="toolPanel">
+                <button onClick={toggleDarkTheme}>Toggle Dark Theme</button>
+            </div>
+            <AgChartsReact ref={chartRef} options={options} />
+        </div>
+    );
+};
+
+const root = createRoot(document.getElementById('root')!);
+root.render(<ChartExample />);
