3930d0f1e77c70e456ceb0154c79f06b8b7e26de
James Swinton-Bland <james.swinton-bland@ag-grid.com>
2023-11-21 12:55:38 +0000

    AG-9489: Add comments to example

--- packages/ag-charts-website/src/content/docs/create-a-basic-chart/_examples/complete-formatted-example/main.ts
+++ packages/ag-charts-website/src/content/docs/create-a-basic-chart/_examples/complete-formatted-example/main.ts
@@ -1,10 +1,15 @@
 import { AgChartOptions, AgCharts } from 'ag-charts-community';
 
 const options: AgChartOptions = {
+    // Container: HTML Element to hold the chart
     container: document.getElementById('myChart') as HTMLElement,
-    title: { text: '' },
-    subtitle: { text: '' },
+    // Chart Title
+    title: { text: 'Ice Cream Sales and Avg Temp' },
+    // Chart Subtitle
+    subtitle: { text: 'UK Data from 2022' },
+    // Theme: Style to apply to the chart
     theme: 'ag-vivid-dark',
+    // Data: Data to be displayed within the chart
     data: [
         { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162 },
         { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302 },
@@ -13,17 +18,27 @@ const options: AgChartOptions = {
         { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950 },
         { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200 },
     ],
+    // Series: Defines which chart type and data to use
     series: [
         { type: 'bar', xKey: 'month', yKey: 'iceCreamSales', yName: 'Ice Cream Sales' },
         { type: 'line', xKey: 'month', yKey: 'avgTemp', yName: 'Average Temperature' },
     ],
+    // Axes: Configure the axes for the chart
     axes: [
+        // Display category (xKey) as the bottom axis
         { type: 'category', position: 'bottom' },
-        { type: 'number', position: 'left', keys: ['iceCreamSales'] },
+        // Use left axis for 'iceCreamSales' series
+        {
+            type: 'number',
+            position: 'left',
+            keys: ['iceCreamSales'],
+        },
+        // Use right axis for 'avgTemp' series
         {
             type: 'number',
             position: 'right',
             keys: ['avgTemp'],
+            // Format the label applied to this axis (append ' °C')
             label: {
                 formatter: (params) => {
                     return params.value + ' °C';
@@ -31,6 +46,7 @@ const options: AgChartOptions = {
             },
         },
     ],
+    // Legend: Matches visual elements to their corresponding series or data categories.
     legend: {
         enabled: true,
         position: 'right',
