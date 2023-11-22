cf0965e9579c7f7400c3c0bf38ca7f79ea9fc7b9
Alan Treadway <alan.treadway@ag-grid.com>
2023-11-22 15:59:19 +0000

    AG-9826 - Add RadarLine test examples.

---
+++ packages/ag-charts-website/src/content/docs/radar-line-series-test/_examples/animation/data.ts
@@ -0,0 +1,56 @@
+export function getData1() {
+    return [
+        {
+            category: 'cat 1',
+            iphone: 18,
+            mac: 27,
+        },
+        {
+            category: 'cat 2',
+            iphone: 138,
+            mac: 35,
+        },
+        {
+            category: 'cat 3',
+            iphone: 107,
+            mac: 32,
+        },
+
+        {
+            category: 'cat 5',
+            iphone: 137,
+            mac: 26,
+        },
+    ];
+}
+
+export function getData2() {
+    return [
+        {
+            category: 'cat 10',
+            iphone: 18,
+            mac: 27,
+        },
+        {
+            category: 'cat 1',
+            iphone: 18,
+            mac: 27,
+        },
+        {
+            category: 'cat 2',
+            iphone: 138,
+            mac: 35,
+        },
+        {
+            category: 'cat 3',
+            iphone: 107,
+            mac: 32,
+        },
+
+        {
+            category: 'cat 5',
+            iphone: 137,
+            mac: 26,
+        },
+    ];
+}
---
+++ packages/ag-charts-website/src/content/docs/radar-line-series-test/_examples/animation/main.ts
@@ -0,0 +1,38 @@
+import { AgChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { getData1, getData2 } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+
+    data: getData1(),
+    series: [
+        {
+            type: 'radar-line',
+            angleKey: 'category',
+            radiusKey: 'iphone',
+        },
+        {
+            type: 'radar-line',
+            angleKey: 'category',
+            radiusKey: 'mac',
+        },
+    ],
+    legend: {},
+    animation: {
+        enabled: false,
+        // duration: 3000
+    },
+};
+
+const chart = AgCharts.create(options);
+
+function data1() {
+    options.data = getData1();
+    AgCharts.update(chart, options);
+}
+
+function data2() {
+    options.data = getData2();
+    AgCharts.update(chart, options);
+}
