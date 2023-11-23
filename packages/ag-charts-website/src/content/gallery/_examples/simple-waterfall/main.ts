dce6cdef883f01b03d02a2c5f8d5a3d8bae8f751
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-11-23 18:15:01 +0000

    AG-9570 - New simple waterfall series gallery example

--- packages/ag-charts-website/src/content/gallery/_examples/simple-waterfall/main.ts
+++ packages/ag-charts-website/src/content/gallery/_examples/simple-waterfall/main.ts
@@ -6,20 +6,63 @@ const options: AgChartOptions = {
     container: document.getElementById('myChart'),
     data: getData(),
     title: {
-        text: 'UK Government Budget',
+        text: 'Manchester United Transfers',
     },
     subtitle: {
-        text: 'All values in £ billions',
+        text: 'Outgoing Sales & Incoming Signings from Season 2023-2024',
+    },
+    footnote: {
+        text: 'Arrivals: 17, Departures: 11',
     },
     series: [
         {
             type: 'waterfall',
-            xKey: 'financials',
-            xName: 'Financials',
-            yKey: 'amount',
-            yName: 'Amount',
+            xKey: 'player',
+            xName: 'Player',
+            yKey: 'fee',
+            yName: 'Fee',
+            item: {
+                positive: {
+                    name: 'Outs',
+                    formatter: ({ value }) => ({
+                        fillOpacity: Math.max(0.5, value / 17.5),
+                    }),
+                    label: {
+                        formatter: ({ value }) => `£${value}M`,
+                    },
+                },
+                negative: {
+                    name: 'Ins',
+                    formatter: ({ value }) => ({
+                        fillOpacity: Math.max(0.5, Math.abs(value) / 75),
+                    }),
+                    label: {
+                        formatter: ({ value }) => `-£${Math.abs(value)}M`,
+                    },
+                },
+            },
         },
     ],
+    axes: [
+        {
+            position: 'left',
+            type: 'number',
+            label: {
+                formatter: ({ value }) => `${value}M`,
+            },
+            tick: {
+                values: [0, -148.1],
+            },
+        },
+        {
+            position: 'top',
+            type: 'category',
+            gridLine: { enabled: true },
+        },
+    ],
+    legend: {
+        position: 'top',
+    },
 };
 
 AgCharts.create(options);
