d70ca1d9cd48373fa72bd90eee1aa897f772eec7
Jacob Parker <jacob@Jacobs-MacBook-Pro.local>
2023-11-23 09:27:08 +0000

    Add gradient legend docs

---
+++ packages/ag-charts-website/src/content/docs/sunburst-series/_examples/gradient-legend-position/main.ts
@@ -0,0 +1,41 @@
+import { AgChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { data } from './data';
+
+const gdpFormatter = new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+});
+
+const percentageFormatter = new Intl.NumberFormat('en-US', {
+    style: 'percent',
+    signDisplay: 'always',
+});
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: data,
+    series: [
+        {
+            type: 'sunburst',
+            labelKey: 'name',
+            sizeKey: 'gdp',
+            sizeName: 'GDP',
+            colorKey: 'gdpChange',
+            colorName: 'Change',
+            colorRange: ['rgb(63, 145, 79)', 'rgb(253, 149, 63)'],
+        },
+    ],
+    gradientLegend: {
+        position: 'right',
+        reverseOrder: true,
+    },
+    title: {
+        text: 'Top 10 countries by GDP',
+    },
+    subtitle: {
+        text: '2023',
+    },
+};
+
+AgCharts.create(options);
---
+++ packages/ag-charts-website/src/content/docs/treemap-series/_examples/gradient-legend-size/main.ts
@@ -0,0 +1,33 @@
+import { AgChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { data } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data,
+    series: [
+        {
+            type: 'treemap',
+            labelKey: 'title',
+            sizeKey: 'total',
+            sizeName: 'Total',
+            colorKey: 'change',
+            colorName: 'Change',
+            colorRange: ['rgb(234, 82, 80)', 'rgb(67, 153, 83)'],
+        },
+    ],
+    gradientLegend: {
+        gradient: {
+            thickness: 50,
+            preferredLength: 400,
+        },
+    },
+    title: {
+        text: 'UK Government Budget',
+    },
+    subtitle: {
+        text: '2024',
+    },
+};
+
+AgCharts.create(options);
