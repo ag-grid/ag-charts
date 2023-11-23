7aef383942f1b6684099c69cb2aec703f7bfab66
Salmen Hichri <salmen.hichri@gmail.com>
2023-11-23 14:58:19 +0000

    AG-9870 Added new doc page with feature comparison table

---
+++ packages/ag-charts-website/src/components/featureComparator/FeatureTitle.tsx
@@ -0,0 +1,27 @@
+import { Icon } from '@components/icon/Icon';
+import classnames from 'classnames';
+
+import styles from './FeatureTitle.module.scss';
+
+type FeatureTitleProps = {
+    title: string;
+    isRoot?: boolean;
+    isEnterprise?: boolean;
+    path?: string;
+};
+
+export const FeatureTitle = ({ title, isRoot, path, isEnterprise }: FeatureTitleProps) => {
+    const textClassName = isRoot ? styles.featureGroup : undefined;
+    return (
+        <div className={styles.featureTitleContainer}>
+            {path ? (
+                <a href={path} className={classnames([styles.featureTitleLink, textClassName])}>
+                    {title}
+                </a>
+            ) : (
+                <span className={classnames([styles.featureTitleLink, textClassName])}>{title}</span>
+            )}
+            {isEnterprise && <Icon name="enterprise" svgClasses={styles.enterpriseIcon} />}
+        </div>
+    );
+};
