b434de1b127babd70f5db2305f5e02107817862d
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-11-23 18:15:55 +0000

    AG-9570 - Update horizontal waterfall gallery example

--- packages/ag-charts-website/src/content/gallery/_examples/horizontal-waterfall/main.ts
+++ packages/ag-charts-website/src/content/gallery/_examples/horizontal-waterfall/main.ts
@@ -19,6 +19,46 @@ const options: AgChartOptions = {
             xName: 'Financials',
             yKey: 'amount',
             yName: 'Amount',
+            totals: [
+                {
+                    totalType: 'total',
+                    index: 4,
+                    axisLabel: 'Total Budget',
+                },
+                {
+                    totalType: 'subtotal',
+                    index: 10,
+                    axisLabel: 'Total Spending',
+                },
+            ],
+            line: {
+                enabled: false,
+            },
+            item: {
+                positive: {
+                    fillOpacity: 0.3,
+                    strokeWidth: 1,
+                    lineDash: [2],
+                    label: {
+                        formatter: ({ value }) => `${value < 0 ? '-' : '+'}${Math.abs(value).toFixed(0)}`,
+                    },
+                },
+                negative: {
+                    fillOpacity: 0.3,
+                    strokeWidth: 1,
+                    lineDash: [2],
+                    label: {
+                        formatter: ({ value }) => `${value < 0 ? '-' : '+'}${Math.abs(value).toFixed(0)}`,
+                    },
+                },
+                total: {
+                    fillOpacity: 0.3,
+                    label: {
+                        placement: 'inside',
+                        formatter: ({ value }) => `${value < 0 ? '-' : '+'}£${Math.abs(value).toFixed(0)} bn`,
+                    },
+                },
+            },
         },
     ],
 };
