c9bb7dda9ff6365dc98ba64975ba28ec42f8e503
Jacob Parker <jacob@Jacobs-MacBook-Pro.local>
2023-11-24 12:50:22 +0000

    fix build

--- packages/ag-charts-website/src/utils/hooks/useIsSsr.ts
+++ packages/ag-charts-website/src/utils/hooks/useIsSsr.ts
@@ -1,25 +1,12 @@
 import { useEffect, useState } from 'react';
 
-import { getIsDev } from '../env';
-
-let didRender = false;
-
 // Manages SSRs and the hydration of a page
 export const useIsSsr = () => {
-    const [isSsr, setIsSsr] = useState(!didRender);
+    const [isSsr, setIsSsr] = useState(false);
 
     useEffect(() => {
-        if (didRender) {
-            return;
-        }
-
-        if (!getIsDev()) {
-            // We get hydration warnings due to hmr
-            didRender = true;
-        }
-
         setIsSsr(false);
-    });
+    }, []);
 
     return isSsr;
 };
