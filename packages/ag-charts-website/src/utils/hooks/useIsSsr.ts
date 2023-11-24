10354a07e14503bfcf88cabb454fe468486cb84e
Jacob Parker <jacobparker1992@gmail.com>
2023-11-24 16:32:52 +0000

    Update packages/ag-charts-website/src/utils/hooks/useIsSsr.ts
    Co-authored-by: Tak Tran <contact@tutaktran.com>

--- packages/ag-charts-website/src/utils/hooks/useIsSsr.ts
+++ packages/ag-charts-website/src/utils/hooks/useIsSsr.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 // Manages SSRs and the hydration of a page
 export const useIsSsr = () => {
-    const [isSsr, setIsSsr] = useState(false);
+    const [isSsr, setIsSsr] = useState(true);
 
     useEffect(() => {
         setIsSsr(false);
