4069b63abe772df8ec8cc08ca82928be5c1f62da
James Swinton-Bland <james.swinton-bland@ag-grid.com>
2023-11-24 16:31:25 +0000

    AG-9489: Minor content improvements

--- packages/ag-charts-website/src/content/docs/create-a-basic-chart/_examples/complete-formatted-example/main.ts
+++ packages/ag-charts-website/src/content/docs/create-a-basic-chart/_examples/complete-formatted-example/main.ts
@@ -24,15 +24,15 @@ const options: AgChartOptions = {
     // Chart Title
     title: { text: 'Ice Cream Sales and Avg Temp' } as AgChartCaptionOptions,
     // Chart Subtitle
-    subtitle: { text: 'UK Data from 2022' } as AgChartSubtitleOptions,
+    subtitle: { text: 'Data from 2022' } as AgChartSubtitleOptions,
     // Data: Data to be displayed within the chart
     data: [
-        { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162 },
-        { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302 },
-        { month: 'May', avgTemp: 16.2, iceCreamSales: 800 },
-        { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254 },
-        { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950 },
-        { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200 },
+        { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
+        { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
+        { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
+        { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
+        { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
+        { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
     ] as IData[],
     // Series: Defines which chart type and data to use
     series: [
@@ -51,6 +51,12 @@ const options: AgChartOptions = {
             type: 'number',
             position: 'left',
             keys: ['iceCreamSales'],
+            // Format the label applied to this axis
+            label: {
+                formatter: (params) => {
+                    return parseFloat(params.value).toLocaleString();
+                },
+            },
         } as AgNumberAxisOptions,
         // Use right axis for 'avgTemp' series
         {
