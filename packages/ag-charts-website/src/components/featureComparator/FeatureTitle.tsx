254c265d861179545b9a47a7a5a4722bc6f200ed
Salmen Hichri <salmen.hichri@gmail.com>
2023-11-24 10:07:16 +0000

    AG-9870 Fixed URLs for feature comparison page

--- packages/ag-charts-website/src/components/featureComparator/FeatureTitle.tsx
+++ packages/ag-charts-website/src/components/featureComparator/FeatureTitle.tsx
@@ -1,4 +1,6 @@
+import type { Framework } from "@ag-grid-types";
 import { Icon } from '@components/icon/Icon';
+import { urlWithPrefix } from '@utils/urlWithPrefix';
 import classnames from 'classnames';
 
 import styles from './FeatureTitle.module.scss';
@@ -8,16 +10,18 @@ type FeatureTitleProps = {
     isRoot?: boolean;
     isEnterprise?: boolean;
     path?: string;
+    framework: Framework;
 };
 
-export const FeatureTitle = ({ title, isRoot, path, isEnterprise }: FeatureTitleProps) => {
+export const FeatureTitle = ({ title, isRoot, path, isEnterprise, framework }: FeatureTitleProps) => {
     const textClassName = isRoot ? styles.featureGroup : undefined;
     return (
         <div className={styles.featureTitleContainer}>
             {path ? (
-                <a href={path} className={classnames([styles.featureTitleLink, textClassName])}>
-                    {title}
-                </a>
+                <a
+                    href={urlWithPrefix({ framework, url: path })}
+                    className={classnames([styles.featureTitleLink, textClassName])}
+                >{title}</a>
             ) : (
                 <span className={classnames([styles.featureTitleLink, textClassName])}>{title}</span>
             )}
