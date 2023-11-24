9d445af060888397c75538862373e2944ac40564
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-11-24 16:14:31 +0000

    AG-9570 - New simple box plot example

--- packages/ag-charts-website/src/content/gallery/_examples/simple-box-plot/main.ts
+++ packages/ag-charts-website/src/content/gallery/_examples/simple-box-plot/main.ts
@@ -5,22 +5,61 @@ import { getData } from './data';
 const options: AgChartOptions = {
     container: document.getElementById('myChart'),
     title: {
-        text: 'HR Analytics',
+        text: 'Migration Flows to Europe',
     },
     subtitle: {
-        text: 'Salary Distribution by Department',
+        text: 'Quarterly Overview (April - June 2023)',
+    },
+    footnote: {
+        text: 'Source: UN International Organization for Migration',
     },
     data: getData(),
     series: [
         {
             type: 'box-plot',
-            yName: 'Employee Salaries',
-            xKey: 'department',
+            xKey: 'countryOfArrival',
+            xName: 'Country Of Arrival',
+            yName: 'Monthly Arrivals',
             minKey: 'min',
             q1Key: 'q1',
             medianKey: 'median',
             q3Key: 'q3',
             maxKey: 'max',
+            strokeOpacity: 0,
+            whisker: {
+                strokeOpacity: 0.9,
+            },
+            cap: {
+                lengthRatio: 0.8,
+            },
+        },
+    ],
+    axes: [
+        {
+            position: 'bottom',
+            type: 'category',
+            groupPaddingInner: 0,
+            paddingInner: 0.7,
+            paddingOuter: 0.2,
+            gridLine: {
+                enabled: true,
+            },
+            line: {
+                enabled: false,
+            },
+            label: {
+                padding: 10,
+            },
+        },
+        {
+            position: 'left',
+            type: 'number',
+            line: {
+                enabled: false,
+            },
+            tick: {
+                values: [105, 385, 2714],
+            },
         },
     ],
 };
