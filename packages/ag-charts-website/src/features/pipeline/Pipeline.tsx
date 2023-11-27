79bbc5398a97bba7247d1fdd2894028faf53b5b4
seanlandsman <sean@thelandsmans.com>
2023-11-27 15:22:12 +0000

    AG-9925 Create new job for charts pipeline/changelog, Update Grid jobs to take account of component field

--- packages/ag-charts-website/src/features/pipeline/Pipeline.tsx
+++ packages/ag-charts-website/src/features/pipeline/Pipeline.tsx
@@ -181,9 +181,9 @@ export const Pipeline = ({ location, currentFramework }) => {
                     <section className={styles.header}>
                         <Alert type="idea">
                             <p>
-                                The AG Charts pipeline lists the feature requests and active bugs in our product backlog.
-                                Use it to see the items scheduled for our next release or to look up the status of a
-                                specific item. If you can’t find the item you’re looking for, check the{' '}
+                                The AG Charts pipeline lists the feature requests and active bugs in our product
+                                backlog. Use it to see the items scheduled for our next release or to look up the status
+                                of a specific item. If you can’t find the item you’re looking for, check the{' '}
                                 <a href={`${SITE_URL}${SITE_BASE_URL ?? ''}${currentFramework}/changelog/`}>
                                     Changelog
                                 </a>{' '}
