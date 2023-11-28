ac67ceec08aafe34e445a9a361b381bc418254ea
Alan Treadway <alan.treadway@ag-grid.com>
2023-11-28 11:11:19 +0000

    CRT-132 - Fix package exports; fix React ESM handling.

---
+++ packages/ag-charts-react/rollup.config.cjs
@@ -0,0 +1,15 @@
+const base = require('@nx/react/plugins/bundle-rollup');
+
+function fixName(name) {
+    return name.replace('.cjs.js', '.cjs').replace('.esm.js', '.mjs');
+}
+
+module.exports = (config) => {
+    const { output, ...result } = base(config);
+
+    output.entryFileNames = fixName(output.entryFileNames);
+    output.chunkFileNames = fixName(output.chunkFileNames);
+
+    console.log({ output });
+    return { output, ...result };
+};
