610db71f14dff33b02c3d80f7488d6a45d1fadc0
Tak Tran <contact@tutaktran.com>
2023-11-28 19:37:06 +0000

    AG-3390 - What's new fixes (#658)
    * AG-3390 - Fix What's New
    
    * AG-3390 - Update Changes List title

--- packages/ag-charts-website/src/components/version/Version.tsx
+++ packages/ag-charts-website/src/components/version/Version.tsx
@@ -12,17 +12,27 @@ type VersionProps = {
     version: string;
     blogUrl?: string;
     highlights?: Array<{ text: string; url: string }>;
+    buttonURL?: string;
+    majorMinor?: boolean;
 };
 
-export const Version = ({ date, version, blogUrl, highlights }: VersionProps) => {
+export const Version = ({ date, version, blogUrl, highlights, buttonURL, majorMinor }: VersionProps) => {
     const { major, minor, isMajor } = parseVersion(version);
-    const blogHref = blogUrl || `https://blog.ag-grid.com/whats-new-in-ag-grid-${minor ? `${major}-${minor}` : major}/`;
+    const blogHref =
+        blogUrl || `https://blog.ag-grid.com/whats-new-in-ag-charts-${minor ? `${major}-${minor}` : major}/`;
 
     return (
         <div className={styles.version}>
             <div className={styles.topheader}>
                 <header>
-                    <span className={`${styles['text-secondary']} ${styles['font-size-small']}`}>{date}</span>
+                    <div className={styles.flex}>
+                        <span className={`${styles['text-secondary']} ${styles['font-size-small']}`}>{date}</span>
+
+                        <div className={styles.flex}>
+                            {version === '31.0.0' && <span className={styles['latest-tag']}>Latest</span>}
+                            {majorMinor && <span className={styles['major-text']}>Major</span>}
+                        </div>
+                    </div>
                     <div className={styles.flex}>
                         <b className={styles['font-size-large']}>Version {version}</b>
                         <a className={styles.bloglink} href={blogHref}>
@@ -43,10 +53,21 @@ export const Version = ({ date, version, blogUrl, highlights }: VersionProps) =>
                         ))}
                     </ul>
                 )}
+
+                {buttonURL && (
+                    <a
+                        className={`${styles.buttonSecondary} button-secondary`}
+                        href={urlWithBaseUrl(buttonURL)}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        See migration guide
+                    </a>
+                )}
             </div>
 
             <a
-                className={`${styles.changelog} button-secondary`}
+                className={`${styles.buttonSecondary} button-secondary`}
                 href={urlWithBaseUrl(`/changelog/?fixVersion=${version}`)}
                 target="_blank"
                 rel="noopener noreferrer"
