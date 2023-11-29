884166c8572fccb3898f4f9b5d0ec77e311a0579
Jacob  Parker <jacobparker1992@gmail.com>
2023-11-29 12:02:50 +0000

    Remove workaround for hdpi canvas for newer versions of safari

--- packages/ag-charts-community/src/util/userAgent.ts
+++ packages/ag-charts-community/src/util/userAgent.ts
@@ -1,11 +1,19 @@
-const MOBILE = [/Android/i, /webOS/i, /iPhone/i, /iPad/i, /iPod/i, /BlackBerry/i, /Windows Phone/i];
+export function hasConstrainedCanvasMemory() {
+    // https://bugs.webkit.org/show_bug.cgi?id=195325
+    // Fixed in https://webkit.org/blog/14390/release-notes-for-safari-technology-preview-174/
+    if (typeof navigator === 'undefined') return false;
 
-export function isDesktop() {
-    if (typeof navigator === 'undefined') {
-        return true;
+    const iPhoneOSMatch = navigator.userAgent.match(/\(iPhone; CPU iPhone OS (\d+_\d+_\d+) like Mac OS X\)/);
+    if (iPhoneOSMatch == null) return false;
+
+    const versionString = iPhoneOSMatch[1].split('_');
+    const major = Number(versionString[0]);
+
+    if (major > 16) {
+        return false;
+    } else if (major === 16) {
+        const minor = Number(versionString[1]);
+        return minor < 6;
     }
-
-    const userAgent = navigator.userAgent;
-
-    return !MOBILE.some((r) => r.test(userAgent));
+    return true;
 }
