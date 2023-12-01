d9d8962e620c1684e47efa5443d4e1c141e8e480
Oli Legat <olivier.legat@ag-grid.com>
2023-12-01 13:19:07 +0000

    AG-8641 Add a failing test

--- packages/ag-charts-community/src/chart/series/cartesian/histogramSeries.test.ts
+++ packages/ag-charts-community/src/chart/series/cartesian/histogramSeries.test.ts
@@ -249,4 +249,18 @@ describe('HistogramSeries', () => {
             });
         }
     });
+
+    // See https://ag-grid.atlassian.net/browse/AG-8641
+    test('explicit binCount with two decimal places', async () => {
+        const options: AgChartOptions = {
+            data: [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1].map((x) => {
+                return { x };
+            }),
+            series: [{ type: 'histogram', xKey: 'x', binCount: 10 }],
+        };
+        prepareTestOptions(options);
+
+        chart = AgCharts.create(options);
+        await compare();
+    });
 });
