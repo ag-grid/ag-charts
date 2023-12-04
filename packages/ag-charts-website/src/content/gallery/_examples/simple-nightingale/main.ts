e5db86c8bacef44a1a8b560519b0023471d115c7
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-12-04 12:38:52 +0000

    AG-9570 - Update gallery simple nightingale example

--- packages/ag-charts-website/src/content/gallery/_examples/simple-nightingale/main.ts
+++ packages/ag-charts-website/src/content/gallery/_examples/simple-nightingale/main.ts
@@ -23,12 +23,49 @@ const options: AgChartOptions = {
             angleKey: 'quarter',
             radiusKey: 'hardware',
             radiusName: 'Hardware',
+            fillOpacity: 0.5,
         },
         {
             type: 'nightingale',
             angleKey: 'quarter',
             radiusKey: 'services',
             radiusName: 'Services',
+            fillOpacity: 0.3,
+        },
+    ],
+    axes: [
+        {
+            type: 'radius-number',
+            shape: 'polygon',
+            innerRadiusRatio: 0,
+            crossLines: [
+                {
+                    type: 'range',
+                    range: [10, 12],
+                    strokeWidth: 0,
+                    fillOpacity: 0.05,
+                },
+                {
+                    type: 'range',
+                    range: [8, 6],
+                    strokeWidth: 0,
+                    fillOpacity: 0.05,
+                },
+                {
+                    type: 'range',
+                    range: [4, 2],
+                    strokeWidth: 0,
+                    fillOpacity: 0.05,
+                },
+            ],
+        },
+        {
+            type: 'angle-category',
+            shape: 'polygon',
+            paddingInner: 0.4,
+            line: {
+                enabled: false,
+            },
         },
     ],
 };
