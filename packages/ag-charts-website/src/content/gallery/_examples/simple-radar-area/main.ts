6f9ea961f9dc90afa19d880f029d8b9e44efbf68
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-12-04 12:36:55 +0000

    AG-9570 - Update gallery simple radar area example

--- packages/ag-charts-website/src/content/gallery/_examples/simple-radar-area/main.ts
+++ packages/ag-charts-website/src/content/gallery/_examples/simple-radar-area/main.ts
@@ -14,12 +14,44 @@ const options: AgChartOptions = {
             angleKey: 'department',
             radiusKey: 'quality',
             radiusName: 'Quality',
+            strokeWidth: 1,
+            fillOpacity: 0.1,
         },
         {
             type: 'radar-area',
             angleKey: 'department',
             radiusKey: 'efficiency',
             radiusName: 'Efficiency',
+            strokeWidth: 1,
+            fillOpacity: 0.1,
+        },
+        {
+            type: 'radar-area',
+            angleKey: 'department',
+            radiusKey: 'revenueGrowth',
+            radiusName: 'Revenue Growth',
+            strokeWidth: 1,
+            fillOpacity: 0.1,
+        },
+    ],
+    axes: [
+        {
+            type: 'angle-category',
+            gridLine: {
+                enabled: true,
+            },
+            line: {
+                enabled: false,
+            },
+        },
+        {
+            type: 'radius-number',
+            gridLine: {
+                enabled: false,
+            },
+            line: {
+                enabled: false,
+            },
         },
     ],
 };
