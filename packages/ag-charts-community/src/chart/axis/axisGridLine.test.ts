9d8e4df96f93a02c86f96128681f55e97d846f28
Oli Legat <olivier.legat@ag-grid.com>
2023-12-05 12:46:20 +0000

    AG-8777 Add minimal repo test case

---
+++ packages/ag-charts-community/src/chart/axis/axisGridLine.test.ts
@@ -0,0 +1,66 @@
+import { afterEach, beforeEach, describe, expect, jest } from '@jest/globals';
+
+import type { AgChartOptions } from '../../options/agChartOptions';
+import { AgCharts } from '../agChartV2';
+import type { Chart } from '../chart';
+import {
+    IMAGE_SNAPSHOT_DEFAULTS,
+    extractImageData,
+    prepareTestOptions,
+    setupMockCanvas,
+    waitForChartStability,
+} from '../test/utils';
+
+describe('GridLine', () => {
+    let chart: Chart;
+
+    afterEach(() => {
+        if (chart) {
+            chart.destroy();
+            (chart as unknown) = undefined;
+        }
+    });
+
+    const ctx = setupMockCanvas();
+    const opts: AgChartOptions = prepareTestOptions({});
+
+    beforeEach(() => {
+        console.warn = jest.fn();
+        console.error = jest.fn();
+    });
+
+    afterEach(() => {
+        expect(console.warn).not.toBeCalled();
+        expect(console.error).not.toBeCalled();
+    });
+
+    const compare = async () => {
+        await waitForChartStability(chart);
+
+        const newImageData = extractImageData(ctx);
+        expect(newImageData).toMatchImageSnapshot(IMAGE_SNAPSHOT_DEFAULTS);
+    };
+
+    // AG-8777
+    test('use theme default stroke', async () => {
+        chart = AgCharts.create({
+            ...opts,
+            data: [{ x: 0, y: 0 }],
+            series: [{ xKey: 'x', yKey: 'y' }],
+            axes: [
+                {
+                    type: 'number',
+                    position: 'bottom',
+                    gridLine: { style: [{ lineDash: [8, 3, 3, 3] }] },
+                },
+                {
+                    type: 'number',
+                    position: 'left',
+                    gridLine: { style: [{ lineDash: [5, 5, 1] }] },
+                },
+            ],
+        }) as Chart;
+
+        await compare();
+    });
+});
