f294059c9afbcada078c709eaaa2b93dad962a76
Mark Durrant <mark.j.durrant@gmail.com>
2023-12-05 17:11:09 +0000

    AG-10044 - Update Style guide styles

--- packages/ag-charts-website/src/design-system/style-guide/StyleGuide.tsx
+++ packages/ag-charts-website/src/design-system/style-guide/StyleGuide.tsx
@@ -1,14 +1,18 @@
+import classnames from 'classnames';
 import type { FunctionComponent } from 'react';
 
 import { Colors } from './Colors';
+import styles from './StyleGuide.module.scss';
 import { TextStyles } from './TextStyles';
 
 export const StyleGuide: FunctionComponent = () => {
     return (
         <>
-            <h1>STYLE GUIDE</h1>
-            <Colors />
-            <TextStyles />
+            <div className={classnames(styles.styleGuide, 'page-margin')}>
+                <h1>STYLE GUIDE</h1>
+                <Colors />
+                <TextStyles />
+            </div>
         </>
     );
 };
