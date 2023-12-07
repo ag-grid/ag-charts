16884e3704ed9412698e483c8a26c47552d66fd3
Mark Durrant <mark.j.durrant@gmail.com>
2023-12-07 15:15:37 +0000

    AG-10044 - Add semantic colours to style guide

--- packages/ag-charts-website/src/design-system/style-guide/Colors.tsx
+++ packages/ag-charts-website/src/design-system/style-guide/Colors.tsx
@@ -7,18 +7,58 @@ const colors = {
     base: ['white', 'black'],
     gray: ['25', '50', '100', '200', '300', '400', '500', '600', '700', '800', '900', '950'],
     brand: ['25', '50', '100', '200', '300', '400', '500', '600', '700', '800', '900', '950'],
+    background: ['bg-primary', 'bg-secondary', 'bg-brand-solid', 'bg-disabled_subtle'],
+    foreground: [
+        'fg-primary',
+        'fg-secondary',
+        'fg-tertiary',
+        'fg-quinary',
+        'fg-disabled',
+        'fg-placeholder',
+        'fg-white',
+    ],
+    border: ['border-primary', 'border-secondary', 'border-tertiary', 'border-disabled_subtle'],
+    button: [
+        'button-primary-bg',
+        'button-primary-bg-hover',
+        'button-primary-bg-active',
+        'button-primary-shadow-focus',
+        'button-primary-fg',
+        'button-primary-border',
+
+        'button-secondary-bg',
+        'button-secondary-bg-hover',
+        'button-secondary-bg-active',
+        'button-secondary-shadow-focus',
+        'button-secondary-fg',
+        'button-secondary-border',
+
+        'button-tertiary-bg',
+        'button-tertiary-bg-hover',
+        'button-tertiary-bg-active',
+        'button-tertiary-shadow-focus',
+        'button-tertiary-fg',
+        'button-tertiary-border',
+
+        'button-disabled-bg',
+        'button-disabled-fg',
+        'button-disabled-border',
+    ],
+    input: ['input-border', 'input-border-hover', 'input-shadow-focus', 'input-disabled-bg', 'input-disabled-fg'],
 };
 
-const Color: FunctionComponent = ({ colorName }) => {
+const Color: FunctionComponent = ({ colorName, showValue = false }) => {
     const cssVar = `--color-${colorName}`;
 
     return (
         <div className={styles.swatch}>
             <div className={styles.swatchTop} style={{ backgroundColor: `var(${cssVar})` }}></div>
             <div className={styles.swatchBottom}>
-                <span>
-                    <b>{getCssVarValue(cssVar)}</b>
-                </span>
+                {showValue && (
+                    <span>
+                        <b>{getCssVarValue(cssVar)}</b>
+                    </span>
+                )}
 
                 <code>{cssVar}</code>
             </div>
@@ -29,26 +69,63 @@ const Color: FunctionComponent = ({ colorName }) => {
 export const Colors: FunctionComponent = () => {
     return (
         <>
-            <h2>Colors</h2>
+            <h2>Abstract Colors</h2>
 
             <h4>Base</h4>
-            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem' }}>
+            <div className={styles.colorsList}>
                 {colors.base.map((clr) => {
-                    return <Color key={clr} colorName={`${clr}`} />;
+                    return <Color key={clr} colorName={`${clr}`} showValue={true} />;
                 })}
             </div>
 
             <h4>Gray</h4>
-            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem' }}>
+            <div className={styles.colorsList}>
                 {colors.gray.map((clr) => {
-                    return <Color key={`gray-${clr}`} colorName={`gray-${clr}`} />;
+                    return <Color key={`gray-${clr}`} colorName={`gray-${clr}`} showValue={true} />;
                 })}
             </div>
 
             <h4>Brand</h4>
-            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem' }}>
+            <div className={styles.colorsList}>
                 {colors.brand.map((clr) => {
-                    return <Color key={`brand-${clr}`} colorName={`brand-${clr}`} />;
+                    return <Color key={`brand-${clr}`} colorName={`brand-${clr}`} showValue={true} />;
+                })}
+            </div>
+
+            <h2>Semantic Colors</h2>
+
+            <h4>Background</h4>
+            <div className={styles.colorsList}>
+                {colors.background.map((clr) => {
+                    return <Color key={clr} colorName={`${clr}`} />;
+                })}
+            </div>
+
+            <h4>Foreground</h4>
+            <div className={styles.colorsList}>
+                {colors.foreground.map((clr) => {
+                    return <Color key={clr} colorName={`${clr}`} />;
+                })}
+            </div>
+
+            <h4>Border</h4>
+            <div className={styles.colorsList}>
+                {colors.border.map((clr) => {
+                    return <Color key={clr} colorName={`${clr}`} />;
+                })}
+            </div>
+
+            <h4>Button</h4>
+            <div className={styles.colorsList}>
+                {colors.button.map((clr) => {
+                    return <Color key={clr} colorName={`${clr}`} />;
+                })}
+            </div>
+
+            <h4>Input</h4>
+            <div className={styles.colorsList}>
+                {colors.input.map((clr) => {
+                    return <Color key={clr} colorName={`${clr}`} />;
                 })}
             </div>
         </>
