5dc2c915313477357841409dadcf0ddc9ad80ac3
Tak Tran <contact@tutaktran.com>
2023-12-12 11:19:14 +0000

    AG-3390 - Remove unused homepage example runner code

--- plugins/ag-charts-build-tools/src/executors/generate-example-files/generator/utils/getDarkModeSnippet.ts
+++ plugins/ag-charts-build-tools/src/executors/generate-example-files/generator/utils/getDarkModeSnippet.ts
@@ -2,19 +2,6 @@ export const DARK_MODE_START = '/** DARK MODE START **/';
 export const DARK_MODE_END = '/** DARK MODE END **/';
 
 const DARK_MODE_SNIPPETS = {
-    homepageHero: () => `
-        options.theme = localStorage['documentation:darkmode'] === 'true' ? 'ag-default-dark' : 'ag-default';
-        agCharts.AgCharts.update(chart, options);
-        document.querySelector('html').setAttribute('data-dark-mode', localStorage['documentation:darkmode'] === 'true');
-        `,
-    homepageHeroListener: () => `
-        window.addEventListener('message', (event) => {
-            const data = event.detail;
-            if (data?.type === 'color-scheme-change') {
-                options.theme = data.darkmode ? 'ag-default-dark' : 'ag-default';
-                agCharts.AgCharts.update(chart, options);
-            }
-        });`,
     vanilla: () => `
         options.theme = {
             ...options.theme,
