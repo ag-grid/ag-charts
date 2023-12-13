94341d3203292dcc808c72e5a3c92dddad8235e9
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2023-12-13 10:19:12 +0000

    AG-9964 - Add polar axis shape option validation

--- packages/ag-charts-community/src/chart/axis/polarAxis.ts
+++ packages/ag-charts-community/src/chart/axis/polarAxis.ts
@@ -1,12 +1,15 @@
 import type { Scale } from '../../scale/scale';
 import type { BBox } from '../../scene/bbox';
-import { RATIO, Validate } from '../../util/validation';
+import { RATIO, UNION, Validate } from '../../util/validation';
 import { Axis } from './axis';
+
+export const POLAR_AXIS_SHAPE = UNION(['polygon', 'circle'], 'a polar axis shape');
 
 export abstract class PolarAxis<S extends Scale<any, any, any> = Scale<any, any, any>> extends Axis<S> {
     gridAngles: number[] | undefined;
     gridRange: number[] | undefined;
 
+    @Validate(POLAR_AXIS_SHAPE)
     shape: 'polygon' | 'circle' = 'polygon';
 
     @Validate(RATIO)
