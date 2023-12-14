b38a5c6513102b2303dbf852f91089dc33883461
Alan Treadway <alan.treadway@ag-grid.com>
2023-12-14 12:10:35 +0000

    AG-10005 - Working nx dev configuration.

--- esbuild.config.cjs
+++ esbuild.config.cjs
@@ -1,9 +1,15 @@
 const { umdWrapper } = require('esbuild-plugin-umd-wrapper');
 
+const exportedNames = {
+    'ag-charts-community': 'agCharts',
+    'ag-charts-enterprise': 'agCharts',
+};
+const exportedName = exportedNames[process.env.NX_TASK_TARGET_PROJECT];
+
 const plugins = [];
 let outExtension = {};
-if (process.env.NX_TASK_TARGET_TARGET.endsWith('umd')) {
-    plugins.push(umdWrapper());
+if (process.env.NX_TASK_TARGET_TARGET?.endsWith('umd')) {
+    plugins.push(umdWrapper({ libraryName: exportedName }));
     outExtension = {
         '.cjs': '.js',
     };
