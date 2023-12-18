06563c3db36c60d6946c0ec77ea539ad4b6b1ea6
Tak Tran <contact@tutaktran.com>
2023-12-18 17:09:01 +0000

    AG-3390 - Add aria label for heading link

--- packages/ag-charts-website/src/components/link-icon/LinkIcon.tsx
+++ packages/ag-charts-website/src/components/link-icon/LinkIcon.tsx
@@ -5,7 +5,7 @@ import type { AllHTMLAttributes } from 'react';
 
 export function LinkIcon({ className, ...props }: AllHTMLAttributes<HTMLAnchorElement> & { children?: never }) {
     return (
-        <a {...props} className={classnames(styles.docsHeaderIcon, className)}>
+        <a {...props} className={classnames(styles.docsHeaderIcon, className)} aria-label="Heading link">
             <Icon name="link" />
         </a>
     );
