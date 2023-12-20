5a041a8189500b830873bf4b7cb21210a2437b95
Oli Legat <olivier.legat@ag-grid.com>
2023-12-20 13:44:26 +0000

    AG-9983 Use chart title as download filename

--- packages/ag-charts-community/src/chart/chartService.ts
+++ packages/ag-charts-community/src/chart/chartService.ts
@@ -1,8 +1,10 @@
+import type { Caption } from './caption';
 import type { ChartMode } from './chartMode';
 import type { ISeries } from './series/seriesTypes';
 
 // Subset of chart.ts exposed in the module context:
 export interface ChartService {
     readonly mode: ChartMode;
+    readonly title?: Caption;
     readonly series: ISeries<any>[];
 }
