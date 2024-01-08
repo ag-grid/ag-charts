8cc9d668b7cc3edc10a4443234c83e2b5586c4cf
Oli Legat <olivier.legat@ag-grid.com>
2024-01-08 15:18:53 +0000

    AG-10160 Use area fill style if markers are disabled.

--- packages/ag-charts-community/src/chart/series/cartesian/areaSeries.ts
+++ packages/ag-charts-community/src/chart/series/cartesian/areaSeries.ts
@@ -605,6 +605,7 @@ export class AreaSeries extends CartesianSeries<
 
         const { yKey, yName, fill, stroke, fillOpacity, strokeOpacity, strokeWidth, marker, visible } = this.properties;
 
+        const useAreaFill = !(marker.enabled && strokeWidth === 0) || marker.fill === undefined;
         return [
             {
                 legendType,
@@ -617,9 +618,9 @@ export class AreaSeries extends CartesianSeries<
                 },
                 marker: {
                     shape: marker.shape,
-                    fill: marker.fill ?? fill,
+                    fill: useAreaFill ? fill : marker.fill,
+                    fillOpacity: useAreaFill ? fillOpacity : marker.fillOpacity,
                     stroke: marker.stroke ?? stroke,
-                    fillOpacity: marker.fillOpacity ?? fillOpacity,
                     strokeOpacity: marker.strokeOpacity ?? strokeOpacity,
                     strokeWidth: marker.strokeWidth ?? 0,
                 },
