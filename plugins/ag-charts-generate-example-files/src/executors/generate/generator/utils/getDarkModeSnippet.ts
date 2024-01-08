b7fb6e7744a7034028d92cb2f2ae16a577e35a32
Jacob  Parker <jacobparker1992@gmail.com>
2024-01-08 14:24:47 +0000

    AG-10131 Apply darkmode to custom theme examples

--- plugins/ag-charts-generate-example-files/src/executors/generate/generator/utils/getDarkModeSnippet.ts
+++ plugins/ag-charts-generate-example-files/src/executors/generate/generator/utils/getDarkModeSnippet.ts
@@ -4,19 +4,31 @@ export const DARK_MODE_END = '/** DARK MODE END **/';
 export const getDarkModeSnippet = ({ chartAPI }: { chartAPI?: string } = {}) =>
     `${DARK_MODE_START}
 ${chartAPI == null ? `import { AgCharts as AgChartsAPI } from 'ag-charts-community';` : ''}
-let darkmode = (localStorage['documentation:darkmode'] || String(matchMedia('(prefers-color-scheme: dark)').matches)) === 'true';
+let darkmode =
+    (localStorage["documentation:darkmode"] ||
+        String(matchMedia("(prefers-color-scheme: dark)").matches)) === "true";
+
+const isAgTheme = (theme) => {
+    return typeof theme === "string" && theme.startsWith("ag-");
+};
+
+const getDarkmodeTheme = (theme, darkmode) => {
+    const baseTheme = theme.replace(/-dark$/, "");
+    return darkmode ? baseTheme + "-dark" : baseTheme;
+};
+
 const applyDarkmode = () => {
-    document.head.setAttribute('data-dark-mode', darkmode);
-    const charts = document.querySelectorAll('[data-ag-charts]')
+    document.documentElement.setAttribute("data-dark-mode", darkmode);
+    const charts = document.querySelectorAll("[data-ag-charts]");
     charts.forEach((element) => {
         const chart = ${chartAPI ?? 'AgChartsAPI'}.getInstance(element);
         if (chart == null) return;
         let options = chart.getOptions();
-        let theme = options.theme || 'ag-default';
-        if (typeof theme === 'string' && theme.startsWith('ag-')) {
-            const baseTheme = theme.replace(/-dark$/, '');
-            theme = darkmode ? baseTheme + '-dark' : baseTheme;
-            options.theme = theme;
+        let theme = options.theme || "ag-default";
+        if (isAgTheme(theme)) {
+            options.theme = getDarkmodeTheme(theme, darkmode);
+        } else if (typeof theme === "object" && isAgTheme(theme.baseTheme)) {
+            options.theme.baseTheme = getDarkmodeTheme(theme.baseTheme, darkmode);
         }
         ${chartAPI ?? 'AgChartsAPI'}.update(chart, options);
     });
@@ -30,10 +42,14 @@ if (!applyDarkmode()) {
             observer.disconnect();
         }
     });
-    observer.observe(document.body, { attributes: true, childList: true, subtree: true });
+    observer.observe(document.body, {
+        attributes: true,
+        childList: true,
+        subtree: true,
+    });
 }
-window.addEventListener('message', (event) => {
-    if (event.data && event.data.type === 'color-scheme-change') {
+window.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "color-scheme-change") {
         darkmode = event.data.darkmode;
         applyDarkmode();
     }
