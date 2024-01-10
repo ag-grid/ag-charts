e2912675956f74fc2fbcd6f56e987506d216c7de
Jacob  Parker <jacobparker1992@gmail.com>
2024-01-10 10:13:44 -0500

    AG-9846 Fix gallery thumbnail generation for horizontal layouts

--- plugins/ag-charts-generate-chart-thumbnail/src/executors/generate/generator/constants.ts
+++ plugins/ag-charts-generate-chart-thumbnail/src/executors/generate/generator/constants.ts
@@ -8,18 +8,19 @@ export const DEFAULT_THUMBNAIL_WIDTH = 600;
 export const DEFAULT_THUMBNAIL_HEIGHT = DEFAULT_THUMBNAIL_WIDTH / DEFAULT_THUMBNAIL_ASPECT_RATIO;
 
 export const MIN_ASPECT_RATIO = 1 / 3;
+export const MAX_ASPECT_RATIO = 12 / 1;
 
 export const THUMBNAIL_POOL_SIZE = 1;
 
 export const BACKGROUND_COLORS: Record<AgChartThemeName, string> = {
     'ag-default': '#ffffff',
-    'ag-default-dark': '#182230',
+    'ag-default-dark': '#141D2C',
     'ag-sheets': '#ffffff',
-    'ag-sheets-dark': '#182230',
+    'ag-sheets-dark': '#141D2C',
     'ag-polychroma': '#ffffff',
-    'ag-polychroma-dark': '#182230',
+    'ag-polychroma-dark': '#141D2C',
     'ag-vivid': '#ffffff',
-    'ag-vivid-dark': '#182230',
+    'ag-vivid-dark': '#141D2C',
     'ag-material': '#ffffff',
-    'ag-material-dark': '#182230',
+    'ag-material-dark': '#141D2C',
 };
