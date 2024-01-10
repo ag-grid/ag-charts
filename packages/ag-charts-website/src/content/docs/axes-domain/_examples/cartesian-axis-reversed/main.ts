27b7904af67a5508acd49181c6c2c3fae12c2da5
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-01-10 17:10:04 +0000

    AG-10245 - Reversed axis domain docs

---
+++ packages/ag-charts-website/src/content/docs/axes-domain/_examples/cartesian-axis-reversed/main.ts
@@ -0,0 +1,42 @@
+import { AgCartesianChartOptions, AgCharts } from 'ag-charts-community';
+
+const options: AgCartesianChartOptions = {
+    container: document.getElementById('myChart'),
+    data: [
+        { os: 'Windows', share: 88.07 },
+        { os: 'macOS', share: 9.44 },
+        { os: 'Linux', share: 1.87 },
+    ],
+    series: [
+        {
+            type: 'bar',
+            xKey: 'os',
+            yKey: 'share',
+        },
+    ],
+    axes: [
+        {
+            type: 'category',
+            position: 'bottom',
+            title: {
+                text: 'Operating System',
+            },
+        },
+        {
+            type: 'number',
+            position: 'left',
+            reverse: true,
+            title: {
+                text: 'Market Share (%)',
+            },
+        },
+    ],
+};
+
+const chart = AgCharts.create(options);
+
+function toggleAxisReverse() {
+    const numberAxisOptions = options.axes![1];
+    numberAxisOptions.reverse = !numberAxisOptions.reverse;
+    AgCharts.update(chart, options);
+}
---
+++ packages/ag-charts-website/src/content/docs/axes-domain/_examples/polar-axis-reversed/data.ts
@@ -0,0 +1,24 @@
+export function getData() {
+    return [
+        {
+            department: 'Sales',
+            quality: 40,
+        },
+        {
+            department: 'Engineering',
+            quality: 45,
+        },
+        {
+            department: 'HR',
+            quality: 80,
+        },
+        {
+            department: 'Marketing',
+            quality: 80,
+        },
+        {
+            department: 'Finance',
+            quality: 85,
+        },
+    ];
+}
