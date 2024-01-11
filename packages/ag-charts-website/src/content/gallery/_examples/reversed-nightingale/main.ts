21a0ae9696b7ac7e9968f85866a2d67ddb614887
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-01-11 11:39:32 +0000

    AG-10245 - Nightingale reverse axis gallery example

---
+++ packages/ag-charts-website/src/content/gallery/_examples/reversed-nightingale/main.ts
@@ -0,0 +1,61 @@
+import { AgChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'Software Revenue',
+    },
+    subtitle: {
+        text: 'Millions USD',
+    },
+    series: [
+        {
+            type: 'nightingale',
+            angleKey: 'quarter',
+            radiusKey: 'software',
+            radiusName: 'Software',
+            fillOpacity: 0.8,
+        },
+    ],
+    axes: [
+        {
+            type: 'radius-number',
+            innerRadiusRatio: 0,
+            reverse: true,
+            gridLine: {
+                width: 2,
+            },
+            positionAngle: 90,
+            label: {
+                rotation: -90,
+                padding: 12,
+                formatter: ({ value }) => `$${value}M`,
+            },
+            tick: {
+                size: 0,
+                values: [1, 3, 5],
+            },
+            line: {
+                enabled: true,
+            },
+        },
+        {
+            type: 'angle-category',
+            paddingInner: 0.1,
+            gridLine: {
+                enabled: true,
+            },
+            tick: {
+                enabled: true,
+            },
+            label: {
+                padding: 5,
+            },
+        },
+    ],
+};
+
+AgCharts.create(options);
