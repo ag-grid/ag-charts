e2957b4cd4fa027cf1ed15147a64c58adc32f2c3
iMoses (Ido Moshe) <imoses.g@gmail.com>
2024-01-15 14:37:19 +0000

    Fix CRT-187

--- packages/ag-charts-community/src/chart/series/polar/pieSeries.ts
+++ packages/ag-charts-community/src/chart/series/polar/pieSeries.ts
@@ -175,6 +175,10 @@ export class PieSeries extends PolarSeries<PieNodeDatum, Sector> {
         this.processSeriesItemEnabled();
     }
 
+    override get visible() {
+        return this.seriesItemEnabled.some((visible) => visible);
+    }
+
     private processSeriesItemEnabled() {
         const { data, visible } = this;
         this.seriesItemEnabled = data?.map(() => visible) ?? [];
