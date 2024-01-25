043dea553879c7d1a402ce4492466d1da1faff8a
Oli Legat <olivier.legat@ag-grid.com>
2024-01-25 16:29:58 +0000

    AG-10475 Use less sensitive scrolling when deltaMode=0
    When the mouse wheel event uses deltaMode 0 ("pixels"), such as on a Mac
    trackpad, the zoom is too sensitive
    
    Reduce this by the factor of 0.1

--- packages/ag-charts-enterprise/src/features/zoom/zoomScroller.ts
+++ packages/ag-charts-enterprise/src/features/zoom/zoomScroller.ts
@@ -24,7 +24,9 @@ export class ZoomScroller {
         const sourceEvent = event.sourceEvent as WheelEvent;
 
         // Scale the zoom bounding box
-        const dir = sourceEvent.deltaY < 0 ? -1 : 1;
+        const deltaY = sourceEvent.deltaY;
+        const factor = sourceEvent.deltaMode === 0 ? 0.1 : 1;
+        const dir = deltaY * factor;
         let newZoom = definedZoomState(oldZoom);
         newZoom.x.max += isScalingX ? step * dir * (oldZoom.x.max - oldZoom.x.min) : 0;
         newZoom.y.max += isScalingY ? step * dir * (oldZoom.y.max - oldZoom.y.min) : 0;
