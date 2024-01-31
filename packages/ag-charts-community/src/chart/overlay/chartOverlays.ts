dd9a77a68a33e5079564ead068277ab40ef9445b
Alan Treadway <alan.treadway@ag-grid.com>
2024-01-31 13:15:47 +0000

    AG-10547 - Force explicit CSS overlay text colour.

--- packages/ag-charts-community/src/chart/overlay/chartOverlays.ts
+++ packages/ag-charts-community/src/chart/overlay/chartOverlays.ts
@@ -1,11 +1,29 @@
+import { injectStyle } from '../../util/dom';
 import type { AnimationManager } from '../interaction/animationManager';
-import { Overlay } from './overlay';
+import { DEFAULT_OVERLAY_CLASS, DEFAULT_OVERLAY_DARK_CLASS, Overlay } from './overlay';
+
+const defaultOverlayCss = `
+.${DEFAULT_OVERLAY_CLASS} {
+    color: #181d1f;
+}
+
+.${DEFAULT_OVERLAY_CLASS}.${DEFAULT_OVERLAY_DARK_CLASS} {
+    color: #ffffff;
+}
+`;
 
 export class ChartOverlays {
+    private static overlayDocuments: Document[] = [];
+
     constructor(parent: HTMLElement, animationManager: AnimationManager) {
         this.noData = new Overlay('ag-chart-no-data-overlay', parent, animationManager);
         this.noVisibleSeries = new Overlay('ag-chart-no-visible-series', parent, animationManager);
         this.noVisibleSeries.text = 'No visible series';
+
+        if (ChartOverlays.overlayDocuments.indexOf(document) < 0) {
+            injectStyle(document, defaultOverlayCss);
+            ChartOverlays.overlayDocuments.push(document);
+        }
     }
 
     noData: Overlay;
