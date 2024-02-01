eb42348a48f8c48a3921dc32faaf2f8f133b7d47
Alan Treadway <alan.treadway@ag-grid.com>
2024-02-01 11:53:57 +0000

    AG-10537 - Fix range-bar update always triggering initial load animation.

--- packages/ag-charts-enterprise/src/series/box-plot/boxPlotThemes.ts
+++ packages/ag-charts-enterprise/src/series/box-plot/boxPlotThemes.ts
@@ -2,5 +2,6 @@ import { _Theme } from 'ag-charts-community';
 
 export const BOX_PLOT_SERIES_THEME = {
     __extends__: _Theme.EXTENDS_SERIES_DEFAULTS,
+    direction: 'vertical' as const,
     strokeWidth: 2,
 };
