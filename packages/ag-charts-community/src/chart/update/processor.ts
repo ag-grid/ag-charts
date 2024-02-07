7115043753bb32c5d6e9035bed6e9ebecd09d55f
Laurence Roberts <laurence.roberts@ag-grid.com>
2024-02-07 11:09:55 +0000

    AG-10430 Add overlay when lazy loading data

--- packages/ag-charts-community/src/chart/update/processor.ts
+++ packages/ag-charts-community/src/chart/update/processor.ts
@@ -6,6 +6,7 @@ import type { Caption } from '../caption';
 /** Interface to abstract from the actual chart implementation. */
 export interface ChartLike {
     axes: Array<AxisLike>;
+    series: Array<SeriesLike>;
     seriesArea: { clip?: boolean };
     seriesRoot: Group;
     padding: Padding;
@@ -18,6 +19,11 @@ export interface AxisLike {
     id: string;
     type: string;
     scale: Scale<any, any>;
+}
+
+export interface SeriesLike {
+    hasData: () => boolean | undefined;
+    visible: boolean;
 }
 
 export interface UpdateProcessor {
