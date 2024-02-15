d3e7b995296ed8a7fdcdda5d5e9db2bf5ba861d9
Laurence Roberts <laurence.roberts@ag-grid.com>
2024-02-15 10:17:21 +0000

    AG-10086 Add data source tests

--- packages/ag-charts-community/src/chart/test/utils.ts
+++ packages/ag-charts-community/src/chart/test/utils.ts
@@ -50,7 +50,7 @@ export const IMAGE_SNAPSHOT_DEFAULTS: MatchImageSnapshotOptions = {
     failureThresholdType: 'percent',
 };
 
-async function delay(ms: number): Promise<void> {
+export async function delay(ms: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
