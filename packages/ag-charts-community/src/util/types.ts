3e67bb18ba4c14f13ffad5ea37c11231610abf04
Alan Treadway <alan.treadway@ag-grid.com>
2024-02-22 12:41:05 +0000

    Upgrade prettier to 3.2.5.

--- packages/ag-charts-community/src/util/types.ts
+++ packages/ag-charts-community/src/util/types.ts
@@ -20,11 +20,8 @@ type _DeepRequiredObject<T> = {
     [K in keyof T]-?: DeepRequired<Defined<T[K]>>;
 };
 
-export type DeepPartial<T> = T extends Array<unknown>
-    ? T
-    : T extends object
-      ? { [K in keyof T]?: DeepPartial<T[K]> }
-      : T;
+export type DeepPartial<T> =
+    T extends Array<unknown> ? T : T extends object ? { [K in keyof T]?: DeepPartial<T[K]> } : T;
 
 export type PickRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] };
 
