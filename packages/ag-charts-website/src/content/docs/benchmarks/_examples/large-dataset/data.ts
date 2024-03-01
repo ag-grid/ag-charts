3f1ce68e9d18ea60ad4139d295c2e34a54f8904f
Alan Treadway <alan.treadway@ag-grid.com>
2024-03-01 14:17:40 +0000

    Add benchmark examples.

---
+++ packages/ag-charts-website/src/content/docs/benchmarks/_examples/large-dataset/data.ts
@@ -0,0 +1,25 @@
+export function getLargeScaleData(count: number) {
+    let baseValue = Math.random() * 1000;
+    let time = +new Date(2011, 0, 1);
+    let smallBaseValue = 0;
+
+    function next(idx: number) {
+        smallBaseValue = idx % 30 === 0 ? Math.random() * 700 : smallBaseValue + Math.random() * 500 - 250;
+        baseValue += Math.random() * 20 - 10;
+        return Math.max(0, Math.round(baseValue + smallBaseValue) + 3000);
+    }
+
+    const data = [];
+    data.length = count;
+
+    let i = 0;
+    while (i < count) {
+        data[i] = {
+            time,
+            value: next(i++),
+        };
+        time += 1000;
+    }
+
+    return data;
+}
