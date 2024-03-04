f109925833b77fb0462f5069f12cd4469d0fdfad
iMoses <imoses.g@gmail.com>
2024-03-04 17:43:02 +0200

    Placement rename + tests (#1150)
    * Placement rename + tests
    
    * cleanup tests
    
    * cleanup tests
    
    * cleanup tests
    
    * cleanup tests

---
+++ packages/ag-charts-community/src/util/placement.test.ts
@@ -0,0 +1,44 @@
+import { calculatePlacement } from './placement';
+
+describe('calculatePlacement function', () => {
+    const naturalWidth = 100;
+    const naturalHeight = 100;
+    const containerWidth = 500;
+    const containerHeight = 500;
+
+    it('should calculate placement with provided bounds (top, left)', () => {
+        const bounds = { top: 50, left: 50 };
+        const placement = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
+        expect(placement).toEqual({ x: 50, y: 50, width: 100, height: 100 });
+    });
+
+    it('should calculate placement with provided bounds (right, bottom)', () => {
+        const bounds = { right: 50, bottom: 50 };
+        const placement = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
+        expect(placement).toEqual({ x: 350, y: 350, width: 100, height: 100 });
+    });
+
+    it('should calculate placement with provided bounds (top, right)', () => {
+        const bounds = { top: 50, right: 50 };
+        const placement = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
+        expect(placement).toEqual({ x: 350, y: 50, width: 100, height: 100 });
+    });
+
+    it('should calculate placement with provided bounds (bottom, left)', () => {
+        const bounds = { bottom: 50, left: 50 };
+        const placement = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
+        expect(placement).toEqual({ x: 50, y: 350, width: 100, height: 100 });
+    });
+
+    it('should calculate placement with provided bounds (width, height)', () => {
+        const bounds = { width: 200, height: 200 };
+        const placement = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
+        expect(placement).toEqual({ x: 150, y: 150, width: 200, height: 200 });
+    });
+
+    it('should calculate placement with natural size if width and height not provided in bounds', () => {
+        const bounds = {};
+        const placement = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
+        expect(placement).toEqual({ x: 200, y: 200, width: 100, height: 100 });
+    });
+});
--- packages/ag-charts-enterprise/src/features/background/backgroundImage.test.ts
+++ packages/ag-charts-enterprise/src/features/background/backgroundImage.test.ts
@@ -26,7 +26,7 @@ describe('BackgroundImage', () => {
         const naturalWidth = 80;
         const naturalHeight = 50;
 
-        const calculatePosition = (params: {
+        const calculatePlacement = (params: {
             left?: number;
             width?: number;
             right?: number;
@@ -40,7 +40,7 @@ describe('BackgroundImage', () => {
 
             image.performLayout(containerWidth, containerHeight);
 
-            return _ModuleSupport.calculatePosition(
+            return _ModuleSupport.calculatePlacement(
                 naturalWidth,
                 naturalHeight,
                 containerWidth,
@@ -50,111 +50,111 @@ describe('BackgroundImage', () => {
         };
 
         it(`By default image has natural size and positioned at the center`, () => {
-            const position = calculatePosition({});
+            const placement = calculatePlacement({});
 
-            expect(position).toEqual({ x: 110, y: 75, width: 80, height: 50 });
+            expect(placement).toEqual({ x: 110, y: 75, width: 80, height: 50 });
         });
 
         it(`If left position specified, the image moves left`, () => {
-            const position = calculatePosition({ left: 20 });
+            const placement = calculatePlacement({ left: 20 });
 
-            expect(position).toEqual({ x: 20, y: 75, width: 80, height: 50 });
+            expect(placement).toEqual({ x: 20, y: 75, width: 80, height: 50 });
         });
 
         it(`If right position specified, the image moves right`, () => {
-            const position = calculatePosition({ right: 20 });
+            const placement = calculatePlacement({ right: 20 });
 
-            expect(position).toEqual({ x: 200, y: 75, width: 80, height: 50 });
+            expect(placement).toEqual({ x: 200, y: 75, width: 80, height: 50 });
         });
 
         it(`If right and left position specified, the image scratched`, () => {
-            const position = calculatePosition({ left: 50, right: 50 });
+            const placement = calculatePlacement({ left: 50, right: 50 });
 
-            expect(position).toEqual({ x: 50, y: 37, width: 200, height: 125 });
+            expect(placement).toEqual({ x: 50, y: 37, width: 200, height: 125 });
         });
 
         it(`If width specified, the image scratched at the center`, () => {
-            const position = calculatePosition({ width: 160 });
+            const placement = calculatePlacement({ width: 160 });
 
-            expect(position).toEqual({ x: 70, y: 50, width: 160, height: 100 });
+            expect(placement).toEqual({ x: 70, y: 50, width: 160, height: 100 });
         });
 
         it(`If width and left specified, the image scratched and moved left`, () => {
-            const position = calculatePosition({ left: 20, width: 160 });
+            const placement = calculatePlacement({ left: 20, width: 160 });
 
-            expect(position).toEqual({ x: 20, y: 50, width: 160, height: 100 });
+            expect(placement).toEqual({ x: 20, y: 50, width: 160, height: 100 });
         });
 
         it(`If width and right specified, the image scratched and moved right`, () => {
-            const position = calculatePosition({ width: 160, right: 20 });
+            const placement = calculatePlacement({ width: 160, right: 20 });
 
-            expect(position).toEqual({ x: 120, y: 50, width: 160, height: 100 });
+            expect(placement).toEqual({ x: 120, y: 50, width: 160, height: 100 });
         });
 
         it(`If left, right and width specified, right ignored`, () => {
-            const position = calculatePosition({ left: 50, width: 200, right: 50 });
+            const placement = calculatePlacement({ left: 50, width: 200, right: 50 });
 
-            expect(position).toEqual({ x: 50, y: 37, width: 200, height: 125 });
+            expect(placement).toEqual({ x: 50, y: 37, width: 200, height: 125 });
         });
 
         it(`If top position specified, the image moves up`, () => {
-            const position = calculatePosition({ top: 20 });
+            const placement = calculatePlacement({ top: 20 });
 
-            expect(position).toEqual({ x: 110, y: 20, width: 80, height: 50 });
+            expect(placement).toEqual({ x: 110, y: 20, width: 80, height: 50 });
         });
 
         it(`If bottom position specified, the image moves down`, () => {
-            const position = calculatePosition({ bottom: 20 });
+            const placement = calculatePlacement({ bottom: 20 });
 
-            expect(position).toEqual({ x: 110, y: 130, width: 80, height: 50 });
+            expect(placement).toEqual({ x: 110, y: 130, width: 80, height: 50 });
         });
 
         it(`If top bottom position specified, the image starches`, () => {
-            const position = calculatePosition({ top: 50, bottom: 50 });
+            const placement = calculatePlacement({ top: 50, bottom: 50 });
 
-            expect(position).toEqual({ x: 70, y: 50, width: 160, height: 100 });
+            expect(placement).toEqual({ x: 70, y: 50, width: 160, height: 100 });
         });
 
         it(`If height and top specified, the image scratches and moves up`, () => {
-            const position = calculatePosition({ top: 20, height: 100 });
+            const placement = calculatePlacement({ top: 20, height: 100 });
 
-            expect(position).toEqual({ x: 70, y: 20, width: 160, height: 100 });
+            expect(placement).toEqual({ x: 70, y: 20, width: 160, height: 100 });
         });
 
         it(`If height and bottom specified, the image scratches`, () => {
-            const position = calculatePosition({ height: 100, bottom: 20 });
+            const placement = calculatePlacement({ height: 100, bottom: 20 });
 
-            expect(position).toEqual({ x: 70, y: 80, width: 160, height: 100 });
+            expect(placement).toEqual({ x: 70, y: 80, width: 160, height: 100 });
         });
 
         it(`If top, bottom and height specified, bottom ignored`, () => {
-            const position = calculatePosition({ top: 50, height: 125, bottom: 50 });
+            const placement = calculatePlacement({ top: 50, height: 125, bottom: 50 });
 
-            expect(position).toEqual({ x: 50, y: 50, width: 200, height: 125 });
+            expect(placement).toEqual({ x: 50, y: 50, width: 200, height: 125 });
         });
 
         it(`If all sides specified, natural proportion ignored`, () => {
-            const position = calculatePosition({ left: 50, top: 50, right: 50, bottom: 50 });
+            const placement = calculatePlacement({ left: 50, top: 50, right: 50, bottom: 50 });
 
-            expect(position).toEqual({ x: 50, y: 50, width: 200, height: 100 });
+            expect(placement).toEqual({ x: 50, y: 50, width: 200, height: 100 });
         });
 
         it(`Width and height specified, natural proportion ignored`, () => {
-            const position = calculatePosition({ width: 200, height: 100 });
+            const placement = calculatePlacement({ width: 200, height: 100 });
 
-            expect(position).toEqual({ x: 50, y: 50, width: 200, height: 100 });
+            expect(placement).toEqual({ x: 50, y: 50, width: 200, height: 100 });
         });
 
         it(`Cross-axes: right and height`, () => {
-            const position = calculatePosition({ right: 20, height: 100 });
+            const placement = calculatePlacement({ right: 20, height: 100 });
 
-            expect(position).toEqual({ x: 120, y: 50, width: 160, height: 100 });
+            expect(placement).toEqual({ x: 120, y: 50, width: 160, height: 100 });
         });
 
         it(`Cross-axes: bottom and width`, () => {
-            const position = calculatePosition({ width: 160, bottom: 20 });
+            const placement = calculatePlacement({ width: 160, bottom: 20 });
 
-            expect(position).toEqual({ x: 70, y: 80, width: 160, height: 100 });
+            expect(placement).toEqual({ x: 70, y: 80, width: 160, height: 100 });
         });
     });
 
