abd03016d0515eb7b5b436469445d77aa5cecb9a
Alan Treadway <alan.treadway@ag-grid.com>
2024-03-04 08:36:42 +0000

    Cleanup.

--- packages/ag-charts-community/benchmarks/largeDataset.test.ts
+++ packages/ag-charts-community/benchmarks/largeDataset.test.ts
@@ -10,27 +10,4 @@ describe('large-dataset benchmark', () => {
         ctx.chart = AgCharts.create(ctx.options);
         await (ctx.chart as any).chart.waitForUpdate();
     });
-
-    // describe('after load', () => {
-    //     beforeAll(async () => {
-    //         ctx.chart = AgCharts.create(ctx.options);
-    //         await (ctx.chart as any).chart.waitForUpdate();
-    //     });
-
-    //     benchmark('legend toggle', ctx, async () => {
-    //         ctx.options.series![0].visible = false;
-    //         AgCharts.update(ctx.chart, ctx.options);
-    //         await (ctx.chart as any).chart.waitForUpdate();
-    //     });
-
-    //     benchmark('3x legend toggle', ctx, async () => {
-    //         for (let i = 0; i < 5; i++) {
-    //             for (const visible of [false, true]) {
-    //                 ctx.options.series![i].visible = visible;
-    //                 AgCharts.update(ctx.chart, ctx.options);
-    //                 await (ctx.chart as any).chart.waitForUpdate();
-    //             }
-    //         }
-    //     });
-    // });
 });
