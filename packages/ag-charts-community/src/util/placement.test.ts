25a773b0f852ca17907e0b164ee3b6f7f7391032
iMoses (Ido Moshe) <imoses.g@gmail.com>
2024-03-04 14:52:50 +0000

    cleanup tests

--- packages/ag-charts-community/src/util/placement.test.ts
+++ packages/ag-charts-community/src/util/placement.test.ts
@@ -6,39 +6,39 @@ describe('calculatePosition function', () => {
     const containerWidth = 500;
     const containerHeight = 500;
 
-    it('should calculate position with provided bounds (top, left)', () => {
+    it('should calculate placement with provided bounds (top, left)', () => {
         const bounds = { top: 50, left: 50 };
-        const position = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
-        expect(position).toEqual({ x: 50, y: 50, width: 100, height: 100 });
+        const placement = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
+        expect(placement).toEqual({ x: 50, y: 50, width: 100, height: 100 });
     });
 
-    it('should calculate position with provided bounds (right, bottom)', () => {
+    it('should calculate placement with provided bounds (right, bottom)', () => {
         const bounds = { right: 50, bottom: 50 };
-        const position = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
-        expect(position).toEqual({ x: 350, y: 350, width: 100, height: 100 });
+        const placement = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
+        expect(placement).toEqual({ x: 350, y: 350, width: 100, height: 100 });
     });
 
-    it('should calculate position with provided bounds (top, right)', () => {
+    it('should calculate placement with provided bounds (top, right)', () => {
         const bounds = { top: 50, right: 50 };
-        const position = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
-        expect(position).toEqual({ x: 350, y: 50, width: 100, height: 100 });
+        const placement = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
+        expect(placement).toEqual({ x: 350, y: 50, width: 100, height: 100 });
     });
 
-    it('should calculate position with provided bounds (bottom, left)', () => {
+    it('should calculate placement with provided bounds (bottom, left)', () => {
         const bounds = { bottom: 50, left: 50 };
-        const position = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
-        expect(position).toEqual({ x: 50, y: 350, width: 100, height: 100 });
+        const placement = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
+        expect(placement).toEqual({ x: 50, y: 350, width: 100, height: 100 });
     });
 
-    it('should calculate position with provided bounds (width, height)', () => {
+    it('should calculate placement with provided bounds (width, height)', () => {
         const bounds = { width: 200, height: 200 };
-        const position = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
-        expect(position).toEqual({ x: 150, y: 150, width: 200, height: 200 });
+        const placement = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
+        expect(placement).toEqual({ x: 150, y: 150, width: 200, height: 200 });
     });
 
-    it('should calculate position with natural size if width and height not provided in bounds', () => {
+    it('should calculate placement with natural size if width and height not provided in bounds', () => {
         const bounds = {};
-        const position = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
-        expect(position).toEqual({ x: 200, y: 200, width: 100, height: 100 });
+        const placement = calculatePlacement(naturalWidth, naturalHeight, containerWidth, containerHeight, bounds);
+        expect(placement).toEqual({ x: 200, y: 200, width: 100, height: 100 });
     });
 });
