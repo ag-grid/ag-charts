22d49b3bccfa24d67a397801db1faa6769c71260
Alan Treadway <alan.treadway@ag-grid.com>
2024-03-04 19:56:13 +0000

    Add cleanup of ag-charts-website build artifacts.

--- tools/tidy-nx-cache.sh
+++ tools/tidy-nx-cache.sh
@@ -9,5 +9,5 @@ find .nx/cache -name "ag-charts-website" | grep packages | while read filename ;
     cacheHash=$(echo "${filename}" | awk -F '/' '{ print $3 }')
     size=$(du -sh .nx/cache/${cacheHash} | awk '{ print $1 }')
     rm -rf .nx/cache/${cacheHash}{,.commit}
-    echo "Removed cache entry of size ${size} with hash ${commit}"
+    echo "Removed cache entry of size ${size} with hash ${cacheHash}"
 done
