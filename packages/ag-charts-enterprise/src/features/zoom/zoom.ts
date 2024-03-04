4b5d6478ccabf7b3dad9f746e5e52f5a1e6e8c5e
Oli Legat <olivier.legat@ag-grid.com>
2024-03-04 12:36:30 +0000

    AG-10657 Always pop InteractionState.ZoomDrag
    There's no harm in popping ZoomDrag if it isn't pushed, because it just
    sets a flag to 0. This ensures that the chart remains interactable after
    the dragging when enablePanning=false.

--- packages/ag-charts-enterprise/src/features/zoom/zoom.ts
+++ packages/ag-charts-enterprise/src/features/zoom/zoom.ts
@@ -320,10 +320,10 @@ export class Zoom extends _ModuleSupport.BaseModuleInstance implements _ModuleSu
     }
 
     private onDragEnd() {
+        this.ctx.interactionManager.popState(_ModuleSupport.InteractionState.ZoomDrag);
+
         // Stop single clicks from triggering drag end and resetting the zoom
         if (!this.enabled || this.dragState === DragState.None) return;
-
-        this.ctx.interactionManager.popState(_ModuleSupport.InteractionState.ZoomDrag);
 
         switch (this.dragState) {
             case DragState.Axis:
