435328dfa0ed088227ca00f223ec78204cede518
Alan Treadway <alan.treadway@ag-grid.com>
2024-03-13 13:55:26 +0000

    Add benchmark summary and report collation.

---
+++ packages/ag-charts-website/src/content/docs/benchmarks/_examples/summary/data.ts
@@ -0,0 +1,24 @@
+export function getData() {
+    return [
+        {
+            name: "latest",
+            results: {
+                "integrated charts large scale benchmark initial load": 594.1213750000024,
+                "integrated charts large scale benchmark after load 1x legend toggle": 826.5434170000008,
+                "integrated charts large scale benchmark after load 4x legend toggle": 1552.844250000002,
+                "large-dataset benchmark initial load": 5868.044292,
+                "large-dataset benchmark after load 1x legend toggle": 7622.215917000001,
+                "large-dataset benchmark after load 1x datum highlight": 2998.8396250000005,
+                "large-dataset benchmark after load 4x datum highlight": 5749.84375,
+                "large-scale multi-series benchmark initial load": 798.6127080000006,
+                "large-scale multi-series benchmark after load 1x legend toggle": 1005.0907920000027,
+                "large-scale multi-series benchmark after load 4x legend toggle": 1894.8821669999961,
+                "multi-series benchmark initial load": 1807.6933329999997,
+                "multi-series benchmark after load 1x legend toggle": 198.61962500000027,
+                "multi-series benchmark after load 10x legend toggle": 893.5050420000011,
+                "multi-series benchmark after load 1x datum highlight": 80.00333399999363,
+                "multi-series benchmark after load 15x datum highlight": 794.3273750000008,
+            },
+        },
+    ];
+}
---
+++ packages/ag-charts-website/src/content/docs/benchmarks/_examples/summary/main.ts
@@ -0,0 +1,81 @@
+/* @ag-options-extract */
+import { AgCartesianChartOptions, AgCharts } from 'ag-charts-community';
+
+import { getData } from './data';
+
+const initialLoadKeys: (keyof ReturnType<typeof getData>[number]['results'])[] = [
+    'integrated charts large scale benchmark initial load',
+    'large-dataset benchmark initial load',
+    'large-scale multi-series benchmark initial load',
+    'multi-series benchmark initial load',
+];
+
+const seriesToggleKeys: (keyof ReturnType<typeof getData>[number]['results'])[] = [
+    'integrated charts large scale benchmark after load 4x legend toggle',
+    'large-dataset benchmark after load 1x legend toggle',
+    'large-scale multi-series benchmark after load 4x legend toggle',
+    'multi-series benchmark after load 10x legend toggle',
+];
+
+const seriesHighlightKeys: (keyof ReturnType<typeof getData>[number]['results'])[] = [
+    'large-dataset benchmark after load 4x datum highlight',
+    'multi-series benchmark after load 15x datum highlight',
+];
+
+const chartOptions1: AgCartesianChartOptions = {
+    container: document.getElementById('myChart1'),
+    autoSize: true,
+    data: getData(),
+    title: {
+        text: 'Initial Load Cases',
+        fontSize: 18,
+    },
+    series: initialLoadKeys.map((key) => ({
+        type: 'bar' as const,
+        xKey: 'name',
+        yKey: `results['${key}']`,
+        yName: key.replace(' initial load', ''),
+    })),
+};
+
+AgCharts.create(chartOptions1);
+
+const chartOptions2: AgCartesianChartOptions = {
+    container: document.getElementById('myChart2'),
+    autoSize: true,
+    data: getData(),
+    title: {
+        text: 'Legend Toggle',
+        fontSize: 18,
+    },
+    series: [
+        ...seriesToggleKeys.map((key) => ({
+            type: 'bar' as const,
+            xKey: 'name',
+            yKey: `results['${key}']`,
+            yName: key.replace(' legend toggle', ''),
+        })),
+    ],
+};
+
+AgCharts.create(chartOptions2);
+
+const chartOptions3: AgCartesianChartOptions = {
+    container: document.getElementById('myChart3'),
+    autoSize: true,
+    data: getData(),
+    title: {
+        text: 'Datum Highlight',
+        fontSize: 18,
+    },
+    series: [
+        ...seriesHighlightKeys.map((key) => ({
+            type: 'bar' as const,
+            xKey: 'name',
+            yKey: `results['${key}']`,
+            yName: key.replace(' datum highlight', ''),
+        })),
+    ],
+};
+
+AgCharts.create(chartOptions3);
