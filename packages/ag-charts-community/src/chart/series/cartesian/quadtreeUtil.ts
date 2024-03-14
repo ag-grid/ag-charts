f0a4062d8f52ab00614f72b9be233bf2d5237bc7
Oli Legat <olivier.legat@ag-grid.com>
2024-03-14 15:55:13 +0000

    AG-8416 Move bar-series quadtree initialisation logic into util file

---
+++ packages/ag-charts-community/src/chart/series/cartesian/quadtreeUtil.ts
@@ -0,0 +1,33 @@
+import type { BBox } from '../../../scene/bbox';
+import type { DistantObject } from '../../../scene/nearest';
+import type { Node } from '../../../scene/node';
+import { QuadtreeNearest } from '../../../scene/util/quadtree';
+import { Logger } from '../../../util/logger';
+import type { CartesianSeriesNodeDatum } from './cartesianSeries';
+
+type QuadtreeCompatibleNode = Node & DistantObject & { readonly midPoint: { x: number; y: number } };
+
+export function* childrenOfChildrenIter<TNode extends Node = Node>(contentGroup: Node): Iterable<TNode> {
+    for (const children of contentGroup.children) {
+        for (const node of children.children) {
+            yield node as TNode;
+        }
+    }
+}
+
+export function createQuadtree<TNode extends QuadtreeCompatibleNode, TDatum extends CartesianSeriesNodeDatum>(
+    seriesRect: BBox | undefined,
+    hitTesters: Iterable<TNode>
+): QuadtreeNearest<TDatum> {
+    const quadtree = new QuadtreeNearest<TDatum>(100, 10, seriesRect);
+    for (const node of hitTesters) {
+        const datum: TDatum | undefined = node.datum;
+        if (datum !== undefined) {
+            quadtree.addValue(node, datum);
+        } else {
+            Logger.error('undefined datum');
+        }
+    }
+
+    return quadtree;
+}
