52f30185075f4bfbd3ccca2511930eddc13ad7b2
Alan Treadway <alan.treadway@ag-grid.com>
2024-03-15 14:40:27 +0000

    Add gallery thumbnail sanity checks.

---
+++ packages/ag-charts-website/tools/compare-gallery-thumbnails.js
@@ -0,0 +1,101 @@
+#!node
+
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const pixelmatch = require('pixelmatch');
+const { PNG } = require('pngjs');
+
+const fail = (msg, usage) => {
+    console.error(msg);
+    if (usage) {
+        console.error(`Usage: ${process.argv[1]} [-u] <input-dir> <compare-dir>`);
+        console.error(`Flags: `);
+        console.error(`-u   Update snapshots.`);
+    }
+    process.exit(1);
+};
+
+// Parse argv.
+let update = false;
+
+let argv = process.argv.slice(2);
+if (argv[0] === '-u') {
+    update = true;
+    argv.shift();
+}
+if (argv.some((v) => v.startsWith('-'))) {
+    fail('Unexpected flag', true);
+} else if (argv.length !== 2) {
+    fail('Wrong number of arguments', true);
+}
+
+const inputDir = argv.shift();
+const compareDir = argv.shift();
+
+// Validate argv inputs.
+if (!fs.existsSync(inputDir)) {
+    fail('Input dir not found: ' + inputDir);
+}
+
+if (!fs.existsSync(compareDir)) {
+    fail('Compare dir not found: ' + compareDir);
+}
+
+// Main process.
+let snapshots = {
+    created: 0,
+    updated: 0,
+    mismatching: 0,
+    matched: 0,
+};
+const mismatched = [];
+const inputs = glob.sync('**/ag-default.png', { cwd: inputDir });
+for (const input of inputs) {
+    const inputPath = path.join(inputDir, input);
+    const comparePath = path.join(compareDir, input);
+
+    if (!fs.existsSync(comparePath)) {
+        fs.mkdirSync(path.dirname(comparePath), { recursive: true });
+        fs.copyFileSync(inputPath, comparePath);
+
+        console.log(`[created] ${comparePath}`);
+
+        snapshots.created++;
+        continue;
+    }
+
+    const inputImage = PNG.sync.read(fs.readFileSync(inputPath));
+    const compareImage = PNG.sync.read(fs.readFileSync(comparePath));
+    const diffImage = new PNG({ width: 600, height: 375 });
+    const match = pixelmatch(inputImage.data, compareImage.data, diffImage.data, 600, 375, {
+        threshold: 0,
+    });
+
+    if (match === 0) {
+        console.log('\x1b[32m%s\x1b[0m', `[matched] ${inputPath}\n     [vs] ${comparePath}`);
+        snapshots.matched++;
+        continue;
+    }
+
+    if (update) {
+        fs.copyFileSync(inputPath, comparePath);
+        console.log('\x1b[33m%s\x1b[0m', `[updated] ${comparePath}`);
+        snapshots.updated++;
+        continue;
+    }
+
+    mismatched.push(`[mismatched] ${inputPath}\n        [vs] ${comparePath}`);
+    snapshots.mismatching++;
+}
+
+console.log('\n\nResults:');
+console.table(snapshots);
+
+if (snapshots.mismatching > 0) {
+    console.log('\x1b[31m%s\x1b[0m', '\n\nFailures:');
+    mismatched.forEach((m) => console.error('\x1b[31m%s\x1b[0m', m));
+    process.exit(1);
+} else {
+    process.exit(0);
+}
