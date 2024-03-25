c4a21584e4acc57a6b99a8398c23292270253e8c
Alan Treadway <alan.treadway@ag-grid.com>
2024-03-25 16:56:10 +0000

    Tweak mock-console.ts to make seeing console stack traces easier.

--- libraries/ag-charts-test/src/console/mock-console.ts
+++ libraries/ag-charts-test/src/console/mock-console.ts
@@ -1,15 +1,30 @@
 /* eslint-disable no-console */
 export function setupMockConsole() {
+    const originalConsole = {
+        warn: console.warn,
+        error: console.error,
+    };
+
     beforeEach(() => {
         console.warn = jest.fn();
         console.error = jest.fn();
     });
 
     afterEach(() => {
-        expect(console.warn).not.toHaveBeenCalled();
-        expect(console.error).not.toHaveBeenCalled();
-        (console.warn as jest.Mock).mockClear();
-        (console.error as jest.Mock).mockClear();
+        const handleMock = (key: 'warn' | 'error') => {
+            const mock = console[key] as jest.Mock;
+            const calls = [...mock.mock.calls];
+            mock.mockClear();
+
+            for (let i = 0; i < calls.length; i++) {
+                originalConsole[key](...calls[i]);
+            }
+
+            expect(calls).toHaveLength(0);
+        };
+
+        handleMock('warn');
+        handleMock('error');
     });
 }
 
