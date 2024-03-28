d2ba7467fafb3ad3672a18f7d5090b9f2467f1d8
iMoses (Ido Moshe) <imoses.g@gmail.com>
2024-03-28 10:29:38 +0000

    fix tests

--- packages/ag-charts-community/jest.setup.ts
+++ packages/ag-charts-community/jest.setup.ts
@@ -1,7 +1,14 @@
 import { expect } from '@jest/globals';
+import { CanvasRenderingContext2D } from 'canvas';
 import { type MatchImageSnapshotOptions, toMatchImageSnapshot } from 'jest-image-snapshot';
+import { Path2D, applyPath2DToCanvasRenderingContext } from 'path2d';
 
 import { toMatchImage } from './src/chart/test/utils';
+
+// @ts-ignore
+global.Path2D = Path2D;
+
+applyPath2DToCanvasRenderingContext(CanvasRenderingContext2D);
 
 declare module 'expect' {
     interface Matchers<R> {
