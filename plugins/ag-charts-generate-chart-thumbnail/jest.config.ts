d84ace4665b4cf4cfb57495e9280a451725b1172
iMoses (Ido Moshe) <imoses.g@gmail.com>
2024-03-28 13:26:26 +0000

    fix thumbnails generation setup

--- plugins/ag-charts-generate-chart-thumbnail/jest.config.ts
+++ plugins/ag-charts-generate-chart-thumbnail/jest.config.ts
@@ -1,6 +1,7 @@
 /* eslint-disable */
 export default {
     displayName: 'plugins-ag-charts-generate-chart-thumbnail',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     preset: '../../jest.preset.js',
     transform: {
         '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
