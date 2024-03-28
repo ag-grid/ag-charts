6acd26db60534b77b4bd50ec4bf539344a7d060c
iMoses (Ido Moshe) <imoses.g@gmail.com>
2024-03-28 13:56:53 +0000

    fix thumbnails generation setup

--- plugins/ag-charts-generate-chart-thumbnail/jest.config.ts
+++ plugins/ag-charts-generate-chart-thumbnail/jest.config.ts
@@ -1,7 +1,6 @@
 /* eslint-disable */
 export default {
     displayName: 'plugins-ag-charts-generate-chart-thumbnail',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     preset: '../../jest.preset.js',
     transform: {
         '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
--- plugins/ag-charts-generate-chart-thumbnail/jest.setup.ts
+++
@@ -1,7 +0,0 @@
-import { CanvasRenderingContext2D } from 'canvas';
-import { Path2D, applyPath2DToCanvasRenderingContext } from 'path2d';
-
-// @ts-ignore
-global.Path2D = Path2D;
-
-applyPath2DToCanvasRenderingContext(CanvasRenderingContext2D);
