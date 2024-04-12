2d7164b6b9561aa02f9177a46b2bd1020bee3a86
iMoses (Ido Moshe) <imoses.g@gmail.com>
2024-04-12 17:13:30 +0100

    wip

--- packages/ag-charts-community/src/sandbox/util/transformMatrix.ts
+++ packages/ag-charts-community/src/sandbox/util/transformMatrix.ts
@@ -13,6 +13,16 @@ export class TransformMatrix {
         public rotationPivotY = 0
     ) {}
 
+    get isIdentity() {
+        return (
+            this.scaleX === 1 &&
+            this.scaleY === 1 &&
+            this.rotation === 0 &&
+            this.translateX === 0 &&
+            this.translateY === 0
+        );
+    }
+
     toMatrix(target?: Matrix) {
         const elements = TransformMatrix.calculateTransformMatrix(
             this.scaleX,
