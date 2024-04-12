6ab0695963336c9de8a786da7784cc3ef1b9ed08
iMoses (Ido Moshe) <imoses.g@gmail.com>
2024-04-12 11:10:16 +0100

    .eslintrc.json

--- packages/ag-charts-community/.eslintrc.js
+++ packages/ag-charts-community/.eslintrc.js
@@ -6,6 +6,7 @@ if (process.env.CI != null) {
 }
 
 module.exports = {
+  parserOptions: { tsconfigRootDir: '../../' },
   extends: ['../../.eslintrc.json', 'plugin:sonarjs/recommended'],
   ignorePatterns: ['!**/*', '.dependency-cruiser.js', '.eslintrc.js'],
   rules: {
