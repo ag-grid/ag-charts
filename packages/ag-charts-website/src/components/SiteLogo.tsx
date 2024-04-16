0669f9afdc694d2760dbcd61000770bb24f05503
Tak Tran <contact@tutaktran.com>
2024-04-16 16:03:42 +0100

    AG-11264 - Remove legacy SiteHeader.module.scss file

--- packages/ag-charts-website/src/components/SiteLogo.tsx
+++ packages/ag-charts-website/src/components/SiteLogo.tsx
@@ -1,7 +1,7 @@
+import siteHeaderStyles from '@ag-website-shared/components/site-header/SiteHeader.module.scss';
 import LogoType from '@ag-website-shared/images/inline-svgs/ag-charts-logotype.svg?react';
 import LogoMark from '@components/logo/LogoMark';
 import { SITE_BASE_URL } from '@constants';
-import gridStyles from '@legacy-design-system/modules/SiteHeader.module.scss';
 import type { FunctionComponent } from 'react';
 import { useState } from 'react';
 
@@ -12,7 +12,7 @@ export const SiteLogo: FunctionComponent = () => {
         <a
             href={SITE_BASE_URL}
             aria-label="Home"
-            className={gridStyles.headerLogo}
+            className={siteHeaderStyles.headerLogo}
             onMouseEnter={() => {
                 setIsLogoHover(true);
             }}
