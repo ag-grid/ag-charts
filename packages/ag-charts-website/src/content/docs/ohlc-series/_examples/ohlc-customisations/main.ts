69f1a504da37d65fa7550dda93b581f61c4463ef
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-04-19 15:29:20 +0100

    AG-11227 - Add ohlc docs page

---
+++ packages/ag-charts-website/src/content/docs/ohlc-series/_examples/ohlc-customisations/main.ts
@@ -0,0 +1,56 @@
+import { AgChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'Dow Jones Industrial Average',
+    },
+    subtitle: {
+        text: 'OHLC Patterns',
+    },
+    footnote: {
+        text: '1 Minute',
+    },
+    series: [
+        {
+            type: 'ohlc',
+            xKey: 'date',
+            xName: 'Time',
+            lowKey: 'low',
+            highKey: 'high',
+            openKey: 'open',
+            closeKey: 'close',
+            item: {
+                up: {
+                    stroke: '#45ba45',
+                    strokeWidth: 2,
+                },
+                down: {
+                    stroke: '#ba4545',
+                    strokeWidth: 2,
+                },
+            },
+        },
+    ],
+    axes: [
+        {
+            type: 'ordinal-time',
+            position: 'bottom',
+            label: {
+                format: '%H:%M',
+            },
+        },
+        {
+            type: 'number',
+            position: 'right',
+            label: {
+                formatter: ({ value }) => Number(value).toLocaleString(),
+            },
+        },
+    ],
+};
+
+AgCharts.create(options);
