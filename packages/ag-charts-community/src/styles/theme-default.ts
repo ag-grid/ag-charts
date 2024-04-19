cd7c116af5a9ad769d046e0135834398bbb47fc7
Alan Treadway <alan.treadway@ag-grid.com>
2024-04-19 11:36:25 +0100

    AG-11338 - Align default focus colour with Quartz.

--- packages/ag-charts-community/src/styles/theme-default.ts
+++ packages/ag-charts-community/src/styles/theme-default.ts
@@ -39,8 +39,8 @@ export default `
     );
 
     /* Focus Indicator */
-    --ag-charts-focus-border: var(--ag-input-focus-border-color, solid 2px) var(--ag-input-focus-border-color, red);
-    --ag-charts-focus-border-shadow: var(--ag-input-focus-box-shadow, none);
+    --ag-charts-focus-border: solid 2px var(--ag-input-focus-border-color, var(--ag-charts-active-color));
+    --ag-charts-focus-border-shadow: var(--ag-input-focus-box-shadow, 0 0 0 3px color-mix(in srgb, transparent, var(--ag-charts-focus-border) 47%));
 
     /* Icons */
     --ag-charts-icon-font-family: 'agChartsDefault';
