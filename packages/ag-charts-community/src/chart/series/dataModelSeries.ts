e7487e5d3b575e55d967306f9d288c4819b241d3
Oli Legat <olivier.legat@ag-grid.com>
2024-04-22 11:57:37 +0100

    AG-11327 Make keyboard skip toggled-off datums in pie/donut series

--- packages/ag-charts-community/src/chart/series/dataModelSeries.ts
+++ packages/ag-charts-community/src/chart/series/dataModelSeries.ts
@@ -1,6 +1,7 @@
 import { ContinuousScale } from '../../scale/continuousScale';
 import { OrdinalTimeScale } from '../../scale/ordinalTimeScale';
 import type { BBox } from '../../scene/bbox';
+import { Logger } from '../../util/logger';
 import { clamp } from '../../util/number';
 import { ChartAxisDirection } from '../chartAxisDirection';
 import type { DataController } from '../data/dataController';
@@ -69,17 +70,60 @@ export abstract class DataModelSeries<
     public abstract getNodeData(): TDatum[] | undefined;
 
     public override pickFocus(opts: PickFocusInputs): PickFocusOutputs<TDatum> | undefined {
+        return this.doPickFocus(opts, this);
+    }
+
+    // The legend behaves differently for Pie and Donut series. We need to use a seriesItemEnabled
+    // array to determine whether a datum has been toggled on/off using the legend.
+    private doPickFocus<TSeries>(
+        opts: PickFocusInputs,
+        derivedSeries: TSeries & { readonly seriesItemEnabled?: readonly boolean[] }
+    ): PickFocusOutputs<TDatum> | undefined {
         const nodeData = this.getNodeData();
         if (nodeData === undefined || nodeData.length === 0) {
             return undefined;
         }
 
         const { seriesRect } = opts;
-        const datumIndex = clamp(0, opts.datumIndex, nodeData.length - 1);
+        const datumIndex = this.computeFocusDatumIndex(opts, nodeData, derivedSeries.seriesItemEnabled);
+
         const datum = nodeData[datumIndex];
         const bbox = this.computeFocusBounds({ datumIndex, seriesRect });
         if (bbox !== undefined) {
             return { bbox, datum, datumIndex };
         }
+    }
+
+    private computeFocusDatumIndex(
+        opts: PickFocusInputs,
+        nodeData: TDatum[],
+        seriesItemEnabled: readonly boolean[] | undefined
+    ): number {
+        if (seriesItemEnabled === undefined) {
+            return clamp(0, opts.datumIndex, nodeData.length - 1);
+        }
+
+        if (nodeData.length !== seriesItemEnabled.length) {
+            Logger.error(
+                `invalid state: nodeData.length (${nodeData.length} !== seriesItemEnabled.length ($seriesItemEnabled.length})`
+            );
+        }
+
+        let datumIndex: number = opts.datumIndex;
+        // Find first visible datum by search forwards then backwards.
+        while (datumIndex < seriesItemEnabled.length && !seriesItemEnabled[datumIndex]) {
+            datumIndex++;
+        }
+        if (datumIndex === seriesItemEnabled.length) {
+            datumIndex = opts.datumIndex - 1;
+            while (datumIndex >= 0 && !seriesItemEnabled[datumIndex]) {
+                datumIndex--;
+            }
+            if (datumIndex === -1) {
+                Logger.error(`invalid state: no enabled datum found`);
+            }
+        }
+
+        return datumIndex;
     }
 }
