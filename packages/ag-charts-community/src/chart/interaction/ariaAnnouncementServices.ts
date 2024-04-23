e4965c1db8cf4cf61b654c497e58a52104525064
Oli Legat <olivier.legat@ag-grid.com>
2024-04-23 11:22:33 +0100

    AG-11380 Add aria announcer debug logging

--- packages/ag-charts-community/src/chart/interaction/ariaAnnouncementServices.ts
+++ packages/ag-charts-community/src/chart/interaction/ariaAnnouncementServices.ts
@@ -1,8 +1,10 @@
 import { setAttribute } from '../../util/attributeUtil';
+import { Debug } from '../../util/debug';
 import { getDocument } from '../../util/dom';
 
 export class AriaAnnouncementService {
     private readonly liveElem: HTMLElement;
+    private readonly debug = Debug.create(true, 'aria');
 
     private static createAnnouncer(): HTMLElement {
         const e = getDocument().createElement('div');
@@ -21,6 +23,7 @@ export class AriaAnnouncementService {
     }
 
     public announceValue(value: string): void {
+        this.debug(`AriaAnnouncementService - announceValue: ${value}`);
         setAttribute(this.liveElem, 'aria-label', value);
     }
 }
