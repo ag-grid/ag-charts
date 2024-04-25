2ec8140838905d144f98806e17cc05d13ad3224d
Alan Treadway <alan.treadway@ag-grid.com>
2024-04-25 13:55:01 +0100

    Improve release create/update scripts.

--- tools/create-release.sh
+++ tools/create-release.sh
@@ -3,12 +3,16 @@
 set -eu
 
 BRANCH=$1
-echo "Preparing release branch ${BRANCH}"
+RELEASE=$(echo "$1" | sed 's/^[a-zA-Z]*//')
+echo "Preparing BRANCH branch ${BRANCH}"
 
-git switch ${BRANCH}
+git checkout -b ${BRANCH}
+./tools/bump-versions.sh ${RELEASE}
+node ./tools/update-release-info.js
+
 NEW_VERSION=$(node ./tools/calculate-next-version.js)
 ./tools/bump-versions.sh ${NEW_VERSION}
-git commit -a -m "Release prep for ${NEW_VERSION}"
+git commit -a -m "BRANCH prep for ${NEW_VERSION}"
 git tag latest-beta-version -f
 
 read -p "Ready to push to ${BRANCH}? " -n 1 -r
