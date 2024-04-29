8945b7abb8dad71e6d4de8dfa49bf7ae78d7d48a
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-04-29 12:20:40 +0100

    AG-11454 Add specific check for isFramework

--- packages/ag-charts-website/src/utils/markdoc/getFirstParagraphText.ts
+++ packages/ag-charts-website/src/utils/markdoc/getFirstParagraphText.ts
@@ -10,10 +10,8 @@ export function getFirstParagraphText(markdocContent: string, currentFramework: 
         }
 
         if (node.type === 'tag') {
-            if (node.tag === 'if') {
-                if (node.annotations[0].value.parameters[0] === currentFramework) {
-                    return findFirstParagraph(node.children[0]);
-                }
+            if (node.tag === 'if' && node.annotations[0].value.name === 'isFramework') {
+                return findFirstParagraph(node.children[0]);
             }
 
             if (node.tag === 'videoSection') {
