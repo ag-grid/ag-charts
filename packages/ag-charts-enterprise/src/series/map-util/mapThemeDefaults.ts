9b1d0fbc962dc6434c84ff4dd7f26ec1ca26e898
Alan Treadway <alan.treadway@ag-grid.com>
2024-04-29 12:21:21 +0100

    CRT-332 - Disable Map zooming due to label rendering issues.

--- packages/ag-charts-enterprise/src/series/map-util/mapThemeDefaults.ts
+++ packages/ag-charts-enterprise/src/series/map-util/mapThemeDefaults.ts
@@ -1,12 +1,12 @@
 import type { _ModuleSupport } from 'ag-charts-community';
 
 export const MAP_THEME_DEFAULTS: _ModuleSupport.ExtensibleTheme<'map-shape' | 'map-line' | 'map-marker'> = {
-    zoom: {
-        axes: 'xy',
-        anchorPointX: 'pointer',
-        anchorPointY: 'pointer',
-        deceleration: 0.01,
-    },
+    // zoom: {
+    //     axes: 'xy',
+    //     anchorPointX: 'pointer',
+    //     anchorPointY: 'pointer',
+    //     deceleration: 0.01,
+    // },
     legend: {
         enabled: false,
     },
