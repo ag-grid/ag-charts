c20f10d43d4376ab9f918898183f15ff2d926d35
Oli Legat <olivier.legat@ag-grid.com>
2024-05-02 09:41:53 +0100

    Add setupMockConsole options

--- packages/ag-charts-community/src/util/test/mockConsole.ts
+++ packages/ag-charts-community/src/util/test/mockConsole.ts
@@ -2,8 +2,8 @@ import * as agChartsTest from 'ag-charts-test';
 
 import { doOnce } from '../../util/function';
 
-export function setupMockConsole() {
-    agChartsTest.setupMockConsole();
+export function setupMockConsole(opts?: { debugShowOutput?: boolean }) {
+    agChartsTest.setupMockConsole(opts?.debugShowOutput);
 
     afterEach(() => {
         doOnce.clear();
