6aa4feae60efb9d2747b2dbdcc7248f5490eab0c
Oli Legat <olivier.legat@ag-grid.com>
2024-05-02 11:08:06 +0100

    AG-11370 Try guess the delta when tabIndex > 0

--- packages/ag-charts-community/src/util/guardedElement.ts
+++ packages/ag-charts-community/src/util/guardedElement.ts
@@ -10,6 +10,8 @@ export class GuardedElement implements GuardedElementProperties {
     private guardTarget?: HTMLElement;
     private guardTabIndex: number = 0;
 
+    private guessedDelta?: -1 | 0 | 1;
+
     constructor(
         public readonly element: HTMLElement,
         public readonly topTabGuard: HTMLElement,
@@ -18,8 +20,8 @@ export class GuardedElement implements GuardedElementProperties {
         this.element.tabIndex = -1;
         this.initEventListener(this.element, 'blur', () => this.onBlur());
         this.initEventListener(this.element, 'focus', () => this.onFocus());
-        this.initEventListener(this.topTabGuard, 'focus', () => this.onTabStart(this.topTabGuard));
-        this.initEventListener(this.botTabGuard, 'focus', () => this.onTabStart(this.botTabGuard));
+        this.initEventListener(this.topTabGuard, 'focus', (ev) => this.onTabStart(ev, this.topTabGuard));
+        this.initEventListener(this.botTabGuard, 'focus', (ev) => this.onTabStart(ev, this.botTabGuard));
     }
 
     set tabIndex(index: number) {
@@ -28,9 +30,9 @@ export class GuardedElement implements GuardedElementProperties {
             this.guardTabIndex = index;
         }
 
-        // Strictly positive tab-indices breaks our ability to detect which direction the focus is coming
-        // from. So if it is the value that the user has specified, then disable the bottom tab-guard to
-        // treat TAB and Shift+TAB the same way.
+        // Strictly positive tab-indices breaks our ability to tab guards to detect which direction the focus
+        // is coming from. So if it is the value that the user has specified, then disable the bottom tab-guard
+        // to treat TAB and Shift+TAB the same way.
         if (index > 0) {
             this.topTabGuard.tabIndex = index;
             this.botTabGuard.style.display = 'none';
@@ -45,7 +47,11 @@ export class GuardedElement implements GuardedElementProperties {
         this.destroyFns.length = 0;
     }
 
-    private initEventListener(elem: HTMLElement, type: 'focus' | 'blur', handler: () => void) {
+    private initEventListener(
+        elem: HTMLElement,
+        type: 'focus' | 'blur',
+        handler: (() => void) | ((e: FocusEvent) => void)
+    ) {
         elem.addEventListener(type, handler);
         this.destroyFns.push(() => elem.removeEventListener(type, handler));
     }
@@ -54,6 +60,7 @@ export class GuardedElement implements GuardedElementProperties {
         if (this.element.tabIndex === -1) {
             this.tabIndex = this.guardTabIndex;
             this.guardTarget = undefined;
+            this.guessedDelta = undefined;
         }
     }
 
@@ -63,13 +70,26 @@ export class GuardedElement implements GuardedElementProperties {
         }
     }
 
-    private onTabStart(target: HTMLElement) {
+    private onTabStart(event: FocusEvent, target: HTMLElement) {
+        // If the userOptions specified `keyboard.tabIndex > 0`, then we can still try to guess the delta
+        // by comparing event.relatedTarget.tabIndex. This isn't guaranteed to work though, so fall back
+        // to delta = 0 if something isn't right.
+        if (target.tabIndex > 0) {
+            this.guessedDelta = 0; // fallback
+            if (event.relatedTarget != null && 'tabIndex' in event.relatedTarget) {
+                const prevTabIndex = Number(event.relatedTarget['tabIndex']);
+                if (!isNaN(prevTabIndex) && prevTabIndex > 0) {
+                    this.guessedDelta = prevTabIndex < target.tabIndex ? 1 : -1;
+                }
+            }
+        }
         this.guardTarget = target;
         this.element.focus();
     }
 
     public getBrowserFocusDelta(): -1 | 0 | 1 {
-        const { guardTarget, topTabGuard, botTabGuard } = this;
+        const { guessedDelta, guardTarget, topTabGuard, botTabGuard } = this;
+        if (guessedDelta !== undefined) return guessedDelta;
         if (guardTarget === topTabGuard) return 1;
         if (guardTarget === botTabGuard) return -1;
         return 0;
