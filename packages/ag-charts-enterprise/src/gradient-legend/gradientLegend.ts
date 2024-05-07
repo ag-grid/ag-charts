e2c88efeec0859c56e0846c65f93537a3c8890f9
Jacob  Parker <jacobparker1992@gmail.com>
2024-05-07 11:48:37 +0100

    AG-10678 Update gradient legend defaults

--- packages/ag-charts-enterprise/src/gradient-legend/gradientLegend.ts
+++ packages/ag-charts-enterprise/src/gradient-legend/gradientLegend.ts
@@ -263,6 +263,7 @@ export class GradientLegend {
     }
 
     private updateGradientRect(shrinkRect: _Scene.BBox, colorRange: string[]) {
+        const { reverseOrder, gradientFill, gradientRect } = this;
         const { preferredLength: gradientLength, thickness } = this.gradient;
 
         const gradientBox = new BBox(0, 0, 0, 0);
@@ -283,15 +284,17 @@ export class GradientLegend {
             gradientBox.height = thickness;
         }
 
-        if (this.reverseOrder) {
-            colorRange = colorRange.slice().reverse();
+        gradientFill.stops = colorRange;
+        if (vertical) {
+            gradientFill.direction = reverseOrder ? 'to-bottom' : 'to-top';
+        } else {
+            gradientFill.direction = reverseOrder ? 'to-left' : 'to-right';
         }
-        this.gradientFill.stops = colorRange;
-        this.gradientFill.direction = vertical ? 'to-bottom' : 'to-right';
-        this.gradientRect.x = gradientBox.x;
-        this.gradientRect.y = gradientBox.y;
-        this.gradientRect.width = gradientBox.width;
-        this.gradientRect.height = gradientBox.height;
+
+        gradientRect.x = gradientBox.x;
+        gradientRect.y = gradientBox.y;
+        gradientRect.width = gradientBox.width;
+        gradientRect.height = gradientBox.height;
 
         return gradientBox;
     }
@@ -299,10 +302,10 @@ export class GradientLegend {
     private updateAxis(data: _ModuleSupport.GradientLegendDatum, gradientBox: _Scene.BBox) {
         const { reverseOrder, axis } = this;
         const vertical = this.getOrientation() === 'vertical';
+        const positiveAxis = reverseOrder !== vertical;
 
         axis.position = vertical ? 'right' : 'bottom';
-
-        axis.colorDomain = reverseOrder ? data.colorDomain.slice().reverse() : data.colorDomain;
+        axis.colorDomain = positiveAxis ? data.colorDomain.slice().reverse() : data.colorDomain;
         axis.calculateDomain();
 
         axis.range = vertical ? [0, gradientBox.height] : [0, gradientBox.width];
