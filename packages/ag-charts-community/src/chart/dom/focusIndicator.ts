e0f139e1d35be534a74d68bc6aefa7fda994ca17
Oli Legat <olivier.legat@ag-grid.com>
2024-05-10 12:52:46 +0100

    AG-11385 Move Region.focusIndicator logic into separate class
    This is prep-work for https://github.com/ag-grid/ag-charts/pull/1557.
    
    The hidden proxy-buttons will need update the focusIndicator bounds. But
    we don't want to need up with a circular dependency where RegionManager
    depends on DOMManager and vice-versa. So separate this out.

---
+++ packages/ag-charts-community/src/chart/dom/focusIndicator.ts
@@ -0,0 +1,31 @@
+import type { DOMManager } from './domManager';
+import * as focusStyles from './focusStyles';
+
+export class FocusIndicator {
+    private readonly element: HTMLElement;
+
+    constructor(private readonly domManager: DOMManager) {
+        const { css, block, elements, modifiers } = focusStyles;
+        domManager.addStyles(block, css);
+        this.element = domManager.addChild('canvas-overlay', block);
+        this.element.classList.add(block, elements.indicator, modifiers.hidden);
+    }
+
+    public destroy() {
+        this.domManager.removeStyles(focusStyles.block);
+        this.domManager.removeChild('canvas-overlay', focusStyles.block);
+    }
+
+    public updateBBox(rect?: { x: number; y: number; width: number; height: number }) {
+        if (rect == null) {
+            this.element.classList.add(focusStyles.modifiers.hidden);
+            return;
+        }
+
+        this.element.classList.remove(focusStyles.modifiers.hidden);
+        this.element.style.width = `${rect.width}px`;
+        this.element.style.height = `${rect.height}px`;
+        this.element.style.left = `${rect.x}px`;
+        this.element.style.top = `${rect.y}px`;
+    }
+}
