8dccc1d2e1afdea2dc79f758a070f52217f8d4d8
Jacob  Parker <jacobparker1992@gmail.com>
2024-05-16 16:25:23 +0100

    Update sankey layout

--- packages/ag-charts-community/src/chart/series/sankey/sankeyLayout.ts
+++ packages/ag-charts-community/src/chart/series/sankey/sankeyLayout.ts
@@ -1,5 +1,5 @@
 import type { SankeyNodeDatum } from './sankeySeriesProperties';
-import type { Link, NodeGraphEntry } from './sankeyUtil';
+import type { Link, LinkedNode, NodeGraphEntry } from './sankeyUtil';
 
 type Column<L extends Link> = {
     nodes: NodeGraphEntry<SankeyNodeDatum, L>[];
@@ -7,16 +7,13 @@ type Column<L extends Link> = {
     readonly x: number;
 };
 
-interface LayoutProperties {
+interface Layout {
     seriesRectHeight: number;
     nodeSpacing: number;
     sizeScale: number;
 }
 
-function justifyNodesAcrossColumn(
-    { nodes, size }: Column<any>,
-    { seriesRectHeight, nodeSpacing, sizeScale }: LayoutProperties
-) {
+function justifyNodesAcrossColumn({ nodes, size }: Column<any>, { seriesRectHeight, nodeSpacing, sizeScale }: Layout) {
     const nodesHeight = seriesRectHeight * size * sizeScale;
     const outerPadding = (seriesRectHeight - (nodesHeight + nodeSpacing * (nodes.length - 1))) / 2;
     let y = outerPadding;
@@ -28,66 +25,104 @@ function justifyNodesAcrossColumn(
     });
 }
 
-function separateNodesInColumn(column: Column<any>, { seriesRectHeight, nodeSpacing }: LayoutProperties) {
-    column.nodes.sort((a, b) => a.datum.y - b.datum.y);
+function separateNodesInColumn(column: Column<any>, layout: Layout) {
+    const { nodes } = column;
+    const { seriesRectHeight, nodeSpacing } = layout;
+    nodes.sort((a, b) => Math.round(a.datum.y - b.datum.y * 100) / 100);
 
     let totalShift = 0;
-    let currentY = 0;
-    for (const { datum: node } of column.nodes) {
-        const shift = Math.max(currentY - node.y, 0);
+    let currentTop = 0;
+    for (let i = 0; i < nodes.length; i += 1) {
+        const { datum: node } = nodes[i];
+        const shift = Math.max(currentTop - node.y, 0);
 
         node.y += shift;
         totalShift += shift;
 
-        currentY = node.y + node.height + nodeSpacing;
+        currentTop = node.y + node.height + nodeSpacing;
     }
 
-    const averageShift = totalShift / column.nodes.length;
-    const lastNodeBottom = currentY - nodeSpacing;
+    const lastNodeBottom = currentTop - nodeSpacing;
 
-    const shiftBackwards = Math.max(
-        Math.min(averageShift / 2, column.nodes[0].datum.y),
-        // Exceeded height
-        lastNodeBottom - seriesRectHeight
-    );
-    if (shiftBackwards === 0) return totalShift !== 0;
+    if (lastNodeBottom < seriesRectHeight) {
+        return totalShift > 0;
+    }
 
-    for (const { datum: node } of column.nodes) {
-        node.y -= shiftBackwards;
+    let currentBottom = seriesRectHeight;
+    for (let i = nodes.length - 1; i >= 0; i -= 1) {
+        const { datum: node } = nodes[i];
+        const nodeBottom = node.y + node.height;
+
+        const shift = Math.min(currentBottom - nodeBottom, 0);
+
+        node.y += shift;
+        totalShift += shift;
+
+        currentBottom = node.y - nodeSpacing;
     }
 
     return true;
 }
 
-function layoutColumnPass(columns: Column<any>[], properties: LayoutProperties) {
-    let didShift = false;
-    for (const column of columns) {
-        column.nodes.forEach(({ datum: node, linksBefore }) => {
-            if (linksBefore.length === 0) return;
+function weightedNodeY(links: LinkedNode<SankeyNodeDatum, any>[]): number | undefined {
+    if (links.length === 0) return;
 
-            const totalMidPoints = linksBefore.reduce(
-                (acc, { node: { datum: other } }) => acc + other.y + other.height / 2,
-                0
-            );
-            const midPoint = totalMidPoints / linksBefore.length;
-            node.y = midPoint - node.height / 2;
-        });
-
-        didShift = separateNodesInColumn(column, properties) || didShift;
+    let totalYValues = 0;
+    let totalSize = 0;
+    for (const {
+        node: { datum: node },
+    } of links) {
+        totalYValues += node.y * node.size;
+        totalSize += node.size;
     }
 
+    return totalYValues / totalSize;
+}
+
+function layoutColumn(column: Column<any>, layout: Layout, weight: number, direction: -1 | 1) {
+    column.nodes.forEach(({ datum: node, linksBefore, linksAfter }) => {
+        const forwardLinks = direction === 1 ? linksBefore : linksAfter;
+        const backwardsLinks = direction === 1 ? linksAfter : linksBefore;
+
+        const nextY = weightedNodeY(forwardLinks);
+        if (nextY != null) {
+            const nodeWeight = backwardsLinks.length !== 0 ? weight : 1;
+            node.y = node.y + (nextY - node.y) * nodeWeight;
+        }
+    });
+
+    return separateNodesInColumn(column, layout);
+}
+
+function layoutColumnsForward(columns: Column<any>[], layout: Layout, weight: number) {
+    let didShift = false;
+    for (let i = 0; i < columns.length; i += 1) {
+        didShift = layoutColumn(columns[i], layout, weight, 1) || didShift;
+    }
     return didShift;
 }
 
-export function layoutColumns(columns: Column<any>[], properties: LayoutProperties) {
+function layoutColumnsBackwards(columns: Column<any>[], layout: Layout, weight: number) {
+    let didShift = false;
+    for (let i = columns.length - 1; i >= 0; i -= 1) {
+        didShift = layoutColumn(columns[i], layout, weight, -1) || didShift;
+    }
+    return didShift;
+}
+
+export function layoutColumns(columns: Column<any>[], layout: Layout) {
     columns.forEach((column) => {
-        justifyNodesAcrossColumn(column, properties);
+        justifyNodesAcrossColumn(column, layout);
     });
 
-    for (let i = 0; i < 1; i += 1) {
-        const didShift = layoutColumnPass(columns, properties);
+    for (let i = 0; i < 6; i += 1) {
+        let didShift = false;
+        didShift = layoutColumnsForward(columns, layout, 1) || didShift;
+        didShift = layoutColumnsBackwards(columns, layout, 0.5) || didShift;
         if (!didShift) {
             break;
         }
     }
+
+    layoutColumnsForward(columns, layout, 1);
 }
