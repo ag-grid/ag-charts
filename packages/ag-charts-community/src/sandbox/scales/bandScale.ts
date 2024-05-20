2ac4417e5aed305ce678be90d68fe6632172fa92
iMoses (Ido Moshe) <imoses.g@gmail.com>
2024-05-20 11:41:43 +0100

    wip

--- packages/ag-charts-community/src/sandbox/scales/bandScale.ts
+++ packages/ag-charts-community/src/sandbox/scales/bandScale.ts
@@ -28,7 +28,10 @@ export class BandScale extends BaseScale<DomainType, number> {
         const [r0, r1] = range;
         const step = this.step();
 
-        let start = Math.min(r0, r1) + (Math.abs(r1 - r0) - step * (domain.length - this.paddingInner)) * this.align;
+        let start =
+            domain.length === 1
+                ? Math.min(r0, r1) + Math.abs(r1 - r0) * this.paddingOuter
+                : Math.min(r0, r1) + (Math.abs(r1 - r0) - step * (domain.length - this.paddingInner)) * this.align;
 
         if (this.round) {
             start = Math.round(start);
@@ -51,6 +54,9 @@ export class BandScale extends BaseScale<DomainType, number> {
     }
 
     bandwidth() {
+        if (this.domain.length === 1) {
+            return this.step();
+        }
         const bandwidth = this.step() * (1 - this.paddingInner);
         return this.round ? Math.round(bandwidth) : bandwidth;
     }
@@ -59,6 +65,10 @@ export class BandScale extends BaseScale<DomainType, number> {
         const [r0, r1] = this.range;
         const count = this.domain.length;
         const { paddingInner, paddingOuter } = this;
+        if (count === 1) {
+            const step = Math.abs(r1 - r0) * (1 - paddingOuter * 2);
+            return this.round ? Math.round(step) : step;
+        }
         const step = Math.abs(r1 - r0) / Math.max(1, count - paddingInner + paddingOuter * 2);
         return this.round ? Math.floor(step) : step;
     }
