2b3a371e2a4846b58016c4d8b1debd042d19f342
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-05-20 14:42:46 +0100

    AG-3390 Remove framework selector from h1
    # Conflicts:
    #	packages/ag-charts-website/src/features/docs/components/Header.tsx

--- packages/ag-charts-website/src/features/docs/components/Header.tsx
+++ packages/ag-charts-website/src/features/docs/components/Header.tsx
@@ -1,4 +1,4 @@
-import type { Framework } from '@ag-grid-types';
+import type { Framework, MenuItem } from '@ag-grid-types';
 import { Icon } from '@ag-website-shared/components/icon/Icon';
 import { FrameworkSelectorInsideDocs } from '@components/framework-selector-inside-doc/FrameworkSelectorInsideDocs';
 import { getFrameworkDisplayText } from '@utils/framework';
@@ -13,34 +13,43 @@ interface Props {
     isEnterprise?: boolean;
     suppressFrameworkHeader?: boolean;
     path: string;
+    menuItems: MenuItem[];
 }
 
-export const Header: FunctionComponent<Props> = ({ title, framework, isEnterprise, suppressFrameworkHeader, path }) => {
+export const Header: FunctionComponent<Props> = ({
+    title,
+    framework,
+    isEnterprise,
+    suppressFrameworkHeader,
+    path,
+    menuItems,
+}) => {
     // Update framework store so it is in sync with the page
     // Done here, because it's run on all docs pages
     useSyncFrameworkStoreState(framework);
 
     return (
         <header className={styles.docsPageHeader}>
-            <h1 id="top" className={styles.docsPageTitle}>
-                <div className={styles.pageTitleContainer}>
-                    <div className={styles.pageTitleGroup}>
+            <div className={styles.pageTitleContainer}>
+                <div className={styles.pageTitleGroup}>
+                    <h1 id="top" className={styles.docsPageTitle}>
                         {!suppressFrameworkHeader && (
-                            <span className={styles.headerFramework}>{getFrameworkDisplayText(framework)} Charts</span>
+                            <span className={styles.headerFramework}>
+                                {`${getFrameworkDisplayText(framework)} Charts`}
+                            </span>
                         )}
                         <span>{title}</span>
-                    </div>
-
-                    <FrameworkSelectorInsideDocs path={path} currentFramework={framework} />
+                    </h1>
                 </div>
+                <FrameworkSelectorInsideDocs path={path} currentFramework={framework} menuItems={menuItems} />
+            </div>
 
-                {isEnterprise && (
-                    <span className={styles.enterpriseLabel}>
-                        Enterprise
-                        <Icon name="enterprise" />
-                    </span>
-                )}
-            </h1>
+            {isEnterprise && (
+                <span className={styles.enterpriseLabel}>
+                    Enterprise
+                    <Icon name="enterprise" />
+                </span>
+            )}
         </header>
     );
 };
