6f01857d4bd6b89cb0c3912fddaa2427bc5d7d8b
Jacob  Parker <jacobparker1992@gmail.com>
2024-05-29 12:58:06 +0100

    AG-4372 Line style docs, validation

---
+++ packages/ag-charts-website/src/content/docs/line-series/_examples/line-style/data.ts
@@ -0,0 +1,17 @@
+// Source: https://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/oxforddata.txt
+export function getData() {
+    return [
+        { month: 'January', min: 8.5, max: 2.6 },
+        { month: 'February', min: 10.4, max: 3.0 },
+        { month: 'March', min: 10.9, max: 4.7 },
+        { month: 'April', min: 13.7, max: 5.0 },
+        { month: 'May', min: 18.2, max: 8.4 },
+        { month: 'June', min: 23.6, max: 12.2 },
+        { month: 'July', min: 21.3, max: 13.0 },
+        { month: 'August', min: 21.9, max: 13.1 },
+        { month: 'September', min: 22.6, max: 13.2 },
+        { month: 'October', min: 17.0, max: 9.7 },
+        { month: 'November', min: 11.1, max: 4.9 },
+        { month: 'December', min: 10.2, max: 5.2 },
+    ];
+}
