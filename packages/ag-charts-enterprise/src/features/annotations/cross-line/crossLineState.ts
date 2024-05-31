128bc8c7427e2e0b464d3dc0e240de8a6502813c
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-05-31 09:36:53 +0100

    AG-9558 - Add crossline on axis click

--- packages/ag-charts-enterprise/src/features/annotations/cross-line/crossLineState.ts
+++ packages/ag-charts-enterprise/src/features/annotations/cross-line/crossLineState.ts
@@ -1,6 +1,6 @@
 import { _ModuleSupport, _Util } from 'ag-charts-community';
 
-import type { StateClickEvent, StateHoverEvent } from '../annotationTypes';
+import type { StateClickEvent } from '../annotationTypes';
 import { CrossLineAnnotation } from './crossLineProperties';
 import type { CrossLine } from './crossLineScene';
 
@@ -8,11 +8,11 @@ export function isHorizontalAxis(region: any) {
     return region === 'horizontal-axes';
 }
 
-export class CrossLineStateMachine extends _ModuleSupport.StateMachine<'start' | 'end', 'click' | 'hover'> {
+export class CrossLineStateMachine extends _ModuleSupport.StateMachine<'start', 'click'> {
     override debug = _Util.Debug.create(true, 'annotations');
 
     constructor(appendDatum: (datum: CrossLineAnnotation) => void) {
-        const onStartClick = ({ region, point }: StateClickEvent<CrossLineAnnotation, CrossLine>) => {
+        const onClick = ({ region, point }: StateClickEvent<CrossLineAnnotation, CrossLine>) => {
             const datum = new CrossLineAnnotation();
             const horizontalAxis = isHorizontalAxis(region);
             let direction = 'horizontal';
@@ -24,30 +24,11 @@ export class CrossLineStateMachine extends _ModuleSupport.StateMachine<'start' |
             appendDatum(datum);
         };
 
-        const onEndHover = ({ datum, node, point, region }: StateHoverEvent<CrossLineAnnotation, CrossLine>) => {
-            const horizontalAxis = isHorizontalAxis(region);
-            datum?.set({ value: horizontalAxis ? point.x : point.y });
-            node?.toggleHandles(false);
-        };
-
-        const onEndClick = ({ datum, node, point, region }: StateClickEvent<CrossLineAnnotation, CrossLine>) => {
-            const horizontalAxis = isHorizontalAxis(region);
-            datum?.set({ value: horizontalAxis ? point.x : point.y });
-            node?.toggleHandles(true);
-        };
-
         super('start', {
             start: {
                 click: {
-                    target: 'end',
-                    action: onStartClick,
-                },
-            },
-            end: {
-                hover: onEndHover,
-                click: {
                     target: '__parent',
-                    action: onEndClick,
+                    action: onClick,
                 },
             },
         });
