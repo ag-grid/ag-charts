9ac8ff6c25d7d9211a52423beaafec6667953171
Jacob  Parker <jacobparker1992@gmail.com>
2024-06-11 11:12:47 +0100

    AG-11853 Add separate react components for each chart type

--- packages/ag-charts-react/src/agChartsReact.ts
+++ packages/ag-charts-react/src/agChartsReact.ts
@@ -1,61 +1,97 @@
-import { type CSSProperties, Component, type RefObject, createElement, createRef } from 'react';
+import {
+    type CSSProperties,
+    RefObject,
+    createElement,
+    forwardRef,
+    useEffect,
+    useImperativeHandle,
+    useLayoutEffect,
+    useMemo,
+    useRef,
+} from 'react';
 
-import { type AgChartInstance, type AgChartOptions, AgCharts } from 'ag-charts-community';
+import {
+    type AgChartInstance,
+    type AgChartOptions,
+    AgCharts as AgChartsAPI,
+    AgFinancialChartOptions,
+} from 'ag-charts-community';
 
-export interface AgChartProps {
-    options: AgChartOptions;
-    onChartReady?: (chart: AgChartInstance) => void;
+interface BaseChartProps {
+    options: {};
     style?: CSSProperties;
     className?: string;
 }
 
-interface AgChartState {}
-
-export class AgChartsReact extends Component<AgChartProps, AgChartState> {
-    static propTypes: any;
-
-    public chart: AgChartInstance | undefined;
-
-    protected containerRef: RefObject<HTMLElement>;
-
-    constructor(public props: AgChartProps) {
-        super(props);
-        this.chart = undefined;
-        this.containerRef = createRef();
-    }
-
-    render() {
-        const { style, className } = this.props;
-        return createElement('div', {
-            style,
-            className,
-            ref: this.containerRef,
-        });
-    }
-
-    componentDidMount() {
-        const options = this.applyContainer(this.props.options);
-
-        const chart = AgCharts.create(options);
-        this.chart = chart;
-
-        chart.waitForUpdate().then(() => this.props.onChartReady?.(chart));
-    }
-
-    componentDidUpdate() {
-        if (this.chart != null) {
-            this.chart.update(this.applyContainer(this.props.options));
-        }
-    }
-
-    componentWillUnmount() {
-        if (this.chart != null) {
-            this.chart.destroy();
-            this.chart = undefined;
-        }
-    }
-
-    private applyContainer(propsOptions: any) {
-        return { ...propsOptions, container: this.containerRef.current };
-    }
+function getOptions(options: AgChartOptions, containerRef: RefObject<HTMLElement | null>): AgChartOptions {
+    return {
+        ...options,
+        container: containerRef.current,
+    };
 }
+
+function ChartWithConstructor<Props extends BaseChartProps>(
+    ctor: (options: Props['options']) => AgChartInstance,
+    displayName: string
+) {
+    const Component = forwardRef<AgChartInstance, Props>(function AgChartsReact(props, ref) {
+        const { options, style, className } = props;
+        const containerRef = useRef<HTMLDivElement>(null);
+        const chartRef = useRef<AgChartInstance | undefined>();
+
+        // This fires earlier than ideal - so has a negative impact on mounting performance
+        // but it's important we do this so refs work as expected
+        useLayoutEffect(() => {
+            const chart = ctor(getOptions(options, containerRef));
+            chartRef.current = chart;
+
+            return () => {
+                chart.destroy();
+            };
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, []);
+
+        // Avoid updating immediately after creating the chart
+        const unsafeIsInitialMount = chartRef.current === undefined;
+        useEffect(() => {
+            if (!unsafeIsInitialMount) {
+                void chartRef.current?.update(getOptions(options, containerRef));
+            }
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, [options]);
+
+        // Note useLayoutEffect is called before useImperativeHandle
+        useImperativeHandle(ref, () => chartRef.current!, []);
+
+        return useMemo(() => {
+            return createElement('div', {
+                ref: containerRef,
+                style: style,
+                className: className,
+            });
+        }, [style, className]);
+    });
+
+    Component.displayName = displayName;
+
+    return Component;
+}
+
+export interface AgChartProps {
+    options: AgChartOptions;
+    style?: CSSProperties;
+    className?: string;
+}
+
+export const AgCharts = ChartWithConstructor<AgChartProps>((options) => AgChartsAPI.create(options), 'AgCharts');
+
+export interface AgFinancialChartProps {
+    options: AgFinancialChartOptions;
+    style?: CSSProperties;
+    className?: string;
+}
+
+export const AgFinancialCharts = ChartWithConstructor<AgFinancialChartProps>(
+    (options) => AgChartsAPI.createFinancialChart(options),
+    'AgFinancialCharts'
+);
