db2492bcf2c01262af4534b44f2afcbe5861cf0c
Oli Legat <olivier.legat@ag-grid.com>
2024-06-12 15:31:27 +0100

    AG-11737 TC3 Add PreventableEvent.defaultPrevented
    This property is intended to match the web standard:
    https://developer.mozilla.org/en-US/docs/Web/API/Event/defaultPrevented
    
    Ignore events when defaultPrevented is true in zoom.ts. This fixes TC3.

--- packages/ag-charts-community/src/chart/interaction/preventableEvent.ts
+++ packages/ag-charts-community/src/chart/interaction/preventableEvent.ts
@@ -2,10 +2,24 @@ import type { Listeners } from '../../util/listeners';
 
 export type PreventableEvent = {
     preventDefault(): void;
+    get defaultPrevented(): boolean;
 };
 
-export function buildPreventable<T>(obj: T & { sourceEvent?: PreventableEvent }): T & PreventableEvent {
-    return { ...obj, preventDefault: () => obj.sourceEvent?.preventDefault() };
+export type Unpreventable<T extends PreventableEvent> = Omit<T, keyof PreventableEvent>;
+
+export function buildPreventable<T>(obj: T & { sourceEvent?: PreventableEvent }): typeof obj & PreventableEvent {
+    let _defaultPrevented = false;
+    const self: typeof obj & PreventableEvent = {
+        ...obj,
+        preventDefault() {
+            self.sourceEvent?.preventDefault();
+            _defaultPrevented = true;
+        },
+        get defaultPrevented() {
+            return _defaultPrevented;
+        },
+    };
+    return self;
 }
 
 export function dispatchTypedEvent<
