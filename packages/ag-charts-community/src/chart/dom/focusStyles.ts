c180de32d857a0482d7143dee7bcc980499702d5
Oli Legat <olivier.legat@ag-grid.com>
2024-06-13 15:05:06 +0100

    AG-11437 Remove unused CSS class ag-charts-focus__wrapper

--- packages/ag-charts-community/src/chart/dom/focusStyles.ts
+++ packages/ag-charts-community/src/chart/dom/focusStyles.ts
@@ -1,10 +1,4 @@
 export const block = 'ag-charts-focus';
-export const elements = {
-    wrapper: 'ag-charts-focus__wrapper',
-    indicator: 'ag-charts-focus__indicator',
-};
-export const modifiers = {
-    hidden: 'ag-charts-focus--hidden',
-};
-
+export const elements = { indicator: 'ag-charts-focus__indicator' };
+export const modifiers = { hidden: 'ag-charts-focus--hidden' };
 export { default as css } from './focusStyles.css';
