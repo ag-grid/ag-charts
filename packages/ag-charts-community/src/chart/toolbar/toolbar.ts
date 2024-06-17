fbf712800f162a38ea2b077200bc9fe3a0aac5f8
Oli Legat <olivier.legat@ag-grid.com>
2024-06-17 14:35:51 +0100

    AG-11509 Add ariaLabel to the public option contract
    The button label on its own is not guaranteed to be accessible. Therefore,
    provide an optional key to tell the screenreader how to announce the
    button label.

--- packages/ag-charts-community/src/chart/toolbar/toolbar.ts
+++ packages/ag-charts-community/src/chart/toolbar/toolbar.ts
@@ -4,6 +4,7 @@ import type { ModuleInstance } from '../../module/baseModule';
 import { BaseModuleInstance } from '../../module/module';
 import type { ModuleContext } from '../../module/moduleContext';
 import type { BBox } from '../../scene/bbox';
+import { setAttribute } from '../../sparklines-util';
 import { createElement } from '../../util/dom';
 import { initToolbarKeyNav, makeAccessibleClickListener } from '../../util/keynavUtil';
 import { ObserveChanges } from '../../util/proxy';
@@ -492,6 +493,7 @@ export class Toolbar extends BaseModuleInstance implements ModuleInstance {
         }
 
         button.innerHTML = inner;
+        setAttribute(button, 'aria-label', options.ariaLabel);
     }
 
     private onButtonPress(group: ToolbarGroup, value: any) {
