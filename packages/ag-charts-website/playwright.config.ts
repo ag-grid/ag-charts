123962c9381b653a4738a4c27aa7e74a86ac6fd5
Alan Treadway <alan.treadway@ag-grid.com>
2024-06-17 18:23:04 +0100

    Fix CI playwright execution.

--- packages/ag-charts-website/playwright.config.ts
+++ packages/ag-charts-website/playwright.config.ts
@@ -20,12 +20,18 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [['html'], ['line'], ['json', { outputFile: '../../reports/ag-charts-website-e2e.json' }]],
+    reporter: [
+        ['html', { open: process.env.CI ? 'on-failure' : 'never' }],
+        ['line'],
+        ['json', { outputFile: '../../reports/ag-charts-website-e2e.json' }],
+    ],
     outputDir: '../../reports/',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
         // baseURL: 'https://localhost:4601',
+
+        ignoreHTTPSErrors: true,
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -37,7 +43,6 @@ export default defineConfig({
             name: 'chromium',
             use: {
                 ...devices['Desktop Chrome'],
-                ignoreHTTPSErrors: true,
                 launchOptions: {
                     args: ['--ignore-certificate-errors'],
                 },
