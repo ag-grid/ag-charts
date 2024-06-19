df645f61ea5318518193c4c169400ff5614c2f3f
Jacob  Parker <jacobparker1992@gmail.com>
2024-06-19 12:46:20 +0100

    AG-10318 Add nore l10n tests

--- packages/ag-charts-locale/src/main.test.ts
+++ packages/ag-charts-locale/src/main.test.ts
@@ -1,9 +1,39 @@
+import { describe, expect, it } from '@jest/globals';
+
 import * as locales from './main';
 
-describe.skip('translations', () => {
-    const enKeys = Object.keys(locales.AG_CHARTS_LOCALE_EN_US);
+const { AG_CHARTS_LOCALE_EN_US } = locales;
+const enKeys = Object.keys(AG_CHARTS_LOCALE_EN_US).sort();
+const formatters = ['number', 'percent', 'date', 'time', 'datetime'];
+const formatterRegExp = /\[([^\]]*)\]/g;
+const variableRegExp = /(\$\{\w+\})/g;
 
-    it.each(Object.keys(locales))('locale %s has all translations defined', (locale) => {
-        expect(Object.keys((locales as any)[locale])).toEqual(enKeys);
+describe('translations', () => {
+    describe.each(Object.keys(locales))('%s', (locale) => {
+        const translations = (locales as any)[locale] as Record<string, string>;
+
+        it('has all translations defined', () => {
+            expect(Object.keys(translations).sort()).toEqual(enKeys);
+        });
+
+        describe.each(Object.keys(translations))('%s', (key) => {
+            const translation = translations[key];
+
+            it('has correct formatters', () => {
+                const formats = Array.from(translation.matchAll(formatterRegExp), (match) => match[1]);
+
+                expect(formatters).toEqual(expect.arrayContaining(formats));
+            });
+
+            it('has the same variables as en-US', () => {
+                const variables = Array.from(translation.matchAll(variableRegExp), (match) => match[1]).sort();
+                const enVariables = Array.from(
+                    AG_CHARTS_LOCALE_EN_US[key].matchAll(variableRegExp),
+                    (match) => match[1]
+                ).sort();
+
+                expect(variables).toEqual(enVariables);
+            });
+        });
     });
 });
