118a722676827321d43f630d3ea095b14cb4e502
Mark Durrant <mark.j.durrant@gmail.com>
2024-06-24 15:17:29 +0100

    AG-11837 - Add copy to clipboard for some code snippets

--- packages/ag-charts-website/src/components/Code.tsx
+++ packages/ag-charts-website/src/components/Code.tsx
@@ -1,30 +1,71 @@
+import { Icon } from '@ag-website-shared/components/icon/Icon';
 import classnames from 'classnames';
 import Prism from 'prismjs';
 import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-diff';
 import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-jsx';
 import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-shell-session';
 import 'prismjs/components/prism-sql';
 import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-xml-doc';
 import 'prismjs/plugins/keep-markup/prism-keep-markup';
 import 'prismjs/plugins/line-numbers/prism-line-numbers';
-import { memo, useEffect, useRef } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
+
+import styles from './Code.module.scss';
 
 const GrammarMap = {
     js: Prism.languages.javascript,
+    json: Prism.languages.json,
     ts: Prism.languages.typescript,
     css: Prism.languages.css,
     bash: Prism.languages.bash,
+    shell: Prism.languages.shellsession,
     html: Prism.languages.html,
     jsx: Prism.languages.jsx,
     java: Prism.languages.java,
     sql: Prism.languages.sql,
     diff: Prism.languages.diff,
     scss: Prism.languages.scss,
+    xml: Prism.languages.xml,
 };
 
 export type Language = keyof typeof GrammarMap;
+
+function CopyToClipboardButton({ code }: { code: string | string[] }) {
+    const [hasCopied, setHasCopied] = useState(false);
+
+    const copyToClipboard = async (code) => {
+        await navigator.clipboard.writeText(code);
+        await setHasCopied(true);
+        await setTimeout(() => {
+            setHasCopied(false);
+        }, 2000);
+    };
+
+    return (
+        <span
+            className={classnames(styles.clipboardButtonOuter, hasCopied ? styles.hasCopied : '')}
+            onClick={() => {
+                copyToClipboard(code);
+            }}
+        >
+            <span className={styles.clipboardButtonCopiedOuter}>
+                {hasCopied ? (
+                    <span className={styles.clipboardButtonCopied}>Copied</span>
+                ) : (
+                    <span className={styles.clipboardButtonCopied}>Copy</span>
+                )}
+            </span>
+            <span className={styles.clipboardButton}>
+                {hasCopied ? <Icon className={styles.check} name={'check'} /> : <Icon name={'copy'} />}
+            </span>
+        </span>
+    );
+}
 
 /**
  * This uses Prism to highlight a provided code snippet.
@@ -35,6 +76,7 @@ function Code({
     className,
     keepMarkup = false,
     lineNumbers = false,
+    copyToClipboard = true,
     ...props
 }: {
     code: string | string[];
@@ -42,6 +84,7 @@ function Code({
     className?: string;
     keepMarkup?: boolean;
     lineNumbers?: boolean;
+    copyToClipboard?: boolean;
 }) {
     if (Array.isArray(code)) {
         code = code.join('\n');
@@ -49,9 +92,17 @@ function Code({
 
     return (
         <pre
-            className={classnames('code', `language-${language}`, className, lineNumbers ? 'line-numbers' : null)}
+            className={classnames(
+                'code',
+                `language-${language}`,
+                className,
+                lineNumbers ? 'line-numbers' : null,
+                copyToClipboard ? 'copy-to-clipboard' : ''
+            )}
             {...props}
         >
+            {copyToClipboard && <CopyToClipboardButton code={code} />}
+
             {keepMarkup || lineNumbers ? (
                 <CodeWithPrismPlugins code={code} keepMarkup={keepMarkup} />
             ) : (
