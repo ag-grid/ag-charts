861f1aee36f549ad3ce6797e7766b9dc129d1589
iMoses (Ido Moshe) <imoses.g@gmail.com>
2024-06-24 15:54:38 +0100

    wip

--- packages/ag-charts-enterprise/src/series/candlestick/candlestickTypes.ts
+++ packages/ag-charts-enterprise/src/series/candlestick/candlestickTypes.ts
@@ -13,10 +13,6 @@ export interface CandlestickNodeDatum extends CandlestickNodeBaseDatum, Readonly
 export interface CandlestickNodeBaseDatum extends Omit<_ModuleSupport.CartesianSeriesNodeDatum, 'yKey' | 'yValue'> {
     readonly itemId: AgCandlestickSeriesItemType;
     readonly bandwidth: number;
-    readonly openKey?: string;
-    readonly closeKey?: string;
-    readonly highKey?: string;
-    readonly lowKey?: string;
     readonly openValue: number;
     readonly closeValue: number;
     readonly highValue?: number;
