61df37eec27e8f4af4262b146098aee41bf2ba4c
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-06-24 17:27:23 +0100

    AG-11136 - Add comment to follow up and re-enable translations tests

--- packages/ag-charts-locale/src/main.test.ts
+++ packages/ag-charts-locale/src/main.test.ts
@@ -8,6 +8,7 @@ const formatters = ['number', 'percent', 'date', 'time', 'datetime'];
 const formatterRegExp = /\[([^\]]*)\]/g;
 const variableRegExp = /(\$\{\w+\})/g;
 
+// TODO: re-enable once all translations are added (https://ag-grid.atlassian.net/browse/AG-11957)
 describe.skip('translations', () => {
     describe.each(Object.keys(locales))('%s', (locale) => {
         const translations = (locales as any)[locale] as Record<string, string>;
