f614dc2fe32f4e01bb011aa51127302e9d371f6b
AG-DavidG <121935568+AG-DavidG@users.noreply.github.com>
2024-06-24 07:51:21 +0000

    Styler docs first pass

---
+++ packages/ag-charts-website/src/content/docs/stylers/_examples/item-styler/main.ts
@@ -0,0 +1,81 @@
+import { AgChartOptions, AgCharts } from 'ag-charts-community';
+
+import { data } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: data,
+    title: {
+        text: 'UK Energy Sources',
+    },
+    footnote: {
+        text: 'Source: Department for Business, Energy & Industrial Strategy',
+    },
+    series: [
+        {
+            type: 'line',
+            xKey: 'month',
+            yKey: 'coal',
+            yName: 'Coal',
+            marker: {
+                itemStyler: (params) => {
+                    if (params.datum.coal > params.datum.nuclear) return { fill: 'red', size: 15 };
+                    else return { fill: params.fill, size: params.size };
+                },
+            },
+        },
+        {
+            type: 'line',
+            xKey: 'month',
+            yKey: 'bioenergyWaste',
+            yName: 'Bioenergy & waste',
+        },
+        {
+            type: 'line',
+            xKey: 'month',
+            yKey: 'nuclear',
+            yName: 'Nuclear',
+        },
+        {
+            type: 'line',
+            xKey: 'month',
+            yKey: 'windSolarHydro',
+            yName: 'Wind, solar & hydro',
+        },
+        {
+            type: 'bar',
+            xKey: 'month',
+            yKey: 'imported',
+            yName: 'Imported',
+            itemStyler: ({ datum, xKey, fill, highlighted }) => {
+                return {
+                    fill: datum[xKey] === 'Jul' ? (highlighted ? 'lime' : 'red') : fill,
+                };
+            },
+        },
+    ],
+    axes: [
+        {
+            type: 'category',
+            position: 'bottom',
+        },
+        {
+            type: 'number',
+            position: 'left',
+            gridLine: {
+                enabled: false,
+            },
+            label: {
+                format: '#{.1f}%',
+            },
+            title: {
+                text: 'Normalized Percentage Energy',
+            },
+        },
+    ],
+    legend: {
+        position: 'top',
+    },
+};
+
+AgCharts.create(options);
