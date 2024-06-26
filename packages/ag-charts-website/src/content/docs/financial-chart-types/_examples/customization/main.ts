5083aaaed0a474847a6254ac3b2e4ca3dbf5a44f
Alan Treadway <alan.treadway@ag-grid.com>
2024-06-26 20:35:26 +0100

    Cleanup debug options in example.

--- packages/ag-charts-website/src/content/docs/financial-chart-types/_examples/customization/main.ts
+++ packages/ag-charts-website/src/content/docs/financial-chart-types/_examples/customization/main.ts
@@ -2,8 +2,6 @@ import { AgCharts, AgFinancialChartOptions } from 'ag-charts-enterprise';
 
 import { getData } from './data';
 
-(window as any).agChartsDebug = 'opts';
-
 const options: AgFinancialChartOptions = {
     theme: {
         palette: {
