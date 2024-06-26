b879e611cf2a6ff001f17fd62e1f56dea12b3a0b
Alan Treadway <alan.treadway@ag-grid.com>
2024-06-26 10:46:42 +0100

    AG-11506 - Fix FC options types.

--- packages/ag-charts-types/src/chart/chartBuilderOptions.ts
+++ packages/ag-charts-types/src/chart/chartBuilderOptions.ts
@@ -42,8 +42,7 @@ export type AgBasePresetOptions = Pick<
     'data' | 'container' | 'width' | 'height' | 'minWidth' | 'minHeight' | 'theme' | 'title' | 'subtitle' | 'footnote'
 >;
 
-export type AgBaseFinancialPresetOptions = AgBasePresetOptions &
-    Pick<AgCartesianChartOptions, 'annotations' | 'initialState'>;
+export type AgBaseFinancialPresetOptions = AgBasePresetOptions & Pick<AgCartesianChartOptions, 'initialState'>;
 
 export type AgFinancialChartOptions = AgFinancialChartPresets & AgBaseFinancialPresetOptions;
 
