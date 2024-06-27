1cf10ff61734c6a57511045c3a5f6f55ae8f6840
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-06-27 13:50:06 +0100

    CRT-360 - Update crosshair examples to fix crosshair label clipping

--- packages/ag-charts-website/src/content/docs/axes-crosshairs/_examples/crosshair-default-label-custom-css/main.ts
+++ packages/ag-charts-website/src/content/docs/axes-crosshairs/_examples/crosshair-default-label-custom-css/main.ts
@@ -31,7 +31,7 @@ const options: AgCartesianChartOptions = {
             crosshair: {
                 label: {
                     className: 'custom-crosshair-label',
-                    xOffset: 60,
+                    xOffset: 50,
                 },
             },
         },
