9e1f9d3f422637f41065ea741f366b36ab3139a7
Laurence Roberts <laurence.roberts@ag-grid.com>
2024-06-28 10:58:41 +0100

    Revert "CRT-364 Fix context menu 'zoom to here' action when not clicking a node"
    This reverts commit 64d6b97c11b8f8824e5a0664f919531cbba00c6e.

--- packages/ag-charts-enterprise/src/features/zoom/zoomContextMenu.ts
+++ packages/ag-charts-enterprise/src/features/zoom/zoomContextMenu.ts
@@ -1,4 +1,4 @@
-import type { AgChartContextMenuEvent, _ModuleSupport, _Scene } from 'ag-charts-community';
+import type { AgNodeContextMenuActionEvent, _ModuleSupport, _Scene } from 'ag-charts-community';
 
 import type { DefinedZoomState, ZoomProperties } from './zoomTypes';
 import {
@@ -34,13 +34,13 @@ export class ZoomContextMenu {
 
         contextMenuRegistry.registerDefaultAction({
             id: CONTEXT_ZOOM_ACTION_ID,
-            type: 'all',
+            type: 'series',
             label: 'contextMenuZoomToCursor',
             action: (params) => this.onZoomToHere(params, props),
         });
         contextMenuRegistry.registerDefaultAction({
             id: CONTEXT_PAN_ACTION_ID,
-            type: 'all',
+            type: 'series',
             label: 'contextMenuPanToCursor',
             action: (params) => this.onPanToHere(params, props),
         });
@@ -64,7 +64,7 @@ export class ZoomContextMenu {
         }
     }
 
-    private onZoomToHere({ event }: AgChartContextMenuEvent, props: ZoomProperties) {
+    private onZoomToHere({ event }: AgNodeContextMenuActionEvent, props: ZoomProperties) {
         const { rect } = this;
         const { enabled, isScalingX, isScalingY, minRatioX, minRatioY } = props;
 
@@ -90,7 +90,7 @@ export class ZoomContextMenu {
         this.updateZoom(constrainZoom(newZoom));
     }
 
-    private onPanToHere({ event }: AgChartContextMenuEvent, props: ZoomProperties) {
+    private onPanToHere({ event }: AgNodeContextMenuActionEvent, props: ZoomProperties) {
         const { rect } = this;
         const { enabled } = props;
 
