992200b0656fcf46469954b86958fd303a137e1a
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-06-28 16:41:20 +0100

    AG-3390 Financial Charts Overview draft

---
+++ packages/ag-charts-website/src/content/docs/financial-charts/_examples/switching-chart-types/main.ts
@@ -0,0 +1,17 @@
+import { AgCharts, AgFinancialChartOptions } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+const options: AgFinancialChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+};
+
+const chart = AgCharts.createFinancialChart(options);
+
+function changeType(
+    type: 'candlestick' | 'hollow-candlestick' | 'ohlc' | 'line' | 'step-line' | 'area' | 'range-area'
+) {
+    options.chartType = type;
+    chart.update(options);
+}
