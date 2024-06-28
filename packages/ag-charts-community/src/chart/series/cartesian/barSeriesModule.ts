b9d347864296c22d9781360c2e4781b5f7bb085b
Alan Treadway <alan.treadway@ag-grid.com>
2024-06-28 14:28:36 +0100

    Revert "CRT-391: Bar series using nice time axis by default"
    This reverts commit 2055171a9c19698485bf792fa15e01673c09a340.

--- packages/ag-charts-community/src/chart/series/cartesian/barSeriesModule.ts
+++ packages/ag-charts-community/src/chart/series/cartesian/barSeriesModule.ts
@@ -27,11 +27,6 @@ export const BarSeriesModule: SeriesModule<'bar'> = {
     ],
     swapDefaultAxesCondition: (series) => series?.direction === 'horizontal',
     themeTemplate: {
-        axes: {
-            [CARTESIAN_AXIS_TYPE.TIME]: {
-                nice: false,
-            },
-        },
         series: {
             direction: 'vertical',
             fillOpacity: 1,
