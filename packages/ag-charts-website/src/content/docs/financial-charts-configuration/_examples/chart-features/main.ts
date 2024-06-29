aa62fedc6e645ced64758f82d47453fccc019060
rmc-software <rmc.software.consultancy@gmail.com>
2024-06-29 15:40:01 +0100

    AG-3390 - Financial Chart doc tweaks

--- packages/ag-charts-website/src/content/docs/financial-charts-configuration/_examples/chart-features/main.ts
+++ packages/ag-charts-website/src/content/docs/financial-charts-configuration/_examples/chart-features/main.ts
@@ -5,11 +5,18 @@ import { getData } from './data';
 const options: AgFinancialChartOptions = {
     container: document.getElementById('myChart'),
     data: getData(),
+
+    navigator: false, // disabled by default!
+    annotations: true,
+    rangeToolbar: true,
+    volume: true,
+    statusBar: true,
+    zoom: true,
 };
 
 const chart = AgCharts.createFinancialChart(options);
 
-function changeType(type: 'candlestick' | 'hollow-candlestick' | 'ohlc' | 'line' | 'step-line' | 'range-area') {
-    options.chartType = type;
+function toggleFeature(type: 'navigator' | 'annotations' | 'rangeToolbar' | 'volume' | 'statusBar' | 'zoom') {
+    options[type] = !options[type];
     chart.update(options);
 }
