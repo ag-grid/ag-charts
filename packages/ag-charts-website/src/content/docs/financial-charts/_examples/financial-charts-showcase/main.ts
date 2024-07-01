d72a13d26390be7a1b821c01c157e6dbff6553b4
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-07-01 15:55:25 +0100

    Update financial charts overview example initial state annotations

--- packages/ag-charts-website/src/content/docs/financial-charts/_examples/financial-charts-showcase/main.ts
+++ packages/ag-charts-website/src/content/docs/financial-charts/_examples/financial-charts-showcase/main.ts
@@ -10,38 +10,40 @@ const options: AgFinancialChartOptions = {
         annotations: [
             {
                 type: 'parallel-channel',
-                start: { x: { __type: 'date', value: new Date('2022-03-01').getTime() }, y: 160.15 },
-                end: { x: { __type: 'date', value: new Date('2024-06-28').getTime() }, y: 160.15 },
-                height: 12,
-                stroke: '#808080B0',
-                background: {
-                    fill: '#808080B0',
-                },
+                start: { x: { __type: 'date', value: 1698200000000 }, y: 148.0 },
+                end: { x: { __type: 'date', value: 1712900000000 }, y: 207.0 },
+                height: 14,
             },
             {
-                type: 'parallel-channel',
-                start: { x: { __type: 'date', value: new Date('2023-03-15').getTime() }, y: 160.0 },
-                end: { x: { __type: 'date', value: new Date('2023-07-21').getTime() }, y: 198.0 },
-                height: 8,
-                stroke: '#4075E0C4',
-                background: {
-                    fill: '#4075E0C4',
+                type: 'horizontal-line',
+                value: 111.0,
+                stroke: '#089981',
+                axisLabel: {
+                    fill: '#089981',
                 },
             },
             {
                 type: 'horizontal-line',
-                value: 180.0,
-                stroke: 'lightseagreen',
+                value: 125.0,
+                stroke: '#089981',
                 axisLabel: {
-                    fill: 'lightseagreen',
+                    fill: '#089981',
                 },
             },
             {
                 type: 'horizontal-line',
-                value: 195.0,
-                stroke: 'red',
+                value: 143.8,
+                stroke: '#F23645',
                 axisLabel: {
-                    fill: 'red',
+                    fill: '#F23645',
+                },
+            },
+            {
+                type: 'horizontal-line',
+                value: 200.8,
+                stroke: '#F23645',
+                axisLabel: {
+                    fill: '#F23645',
                 },
             },
         ],
