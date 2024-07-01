1bc4554eb12a8716c8ac7de5aae0aeb5b5417048
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-07-01 12:26:33 +0100

    Update data

--- packages/ag-charts-website/src/content/docs/financial-charts/_examples/financial-charts-showcase/main.ts
+++ packages/ag-charts-website/src/content/docs/financial-charts/_examples/financial-charts-showcase/main.ts
@@ -5,7 +5,7 @@ import { getData } from './data';
 const options: AgFinancialChartOptions = {
     container: document.getElementById('myChart'),
     data: getData(),
-    title: { text: 'FTSE: XYZ' },
+    title: { text: 'Acme Inc.' },
     initialState: {
         annotations: [
             {
