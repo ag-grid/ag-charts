1a819339e78895aa77536953472c38eeb7155af9
Mark Durrant <mark.j.durrant@gmail.com>
2024-07-02 10:13:40 +0100

    AG-12051 - Add dummy video component

---
+++ external/ag-website-shared/src/components/video/Video.tsx
@@ -0,0 +1,5 @@
+import styles from './Video.module.scss';
+
+export const Video = () => {
+    return <h1 className={styles.dummyVideoStyle}>I AM A VIDEO</h1>;
+};
---
+++ external/ag-website-shared/src/markdoc/tags/video.ts
@@ -0,0 +1,7 @@
+import { type Render, component } from '@astrojs/markdoc/config';
+import type { Config, Schema } from '@markdoc/markdoc';
+
+export const video: Schema<Config, Render> = {
+    render: component('../../external/ag-website-shared/src/components/video/Video.astro'),
+    attributes: {},
+};
