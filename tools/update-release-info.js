1bad142cb75fa77f393af4f6647cd715b511781a
Alan Treadway <alan.treadway@ag-grid.com>
2024-07-03 16:55:55 +0100

    Fix update-release-info.js to correctly bump RELEASE_INFORMATION.

--- tools/update-release-info.js
+++ tools/update-release-info.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 
 const filename = 'packages/ag-charts-enterprise/src/license/licenseManager.ts';
-const prefix = 'private static RELEASE_INFORMATION: string = ';
+const prefix = 'private static readonly RELEASE_INFORMATION: string = ';
 
 console.log(`Updating RELEASE_INFORMATION in ${filename}`);
 
 const newValue = btoa(String(new Date().getTime()));
 
+let updateCount = 0;
 const content = fs
     .readFileSync(filename)
     .toString()
@@ -14,6 +15,7 @@ const content = fs
     .map((line) => {
         if (line.trim().startsWith(prefix)) {
             const [start, end] = line.split(' = ');
+            updateCount++;
             return `${start} = '${newValue}';`;
         }
 
@@ -22,3 +24,8 @@ const content = fs
     .join('\n');
 
 fs.writeFileSync(filename, content);
+
+if (updateCount === 0) {
+    console.error('RELEASE_INFORMATION was not updated!');
+    process.exit(1);
+}
