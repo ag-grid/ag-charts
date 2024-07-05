1f5ba07195a0e87baca03ccc705be545f94d266a
Alan Treadway <alan.treadway@ag-grid.com>
2024-07-05 13:32:48 +0100

    Add dynamic E2E matrix calculation.

--- tools/test/calculate-shards.js
+++ tools/test/calculate-shards.js
@@ -4,16 +4,16 @@ const { hideBin } = require('yargs/helpers');
 const args = yargs(hideBin(process.argv))
     .command('eval <test-count>', 'calculate GHA shards', (yargs) => {
         yargs
-            .option('min', { number: true, default: 1 })
+            .option('min', { number: true, default: 0 })
             .option('max', { number: true, default: 10 })
             .option('ratio', { number: true, default: 100 })
             .positional('test-count', { alias: 'count', type: 'number', demandOption: true });
     })
     .parse();
 
-console.log({ count, min, max, ratio });
+const { count, min, max, ratio } = args;
 
-const shardCount = Math.max(Math.min(Math.floor(count / ratio), max), min);
+const shardCount = Math.max(Math.min(Math.ceil(count / ratio), max), min);
 const result = { shards: [] };
 for (let i = 1; i <= shardCount; i++) {
     result.shards.push(i);
