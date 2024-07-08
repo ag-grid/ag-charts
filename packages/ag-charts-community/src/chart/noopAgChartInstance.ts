3f47802b2575d2e61400eac28d5fe95e92aee713
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-07-08 16:52:27 +0100

    AG-12036 - include watermark in downloaded image

--- packages/ag-charts-community/src/chart/noopAgChartInstance.ts
+++ packages/ag-charts-community/src/chart/noopAgChartInstance.ts
@@ -1,37 +1,38 @@
-import type { AgChartInstance, AgChartOptions, AgChartState } from 'ag-charts-types';
+import type { AgChartOptions, AgChartState } from 'ag-charts-types';
 
-export class NoopAgChartInstance implements AgChartInstance {
+const errorMessage = 'AG Charts - Method not implemented.';
+export class NoopAgChartInstance {
     update(): Promise<void> {
-        throw new Error('AG Charts - AG Charts - Method not implemented.');
+        throw new Error(errorMessage);
     }
     updateDelta(): Promise<void> {
-        throw new Error('AG Charts - Method not implemented.');
+        throw new Error(errorMessage);
     }
     getOptions(): AgChartOptions {
-        throw new Error('AG Charts - Method not implemented.');
+        throw new Error(errorMessage);
     }
     waitForUpdate(): Promise<void> {
-        throw new Error('AG Charts - Method not implemented.');
+        throw new Error(errorMessage);
     }
     download(): Promise<void> {
-        throw new Error('AG Charts - Method not implemented.');
+        throw new Error(errorMessage);
     }
     resetAnimations(): void {
-        throw new Error('AG Charts - Method not implemented.');
+        throw new Error(errorMessage);
     }
     skipAnimations(): void {
-        throw new Error('AG Charts - Method not implemented.');
+        throw new Error(errorMessage);
     }
     getImageDataURL(): Promise<string> {
-        throw new Error('AG Charts - Method not implemented.');
+        throw new Error(errorMessage);
     }
     getState(): Required<AgChartState> {
-        throw new Error('AG Charts - Method not implemented.');
+        throw new Error(errorMessage);
     }
     setState(): Promise<void> {
-        throw new Error('AG Charts - Method not implemented.');
+        throw new Error(errorMessage);
     }
     destroy(): void {
-        throw new Error('AG Charts - Method not implemented.');
+        throw new Error(errorMessage);
     }
 }
