f4737ada384be4f23d7cf62f29e1135f1fc3a017
Alan Treadway <alan.treadway@ag-grid.com>
2024-07-19 12:02:08 +0100

    AG-11951 - Cleanup & fix sprite rendering corruption.

--- packages/ag-charts-community/src/scene/spriteRenderer.ts
+++ packages/ag-charts-community/src/scene/spriteRenderer.ts
@@ -4,12 +4,12 @@ import type { Node, RenderContext } from './node';
 type RenderSpriteOptions = { translateY: number };
 
 export class SpriteRenderer {
-    private readonly offCanvas: OffscreenCanvas;
+    private readonly offscreenCanvas: OffscreenCanvas;
     private readonly renderCtx: RenderContext;
 
     constructor() {
-        this.offCanvas = new OffscreenCanvas(1, 1);
-        const ctx = this.offCanvas.getContext('2d');
+        this.offscreenCanvas = new OffscreenCanvas(1, 1);
+        const ctx = this.offscreenCanvas.getContext('2d');
         if (ctx == null) throw new TypeError(`AG Charts - invalid 2d context`);
         this.renderCtx = {
             ctx,
@@ -24,22 +24,30 @@ export class SpriteRenderer {
         if (!(width > 0 && height > 0)) {
             throw new Error(`AG Charts - invalid dimensions: ${width}x${height}`);
         }
-        this.offCanvas.width = width;
-        this.offCanvas.height = height;
+        this.offscreenCanvas.width = width;
+        this.offscreenCanvas.height = height;
     }
 
-    renderSprite(nodes: Node | Iterable<Node>, opts?: RenderSpriteOptions): ImageBitmap {
+    renderSprite(nodes: Node | Iterable<Node>, opts?: RenderSpriteOptions) {
         nodes = toIterable(nodes);
-        const { renderCtx, offCanvas } = this;
+        const {
+            renderCtx,
+            renderCtx: { ctx },
+            offscreenCanvas,
+        } = this;
         const { translateY = 0 } = opts ?? {};
 
-        renderCtx.ctx.beginPath();
-        renderCtx.ctx.clearRect(0, 0, offCanvas.width, offCanvas.height);
-        renderCtx.ctx.setTransform(1, 0, 0, 1, 0, translateY);
+        ctx.resetTransform();
+        ctx.clearRect(0, 0, offscreenCanvas.width, offscreenCanvas.height);
+        ctx.save();
+        ctx.beginPath();
+        ctx.setTransform(1, 0, 0, 1, 0, translateY);
         for (const node of nodes) {
             node.render(renderCtx);
         }
+        ctx.closePath();
+        ctx.restore();
 
-        return offCanvas.transferToImageBitmap();
+        return offscreenCanvas.transferToImageBitmap();
     }
 }
