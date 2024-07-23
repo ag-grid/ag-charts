409386f1844bddb21d2717973e1fd797705dfe28
AG-DavidG <121935568+AG-DavidG@users.noreply.github.com>
2024-07-23 12:55:11 +0000

    git pu

--- packages/ag-charts-website/src/content/docs/financial-charts-toolbar/_examples/line-drawings/main.ts
+++ packages/ag-charts-website/src/content/docs/financial-charts-toolbar/_examples/line-drawings/main.ts
@@ -12,21 +12,105 @@ const options: AgFinancialChartOptions = {
         annotations: [
             {
                 type: 'line',
-                locked: true,
-                start: { x: { __type: 'date', value: '2024-03-21T18:55:00.000Z' }, y: 39838.38 },
-                end: { x: { __type: 'date', value: '2024-03-21T19:02:00.000Z' }, y: 39829.71 },
+                start: {
+                    x: {
+                        __type: 'date',
+                        value: 'Thu Mar 21 2024 18:30:00 GMT+0000 (Greenwich Mean Time)',
+                    },
+                    y: 39862.89027982327,
+                },
+                end: {
+                    x: {
+                        __type: 'date',
+                        value: 'Thu Mar 21 2024 18:38:00 GMT+0000 (Greenwich Mean Time)',
+                    },
+                    y: 39844.70176730486,
+                },
+
+                stroke: '#5090dc',
+                strokeOpacity: 1,
+                strokeWidth: 2,
             },
             {
-                type: 'line',
-                locked: true,
-                start: { x: { __type: 'date', value: '2024-03-21T19:02:00.000Z' }, y: 39830.63 },
-                end: { x: { __type: 'date', value: '2024-03-21T19:16:00.000Z' }, y: 39833.59 },
+                type: 'parallel-channel',
+                height: 13.282400589101599,
+                middle: {
+                    strokeWidth: 1,
+                    lineDash: [6, 5],
+                },
+                background: {
+                    fill: '#5090dc',
+                    fillOpacity: 0.2,
+                },
+                start: {
+                    x: {
+                        __type: 'date',
+                        value: 'Thu Mar 21 2024 18:45:00 GMT+0000 (Greenwich Mean Time)',
+                    },
+                    y: 39831.539027982326,
+                },
+                end: {
+                    x: {
+                        __type: 'date',
+                        value: 'Thu Mar 21 2024 18:59:00 GMT+0000 (Greenwich Mean Time)',
+                    },
+                    y: 39842.30854197349,
+                },
+
+                stroke: '#5090dc',
+                strokeOpacity: 1,
+                strokeWidth: 2,
             },
             {
-                type: 'line',
-                locked: true,
-                start: { x: { __type: 'date', value: '2024-03-21T19:16:00.000Z' }, y: 39835.38 },
-                end: { x: { __type: 'date', value: '2024-03-21T19:32:00.000Z' }, y: 39833.49 },
+                type: 'disjoint-channel',
+                startHeight: 16.872238586154708,
+                endHeight: 4.666789396178501,
+                background: {
+                    fill: '#5090dc',
+                    fillOpacity: 0.2,
+                },
+                start: {
+                    x: {
+                        __type: 'date',
+                        value: 'Thu Mar 21 2024 19:27:00 GMT+0000 (Greenwich Mean Time)',
+                    },
+                    y: 39851.402798232695,
+                },
+                end: {
+                    x: {
+                        __type: 'date',
+                        value: 'Thu Mar 21 2024 19:17:00 GMT+0000 (Greenwich Mean Time)',
+                    },
+                    y: 39845.30007363771,
+                },
+
+                stroke: '#5090dc',
+                strokeOpacity: 1,
+                strokeWidth: 2,
+            },
+            {
+                type: 'vertical-line',
+                value: {
+                    __type: 'date',
+                    value: 'Thu Mar 21 2024 19:08:00 GMT+0000 (Greenwich Mean Time)',
+                },
+
+                axisLabel: {
+                    enabled: true,
+                },
+                stroke: '#5090dc',
+                strokeOpacity: 1,
+                strokeWidth: 2,
+            },
+            {
+                type: 'horizontal-line',
+                value: 39863.48858615611,
+                axisLabel: {
+                    enabled: true,
+                },
+                stroke: '#5090dc',
+                strokeOpacity: 1,
+                strokeWidth: 2,
             },
         ],
     },
