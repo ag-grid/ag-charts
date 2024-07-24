e13b2e527a09ab92f3c653546e39235ba14b206d
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-07-24 13:13:48 +0100

    AG-12160 - Update callout

--- packages/ag-charts-enterprise/src/features/annotations/callout/calloutProperties.ts
+++ packages/ag-charts-enterprise/src/features/annotations/callout/calloutProperties.ts
@@ -1,11 +1,11 @@
 import { _ModuleSupport, _Scene } from 'ag-charts-community';
 
 import { AnnotationType } from '../annotationTypes';
-import { TextualProperties } from '../properties/textualProperties';
+import { TextualStartEndProperties } from '../properties/textualStartEndProperties';
 
 const { STRING, Validate, isObject } = _ModuleSupport;
 
-export class CalloutProperties extends TextualProperties {
+export class CalloutProperties extends TextualStartEndProperties {
     static is(value: unknown): value is CalloutProperties {
         return isObject(value) && value.type === AnnotationType.Callout;
     }
