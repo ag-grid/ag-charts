8ed480a264542cc93029e99bb36683a9f1dab369
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-07-24 13:41:58 +0100

    AG-12160 - Add fill and stroke properties to callout

--- packages/ag-charts-enterprise/src/features/annotations/callout/calloutProperties.ts
+++ packages/ag-charts-enterprise/src/features/annotations/callout/calloutProperties.ts
@@ -1,11 +1,12 @@
 import { _ModuleSupport, _Scene } from 'ag-charts-community';
 
+import { Fill, Stroke } from '../annotationProperties';
 import { AnnotationType } from '../annotationTypes';
 import { TextualStartEndProperties } from '../properties/textualStartEndProperties';
 
 const { STRING, Validate, isObject } = _ModuleSupport;
 
-export class CalloutProperties extends TextualStartEndProperties {
+export class CalloutProperties extends Fill(Stroke(TextualStartEndProperties)) {
     static is(value: unknown): value is CalloutProperties {
         return isObject(value) && value.type === AnnotationType.Callout;
     }
