a865fb3886803c174042582126598b15b02c8564
Oli Legat <olivier.legat@ag-grid.com>
2024-08-02 09:26:11 +0100

    AG-12369 Fix keynav using initMenuKeyNav and correct aria roles

--- packages/ag-charts-enterprise/src/features/popover/popover.ts
+++ packages/ag-charts-enterprise/src/features/popover/popover.ts
@@ -1,6 +1,6 @@
 import { type AgIconName, _ModuleSupport, _Scene, _Util } from 'ag-charts-community';
 
-const { createElement } = _ModuleSupport;
+const { createElement, initMenuKeyNav } = _ModuleSupport;
 
 const canvasOverlay = 'canvas-overlay';
 
@@ -13,7 +13,6 @@ export interface MenuItem<Value = any> {
 export class Popover extends _ModuleSupport.BaseModuleInstance implements _ModuleSupport.ModuleInstance {
     private readonly element: HTMLElement;
     private anchor?: { x: number; y: number } = undefined;
-    private removeGlobalEventListeners?: () => void = undefined;
 
     private readonly moduleId: string;
 
@@ -40,9 +39,8 @@ export class Popover extends _ModuleSupport.BaseModuleInstance implements _Modul
     }) {
         const { domManager } = this.ctx;
         const popover = createElement('div');
-        popover.setAttribute('data-pointer-capture', 'exclusive');
-        popover.setAttribute('tabindex', '0');
         popover.setAttribute('role', 'menu');
+        popover.setAttribute('data-pointer-capture', 'exclusive');
         popover.className = `ag-charts-popover`;
 
         if (opts.class != null) {
@@ -52,13 +50,12 @@ export class Popover extends _ModuleSupport.BaseModuleInstance implements _Modul
         const rows = opts.items.map((item) => {
             const active = item.value === opts.value;
             const row = createElement('div');
+            row.setAttribute('role', 'menuitem');
             if (typeof item.value === 'string') {
                 row.dataset.popoverId = item.value;
             }
             row.className = `ag-charts-popover__row`;
             row.classList.toggle(`ag-charts-popover__row--active`, active);
-            row.setAttribute('tabindex', '0');
-            popover.setAttribute('role', 'menuitem');
 
             if (item.icon != null) {
                 const icon = createElement('span');
@@ -101,62 +98,14 @@ export class Popover extends _ModuleSupport.BaseModuleInstance implements _Modul
             return row;
         });
 
-        // const popoverContainer = createElement('div');
-        // popoverContainer.role = 'presentation';
-        // popoverContainer.appendChild(popover);
-        // this.element.replaceChildren(popoverContainer);
+        initMenuKeyNav({ orientation: 'vertical', menu: popover, buttons: rows });
+
         this.element.replaceChildren(popover);
 
         // If an anchor has already been provided, apply it to prevent a flash of the picker in the wrong location
         if (this.anchor) {
             this.setAnchor(this.anchor);
         }
-
-        const focusIndexDelta = (delta: -1 | 1) => {
-            const { activeElement } = document;
-            const activeIndex = rows.findIndex((element) => element.contains(activeElement));
-            const nextIndex = activeIndex !== -1 ? Math.min(Math.max(activeIndex + delta, 0), rows.length - 1) : 0;
-            rows[nextIndex].focus();
-        };
-
-        popover.addEventListener('keydown', (e) => {
-            switch (e.key) {
-                case 'ArrowUp':
-                    focusIndexDelta(-1);
-                    break;
-                case 'ArrowDown':
-                    focusIndexDelta(1);
-                    break;
-                case 'Escape':
-                case 'Enter':
-                    opts.onClose();
-                    break;
-                default:
-                    return;
-            }
-
-            e.preventDefault();
-            e.stopPropagation();
-        });
-
-        const windowMouseMoveEvent = (e: MouseEvent) => {
-            if (popover.contains(e.target as any)) return;
-            popover.focus();
-        };
-        window.addEventListener('mousemove', windowMouseMoveEvent);
-
-        const windowMouseMoveDown = (e: MouseEvent) => {
-            if (e.button !== 0 || popover.contains(e.target as any)) return;
-
-            opts.onClose();
-        };
-        window.addEventListener('mousedown', windowMouseMoveDown);
-
-        this.removeGlobalEventListeners?.();
-        this.removeGlobalEventListeners = () => {
-            window.removeEventListener('mousemove', windowMouseMoveEvent);
-            window.removeEventListener('mousedown', windowMouseMoveDown);
-        };
 
         popover.focus();
     }
@@ -172,8 +121,6 @@ export class Popover extends _ModuleSupport.BaseModuleInstance implements _Modul
 
     hide() {
         this.element.replaceChildren();
-        this.removeGlobalEventListeners?.();
-        this.removeGlobalEventListeners = undefined;
     }
 
     isChildElement(element: HTMLElement) {
