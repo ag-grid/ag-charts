9481a68a506b095f1e25c07e7fedd06446e77242
Laurence Roberts <laurence.roberts@ag-grid.com>
2024-08-07 13:13:00 +0100

    Revert "AG-12417 Fix comment annotation radius"
    This reverts commit a8dc9fe30b97e6806d94d99fff2954ea82f33b5b.

--- packages/ag-charts-enterprise/src/features/annotations/comment/commentScene.ts
+++ packages/ag-charts-enterprise/src/features/annotations/comment/commentScene.ts
@@ -73,7 +73,8 @@ export class CommentScene extends TextualPointScene<CommentProperties> {
 
         const top = y - height;
         const right = x + width;
-        const cornerRadius = height / 2;
+
+        const cornerRadius = Math.min(20, Math.min(width, height) / 2);
 
         const { path } = this.shape;
         path.clear();
