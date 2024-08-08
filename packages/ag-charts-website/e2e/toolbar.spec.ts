25345d2502fbe513518116be8b08ccd5793b12c7
Laurence Roberts <laurence.roberts@ag-grid.com>
2024-08-08 11:15:55 +0100

    AG-12515 Add text annotation e2e tests and update line e2e tests

--- packages/ag-charts-website/e2e/toolbar.spec.ts
+++ packages/ag-charts-website/e2e/toolbar.spec.ts
@@ -1,29 +1,55 @@
 import { expect, test } from '@playwright/test';
 
-import { gotoExample, setupIntrinsicAssertions, toExamplePageUrls } from './util';
+import { gotoExample, setupIntrinsicAssertions, toExamplePageUrl } from './util';
 
 test.describe('toolbar', () => {
     setupIntrinsicAssertions();
 
-    const testUrls = toExamplePageUrls('financial-charts', 'financial-charts-showcase');
+    const { url } = toExamplePageUrl('financial-charts-test', 'e2e-toolbar', 'reactFunctionalTs');
 
-    for (const { framework, url } of testUrls) {
-        test.describe(`for ${framework}`, () => {
-            test('annotations', async ({ page }) => {
-                await gotoExample(page, url);
+    test('line', async ({ page }) => {
+        await gotoExample(page, url);
 
-                await page.locator('[data-toolbar-group="annotations"][data-toolbar-id="line-menu"]').click();
-                await page.locator('[data-popover-id="line"]').click();
-                await expect(page).toHaveScreenshot('line-annotation-1-button-active.png', { animations: 'disabled' });
+        await page.locator('[data-toolbar-group="annotations"][data-toolbar-id="line-menu"]').click();
+        await expect(page).toHaveScreenshot('line-1-popover.png', { animations: 'disabled' });
 
-                await page.hover('canvas', { position: { x: 100, y: 100 } });
-                await page.click('canvas', { position: { x: 100, y: 100 } });
-                await page.hover('canvas', { position: { x: 200, y: 200 } });
-                await expect(page).toHaveScreenshot('line-annotation-2-drawing.png', { animations: 'disabled' });
+        await page.locator('[data-popover-id="line"]').click();
+        await expect(page).toHaveScreenshot('line-2-button-active.png', { animations: 'disabled' });
 
-                await page.click('canvas', { position: { x: 200, y: 200 } });
-                await expect(page).toHaveScreenshot('line-annotation-3-complete.png', { animations: 'disabled' });
-            });
-        });
-    }
+        await page.hover('canvas', { position: { x: 100, y: 100 } });
+        await page.click('canvas', { position: { x: 100, y: 100 } });
+        await page.hover('canvas', { position: { x: 200, y: 200 } });
+        await expect(page).toHaveScreenshot('line-3-drawing.png', { animations: 'disabled' });
+
+        await page.click('canvas', { position: { x: 200, y: 200 } });
+        await expect(page).toHaveScreenshot('line-4-complete.png', { animations: 'disabled' });
+    });
+
+    test('text', async ({ page }) => {
+        await gotoExample(page, url);
+
+        await page.locator('[data-toolbar-group="annotations"][data-toolbar-id="text-menu"]').click();
+        await expect(page).toHaveScreenshot('text-1-popover.png', { animations: 'disabled' });
+
+        await page.locator('[data-popover-id="text"]').click();
+        await expect(page).toHaveScreenshot('text-2-button-active.png', { animations: 'disabled' });
+
+        await page.hover('canvas', { position: { x: 200, y: 200 } });
+        await page.click('canvas', { position: { x: 200, y: 200 } });
+        await expect(page).toHaveScreenshot('text-3-start.png', { animations: 'disabled' });
+
+        await page.keyboard.type('Hello, world!');
+        await expect(page).toHaveScreenshot('text-4-input.png', { animations: 'disabled' });
+
+        await page.keyboard.down('Enter');
+        await expect(page).toHaveScreenshot('text-5-save.png', { animations: 'disabled' });
+
+        await page.hover('canvas', { position: { x: 210, y: 190 } });
+        await page.click('canvas', { position: { x: 210, y: 190 } });
+        await page.locator('[data-toolbar-id="text-size"]').click();
+        await expect(page).toHaveScreenshot('text-6-font-size-popover.png', { animations: 'disabled' });
+
+        await page.locator('.ag-charts-popover__label', { hasText: '46' }).click();
+        await expect(page).toHaveScreenshot('text-7-change-font-size.png', { animations: 'disabled' });
+    });
 });
