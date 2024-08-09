c4ab10bbfa6c429503a1432573b03f5dd9324455
Oli Legat <olivier.legat@ag-grid.com>
2024-08-09 12:31:37 +0100

    AG-12369 Remove aria-hidden attributes
    According to w3.org, these are redundant because `display: none` is
    set when the CSS class is applied.
    
    See https://www.w3.org/TR/using-aria/Overview.html#4thrule

--- packages/ag-charts-community/src/chart/toolbar/toolbar.ts
+++ packages/ag-charts-community/src/chart/toolbar/toolbar.ts
@@ -4,7 +4,7 @@ import type { LayoutContext, ModuleInstance } from '../../module/baseModule';
 import { BaseModuleInstance } from '../../module/module';
 import type { ModuleContext } from '../../module/moduleContext';
 import { BBox } from '../../scene/bbox';
-import { setAttribute, setHidden } from '../../util/attributeUtil';
+import { setAttribute } from '../../util/attributeUtil';
 import { createElement } from '../../util/dom';
 import { initToolbarKeyNav, makeAccessibleClickListener } from '../../util/keynavUtil';
 import { clamp } from '../../util/number';
@@ -255,7 +255,7 @@ export class Toolbar extends BaseModuleInstance implements ModuleInstance {
         for (const button of this.groupButtons[group]) {
             if (button.dataset.toolbarId !== `${id}`) continue;
             button.ariaDisabled = `${!enabled}`;
-            setHidden(button, styles.modifiers.button.hiddenToggled, !visible);
+            button.classList.toggle(styles.modifiers.button.hiddenToggled, !visible);
             this.setButtonActive(button, active);
             this.setButtonChecked(button, checked);
         }
@@ -618,7 +618,7 @@ export class Toolbar extends BaseModuleInstance implements ModuleInstance {
 
         for (const position of TOOLBAR_POSITIONS) {
             const visible = this.enabled && Array.from(this.positions[position].values()).some(isGroupVisible);
-            setHidden(this.elements[position], styles.modifiers.hidden, !visible);
+            this.elements[position].classList.toggle(styles.modifiers.hidden, !visible);
         }
 
         for (const group of TOOLBAR_GROUPS) {
@@ -626,7 +626,7 @@ export class Toolbar extends BaseModuleInstance implements ModuleInstance {
             const groupVisible = isGroupVisible(group);
             for (const button of this.groupButtons[group]) {
                 const buttonVisible = groupVisible && this[group].buttonConfigurations().some(isButtonVisible(button));
-                setHidden(button, styles.modifiers.button.hiddenValue, !buttonVisible);
+                button.classList.toggle(styles.modifiers.button.hiddenValue, !buttonVisible);
             }
         }
     }
