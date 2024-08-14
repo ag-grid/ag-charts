dbb8ae72ac51cdce2c6d613ef71756984a4defb1
Laurence Roberts <laurence.roberts@ag-grid.com>
2024-08-14 15:39:55 +0100

    AG-11958 Remove annotations enabled toggle hack

--- packages/ag-charts-enterprise/src/features/annotations/annotations.ts
+++ packages/ag-charts-enterprise/src/features/annotations/annotations.ts
@@ -45,6 +45,7 @@ const {
     ChartUpdateType,
     Cursor,
     InteractionState,
+    ObserveChanges,
     PropertiesArray,
     ToolbarManager,
     Validate,
@@ -134,26 +135,19 @@ class AxesButtons {
 }
 
 export class Annotations extends _ModuleSupport.BaseModuleInstance implements _ModuleSupport.ModuleInstance {
-    // TODO: When the 'restore-annotations' event is triggered from `ActionsOnSet.newValue()`, the module is still
-    // disabled when `onRestoreAnnotations()` is called, preventing the state from being restored. However,
-    // when `ObserveChanges()` is first called `target.enabled === false`, rather than `undefined`. So
-    // there is no way to detect if the module was actively disabled. This flag simulates a combined
-    // behaviour of both and is toggled when the module is actively disabled and enabled.
-    private __hackWasDisabled = false;
-
-    @_ModuleSupport.ObserveChanges<Annotations, boolean>((target, enabled) => {
+    @ObserveChanges<Annotations>((target, newValue?: boolean, oldValue?: boolean) => {
         const {
             ctx: { annotationManager, stateManager, toolbarManager },
         } = target;
 
-        toolbarManager.toggleGroup('annotations', 'annotations', { visible: Boolean(enabled) });
+        if (newValue === oldValue) return;
+
+        toolbarManager.toggleGroup('annotations', 'annotations', { visible: Boolean(newValue) });
 
         // Restore the annotations only if this module was previously disabled
-        if (target.__hackWasDisabled && enabled) {
+        if (oldValue === false && newValue === true) {
             stateManager.restoreState(annotationManager);
-            target.__hackWasDisabled = false;
-        } else if (enabled === false) {
-            target.__hackWasDisabled = true;
+        } else if (newValue === false) {
             target.clear();
         }
     })
