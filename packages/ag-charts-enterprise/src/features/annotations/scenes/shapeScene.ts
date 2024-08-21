738965484fe8eeef9928f28fed909329aeaef0eb
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-08-21 14:51:33 +0100

    AG-12068 - Move shape related stuff to shapeScene

--- packages/ag-charts-enterprise/src/features/annotations/scenes/shapeScene.ts
+++ packages/ag-charts-enterprise/src/features/annotations/scenes/shapeScene.ts
@@ -1,6 +1,7 @@
 import { _Scene, _Util } from 'ag-charts-community';
 
-import { AnnotationType } from '../annotationTypes';
+import { type AnnotationContext, AnnotationType } from '../annotationTypes';
+import { convertPoint } from '../annotationUtils';
 import type { ShapePointProperties } from '../properties/shapePointProperties';
 import { PointScene } from './pointScene';
 
@@ -14,18 +15,33 @@ export abstract class ShapeScene<Datum extends ShapePointProperties> extends Poi
         this.append([this.handle]);
     }
 
-    protected override updateShape(datum: Datum, point: _Util.Vec2) {
-        const { shape } = this;
+    override update(datum: Datum, context: AnnotationContext) {
+        super.update(datum, context);
 
-        shape.x = point.x;
-        shape.y = point.y;
-        shape.size = datum.size;
+        const coords = convertPoint(datum, context);
+        this.updateShape(datum, coords);
+    }
+
+    private updateShape(datum: Datum, point: _Util.Vec2) {
+        this.updateShapeStyles(datum);
+        this.updateShapePath(datum, point);
+    }
+
+    protected updateShapeStyles(datum: Datum) {
+        const { shape } = this;
 
         shape.fill = datum.fill;
         shape.fillOpacity = datum.fillOpacity ?? 1;
         shape.stroke = datum.stroke;
         shape.strokeWidth = datum.strokeWidth ?? 1;
         shape.strokeOpacity = datum.strokeOpacity ?? 1;
+    }
+
+    protected updateShapePath(datum: Datum, point: _Util.Vec2) {
+        const { shape } = this;
+        shape.x = point.x;
+        shape.y = point.y;
+        shape.size = datum.size;
     }
 
     override containsPoint(x: number, y: number) {
