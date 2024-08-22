a25259653a4731e34c6601cb4fafb61b76499d29
Tak Tran <contact@tutaktran.com>
2024-08-22 12:48:48 +0100

    AG-12635 - Move robots disallow to json endpoint

--- packages/ag-charts-website/src/pages/robots-disallow.json.ts
+++ packages/ag-charts-website/src/pages/robots-disallow.json.ts
@@ -1,21 +1,13 @@
 import { getSitemapIgnorePaths } from '@utils/sitemapPages';
 import type { APIContext } from 'astro';
 
-const productionRobotsTxt = (disallowPaths: string[] = []) =>
-    disallowPaths
-        .map((path) => {
-            return `Disallow: ${path}`;
-        })
-        .join('\n');
-
 /**
  * Generate robots disallow list, for root URL to consume
  */
 export async function GET(_context: APIContext) {
     const ignorePaths = await getSitemapIgnorePaths();
-    const output = productionRobotsTxt(ignorePaths);
 
-    return new Response(output, {
+    return new Response(JSON.stringify(ignorePaths), {
         status: 200,
         headers: {
             'Content-Type': 'text/plain',
