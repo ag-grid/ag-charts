61d29f7f0ea198b3c4087ea0ed96d4776ec57374
Alan Treadway <alan.treadway@ag-grid.com>
2024-08-22 14:09:54 +0100

    AG-12646 - Improve e2e worker utilisation.

--- packages/ag-charts-website/playwright.config.ts
+++ packages/ag-charts-website/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 1 : 0,
     /* Limit parallel tests on CI. */
-    workers: process.env.CI ? 3 : undefined,
+    workers: process.env.CI ? 2 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
         [
