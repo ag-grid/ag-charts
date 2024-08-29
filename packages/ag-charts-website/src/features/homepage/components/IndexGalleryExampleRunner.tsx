45923da429318cd301a42b8304dc47eca8be4181
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-08-29 16:08:12 +0100

    Create unique component

---
+++ packages/ag-charts-website/src/features/homepage/components/IndexGalleryExampleRunner.tsx
@@ -0,0 +1,162 @@
+import { ExampleRunner } from '@features/example-runner/components/ExampleRunner';
+import { ExternalLinks } from '@features/example-runner/components/ExternalLinks';
+import { useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+
+import { GALLERY_EXAMPLE_TYPE, GALLERY_INTERNAL_FRAMEWORK } from '../constants';
+import {
+    getExampleCodeSandboxUrl,
+    getExampleContentsUrl,
+    getExamplePlunkrUrl,
+    getExampleRunnerExampleUrl,
+    getExampleUrl,
+} from '../utils/urlPaths';
+
+interface Props {
+    title: string;
+    exampleName: string;
+    loadingIFrameId: string;
+}
+
+// NOTE: Not on the layout level, as that is generated at build time, and queryClient needs to be
+// loaded on the client side
+const queryClient = new QueryClient();
+
+const queryOptions = {
+    retry: false,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+};
+
+const GalleryExampleRunnerInner = ({ title, exampleName, loadingIFrameId }: Props) => {
+    const [initialSelectedFile, setInitialSelectedFile] = useState();
+    const [exampleUrl, setExampleUrl] = useState<string>();
+    const [exampleRunnerExampleUrl, setExampleRunnerExampleUrl] = useState<string>();
+    const [codeSandboxHtmlUrl, setCodeSandboxHtmlUrl] = useState<string>();
+    const [plunkrHtmlUrl, setPlunkrHtmlUrl] = useState<string>();
+    const [exampleFiles, setExampleFiles] = useState();
+    const [exampleBoilerPlateFiles, setExampleBoilerPlateFiles] = useState();
+    const [packageJson, setPackageJson] = useState();
+
+    const internalFramework = GALLERY_INTERNAL_FRAMEWORK;
+    const exampleType = GALLERY_EXAMPLE_TYPE;
+    const id = `example-${exampleName}`;
+
+    const {
+        isLoading: exampleFilesIsLoading,
+        isError: exampleFilesIsError,
+        data: [contents, exampleFileHtml] = [],
+    } = useQuery(
+        ['galleryExampleFiles', exampleName],
+        () => {
+            const getContents = fetch(
+                getExampleContentsUrl({
+                    exampleName,
+                })
+            ).then((res) => res.json());
+
+            const getExampleFileHtml = fetch(
+                getExampleUrl({
+                    exampleName,
+                })
+            ).then((res) => res.text());
+
+            return Promise.all([getContents, getExampleFileHtml]);
+        },
+        queryOptions
+    );
+
+    useEffect(() => {
+        if (!exampleName) {
+            return;
+        }
+
+        setExampleUrl(
+            getExampleUrl({
+                exampleName,
+            })
+        );
+
+        setExampleRunnerExampleUrl(
+            getExampleRunnerExampleUrl({
+                exampleName,
+            })
+        );
+    }, [exampleName]);
+
+    useEffect(() => {
+        if (!contents || exampleFilesIsLoading || exampleFilesIsError) {
+            return;
+        }
+        setInitialSelectedFile(contents?.mainFileName);
+    }, [contents, exampleFilesIsLoading, exampleFilesIsError]);
+
+    useEffect(() => {
+        setCodeSandboxHtmlUrl(
+            getExampleCodeSandboxUrl({
+                exampleName,
+            })
+        );
+
+        setPlunkrHtmlUrl(
+            getExamplePlunkrUrl({
+                exampleName,
+            })
+        );
+    }, [exampleName]);
+
+    useEffect(() => {
+        if (!contents || exampleFilesIsLoading || exampleFilesIsError || !exampleFileHtml) {
+            return;
+        }
+        const files = {
+            ...contents.files,
+            // Override `index.html` with generated file as
+            // exampleFiles endpoint only gets the index html fragment
+            'index.html': exampleFileHtml,
+        };
+
+        setExampleFiles(files);
+        setPackageJson(contents.packageJson);
+        setExampleBoilerPlateFiles(contents.boilerPlateFiles);
+    }, [contents, exampleFilesIsLoading, exampleFilesIsError, exampleFileHtml]);
+
+    const externalLinks = (
+        <ExternalLinks
+            title={title}
+            internalFramework={internalFramework}
+            exampleFiles={exampleFiles}
+            exampleBoilerPlateFiles={exampleBoilerPlateFiles}
+            packageJson={packageJson}
+            initialSelectedFile={initialSelectedFile}
+            plunkrHtmlUrl={plunkrHtmlUrl}
+            codeSandboxHtmlUrl={codeSandboxHtmlUrl}
+        />
+    );
+
+    return (
+        <ExampleRunner
+            id={id}
+            title={title}
+            exampleUrl={exampleUrl}
+            exampleRunnerExampleUrl={exampleRunnerExampleUrl}
+            exampleType={exampleType}
+            exampleFiles={exampleFiles}
+            initialSelectedFile={initialSelectedFile}
+            internalFramework={internalFramework}
+            externalLinks={externalLinks}
+            hideInternalFrameworkSelection={true}
+            exampleHeight={620}
+            loadingIFrameId={loadingIFrameId}
+        />
+    );
+};
+
+export const IndexGalleryExampleRunner = (props: Props) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <GalleryExampleRunnerInner {...props} />
+        </QueryClientProvider>
+    );
+};
