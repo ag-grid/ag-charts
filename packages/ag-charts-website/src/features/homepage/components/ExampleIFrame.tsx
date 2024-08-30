c0d0fe6106071790aff55cd473fb4f570949699e
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-08-30 10:16:35 +0100

    Hack the docs examples to work

---
+++ packages/ag-charts-website/src/features/homepage/components/ExampleIFrame.tsx
@@ -0,0 +1,53 @@
+import { useIntersectionObserver } from '@utils/hooks/useIntersectionObserver';
+import classnames from 'classnames';
+import { type FunctionComponent, useEffect, useRef, useState } from 'react';
+
+import styles from './ExampleIFrame.module.scss';
+
+interface Props {
+    title: string;
+    isHidden?: boolean;
+    url?: string;
+    loadingIFrameId: string;
+}
+
+export const ExampleIFrame: FunctionComponent<Props> = ({ title, isHidden, url, loadingIFrameId }) => {
+    const [isIntersecting, setIsIntersecting] = useState(false);
+    const iFrameRef = useRef<HTMLIFrameElement>(null);
+
+    // Only show example iFrame if it is visible on the screen
+    useIntersectionObserver({
+        elementRef: iFrameRef,
+        onChange: ({ isIntersecting: newIsIntersecting }) => {
+            if (url != null && newIsIntersecting && iFrameRef.current && !iFrameRef.current.src) {
+                iFrameRef.current.src = url;
+            }
+            setIsIntersecting(newIsIntersecting);
+        },
+    });
+
+    useEffect(() => {
+        const currentSrc = iFrameRef.current?.src && new URL(iFrameRef.current.src);
+        if (!isIntersecting || !url || !iFrameRef.current || (currentSrc as URL)?.pathname === url) {
+            return;
+        }
+
+        iFrameRef.current.src = url;
+    }, [isIntersecting, url]);
+
+    return (
+        <div
+            className={classnames(styles.container, {
+                [styles.hidden]: isHidden,
+            })}
+        >
+            {/*`exampleRunner` class is used by the dark mode toggle to post a message to this iFrame*/}
+            <iframe
+                id={loadingIFrameId}
+                title={title}
+                ref={iFrameRef}
+                className={classnames('exampleRunner', styles.iframe)}
+            />
+        </div>
+    );
+};
---
+++ packages/ag-charts-website/src/features/homepage/components/ExampleRunner.tsx
@@ -0,0 +1,106 @@
+import type { InternalFramework } from '@ag-grid-types';
+import { Icon } from '@ag-website-shared/components/icon/Icon';
+import { OpenInCTA } from '@ag-website-shared/components/open-in-cta/OpenInCTA';
+import type { ExampleType, FileContents } from '@features/example-generator/types';
+import classnames from 'classnames';
+import { type FunctionComponent, type ReactElement, useState } from 'react';
+
+import { CodeViewer } from './CodeViewer';
+import { ExampleIFrame } from './ExampleIFrame';
+import styles from './ExampleRunner.module.scss';
+// Charts specific example runner styles
+import chartsStyles from './LegacyExampleRunner.module.scss';
+
+interface Props {
+    id: string;
+    title: string;
+    exampleUrl?: string;
+    exampleRunnerExampleUrl?: string;
+    exampleType?: ExampleType;
+    initialShowCode?: boolean;
+    externalLinks?: ReactElement;
+    exampleHeight?: number;
+    exampleWidth?: number;
+    exampleFiles?: FileContents;
+    initialSelectedFile?: string;
+    internalFramework: InternalFramework;
+    hideInternalFrameworkSelection?: boolean;
+    loadingIFrameId: string;
+}
+
+const DEFAULT_HEIGHT = 500;
+
+export const ExampleRunner: FunctionComponent<Props> = ({
+    id,
+    title,
+    exampleUrl,
+    exampleRunnerExampleUrl,
+    exampleType,
+    initialShowCode,
+    externalLinks,
+    exampleHeight = DEFAULT_HEIGHT,
+    exampleFiles,
+    initialSelectedFile,
+    internalFramework,
+    hideInternalFrameworkSelection,
+    loadingIFrameId,
+}) => {
+    const [showCode, setShowCode] = useState(initialShowCode);
+
+    return (
+        <div id={id} className={styles.exampleOuter}>
+            <div className={styles.tabsContainer}>
+                <div
+                    className={classnames(chartsStyles.content, styles.content)}
+                    role="tabpanel"
+                    aria-labelledby={`${showCode ? 'Preview' : 'Code'} tab`}
+                    style={{ height: exampleHeight }}
+                >
+                    <ExampleIFrame
+                        title={title}
+                        isHidden={showCode}
+                        url={exampleRunnerExampleUrl!}
+                        loadingIFrameId={loadingIFrameId}
+                    />
+                    {exampleFiles && (
+                        <CodeViewer
+                            id={id}
+                            isActive={showCode}
+                            files={exampleFiles}
+                            initialSelectedFile={initialSelectedFile!}
+                            exampleType={exampleType!}
+                            internalFramework={internalFramework}
+                            hideInternalFrameworkSelection={hideInternalFrameworkSelection}
+                        />
+                    )}
+                </div>
+                <footer className={styles.footer}>
+                    <button
+                        className={classnames(styles.previewCodeToggle, 'button-secondary')}
+                        onClick={(e) => {
+                            setShowCode(!showCode);
+                        }}
+                    >
+                        {showCode && (
+                            <span>
+                                <Icon name="eye" /> Preview
+                            </span>
+                        )}
+                        {!showCode && (
+                            <span>
+                                <Icon name="code" /> Code
+                            </span>
+                        )}
+                    </button>
+
+                    <ul className={classnames('list-style-none', styles.externalLinks)}>
+                        <li>
+                            <OpenInCTA type="newTab" href={exampleUrl!} />
+                        </li>
+                        {externalLinks}
+                    </ul>
+                </footer>
+            </div>
+        </div>
+    );
+};
