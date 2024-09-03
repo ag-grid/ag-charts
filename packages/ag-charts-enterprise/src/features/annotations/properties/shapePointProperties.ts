59959934c0132ed34b33848ff3062c4ec769c1ed
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-09-03 17:04:41 +0100

    AG-12068 -Remove stroke options and size from theme and options API. Add fill options.

--- packages/ag-charts-enterprise/src/features/annotations/properties/shapePointProperties.ts
+++ packages/ag-charts-enterprise/src/features/annotations/properties/shapePointProperties.ts
@@ -1,37 +1,21 @@
 import { _ModuleSupport } from 'ag-charts-community';
 
-import { Fill, Stroke } from '../annotationProperties';
+import { Fill } from '../annotationProperties';
 import { type AnnotationOptionsColorPickerType } from '../annotationTypes';
 import { PointProperties } from './pointProperties';
 
-const { Validate, POSITIVE_NUMBER } = _ModuleSupport;
-
-export class ShapePointProperties extends Fill(Stroke(PointProperties)) {
+export class ShapePointProperties extends Fill(PointProperties) {
     static is(value: unknown): value is ShapePointProperties {
         return value instanceof ShapePointProperties;
     }
 
-    @Validate(POSITIVE_NUMBER)
     size: number = 32;
 
     override getDefaultColor(colorPickerType: AnnotationOptionsColorPickerType) {
-        switch (colorPickerType) {
-            case `fill-color`:
-                return this.fill;
-            case `line-color`:
-            default:
-                return this.stroke;
-        }
+        return colorPickerType === `fill-color` ? this.fill : undefined;
     }
 
     override getDefaultOpacity(colorPickerType: AnnotationOptionsColorPickerType) {
-        switch (colorPickerType) {
-            case `fill-color`:
-                return this.fillOpacity;
-            case `line-color`:
-                return this.strokeOpacity;
-            default:
-                return undefined;
-        }
+        return colorPickerType === `fill-color` ? this.fillOpacity : undefined;
     }
 }
