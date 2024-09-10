7238ba6da32ba167c1ac033366699369f74afbba
Kyler Phillips <kylerphillips@hotmail.co.uk>
2024-09-10 12:48:46 +0100

    AG-12805 Implement new charts hero scrolling carousel

---
+++ packages/ag-charts-website/src/features/homepage/components/hero/ImageCarousel.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useRef } from 'react';
+
+import './ImageCarousel.scss';
+
+const ImageCarousel = () => {
+    const carouselRef = useRef(null);
+
+    useEffect(() => {
+        const carousel = carouselRef.current;
+        let animationId;
+
+        const animate = () => {
+            if (carousel.scrollTop >= carousel.scrollHeight / 2) {
+                carousel.scrollTop = 0;
+            } else {
+                carousel.scrollTop += 1;
+            }
+            animationId = requestAnimationFrame(animate);
+        };
+
+        animationId = requestAnimationFrame(animate);
+
+        return () => cancelAnimationFrame(animationId);
+    }, []);
+
+    const images = [
+        'charts/images/scroller-1.png',
+        'charts/images/scroller-2.png',
+        'charts/images/scroller-3.png',
+        'charts/images/scroller-4.png',
+        'charts/images/scroller-5.png',
+        'charts/images/scroller-6.png',
+        'charts/images/scroller-7.png',
+        'charts/images/scroller-8.png',
+    ];
+
+    const createImagePairs = (imgs) => {
+        const pairs = [];
+        for (let i = 0; i < imgs.length; i += 2) {
+            pairs.push([imgs[i], imgs[i + 1]]);
+        }
+        return pairs;
+    };
+
+    const imagePairs = createImagePairs([...images, ...images]);
+
+    return (
+        <div className="carousel-container">
+            <div ref={carouselRef} className="carousel-scroll">
+                <div className="carousel-content">
+                    {imagePairs.map((pair, index) => (
+                        <div key={index} className="image-row">
+                            {pair.map((src, i) => (
+                                <div key={`${index}-${i}`} className="image-column">
+                                    <img src={src} alt={`Image ${index * 2 + i + 1}`} className="carousel-image" />
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="gradient-overlay"></div>
+        </div>
+    );
+};
+
+export default ImageCarousel;
