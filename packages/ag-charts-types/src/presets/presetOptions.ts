86e2014d1e780f1a0b80d3a1f9b776594c48456a
Jacob  Parker <jacobparker1992@gmail.com>
2024-09-11 09:50:36 +0100

    Move types

--- packages/ag-charts-types/src/presets/presetOptions.ts
+++ packages/ag-charts-types/src/presets/presetOptions.ts
@@ -1,7 +1,4 @@
 import type { AgFinancialChartPresets } from './financial/financialOptions';
 import type { AgGaugePreset } from './gauge/gaugeOptions';
 
-export type * from './financial/financialOptions';
-export type * from './gauge/gaugeOptions';
-
 export type Preset = AgFinancialChartPresets | AgGaugePreset;
