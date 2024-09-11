205e17fdfb726410845a20ef211176d4b3f10fe8
Tak Tran <contact@tutaktran.com>
2024-09-11 12:55:59 +0100

    AG-12758 - Fix spacing around example runner on homepage
    * Only add margin bottom on ExampleRunnerContainer if the component is inline, otherwise rely on the container elements to define a gap/margin
      * Add `isInline` prop to allow it to be added to example runners in markdoc documents
    * Clean up gallery example runner
      * Remove description - only used in 1 place
      * Remove inline padding
      * Use Astro.params.pageName instead of url segment

--- packages/ag-charts-website/src/features/homepage/constants.ts
+++ packages/ag-charts-website/src/features/homepage/constants.ts
@@ -1,5 +1,4 @@
 import type { InternalFramework } from '@ag-grid-types';
-import { SITE_BASE_URL_SEGMENTS } from '@constants';
 
 import type { ExampleType } from '../examples-generator/types';
 
@@ -12,5 +11,3 @@ export const PLAIN_ENTRY_FILE_NAME = 'plain-main';
 export const DEFAULT_THUMBNAIL_ASPECT_RATIO = 16 / 10;
 export const DEFAULT_THUMBNAIL_WIDTH = 600;
 export const DEFAULT_THUMBNAIL_HEIGHT = DEFAULT_THUMBNAIL_WIDTH / DEFAULT_THUMBNAIL_ASPECT_RATIO;
-
-export const GALLERY_EXAMPLE_NAME_PATH_INDEX = SITE_BASE_URL_SEGMENTS + 2;
