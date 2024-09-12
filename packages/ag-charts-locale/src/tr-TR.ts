3bfe42cc8b445a92daf7688ce0a0245171e8f48a
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-09-12 15:39:58 +0100

    Fix typo

--- packages/ag-charts-locale/src/tr-TR.ts
+++ packages/ag-charts-locale/src/tr-TR.ts
@@ -4,7 +4,7 @@
  * Translations are provided as an illustration only and are
  * not guaranteed to be accurate or error-free.
  *
- * They are designed to show developers where to store t heir
+ * They are designed to show developers where to store their
  * chosen phrase or spelling variant in the target language.
  */
 
