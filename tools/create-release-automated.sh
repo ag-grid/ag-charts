92e3c94cae094573568a8f7a0dfe2bbc8d1231e8
seanlandsman <sean@thelandsmans.com>
2024-09-16 09:29:13 +0100

    Update to release script and correct base url

--- tools/create-release-automated.sh
+++ tools/create-release-automated.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eu
 
 BRANCH=$1
 RELEASE=$(echo "$1" | sed 's/^[a-zA-Z]*//')
 echo "Preparing BRANCH branch ${BRANCH}"
+
+SKIP_PROMPT=${2:-prompt} # optional
 
 git checkout -b ${BRANCH}
 ./tools/bump-versions.sh ${RELEASE}
@@ -13,14 +15,6 @@ node ./tools/update-release-info.js
 NEW_VERSION=$(node ./tools/calculate-next-version.js)
 ./tools/bump-versions.sh ${NEW_VERSION}
 node ./tools/readme/sync-readme.js
-git commit -a -m "BRANCH prep for ${NEW_VERSION}"
-git tag latest-beta-version -f
 
-read -p "Ready to push to ${BRANCH}? " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]] ; then
-    git push -f origin ${BRANCH} latest-beta-version
-else
-    echo
-    echo "Run this command when ready:"
-    echo "  git push -f origin ${BRANCH} latest-beta-version"
-fi
+git commit -a -m "BRANCH prep for ${NEW_VERSION}"
+git push
