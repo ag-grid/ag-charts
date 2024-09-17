333935fa175c9903c2fce73e968eecf03e212b9b
Tak Tran <contact@tutaktran.com>
2024-09-17 15:57:44 +0100

    AG-3390 - Add redirects checker

--- packages/ag-charts-website/src/utils/htaccess/redirects.ts
+++ packages/ag-charts-website/src/utils/htaccess/redirects.ts
@@ -2,6 +2,15 @@ export type SimpleRedirectRule = { from: string; to: string };
 export type RedirectMatchRule = { fromPattern: string; to: string };
 export type Redirect = SimpleRedirectRule | RedirectMatchRule;
 
+/**
+ * Where this file lives
+ *
+ * Useful for debugging
+ */
+export const REDIRECTS_FILE = 'packages/ag-charts-website/src/utils/htaccess/redirects.ts';
+
+export const IGNORE_PAGES = [];
+
 export const SITE_301_REDIRECTS: Redirect[] = [
     { from: '/javascript/bullet-series', to: '/javascript/linear-gauge/#bullet-series' },
     { from: '/angular/bullet-series', to: '/angular/linear-gauge/#bullet-series' },
