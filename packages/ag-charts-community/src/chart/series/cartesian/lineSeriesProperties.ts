76ba53424d1fb760db23f67eeb8806e188c14280
iMoses (Ido Moshe) <imoses.g@gmail.com>
2024-09-17 17:09:51 +0100

    Data processing cleanup

--- packages/ag-charts-community/src/chart/series/cartesian/lineSeriesProperties.ts
+++ packages/ag-charts-community/src/chart/series/cartesian/lineSeriesProperties.ts
@@ -3,8 +3,6 @@ import type {
     AgLineSeriesOptions,
     AgLineSeriesOptionsKeys,
     AgLineSeriesTooltipRendererParams,
-    FontStyle,
-    FontWeight,
 } from 'ag-charts-types';
 
 import {
@@ -28,16 +26,7 @@ export interface LineNodeDatum extends CartesianSeriesNodeDatum, ErrorBoundSerie
     readonly point: CartesianSeriesNodeDatum['point'] & {
         readonly moveTo: boolean;
     };
-    readonly label?: {
-        readonly text: string;
-        readonly fontStyle?: FontStyle;
-        readonly fontWeight?: FontWeight;
-        readonly fontSize: number;
-        readonly fontFamily: string;
-        readonly textAlign: CanvasTextAlign;
-        readonly textBaseline: CanvasTextBaseline;
-        readonly fill?: string;
-    };
+    readonly labelText?: string;
     readonly selected: boolean | undefined;
 }
 
