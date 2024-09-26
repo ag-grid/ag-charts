c7e98165b36f122180ff3fb8124dcf60420b09c8
Jacob  Parker <jacobparker1992@gmail.com>
2024-09-26 15:59:25 +0100

    Lint

--- packages/ag-charts-community/src/chart/themes/util.ts
+++ packages/ag-charts-community/src/chart/themes/util.ts
@@ -4,13 +4,11 @@ type CartesianAxis = Exclude<AgCartesianChartOptions['axes'], undefined>[0];
 
 export function swapAxisCondition(axes: [CartesianAxis, CartesianAxis], swap: (series: any) => boolean) {
     return (series: any) => {
-        if (swap(series)) {
-            return [
-                { ...axes[0], position: axes[1].position },
-                { ...axes[1], position: axes[0].position },
-            ];
-        } else {
-            return axes;
-        }
+        if (!swap(series)) return axes;
+
+        return [
+            { ...axes[0], position: axes[1].position },
+            { ...axes[1], position: axes[0].position },
+        ];
     };
 }
