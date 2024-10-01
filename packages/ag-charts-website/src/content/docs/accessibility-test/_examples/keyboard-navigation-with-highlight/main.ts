23830b78d112181caa1beeadcf31ea2901423707
Oli Legat <olivier.legat@ag-grid.com>
2024-10-01 11:48:00 +0100

    AG-13025 Add accessibility-test/keyboard-navigation-with-highlight example
    This example is the same as accessibility/keyboard-navigation, but with
    `{ highlightStyle: { series: { dimOpacity: 0.05 } } }` set. This example
    is very useful for testing hover & focus events on legend items.

---
+++ packages/ag-charts-website/src/content/docs/accessibility-test/_examples/keyboard-navigation-with-highlight/main.ts
@@ -0,0 +1,75 @@
+import { AgCartesianChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+const options: AgCartesianChartOptions = {
+    container: document.getElementById('myChart'),
+    title: {
+        text: `Renewable sources used to generate electricity for transport fuels`,
+    },
+    theme: {
+        overrides: { bar: { series: { highlightStyle: { series: { dimOpacity: 0.05, strokeWidth: 4 } } } } },
+    },
+    data: getData(),
+    animation: { enabled: false },
+    series: [
+        {
+            type: 'bar',
+            xKey: 'year',
+            yKey: 'Onshore wind',
+            yName: 'Onshore Wind',
+        },
+        {
+            type: 'bar',
+            xKey: 'year',
+            yKey: 'Offshore wind',
+            yName: 'Offshore Wind',
+        },
+        {
+            type: 'bar',
+            xKey: 'year',
+            yKey: 'Landfill gas',
+            yName: 'Landfill Gas',
+        },
+        {
+            type: 'bar',
+            xKey: 'year',
+            yKey: 'Solar photovoltaics',
+            yName: 'Solar Photovoltaics',
+        },
+        {
+            type: 'bar',
+            xKey: 'year',
+            yKey: 'Small scale Hydro',
+            yName: 'Small Scale Hydro',
+        },
+        {
+            type: 'bar',
+            xKey: 'year',
+            yKey: 'Large scale Hydro',
+            yName: 'Large Scale Hydro',
+        },
+    ],
+    axes: [
+        {
+            position: 'bottom',
+            type: 'category',
+        },
+        {
+            position: 'left',
+            type: 'number',
+            title: {
+                text: `kilotonnes of oil equivalent (ktoe)`,
+            },
+            label: {
+                formatter: (params) => `${params.value / 1000}K`,
+            },
+        },
+    ],
+    legend: {
+        maxHeight: 40,
+        maxWidth: 800,
+    },
+};
+
+const chart = AgCharts.create(options);
