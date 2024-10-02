52d85a428ba9f5a73d1e974c2e88653fa1c8045d
Mark Durrant <mark.j.durrant@gmail.com>
2024-10-02 12:28:39 +0100

    AG-12631 - Add active state for menu items

--- external/ag-website-shared/src/components/new-docs-navigation/NewDocsNav.tsx
+++ external/ag-website-shared/src/components/new-docs-navigation/NewDocsNav.tsx
@@ -9,11 +9,14 @@ function getLinkUrl({ framework, path, url }: { framework: Framework; path?: str
     return url ? url : getExamplePageUrl({ framework, path: path! });
 }
 
-function Item({ itemData, framework }: { itemData?: any; framework: Framework }) {
+function Item({ itemData, framework, pageName }: { itemData?: any; framework: Framework; pageName: string }) {
     const linkUrl = getLinkUrl({ framework, path: itemData.path });
+    const isActive = pageName === itemData.path;
+
+    const className = classnames(styles.item, itemData.icon ? styles.hasIcon : '', isActive ? styles.isIsActive : '');
 
     return (
-        <a href={linkUrl} className={classnames(styles.item, itemData.icon ? styles.hasIcon : '')}>
+        <a href={linkUrl} className={className}>
             {itemData.icon && <Icon name={itemData.icon} svgClasses={styles.itemIcon} />}
 
             {itemData.title}
@@ -23,7 +26,7 @@ function Item({ itemData, framework }: { itemData?: any; framework: Framework })
     );
 }
 
-function Group({ groupData, framework }: { groupData?: any; framework: Framework }) {
+function Group({ groupData, framework, pageName }: { groupData?: any; framework: Framework; pageName: string }) {
     return (
         <div className={styles.group}>
             <button className={classnames('button-style-none', styles.groupTitle)}>
@@ -36,14 +39,14 @@ function Group({ groupData, framework }: { groupData?: any; framework: Framework
 
             <div className={styles.groupChildren}>
                 {groupData.children.map((childData) => {
-                    return <Item itemData={childData} framework={framework} />;
+                    return <Item itemData={childData} framework={framework} pageName={pageName} />;
                 })}
             </div>
         </div>
     );
 }
 
-function Section({ sectionData, framework }: { sectionData?: any; framework: Framework }) {
+function Section({ sectionData, framework, pageName }: { sectionData?: any; framework: Framework; pageName: string }) {
     return (
         <div className={styles.section}>
             <h5 className={styles.sectionTitle}>{sectionData.title}</h5>
@@ -51,8 +54,12 @@ function Section({ sectionData, framework }: { sectionData?: any; framework: Fra
             {sectionData.children.map((childData) => {
                 return (
                     <>
-                        {childData.type === 'item' && <Item itemData={childData} framework={framework} />}
-                        {childData.type === 'group' && <Group groupData={childData} framework={framework} />}
+                        {childData.type === 'item' && (
+                            <Item itemData={childData} framework={framework} pageName={pageName} />
+                        )}
+                        {childData.type === 'group' && (
+                            <Group groupData={childData} framework={framework} pageName={pageName} />
+                        )}
                     </>
                 );
             })}
@@ -60,14 +67,22 @@ function Section({ sectionData, framework }: { sectionData?: any; framework: Fra
     );
 }
 
-export function NewDocsNav({ menuData, framework }: { menuData?: any; framework: Framework }) {
+export function NewDocsNav({
+    menuData,
+    framework,
+    pageName,
+}: {
+    menuData?: any;
+    framework: Framework;
+    pageName: string;
+}) {
     return (
         <div className={styles.docsNavOuter}>
             <div className={styles.docsNavInner}>
                 {menuData.sections.map((sectionData, i) => {
                     return (
                         <>
-                            <Section sectionData={sectionData} framework={framework} />
+                            <Section sectionData={sectionData} framework={framework} pageName={pageName} />
                             {i !== menuData.sections.length - 1 && <hr className={styles.divider} />}
                         </>
                     );
