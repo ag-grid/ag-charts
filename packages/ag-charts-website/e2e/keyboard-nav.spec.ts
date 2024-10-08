674cf05eb372d9a8302033a1d161afcdcdbed74a
Oli Legat <olivier.legat@ag-grid.com>
2024-10-08 11:02:45 +0100

    Find & replace '.ag-charts-canvas-center' -> SELECTORS.canvasCenter

--- packages/ag-charts-website/e2e/keyboard-nav.spec.ts
+++ packages/ag-charts-website/e2e/keyboard-nav.spec.ts
@@ -15,41 +15,37 @@ test.describe('keyboard-nav', () => {
 
                 // Tab into chart, 1st series + 1st datum should be highlighted.
                 await page.keyboard.press('Tab');
-                await expect(page.locator('.ag-charts-canvas-center')).toHaveScreenshot('1st-datum-focus.png');
+                await expect(page.locator(SELECTORS.canvasCenter)).toHaveScreenshot('1st-datum-focus.png');
 
                 // Move to 3rd datum, then 2nd series.
                 await page.keyboard.press('ArrowRight');
                 await page.keyboard.press('ArrowRight');
                 await page.keyboard.press('ArrowDown');
-                await expect(page.locator('.ag-charts-canvas-center')).toHaveScreenshot(
-                    '3rd-datum-2nd-series-focus.png'
-                );
+                await expect(page.locator(SELECTORS.canvasCenter)).toHaveScreenshot('3rd-datum-2nd-series-focus.png');
 
                 // Move to legend items.
                 await page.keyboard.press('Tab');
-                await expect(page.locator('.ag-charts-canvas-center')).toHaveScreenshot('legend-focus.png');
+                await expect(page.locator(SELECTORS.canvasCenter)).toHaveScreenshot('legend-focus.png');
 
                 // Move to 2nd page of legend items.
                 await page.keyboard.press('ArrowRight');
                 await page.keyboard.press('ArrowRight');
                 await page.keyboard.press('ArrowRight');
                 await page.keyboard.press('ArrowRight');
-                await expect(page.locator('.ag-charts-canvas-center')).toHaveScreenshot('legend-2nd-page-focus.png');
+                await expect(page.locator(SELECTORS.canvasCenter)).toHaveScreenshot('legend-2nd-page-focus.png');
 
                 // Move to page back control.
                 await page.keyboard.press('Tab');
-                await expect(page.locator('.ag-charts-canvas-center')).toHaveScreenshot(
-                    'legend-page-control-focus.png'
-                );
+                await expect(page.locator(SELECTORS.canvasCenter)).toHaveScreenshot('legend-page-control-focus.png');
 
                 // Tab outside of chart.
                 await page.keyboard.press('Tab');
                 await page.keyboard.press('Tab');
-                await expect(page.locator('.ag-charts-canvas-center')).toHaveScreenshot('tabbed-out-of-chart.png');
+                await expect(page.locator(SELECTORS.canvasCenter)).toHaveScreenshot('tabbed-out-of-chart.png');
 
                 // Tab back into chart.
                 await page.keyboard.press('Shift+Tab');
-                await expect(page.locator('.ag-charts-canvas-center')).toHaveScreenshot('tabbed-back-into-chart.png');
+                await expect(page.locator(SELECTORS.canvasCenter)).toHaveScreenshot('tabbed-back-into-chart.png');
             });
         });
 
