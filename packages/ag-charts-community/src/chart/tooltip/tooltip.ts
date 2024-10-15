92c7e5b3874fe82ae98c9a95530974738175bf72
Oli Legat <olivier.legat@ag-grid.com>
2024-10-15 12:48:07 +0100

    Revert "AG-12885 Remove unused import"
    This reverts commit 3e001acc8c8a252bf5c063a34da6121359716a26.

--- packages/ag-charts-community/src/chart/tooltip/tooltip.ts
+++ packages/ag-charts-community/src/chart/tooltip/tooltip.ts
@@ -2,6 +2,7 @@ import type { AgTooltipRendererResult, InteractionRange, TextWrap } from 'ag-cha
 
 import type { DOMManager } from '../../dom/domManager';
 import { enterpriseModule } from '../../module/enterpriseModule';
+import { setAttribute } from '../../util/attributeUtil';
 import { clamp } from '../../util/number';
 import { type Bounds, calculatePlacement } from '../../util/placement';
 import { BaseProperties } from '../../util/properties';
