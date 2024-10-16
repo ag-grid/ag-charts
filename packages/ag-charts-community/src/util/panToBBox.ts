bf71e04729b63cca282234d97b361ac0a1f2be40
Oli Legat <olivier.legat@ag-grid.com>
2024-10-16 11:53:58 +0100

    Apply suggestions from code review to panToBBox.ts
    Co-authored-by: Laurence Roberts <laurence.roberts@ag-grid.com>

--- packages/ag-charts-community/src/util/panToBBox.ts
+++ packages/ag-charts-community/src/util/panToBBox.ts
@@ -1,8 +1,9 @@
-import type { BBoxValues } from "./bboxinterface";
-import { clamp } from "./number";
+import type { BBoxValues } from './bboxinterface';
+import { clamp } from './number';
+import { Vec4 } from './vector4';
 
-type Ratios = { min: number, max: number };
-type XYRatios = { x: Ratios, y: Ratios };
+type Ratios = { min: number; max: number };
+type XYRatios = { x: Ratios; y: Ratios };
 
 function normalize(screenMin: number, min: number, screenMax: number, max: number, target: number): number {
     return min + (max - min) * ((target - screenMin) / (screenMax - screenMin));
@@ -12,12 +13,19 @@ function unnormalize(screenMin: number, min: number, screenMax: number, max: num
     return screenMin + (ratio - min) * ((screenMax - screenMin) / (max - min));
 }
 
-function calcWorld(viewportMin: number, viewportMax: number, ratio: Ratios): [number, number] {
+function calcWorldAxis(viewportMin: number, viewportMax: number, ratio: Ratios): [number, number] {
     return [
         unnormalize(viewportMin, ratio.min, viewportMax, ratio.max, 0),
         unnormalize(viewportMin, ratio.min, viewportMax, ratio.max, 1),
     ];
 }
+
+function calcWorldVec4(viewport: Vec4, ratioX: Ratios, ratioY: Ratios): Vec4 {
+    const [x1, x2] = calcWorldAxis(viewport.x1, viewport.x2, ratioX);
+    const [y1, y2] = calcWorldAxis(viewport.y1, viewport.y2, ratioY);
+    return { x1, x2, y1, y2 };
+}
+
 /* Pan viewport min (unnormalised, i.e. pixel coords.) by the smallest amount
    such that the viewport range includes the input target range but clamped at
    the world range. The function assumes:
@@ -41,25 +49,27 @@ function panAxesUnnormalized(
 
 // The calculations of the new desired viewport (i.e. ZoomState) is done in pixel coords (unnormalised).
 // The desired (x, y) for the new viewport is found, the pixel coords are converted into normalized values
-export function calcPanToBBoxRatios(viewport: BBoxValues, ratioX: Ratios, ratioY: Ratios, target: BBoxValues): XYRatios {
-    const [targetXMin, targetXMax] = [target.x, target.x + target.width];
-    const [targetYMin, targetYMax] = [target.y, target.y + target.height];
-    const [viewportXMin, viewportXMax] = [viewport.x, viewport.x + viewport.width];
-    const [viewportYMin, viewportYMax] = [viewport.y, viewport.y + viewport.height];
-    const [worldXMin, worldXMax] = calcWorld(viewportXMin, viewportXMax, ratioX);
-    const [worldYMin, worldYMax] = calcWorld(viewportYMin, viewportYMax, ratioY);
+export function calcPanToBBoxRatios(
+    viewportBBox: BBoxValues,
+    ratioX: Ratios,
+    ratioY: Ratios,
+    targetBBox: BBoxValues
+): XYRatios {
+    const target = Vec4.from(targetBBox);
+    const viewport = Vec4.from(viewportBBox);
+    const world = calcWorldVec4(viewport, ratioX, ratioY);
 
-    const x = panAxesUnnormalized(worldXMin, worldXMax, viewportXMin, viewportXMax, targetXMin, targetXMax);
-    const y = panAxesUnnormalized(worldYMin, worldYMax, viewportYMin, viewportYMax, targetYMin, targetYMax);
+    const x = panAxesUnnormalized(world.x1, world.x2, viewport.x1, viewport.x2, target.x1, target.x2);
+    const y = panAxesUnnormalized(world.y1, world.y2, viewport.y1, viewport.y2, target.y1, target.y2);
 
     return {
         x: {
-            min: normalize(viewportXMin, ratioX.min, viewportXMax, ratioX.max, x),
-            max: normalize(viewportXMin, ratioX.min, viewportXMax, ratioX.max, x + viewport.width),
+            min: normalize(viewport.x1, ratioX.min, viewport.x2, ratioX.max, x),
+            max: normalize(viewport.x1, ratioX.min, viewport.x2, ratioX.max, x + viewportBBox.width),
         },
         y: {
-            min: normalize(viewportYMin, ratioY.min, viewportYMax, ratioY.max, y),
-            max: normalize(viewportYMin, ratioY.min, viewportYMax, ratioY.max, y + viewport.height),
+            min: normalize(viewport.y1, ratioY.min, viewport.y2, ratioY.max, y),
+            max: normalize(viewport.y1, ratioY.min, viewport.y2, ratioY.max, y + viewportBBox.height),
         },
     };
 }
