b11664970ce727c746578c0228b272d9311229e9
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-10-17 14:55:53 +0100

    AG-13181 - Update funnel/cone-funnel gallery examples

--- packages/ag-charts-website/src/content/gallery/_examples/simple-cone-funnel/main.ts
+++ packages/ag-charts-website/src/content/gallery/_examples/simple-cone-funnel/main.ts
@@ -5,14 +5,37 @@ import { getData } from './data';
 const options: AgChartOptions = {
     container: document.getElementById('myChart'),
     data: getData(),
-    title: {
-        text: 'Conversion Drop Off',
+    theme: {
+        overrides: {
+            'cone-funnel': {
+                axes: {
+                    category: {
+                        line: {
+                            enabled: true,
+                        },
+                        tick: {
+                            enabled: true,
+                        },
+                    },
+                },
+            },
+        },
     },
     series: [
         {
             type: 'cone-funnel',
             stageKey: 'group',
             valueKey: 'value',
+            stageLabel: {
+                placement: 'after',
+            },
+            fillOpacity: 0.2,
+            strokeWidth: 1,
+            lineDash: [5],
+            label: {
+                formatter: ({ value, datum }) => (datum.group === 'INITIAL CONTACT' ? '' : value.toLocaleString()),
+                spacing: 20,
+            },
         },
     ],
 };
