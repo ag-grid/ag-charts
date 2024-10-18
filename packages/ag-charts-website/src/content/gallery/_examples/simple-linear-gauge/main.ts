f9fbe691ba72035fd182a4bccb34570f56d0ae20
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-10-18 16:21:11 +0100

    AG-13108 - Update Linear Gauge examples

--- packages/ag-charts-website/src/content/gallery/_examples/simple-linear-gauge/main.ts
+++ packages/ag-charts-website/src/content/gallery/_examples/simple-linear-gauge/main.ts
@@ -1,12 +1,78 @@
 import { AgCharts, AgLinearGaugeOptions } from 'ag-charts-enterprise';
 
+const targetMarkerStyles = {
+    placement: 'before' as const,
+    size: 10,
+    fillOpacity: 0,
+    shape: 'circle',
+    strokeWidth: 1,
+    strokeOpacity: 0.5,
+    stroke: 'orange',
+};
 const options: AgLinearGaugeOptions = {
     type: 'linear-gauge',
     container: document.getElementById('myChart'),
-    value: 80,
+    title: {
+        text: 'Assessment of Chemical Concentration Levels',
+        spacing: 50,
+    },
+    value: 88,
+    segmentation: {
+        interval: {
+            count: 2,
+        },
+        spacing: 4,
+    },
+    cornerRadius: 50,
     scale: {
         min: 0,
         max: 100,
+        interval: {
+            step: 33,
+        },
+        label: {
+            enabled: false,
+        },
+    },
+    bar: {
+        fillMode: 'discrete',
+        strokeWidth: 1,
+        strokeOpacity: 0.2,
+        fillOpacity: 0.6,
+    },
+    targets: [
+        {
+            value: 20,
+            text: 'Low',
+            ...targetMarkerStyles,
+        },
+        {
+            value: 33,
+            text: 'Suboptimal Concentration',
+            ...targetMarkerStyles,
+        },
+        {
+            value: 66,
+            text: 'Operational Range',
+            ...targetMarkerStyles,
+        },
+        {
+            value: 80,
+            text: 'Optimal',
+            ...targetMarkerStyles,
+            stroke: 'green',
+        },
+        {
+            value: 88,
+            text: 'Threshold Limit',
+            ...targetMarkerStyles,
+            fillOpacity: 1,
+            stroke: 'red',
+            fill: 'red',
+        },
+    ],
+    label: {
+        enabled: false,
     },
 };
 
