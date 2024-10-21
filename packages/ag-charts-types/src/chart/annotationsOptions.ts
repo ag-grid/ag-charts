8388d9a0406feba2118076a3aff80d504c4fe349
Laurence Roberts <laurence.roberts@ag-grid.com>
2024-10-21 14:06:08 +0100

    CRT-535 Add missing docs for measurer extends options

--- packages/ag-charts-types/src/chart/annotationsOptions.ts
+++ packages/ag-charts-types/src/chart/annotationsOptions.ts
@@ -298,14 +298,34 @@ export interface AgArrowDownAnnotation extends AgArrowMarkAnnotation {
 export interface AgDateRangeAnnotation extends AgMeasurerAnnotation {
     /** Configuration for the date range annotation.*/
     type: 'date-range';
+    /**
+     * Whether the annotation should be extended up above.
+     *
+     * Default: `false`
+     */
     extendAbove?: boolean;
+    /**
+     * Whether the annotation should be extended down below.
+     *
+     * Default: `false`
+     */
     extendBelow?: boolean;
 }
 
 export interface AgPriceRangeAnnotation extends AgMeasurerAnnotation {
     /** Configuration for the price range annotation.*/
     type: 'price-range';
+    /**
+     * Whether the annotation should be extended to the left.
+     *
+     * Default: `false`
+     */
     extendLeft?: boolean;
+    /**
+     * Whether the annotation should be extended to the right.
+     *
+     * Default: `false`
+     */
     extendRight?: boolean;
 }
 
