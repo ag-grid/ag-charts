1892c93bbf5308fc4f53f8569ec1a701777aa598
AgGitDeployment <deployments.team@ag-grid.com>
2024-10-22 09:52:24 +0100

    Release 10.3.0 Prep

--- packages/ag-charts-enterprise/src/license/licenseManager.ts
+++ packages/ag-charts-enterprise/src/license/licenseManager.ts
@@ -21,7 +21,7 @@ const LICENSE_TYPES = {
 const LICENSING_HELP_URL = 'https://ag-grid.com/charts/licensing/';
 
 export class LicenseManager {
-    private static readonly RELEASE_INFORMATION: string = 'MTcyOTQ5NDg1NTMzMQ==';
+    private static readonly RELEASE_INFORMATION: string = 'MTcyOTU4NzExMTY5Ng==';
     private licenseKey?: string;
     private gridContext: boolean = false;
     private watermarkMessage: string | undefined = undefined;
