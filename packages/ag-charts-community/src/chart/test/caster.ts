c03ac169f193bd41914b220c13fa906e94926be1
Oli Legat <olivier.legat@ag-grid.com>
2024-11-19 17:10:45 +0000

    AG-13306 Add caster.ts to read private properties & type check

---
+++ packages/ag-charts-community/src/chart/test/caster.ts
@@ -0,0 +1,39 @@
+import { expect } from '@jest/globals';
+
+function cast<T>(thing: unknown, ctor: { new (...args: any[]): T }): T {
+    expect(thing).toBeDefined();
+    expect(thing).toBeInstanceOf(ctor);
+    return thing as T;
+}
+
+function findProperty<T, K extends string>(thing: T, propertyName: K): Omit<T, K> & { [P in K]: unknown } {
+    expect(thing).toHaveProperty(propertyName);
+    return thing as T & { [P in K]: unknown };
+}
+
+function castProperty<T, V, K extends keyof T>(
+    thing: T,
+    propertyName: K,
+    propertyCtor: { new (...args: any[]): V }
+): Omit<T, K> & { [P in K]: V } {
+    expect(thing[propertyName]).toBeDefined();
+    expect(thing[propertyName]).toBeInstanceOf(propertyCtor);
+    return thing as T & { [P in K]: V };
+}
+
+export class Caster<T> {
+    constructor(readonly value: T) {}
+
+    cast<NewT>(ctor: { new (...args: any[]): NewT }): Caster<NewT> {
+        return new Caster(cast(this.value, ctor));
+    }
+    findProperty<K extends string>(propertyName: K): Caster<Omit<T, K> & { [P in K]: unknown }> {
+        return new Caster(findProperty(this.value, propertyName));
+    }
+    castProperty<V, K extends keyof T>(
+        propertyName: K,
+        propertyCtor: { new (...args: any[]): V }
+    ): Caster<Omit<T, K> & { [P in K]: V }> {
+        return new Caster(castProperty(this.value, propertyName, propertyCtor));
+    }
+}
