cd1e58e49296727eebe39b9142c1a5e4466adad2
Oli Legat <olivier.legat@ag-grid.com>
2024-11-21 10:24:27 +0000

    AG-13306 Move Caster implementation into the methods
    This significantly reduces the verbosity by removing the local functions.

--- packages/ag-charts-community/src/chart/test/caster.ts
+++ packages/ag-charts-community/src/chart/test/caster.ts
@@ -60,72 +60,8 @@ type KeysOfUnionOfArrayPropertiesOf<T> = keyof ElementsOfArrayPropertiesOf<T>;
 
 type UnknownConstructor<T> = new (...args: unknown[]) => T;
 
-function cast<T>(thing: unknown, ctor: UnknownConstructor<T>): T {
-    expect(thing).toBeDefined();
-    expect(thing).toBeInstanceOf(ctor);
-    return thing as T;
-}
-
-function findProperty<T, K extends string>(thing: T, propertyName: K): Omit<T, K> & { [P in K]: unknown } {
-    expect(thing).toHaveProperty(propertyName);
-    return thing as T & { [P in K]: unknown };
-}
-
-function findBoolean<T, K extends string>(thing: T, propertyName: K): Omit<T, K> & { [P in K]: boolean } {
-    const property: unknown = findProperty(thing, propertyName)[propertyName];
-    expect(typeof property).toBe('boolean');
-    return thing as T & { [P in K]: boolean };
-}
-
-function findArrayElementProperties<T, K extends keyof ArraysPropertiesOf<T>, L extends string>(
-    thing: T,
-    arrayName: K,
-    elementPropertyName: L
-): Omit<T, K> & { [Pk in K]: ArraysPropertiesOf<T>[K] & { [Pl in L]: unknown }[] } {
-    for (const elem of thing[arrayName] as unknown[]) {
-        expect(elem).toHaveProperty(elementPropertyName);
-    }
-    return thing as ReturnType<typeof findArrayElementProperties<T, K, L>>;
-}
-
-function castProperty<T, V, K extends keyof T>(
-    thing: T,
-    propertyName: K,
-    propertyCtor: UnknownConstructor<V>
-): Omit<T, K> & { [P in K]: V } {
-    expect(thing[propertyName]).toBeDefined();
-    expect(thing[propertyName]).toBeInstanceOf(propertyCtor);
-    return thing as T & { [P in K]: V };
-}
-
-function castPropertyArray<T, V, K extends keyof T>(
-    thing: T,
-    propertyName: K,
-    elementCtor: UnknownConstructor<V>
-): Omit<T, K> & { [P in K]: V[] } {
-    for (const elem of castProperty(thing, propertyName, Array)[propertyName]) {
-        expect(elem).toBeInstanceOf(elementCtor);
-    }
-    return thing as T & { [P in K]: V[] };
-}
-
-function castArrayElementProperties<
-    T,
-    V,
-    K extends string & keyof ArraysPropertiesOf<T>,
-    L extends string & KeysOfUnionOfArrayPropertiesOf<T>,
->(
-    thing: T,
-    arrayName: K,
-    elementPropertyName: L,
-    elementPropertyCtor: UnknownConstructor<V>
-): Omit<T, K> & { [Pk in K]: ArraysPropertiesOf<T>[K] & { [Pl in L]: V }[] } {
-    expect(thing[arrayName]).toBeInstanceOf(Array);
-    for (const unknownElem of thing[arrayName] as unknown[]) {
-        const elem = findProperty(unknownElem, elementPropertyName);
-        expect(elem[elementPropertyName]).toBeInstanceOf(elementPropertyCtor);
-    }
-    return thing as ReturnType<typeof castArrayElementProperties<T, V, K, L>>;
+function convert<NewT>(caster: Caster<unknown>): Caster<NewT> {
+    return caster as Caster<NewT>;
 }
 
 /**
@@ -136,52 +72,73 @@ function castArrayElementProperties<
  * This is similar to a Java cast or C++ dynamic_cast.
  */
 export class Caster<T> {
-    // FIXME: Each method returns a new object. This could be optimised.
     constructor(readonly value: T) {}
 
     cast<NewT>(ctor: UnknownConstructor<NewT>): Caster<NewT> {
-        return new Caster(cast(this.value, ctor));
+        expect(this.value).toBeDefined();
+        expect(this.value).toBeInstanceOf(ctor);
+        return convert<NewT>(this);
     }
 
-    findProperty<K extends string>(propertyName: K): Caster<Omit<T, K> & { [P in K]: unknown }> {
-        return new Caster(findProperty(this.value, propertyName));
+    findProperty<K extends string>(propertyName: K) {
+        type NewT = Omit<T, K> & { [P in K]: unknown };
+        expect(this.value).toHaveProperty(propertyName);
+        return convert<NewT>(this);
     }
-    findBoolean<K extends string>(propertyName: K): Caster<Omit<T, K> & { [P in K]: boolean }> {
-        return new Caster(findBoolean(this.value, propertyName));
+
+    findBoolean<K extends string>(propertyName: K) {
+        type NewT = Omit<T, K> & { [P in K]: boolean };
+        const property: unknown = this.findProperty(propertyName).value[propertyName];
+        expect(typeof property).toBe('boolean');
+        return convert<NewT>(this);
     }
+
     findArrayElementProperties<K extends keyof ArraysPropertiesOf<T>, L extends string>(
         arrayName: K,
         elementPropertyName: L
-    ): Caster<Omit<T, K> & ReturnType<typeof findArrayElementProperties<T, K, L>>> {
-        return new Caster(findArrayElementProperties(this.value, arrayName, elementPropertyName));
+    ) {
+        type NewT = Omit<T, K> & { [Pk in K]: ArraysPropertiesOf<T>[K] & { [Pl in L]: unknown }[] };
+        for (const elem of this.value[arrayName] as unknown[]) {
+            expect(elem).toHaveProperty(elementPropertyName);
+        }
+        return convert<NewT>(this);
     }
 
-    castProperty<V, K extends keyof T>(
-        propertyName: K,
-        propertyCtor: UnknownConstructor<V>
-    ): Caster<Omit<T, K> & { [P in K]: V }> {
-        return new Caster(castProperty(this.value, propertyName, propertyCtor));
+    castProperty<V, K extends keyof T>(propertyName: K, propertyCtor: UnknownConstructor<V>) {
+        type NewT = Omit<T, K> & { [P in K]: V };
+        expect(this.value[propertyName]).toBeDefined();
+        expect(this.value[propertyName]).toBeInstanceOf(propertyCtor);
+        return convert<NewT>(this);
     }
+
     castPropertyArray<V, K extends keyof T>(
         propertyName: K,
         elementCtor: UnknownConstructor<V>
     ): Caster<Omit<T, K> & { [P in K]: V[] }> {
-        return new Caster(castPropertyArray(this.value, propertyName, elementCtor));
+        type NewT = Omit<T, K> & { [P in K]: V[] };
+        for (const elem of this.castProperty(propertyName, Array).value[propertyName]) {
+            expect(elem).toBeInstanceOf(elementCtor);
+        }
+        return convert<NewT>(this);
     }
 
     castArrayElementProperties<
         V,
         K extends string & keyof ArraysPropertiesOf<T>,
         L extends string & KeysOfUnionOfArrayPropertiesOf<T>,
-    >(
-        arrayName: K,
-        elementPropertyName: L,
-        elementPropertyCtor: UnknownConstructor<V>
-    ): Caster<Omit<T, K> & ReturnType<typeof castArrayElementProperties<T, V, K, L>>> {
-        return new Caster(castArrayElementProperties(this.value, arrayName, elementPropertyName, elementPropertyCtor));
+    >(arrayName: K, elementPropertyName: L, elementPropertyCtor: UnknownConstructor<V>) {
+        type NewT = Omit<T, K> & { [Pk in K]: ArraysPropertiesOf<T>[K] & { [Pl in L]: V }[] };
+        expect(this.value[arrayName]).toBeInstanceOf(Array);
+        for (const unknownElem of this.value[arrayName] as unknown[]) {
+            expect(unknownElem).toHaveProperty(elementPropertyName);
+            const elem = unknownElem as typeof unknownElem & { [Pl in L]: unknown };
+            expect(elem[elementPropertyName]).toBeInstanceOf(elementPropertyCtor);
+        }
+        return convert<NewT>(this);
     }
 
     accessProperty<K extends string>(propertyName: K): Caster<unknown> {
-        return new Caster(findProperty(this.value, propertyName)[propertyName]);
+        const property = this.findProperty(propertyName).value[propertyName];
+        return new Caster(property);
     }
 }
