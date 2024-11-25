56901709f52e462d2ee85413b844d1d67b8c9aa3
Alan Treadway <alan.treadway@ag-grid.com>
2024-11-25 10:42:31 +0000

    Fix eslint break due to new benchmark tests.

--- packages/ag-charts-enterprise/eslint.config.mjs
+++ packages/ag-charts-enterprise/eslint.config.mjs
@@ -1,3 +1,13 @@
-import base from '../../eslint.config.mjs';
+import base, { testDefaults } from '../../eslint.config.mjs';
 
-export default [...base];
+export default [
+    ...base,
+    {
+        ...testDefaults,
+        files: ['**/benchmarks/**/*'],
+        rules: {
+            ...testDefaults.rules,
+            'sonarjs/no-empty-test-file': 0,
+        },
+    },
+];
