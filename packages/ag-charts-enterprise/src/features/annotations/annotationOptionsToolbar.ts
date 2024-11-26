8bfbded0e8f2be9395c1e7e3d0227a0847ca2666
Laurence Roberts <laurence.roberts@ag-grid.com>
2024-11-26 15:58:37 +0000

    AG-13397 Fix lock button toggling

--- packages/ag-charts-enterprise/src/features/annotations/annotationOptionsToolbar.ts
+++ packages/ag-charts-enterprise/src/features/annotations/annotationOptionsToolbar.ts
@@ -450,6 +450,10 @@ export class AnnotationOptionsToolbar extends _ModuleSupport.BaseProperties {
             if (!button) continue;
             if (button.value === AnnotationOptions.Lock) {
                 this.toolbar.toggleSwitchCheckedByIndex(index, locked);
+                this.updateButtonByValue(
+                    AnnotationOptions.Lock,
+                    locked ? button.checkedOverrides.toJson() : button.toJson()
+                );
             } else {
                 this.toolbar.toggleButtonEnabledByIndex(index, !locked);
             }
@@ -481,7 +485,6 @@ export class AnnotationOptionsToolbar extends _ModuleSupport.BaseProperties {
         const index = this.visibleButtons.findIndex((button) => button.value === value);
         if (index === -1) return;
         const button = this.visibleButtons.at(index)!;
-        this.visibleButtons.at(index)!.set({ ...button.toJson(), value, ...change });
         this.toolbar.updateButtonByIndex(index, { ...button.toJson(), value, ...change });
     }
 }
