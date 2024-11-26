aca38b1f5a369b932b30ad83a4fb90d2effee158
iMoses (Ido Moshe) <imoses.g@gmail.com>
2024-11-26 10:00:05 +0000

    Add more grouped category test examples

---
+++ packages/ag-charts-website/src/content/docs/axes-types-test/_examples/grouped-category-6/main.ts
@@ -0,0 +1,55 @@
+import { AgCartesianChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+const options: AgCartesianChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'Proportion of Hits for Recollection, Familiarity, and Guessing Across Different Stimuli and Task Conditions',
+    },
+    series: [
+        {
+            type: 'bar',
+            xKey: 'category',
+            yKey: 'recollection',
+            yName: 'Recollection',
+            errorBar: {
+                yLowerKey: 'recollection_error_low',
+                yUpperKey: 'recollection_error_high',
+            },
+        },
+        {
+            type: 'bar',
+            xKey: 'category',
+            yKey: 'familiarity',
+            yName: 'Familiarity',
+            errorBar: {
+                yLowerKey: 'familiarity_error_low',
+                yUpperKey: 'familiarity_error_high',
+            },
+        },
+        {
+            type: 'bar',
+            xKey: 'category',
+            yKey: 'guessing',
+            yName: 'Guessing',
+            errorBar: {
+                yLowerKey: 'guessing_error_low',
+                yUpperKey: 'guessing_error_high',
+            },
+        },
+    ],
+    axes: [
+        {
+            type: 'grouped-category',
+            position: 'bottom',
+        },
+        {
+            type: 'number',
+            position: 'left',
+        },
+    ],
+};
+
+const chart = AgCharts.create(options);
---
+++ packages/ag-charts-website/src/content/docs/axes-types-test/_examples/grouped-category-8/main.ts
@@ -0,0 +1,65 @@
+import { AgCartesianChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+const options: AgCartesianChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'Comparison of Sales, Costs, and Profit Across Previous Years and Origins',
+    },
+    series: [
+        {
+            type: 'bar',
+            xKey: 'category',
+            yKey: 'salary',
+            yName: 'Salary',
+            stacked: true,
+            label: {},
+        },
+        {
+            type: 'bar',
+            xKey: 'category',
+            yKey: 'subcon',
+            yName: 'Subcon',
+            stacked: true,
+            label: {},
+        },
+        {
+            type: 'bar',
+            xKey: 'category',
+            yKey: 'travel',
+            yName: 'Travel',
+            stacked: true,
+            label: {},
+        },
+        {
+            type: 'bar',
+            xKey: 'category',
+            yKey: 'visa',
+            yName: 'Visa',
+            stacked: true,
+            label: {},
+        },
+        {
+            type: 'bar',
+            xKey: 'category',
+            yKey: 'others',
+            yName: 'Others',
+            stacked: true,
+            label: {},
+        },
+    ],
+    axes: [
+        {
+            type: 'grouped-category',
+            position: 'bottom',
+        },
+        {
+            type: 'number',
+            position: 'left',
+        },
+    ],
+};
+
+const chart = AgCharts.create(options);
