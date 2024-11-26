68d0b731fc1cda4961d14b135e89896e2361b058
iMoses <imoses.g@gmail.com>
2024-11-26 10:16:42 +0000

    Add more grouped category test examples (#3044)

---
+++ packages/ag-charts-website/src/content/docs/axes-types-test/_examples/grouped-category-5/data.ts
@@ -0,0 +1,40 @@
+export function getData() {
+    return [
+        {
+            category: ['Bare NP', 'BrP Speakers'],
+            plural_interpretation: 20,
+            plural_and_singular_interpretation: 70,
+            singular_interpretation: 10,
+        },
+        {
+            category: ['Bare NP', 'AmE Speakers'],
+            plural_interpretation: 15,
+            plural_and_singular_interpretation: 75,
+            singular_interpretation: 10,
+        },
+        {
+            category: ['Singular-Marked NP', 'BrP Speakers'],
+            plural_interpretation: 5,
+            plural_and_singular_interpretation: 15,
+            singular_interpretation: 80,
+        },
+        {
+            category: ['Singular-Marked NP', 'AmE Speakers'],
+            plural_interpretation: 10,
+            plural_and_singular_interpretation: 5,
+            singular_interpretation: 85,
+        },
+        {
+            category: ['Plural-Marked NP', 'BrP Speakers'],
+            plural_interpretation: 90,
+            plural_and_singular_interpretation: 5,
+            singular_interpretation: 5,
+        },
+        {
+            category: ['Plural-Marked NP', 'AmE Speakers'],
+            plural_interpretation: 95,
+            plural_and_singular_interpretation: 3,
+            singular_interpretation: 2,
+        },
+    ];
+}
