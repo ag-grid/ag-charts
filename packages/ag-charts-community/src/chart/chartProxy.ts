8bb21a0aa1a80be6de0c8ea2d87f3276e88fecac
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2024-12-02 15:35:12 +0000

    AG-13178 - Fix legend initial state for pie and donut series

--- packages/ag-charts-community/src/chart/chartProxy.ts
+++ packages/ag-charts-community/src/chart/chartProxy.ts
@@ -6,7 +6,7 @@ import type {
     ImageDataUrlOptions,
 } from 'ag-charts-types';
 
-import type { MementoCaretaker } from '../api/state/memento';
+import type { MementoCaretaker, MementoOriginator } from '../api/state/memento';
 import type { LicenseManager } from '../module/enterpriseModule';
 import { moduleRegistry } from '../module/module';
 import type { ChartInternalOptionMetadata, ChartSpecialOverrides } from '../module/optionsModule';
@@ -243,7 +243,7 @@ export class AgChartInstanceProxy implements AgChartProxy {
             ctx: { annotationManager, chartTypeOriginator, zoomManager, legendManager },
         } = this.chart;
 
-        const originators = [];
+        const originators: MementoOriginator[] = [legendManager];
 
         if ('annotations' in processedOptions && processedOptions.annotations?.enabled) {
             originators.push(annotationManager);
@@ -256,10 +256,6 @@ export class AgChartInstanceProxy implements AgChartProxy {
 
         if (processedOptions.navigator?.enabled || processedOptions.zoom?.enabled) {
             originators.push(zoomManager);
-        }
-
-        if (processedOptions.legend?.enabled) {
-            originators.push(legendManager);
         }
 
         return originators;
