229596363801835236a16f1e54140da3d2fe9c40
Oli Legat <olivier.legat@ag-grid.com>
2024-12-02 14:29:00 +0000

    AG-13357 Draft `seriesVisibilityChange` API

--- packages/ag-charts-types/src/chart/legendOptions.ts
+++ packages/ag-charts-types/src/chart/legendOptions.ts
@@ -1,4 +1,5 @@
 import type { Formatter } from './callbackOptions';
+import type { AgPreventableEvent } from './eventOptions';
 import type { AgMarkerShape, CssColor, FontFamily, FontSize, FontStyle, FontWeight, Opacity, PixelSize } from './types';
 
 export type AgChartLegendPosition = 'top' | 'right' | 'bottom' | 'left';
@@ -71,18 +72,14 @@ export interface AgChartLegendEvent<T extends string> {
     /** Legend item id - usually yKey value for cartesian series. */
     itemId: string;
     /** Whether the legend item is currently enabled or not. */
-    enabled: boolean;
+    enabled: undefined;
     /** The browser event that triggered the legend event. */
     event: Event;
 }
 
-export interface AgChartLegendClickEvent extends AgChartLegendEvent<'click'> {
-    preventDefault(): void;
-}
+export interface AgChartLegendClickEvent extends AgChartLegendEvent<'click'>, AgPreventableEvent {}
 
-export interface AgChartLegendDoubleClickEvent extends AgChartLegendEvent<'dblclick'> {
-    preventDefault(): void;
-}
+export interface AgChartLegendDoubleClickEvent extends AgChartLegendEvent<'dblclick'>, AgPreventableEvent {}
 
 export interface AgChartLegendContextMenuEvent extends AgChartLegendEvent<'contextmenu'> {}
 
