0f90322fd3e284314938924609e7567c3ca0bdaa
Jacob  Parker <jacobparker1992@gmail.com>
2024-12-04 18:32:35 +0000

    update zoom e2e

--- packages/ag-charts-website/e2e/zoom.spec.ts
+++ packages/ag-charts-website/e2e/zoom.spec.ts
@@ -27,8 +27,8 @@ test.describe('zoom', () => {
         const withNavigatorYAxisTop = { x: width - 30, y: height / 4 };
         const withNavigatorYAxisBottom = { x: width - 30, y: (height * 3) / 4 };
 
-        const withNavigatorXAxisLeft = { x: (width * 3) / 4, y: height - 80 };
-        const withNavigatorXAxisRight = { x: width / 4, y: height - 80 };
+        const withNavigatorXAxisLeft = { x: (width * 3) / 4, y: height - 70 };
+        const withNavigatorXAxisRight = { x: width / 4, y: height - 70 };
 
         // 1. Click the zoom-in button the floating zoom buttons
         await page.hover(SELECTORS.canvas, { position: { x: 100, y: height - 100 } });
