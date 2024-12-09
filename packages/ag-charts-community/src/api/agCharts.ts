9e0eb246f4c9b1b231062d25058b215d56f42ca2
Alan Treadway <alan.treadway@ag-grid.com>
2024-12-09 16:18:44 +0000

    CRT-564 - Fix lost synchronous updateDelta() calls.

--- packages/ag-charts-community/src/api/agCharts.ts
+++ packages/ag-charts-community/src/api/agCharts.ts
@@ -257,6 +257,7 @@ class AgChartsInternal {
             // so we need to remove all queue items up to the last successfully applied item.
             const queueIdx = chartRef.queuedUserOptions.indexOf(chartOptions.userOptions) + 1;
             chartRef.queuedUserOptions.splice(0, queueIdx);
+            chartRef.queuedChartOptions.splice(0, queueIdx);
         });
 
         return proxy;
