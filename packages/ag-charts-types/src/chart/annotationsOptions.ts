a0ae7b9e840a25a49abb235f6631ffee2ff88020
Laurence Roberts <laurence.roberts@ag-grid.com>
2024-12-09 14:22:32 +0000

    CRT-582 Fix quick measure handle styles

--- packages/ag-charts-types/src/chart/annotationsOptions.ts
+++ packages/ag-charts-types/src/chart/annotationsOptions.ts
@@ -118,6 +118,7 @@ export interface AgQuickMeasurerAnnotationStyles extends Visible {
 }
 
 export interface AgQuickMeasurerAnnotationDirectionStyles extends FillOptions, StrokeOptions, LineOptions {
+    handle?: AgAnnotationHandleStyles;
     statistics?: AgMeasurerAnnotationStatistics;
 }
 
