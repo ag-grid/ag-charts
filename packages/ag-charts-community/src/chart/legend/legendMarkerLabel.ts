18e63c45734582712d90a8b362e40b1b7a40351b
Jacob  Parker <jacobparker1992@gmail.com>
2024-12-13 12:53:24 +0000

    lint

--- packages/ag-charts-community/src/chart/legend/legendMarkerLabel.ts
+++ packages/ag-charts-community/src/chart/legend/legendMarkerLabel.ts
@@ -1,8 +1,9 @@
 import type { FontStyle, FontWeight } from 'ag-charts-types';
 
-import type { BBox, ChildNodeCounts } from '../../module-support';
+import type { BBox } from '../../scene/bbox';
 import { SceneChangeDetection } from '../../scene/changeDetectable';
 import { Group } from '../../scene/group';
+import type { ChildNodeCounts } from '../../scene/node';
 import { Line } from '../../scene/shape/line';
 import { Text } from '../../scene/shape/text';
 import { Translatable } from '../../scene/transformable';
