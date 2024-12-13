ac04425ecc55e0c125f83783b06859e1e5373764
Oli Legat <olivier.legat@ag-grid.com>
2024-12-13 16:13:02 +0000

    AG-13668 Fix crosshair updates on panToBBox
    HighlightManager.updateHighlight only dispatches 'highlight-change' if the
    highlighted datum has changed. In the case of panToBBox, the new datum is almost
    identical except that screen positioning values like `midPoint` have
    changed. Therefore, check for `midPoint` changes in `isEqual`.

--- packages/ag-charts-community/src/chart/interaction/highlightManager.ts
+++ packages/ag-charts-community/src/chart/interaction/highlightManager.ts
@@ -60,9 +60,14 @@ export class HighlightManager extends BaseManager<'highlight-change', HighlightC
     }
 
     private isEqual(a?: SeriesNodeDatum, b?: SeriesNodeDatum) {
-        return (
-            a === b ||
-            (a != null && b != null && a?.series === b?.series && a?.itemId === b?.itemId && a?.datum === b?.datum)
-        );
+        const notEqual =
+            a !== b ||
+            a == null ||
+            b == null ||
+            a.series !== b.series ||
+            a.itemId !== b.itemId ||
+            a.datum !== b.datum ||
+            a.midPoint !== b.midPoint;
+        return !notEqual;
     }
 }
