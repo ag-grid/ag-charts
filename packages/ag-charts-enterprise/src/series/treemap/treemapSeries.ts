731695df713b6d9a7a45086e6f7ce987501ee596
Jacob  Parker <jacobparker1992@gmail.com>
2024-12-23 13:59:08 +0000

    Update treemap tooltip position

--- packages/ag-charts-enterprise/src/series/treemap/treemapSeries.ts
+++ packages/ag-charts-enterprise/src/series/treemap/treemapSeries.ts
@@ -212,7 +212,7 @@ export class TreemapSeries extends _ModuleSupport.HierarchySeries<DistantGroup, 
             node.bbox = bbox;
             node.padding = padding;
             node.midPoint.x = bbox.x + bbox.width / 2;
-            node.midPoint.y = bbox.y + bbox.height / 2;
+            node.midPoint.y = bbox.y;
         }
 
         const { sortedChildrenIndices, childAt } = this.sortChildren(node);
@@ -845,40 +845,6 @@ export class TreemapSeries extends _ModuleSupport.HierarchySeries<DistantGroup, 
             }
         );
     }
-
-    // public override pickFocus(_opts: _ModuleSupport.PickFocusInputs): _ModuleSupport.PickFocusOutputs | undefined {
-    //     return undefined;
-    // // Initialise this.focusSorted
-    // if (path.length < 2 || this.focusSorted == null) {
-    //     path.length = 1;
-    //     this.focusSorted = this.sortChildren(path[0].nodeDatum);
-    //     path.push({ nodeDatum: this.focusSorted.childAt(0), childIndex: 0 });
-    // }
-
-    // const { datumIndexDelta: childDelta, otherIndexDelta: depthDelta } = opts;
-    // const current = path[path.length - 1];
-
-    // if (depthDelta === 1) {
-    //     if (current.nodeDatum.children.length > 0) {
-    //         this.focusSorted = this.sortChildren(current.nodeDatum);
-    //         const newFocus = { nodeDatum: this.focusSorted.childAt(0), childIndex: 0 };
-    //         path.push(newFocus);
-    //         return this.computeFocusOutputs(newFocus);
-    //     }
-    // } else if (childDelta !== 0) {
-    //     const targetIndex = current.childIndex + childDelta;
-    //     const maxIndex = (current.nodeDatum.parent?.children.length ?? 1) - 1;
-    //     current.childIndex = clamp(0, targetIndex, maxIndex);
-    //     current.nodeDatum = this.focusSorted.childAt(current.childIndex);
-    //     return this.computeFocusOutputs(current);
-    // }
-
-    // const result = super.pickFocus(opts);
-    // if (depthDelta < 0) {
-    //     this.focusSorted = this.sortChildren(path[path.length - 1].nodeDatum.parent!);
-    // }
-    // return result;
-    // }
 
     protected getAnimationData() {
         return {
