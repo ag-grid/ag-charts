4a9aede9dd627a0a12c4ccc958493be1d1bd9b28
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-01-03 13:33:05 +0000

    AG-12393 - Disable tooltip when in annotation active state

--- packages/ag-charts-community/src/chart/series/seriesAreaManager.ts
+++ packages/ag-charts-community/src/chart/series/seriesAreaManager.ts
@@ -599,8 +599,10 @@ export class SeriesAreaManager extends BaseManager {
     }
 
     private handleHoverTooltip(event: TooltipWidgetEvent, redisplay: boolean) {
-        if (!this.isState(InteractionState.Clickable)) return;
-
+        if (!this.isState(InteractionState.Default)) {
+            this.clearTooltip();
+            return;
+        }
         const { type, currentX, currentY } = event;
         const canvasX = currentX + (this.hoverRect?.x ?? 0);
         const canvasY = currentY + (this.hoverRect?.y ?? 0);
