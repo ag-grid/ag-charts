507d0f8dbcecbba7319c7c0876626df8fb1e0763
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-01-08 16:49:51 +0000

    AG-12393 -use tooltipManager suppress/unsuppress tooltip for annotations

--- packages/ag-charts-community/src/chart/series/seriesAreaManager.ts
+++ packages/ag-charts-community/src/chart/series/seriesAreaManager.ts
@@ -600,6 +600,7 @@ export class SeriesAreaManager extends BaseManager {
 
     private handleHoverTooltip(event: TooltipWidgetEvent, redisplay: boolean) {
         if (!this.isState(InteractionState.Clickable)) return;
+
         const { type, currentX, currentY } = event;
         const canvasX = currentX + (this.hoverRect?.x ?? 0);
         const canvasY = currentY + (this.hoverRect?.y ?? 0);
