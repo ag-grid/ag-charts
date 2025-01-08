06f404de5c6ef1936a2804bc091e45d07fac865d
Oli Legat <olivier.legat@ag-grid.com>
2025-01-08 11:47:52 +0000

    AG-13737 Implement ZoomTwoFingers by solving two unknowns/equations
    This seems to work as intended on the X-axis. The Y-axis has an issue at the
    moment (perhaps because the direction of the screen and normalised Y-values is
    reversed).
    
    I'll write up my paper math notes once I've got a working solution.

--- packages/ag-charts-enterprise/src/features/zoom/zoomTwoFingers.ts
+++ packages/ag-charts-enterprise/src/features/zoom/zoomTwoFingers.ts
@@ -1,29 +1,36 @@
 import { _ModuleSupport, _Widget } from 'ag-charts-community';
 
-type Finger = {
-    identifier: number;
-    normalOriginX: number;
-    normalOriginY: number;
-};
+type Origin = { identifier: number; normalX: number; normalY: number };
 
-function clientToNormal(pxOffset: number, pxLength: number, { min, max }: _ModuleSupport.ZoomState): number {
-    if (pxLength === 0) return 0;
-    // Interpolate pxOffset from [0, pxLength] to [min, max]
-    return (pxOffset / pxLength) * (max - min) + min;
+// Interpolate `a` from [Rx, Rw] to [min, max]
+function clientToNormal({ min, max }: _ModuleSupport.ZoomState, a: number, Rx: number, Rw: number): number {
+    if (Rw === 0) return 0; // don't divide by 0.
+    return ((a - Rx) / Rw) * (max - min) + min;
+}
+
+function solveTwoUnknowns(
+    x0: number,
+    x1: number,
+    a0: number,
+    a1: number,
+    Rx: number,
+    Rw: number
+): _ModuleSupport.ZoomState {
+    const k0 = (a0 - Rx) / Rw;
+    const k1 = (a1 - Rx) / Rw;
+    const g = (a0 - Rx) / (a1 - Rx); // === k0 / k1;
+
+    const min = (x0 - g * x1) / (1 - k0 - g * (k1 - 1));
+    const max = (x1 + (k1 - 1) * min) / k1;
+
+    return { min, max };
 }
 
 export class ZoomTwoFingers {
-    private readonly fingers: [Finger, Finger] = [
-        { identifier: 0, normalOriginX: NaN, normalOriginY: NaN },
-        { identifier: 0, normalOriginX: NaN, normalOriginY: NaN },
+    private readonly origins: [Origin, Origin] = [
+        { identifier: 0, normalX: NaN, normalY: NaN },
+        { identifier: 0, normalX: NaN, normalY: NaN },
     ];
-    private readonly originZoom: Readonly<_ModuleSupport.DefinedZoomState> = {
-        x: { min: 0, max: 1 },
-        y: { min: 0, max: 1 },
-    };
-    private initialDeltaX = 0;
-    private initialDeltaY = 0;
-
     start(
         event: _Widget.TouchWidgetEvent<'touchstart'>,
         target: _Widget.Widget,
@@ -34,24 +41,13 @@ export class ZoomTwoFingers {
         const targetTouches = Array.from(event.sourceEvent.targetTouches);
         const rect = target.getBoundingClientRect();
 
-        this.originZoom.x.min = zoom.x?.min ?? 0;
-        this.originZoom.x.max = zoom.x?.max ?? 1;
-        this.originZoom.y.min = zoom.y?.min ?? 0;
-        this.originZoom.y.max = zoom.y?.max ?? 1;
-
-        const [touch0, touch1] = targetTouches;
-
-        this.fingers[0].identifier = touch0.identifier;
-        this.fingers[0].normalOriginX = clientToNormal(touch0.clientX - rect.x, rect.width, this.originZoom.x);
-        this.fingers[0].normalOriginY = clientToNormal(touch0.clientY - rect.y, rect.height, this.originZoom.y);
-
-        this.fingers[1].identifier = touch1.identifier;
-        this.fingers[1].normalOriginX = clientToNormal(touch1.clientX - rect.x, rect.width, this.originZoom.x);
-        this.fingers[1].normalOriginY = clientToNormal(touch1.clientY - rect.y, rect.height, this.originZoom.y);
-
-        this.initialDeltaX = Math.abs(touch1.clientX - touch0.clientX);
-        this.initialDeltaY = Math.abs(touch1.clientY - touch0.clientY);
-
+        for (const i of [0, 1]) {
+            const origin = this.origins[i];
+            const touch = targetTouches[i];
+            origin.identifier = touch.identifier;
+            origin.normalX = clientToNormal(zoom.x ?? { min: 0, max: 1 }, touch.clientX, rect.x, rect.width);
+            origin.normalY = clientToNormal(zoom.y ?? { min: 0, max: 1 }, touch.clientY, rect.y, rect.height);
+        }
         return true;
     }
 
@@ -59,37 +55,32 @@ export class ZoomTwoFingers {
         event.sourceEvent.preventDefault();
         const targetTouches = Array.from(event.sourceEvent.targetTouches);
 
+        const { origins } = this;
         const rect = target.getBoundingClientRect();
-        const [touch0, touch1] = targetTouches;
-        const deltaX = Math.abs(touch1.clientX - touch0.clientX);
-        const deltaY = Math.abs(touch1.clientY - touch0.clientY);
-
-        const scaleX = deltaX / this.initialDeltaX;
-        const scaleY = deltaY / this.initialDeltaY;
-
-        const panX0 = touch0.clientX - rect.x;
-        const panY0 = touch0.clientY - rect.y;
-
-        const anchorX = this.fingers[0].normalOriginX;
-        const anchorY = this.fingers[0].normalOriginY;
-
-        const newMinX = anchorX - (anchorX - this.originZoom.x.min) * scaleX;
-        const newMaxX = anchorX + (this.originZoom.x.max - anchorX) * scaleX;
-
-        const newMinY = anchorY - (anchorY - this.originZoom.y.min) * scaleY;
-        const newMaxY = anchorY + (this.originZoom.y.max - anchorY) * scaleY;
-
-        const updatedZoom = {
-            x: { min: Math.min(newMinX, newMaxX), max: Math.max(newMinX, newMaxX) },
-            y: { min: Math.min(newMinY, newMaxY), max: Math.max(newMinY, newMaxY) },
+        const touches = [0, 1].map((i) => targetTouches.find((t) => t.identifier === origins[i].identifier)!);
+        return {
+            x: solveTwoUnknowns(
+                origins[0].normalX,
+                origins[1].normalX,
+                touches[0].clientX,
+                touches[1].clientX,
+                rect.x,
+                rect.width
+            ),
+            // y: solveTwoUnknowns(
+            //     origins[0].normalY,
+            //     origins[1].normalY,
+            //     touches[0].clientY,
+            //     touches[1].clientY,
+            //     rect.y,
+            //     rect.height
+            // ),
+            y: { min: 0, max: 1 },
         };
-
-        panX0; panY0;
-        return updatedZoom;
     }
 
     end(event: _Widget.TouchWidgetEvent<'touchend' | 'touchcancel'>): boolean {
         const identifiers = Array.from(event.sourceEvent.targetTouches).map((t) => t.identifier);
-        return !identifiers.includes(this.fingers[0].identifier) || !identifiers.includes(this.fingers[1].identifier);
+        return !identifiers.includes(this.origins[0].identifier) || !identifiers.includes(this.origins[1].identifier);
     }
 }
