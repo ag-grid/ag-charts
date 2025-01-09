cbdcc85d5776ebb0bbf3a947de3cfc9598fa3a30
Alan Treadway <alan.treadway@ag-grid.com>
2025-01-09 10:24:41 +0000

    Re-add /scene module entry point for TC verification of runtime bindings.

--- packages/ag-charts-community/src/integrated-charts-scene.ts
+++ packages/ag-charts-community/src/integrated-charts-scene.ts
@@ -1,3 +1,7 @@
+// The /scene entry-point is used when bumping Charts versions in the ag-grid repo to
+// verify that runtime bindings match the ag-charts-types/scene types, which are used
+// to assist Grid tree-shaking.
+
 // Only these imports are used by ag-grid.
 // DO NOT ADD EXPORTS UNLESS REQUIRED BY INTEGRATED CHARTS.
 export { Caption } from './chart/caption';
