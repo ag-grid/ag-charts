494b8e70dc71af20c23c9c3e11f446bab108ed42
Oli Legat <olivier.legat@ag-grid.com>
2025-01-10 13:06:57 +0000

    AG-13488 Assert no scene renders in tooltip e2e test (TC4)

--- packages/ag-charts-website/e2e/interactive-tooltip.spec.ts
+++ packages/ag-charts-website/e2e/interactive-tooltip.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from './fixture';
-import { gotoExample, setupIntrinsicAssertions, toExamplePageUrl } from './util';
+import { SELECTORS, gotoExample, setupIntrinsicAssertions, toExamplePageUrl } from './util';
 
 test.describe('interactive-tooltip', () => {
     setupIntrinsicAssertions();
@@ -11,10 +11,14 @@ test.describe('interactive-tooltip', () => {
 
         await page.mouse.move(400, 150);
         await expect(page).toHaveScreenshot('interactive-tooltip-visible.png');
+        await expect(page.locator(SELECTORS.wrapper)).toHaveAttribute('data-scene-renders');
+        const expectedRenders: string = await page.locator(SELECTORS.wrapper).getAttribute('data-scene-renders');
 
         const bbox = await page.getByText('Click here').boundingBox();
         await page.mouse.move(bbox.x, bbox.y);
         await expect(page).toHaveScreenshot('interactive-tooltip-visible.png');
+        const actualRenders: string = await page.locator(SELECTORS.wrapper).getAttribute('data-scene-renders');
+        expect(actualRenders).toBe(expectedRenders);
 
         await page.mouse.move(400, 400);
         await expect(page).toHaveScreenshot('interactive-tooltip-moved-down.png');
