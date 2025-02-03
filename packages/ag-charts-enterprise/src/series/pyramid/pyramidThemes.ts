3d1f52fb20fb97f38dde1a5a45c984580f37cf24
Jacob  Parker <jacobparker1992@gmail.com>
2025-02-03 11:04:35 +0000

    AG-13975 Fix funnel shadows

--- packages/ag-charts-enterprise/src/series/pyramid/pyramidThemes.ts
+++ packages/ag-charts-enterprise/src/series/pyramid/pyramidThemes.ts
@@ -1,5 +1,9 @@
 import { _ModuleSupport } from 'ag-charts-community';
 
+const {
+    ThemeSymbols: { DEFAULT_SHADOW_COLOUR },
+} = _ModuleSupport;
+
 export const PYRAMID_SERIES_THEME: _ModuleSupport.SeriesModule<'pyramid'>['themeTemplate'] = {
     series: {
         direction: 'vertical',
@@ -19,6 +23,13 @@ export const PYRAMID_SERIES_THEME: _ModuleSupport.SeriesModule<'pyramid'>['theme
             fontWeight: { $ref: 'fontWeight' },
             color: { $ref: 'textColor' },
             spacing: 12,
+        },
+        shadow: {
+            enabled: false,
+            color: DEFAULT_SHADOW_COLOUR,
+            xOffset: 3,
+            yOffset: 3,
+            blur: 5,
         },
     },
 };
