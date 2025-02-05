b7769c7e973409396e0b2e7c67b8b35cc9e91435
Alan Treadway <alan.treadway@ag-grid.com>
2025-02-05 08:00:28 +0000

    AG-11720 - Fix RadialBar stacking with multi-series data.

--- packages/ag-charts-enterprise/src/series/radial-bar/radialBar.test.ts
+++ packages/ag-charts-enterprise/src/series/radial-bar/radialBar.test.ts
@@ -135,6 +135,24 @@ describe('RadialBarSeries', () => {
         await compare();
     });
 
+    it(`should render stacked radial bar with per-series data as expected`, async () => {
+        const { data, ...exampleOptions } = EXAMPLE_OPTIONS;
+        const options: AgChartOptions = {
+            ...exampleOptions,
+            series: EXAMPLE_OPTIONS.series?.map((series) => {
+                return {
+                    ...series,
+                    stacked: true,
+                    data: [...(data ?? [])],
+                };
+            }),
+        };
+        prepareEnterpriseTestOptions(options as any);
+
+        chart = AgCharts.create(options);
+        await compare();
+    });
+
     it(`should render normalized radial bar as expected`, async () => {
         const options: AgChartOptions = {
             ...EXAMPLE_OPTIONS,
