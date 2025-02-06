0d5261fead8b1d7a0c86ed25899dec923f7d5b7e
Laurence Roberts <laurence.roberts@ag-grid.com>
2025-02-06 14:16:47 +0000

    Reduce pixel color diff threshold from 0.2 on playwright config

--- packages/ag-charts-website/playwright.config.ts
+++ packages/ag-charts-website/playwright.config.ts
@@ -43,6 +43,16 @@ export default defineConfig({
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
     },
+    expect: {
+        toMatchSnapshot: {
+            maxDiffPixels: 0,
+            threshold: 0.01,
+        },
+        toHaveScreenshot: {
+            maxDiffPixels: 0,
+            threshold: 0.01,
+        },
+    },
 
     /* Configure projects for major browsers */
     projects: [
