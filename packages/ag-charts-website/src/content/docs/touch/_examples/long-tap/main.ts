24c9ec22d0a9377206d11c9e53660efc168d6078
Oli Legat <olivier.legat@ag-grid.com>
2025-02-07 16:48:01 +0000

    AG-13967 Add long-tap placeholder

---
+++ packages/ag-charts-website/src/content/docs/touch/_examples/long-tap/main.ts
@@ -0,0 +1,76 @@
+import { AgCartesianChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+const options: AgCartesianChartOptions = {
+    container: document.getElementById('myChart'),
+    title: {
+        text: 'Sweaters made',
+    },
+    animation: { enabled: false },
+    contextMenu: {
+        extraActions: [
+            {
+                label: 'Say hello',
+                action: () => {
+                    window.alert('Hello world!');
+                },
+            },
+        ],
+        extraSeriesAreaActions: [
+            {
+                label: 'Say hello in the series area',
+                action: () => {
+                    window.alert('Hello in the series area!');
+                },
+            },
+        ],
+        extraNodeActions: [
+            {
+                label: 'Say hello to a node',
+                action: ({ datum, yKey }) => {
+                    window.alert(`Hello ${yKey} in ${datum.month}!`);
+                },
+            },
+        ],
+        extraLegendItemActions: [
+            {
+                label: 'Say hello to a legend item',
+                action: ({ itemId }) => {
+                    window.alert(`Hello ${itemId}!`);
+                },
+            },
+        ],
+    },
+    data: [
+        {
+            month: 'Jun',
+            sweaters: 50,
+            hats: 40,
+        },
+        {
+            month: 'Jul',
+            sweaters: 70,
+            hats: 50,
+        },
+        {
+            month: 'Aug',
+            sweaters: 60,
+            hats: 30,
+        },
+    ],
+    series: [
+        {
+            type: 'line',
+            xKey: 'month',
+            yKey: 'sweaters',
+            yName: 'Sweaters Made',
+        },
+        {
+            type: 'line',
+            xKey: 'month',
+            yKey: 'hats',
+            yName: 'Hats Made',
+        },
+    ],
+};
+
+AgCharts.create(options);
