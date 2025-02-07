530b1021c1d1965a169f9ced23916f80c79190d2
Mark Durrant <mark.j.durrant@gmail.com>
2025-02-07 11:31:17 +0000

    AG-3390 - Seperate out Grid & Charts getting started cards data

--- external/ag-website-shared/src/components/getting-started/GettingStarted.tsx
+++ external/ag-website-shared/src/components/getting-started/GettingStarted.tsx
@@ -1,3 +1,4 @@
+import type { Library } from '@ag-grid-types';
 import { useFrameworkFromStore } from '@utils/hooks/useFrameworkFromStore';
 import { urlWithPrefix } from '@utils/urlWithPrefix';
 import type { FunctionComponent } from 'react';
@@ -5,29 +6,56 @@ import type { FunctionComponent } from 'react';
 import { Icon } from '../icon/Icon';
 import styles from './getting-started.module.scss';
 
-const GettingStarted: FunctionComponent = () => {
+interface Props {
+    library: Library;
+}
+
+const GRID_FEATURES = [
+    {
+        icon: 'concepts',
+        title: 'Key Features',
+        description: 'Browse an overview of our commonly used features',
+        link: './key-features/',
+    },
+    {
+        icon: 'tutorials',
+        title: 'Tutorials',
+        description: 'Get started with our step-by-step tutorials',
+        link: './deep-dive/',
+    },
+    {
+        icon: 'communityEnterprise',
+        title: 'Community vs. Enterprise',
+        description: 'Understand the differences between each version',
+        link: './community-vs-enterprise/',
+    },
+];
+
+const CHARTS_FEATURES = [
+    {
+        icon: 'concepts',
+        title: 'Key Features',
+        description: 'Browse an overview of our commonly used features',
+        link: './key-features/',
+    },
+    {
+        icon: 'tutorials',
+        title: 'Tutorials',
+        description: 'Get started with our step-by-step tutorials',
+        link: './create-a-basic-chart/',
+    },
+    {
+        icon: 'communityEnterprise',
+        title: 'Community vs. Enterprise',
+        description: 'Understand the differences between each version',
+        link: './community-vs-enterprise/',
+    },
+];
+
+const GettingStarted: FunctionComponent<Props> = ({ library }) => {
     const framework = useFrameworkFromStore();
 
-    const features = [
-        {
-            icon: 'concepts',
-            title: 'Key Features',
-            description: 'Browse an overview of our commonly used features',
-            link: './key-features/',
-        },
-        {
-            icon: 'tutorials',
-            title: 'Tutorials',
-            description: 'Get started with our step-by-step tutorials',
-            link: './deep-dive/',
-        },
-        {
-            icon: 'communityEnterprise',
-            title: 'Community vs. Enterprise',
-            description: 'Understand the differences between each version',
-            link: './community-vs-enterprise/',
-        },
-    ];
+    const features = library === 'grid' ? GRID_FEATURES : CHARTS_FEATURES;
 
     return (
         <div className={styles.container}>
