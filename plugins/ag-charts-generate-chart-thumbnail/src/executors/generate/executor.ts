1430df257ec95a3b8054c799d950276b612ddcb0
Alan Treadway <alan.treadway@ag-grid.com>
2025-02-10 15:11:24 +0000

    CRT-644 - Fix archive build not formatting examples.

--- plugins/ag-charts-generate-chart-thumbnail/src/executors/generate/executor.ts
+++ plugins/ag-charts-generate-chart-thumbnail/src/executors/generate/executor.ts
@@ -53,7 +53,7 @@ export async function generateFiles(
     const name = `${ctx.projectName}:${ctx.targetName}:${ctx.configurationName ?? ''}`;
     const jsonPath = path.join(generatedExamplePath, 'plain', 'vanilla', 'contents.json');
     const example = await readJSONFile(jsonPath);
-    const production = ['production', 'staging', 'archive'].includes(process.env.NX_TASK_TARGET_CONFIGURATION);
+    const production = ['production', 'staging', 'archive'].includes(ctx.configurationName ?? '');
     const dpiOutputs = production ? [1, 2] : [1];
 
     if (example == null) {
