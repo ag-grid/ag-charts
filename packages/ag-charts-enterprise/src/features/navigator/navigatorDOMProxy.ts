da654b8569ef07536f618648e39475a3913b5300
Oli Legat <olivier.legat@ag-grid.com>
2025-02-11 11:31:22 +0000

    CRT-667 Add device==="touch" check

--- packages/ag-charts-enterprise/src/features/navigator/navigatorDOMProxy.ts
+++ packages/ag-charts-enterprise/src/features/navigator/navigatorDOMProxy.ts
@@ -135,7 +135,9 @@ export class NavigatorDOMProxy {
         const sliderLeft = slider.cssLeft();
         this.dragStartX = toolbarLeft + sliderLeft + event.offsetX;
         this.moveToFront(index); // AG-13780
-        event.sourceEvent.preventDefault();
+        if (event.device === 'touch') {
+            event.sourceEvent.preventDefault();
+        }
         this.sliderHandlers.onDragStart(key, this.toCanvasOffsets(event));
     }
 
@@ -144,7 +146,9 @@ export class NavigatorDOMProxy {
         event: _ModuleSupport.DragWidgetEvent<'drag-move'>,
         key: NavigatorButtonType
     ) {
-        event.sourceEvent.preventDefault();
+        if (event.device === 'touch') {
+            event.sourceEvent.preventDefault();
+        }
         this.sliderHandlers.onDrag(key, this.toCanvasOffsets(event));
     }
 
