318941de1540e3b438026deebe0704bb97247516
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-02-14 17:12:34 +0000

    AG-13860 - Remove unused import

--- packages/ag-charts-community/src/chart/series/cartesian/areaSeriesModule.ts
+++ packages/ag-charts-community/src/chart/series/cartesian/areaSeriesModule.ts
@@ -4,7 +4,7 @@ import type { AgAreaSeriesOptions } from 'ag-charts-types';
 import type { SeriesModule } from '../../../module/coreModules';
 import type { ModuleContext } from '../../../module/moduleContext';
 import { CARTESIAN_AXIS_TYPE, CARTESIAN_POSITION } from '../../themes/constants';
-import { DEFAULT_COLOR_RANGE, DEFAULT_SHADOW_COLOUR } from '../../themes/symbols';
+import { DEFAULT_SHADOW_COLOUR } from '../../themes/symbols';
 import { markerPaletteFactory } from '../../themes/util';
 import { AreaSeries } from './areaSeries';
 import { areaSeriesOptionsDef } from './areaSeriesOptionsDef';
