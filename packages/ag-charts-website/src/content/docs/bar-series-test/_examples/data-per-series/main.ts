812c885d343c8fa53997260fa431c846b666a734
Alan Treadway <alan.treadway@ag-grid.com>
2025-02-18 09:23:20 +0000

    AG-11612 - Add regression testing data-per-series example.

---
+++ packages/ag-charts-website/src/content/docs/bar-series-test/_examples/data-per-series/main.ts
@@ -0,0 +1,54 @@
+import { AgCartesianChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+const options: AgCartesianChartOptions = {
+    container: document.getElementById('myChart'),
+    series: [
+        {
+            type: 'bar',
+            xKey: 'product',
+            yKey: 'value',
+            yName: 'iPad - Retail',
+            data: [
+                { product: 'Air', value: 400 },
+                { product: 'Pro', value: 280 },
+            ],
+            stackGroup: 'ipad',
+        },
+        {
+            type: 'bar',
+            xKey: 'product',
+            yKey: 'value',
+            yName: 'iPad - Student',
+            data: [
+                { product: 'Air', value: 140 },
+                { product: 'Pro', value: 90 },
+            ],
+            stackGroup: 'ipad',
+        },
+        {
+            type: 'bar',
+            xKey: 'product',
+            yKey: 'value',
+            yName: 'MacBook - Retail',
+            data: [
+                { product: 'Air', value: 205 },
+                { product: 'Pro 15"', value: 195 },
+                { product: 'Pro 16"', value: 500 },
+            ],
+            stackGroup: 'macbook',
+        },
+        {
+            type: 'bar',
+            xKey: 'product',
+            yKey: 'value',
+            yName: 'MacBook - Student',
+            data: [
+                { product: 'Air', value: 20 },
+                { product: 'Pro 16"', value: 50 },
+            ],
+            stackGroup: 'macbook',
+        },
+    ],
+};
+
+const chart = AgCharts.create(options);
