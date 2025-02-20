6cc6859216b37f7fd955b5c376b9825d8cd0d1d4
Alan Treadway <alan.treadway@ag-grid.com>
2025-02-20 15:31:10 +0000

    git subrepo pull external/ag-shared
    subrepo:
      subdir:   "external/ag-shared"
      merged:   "ea577fcfeb"
    upstream:
      origin:   "https://github.com/ag-grid/ag-shared.git"
      branch:   "latest"
      commit:   "ea577fcfeb"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/Homebrew/brew"
      commit:   "47ef1a3b0b"

--- external/ag-shared/scripts/shard/calculate-pkg-shards.js
+++ external/ag-shared/scripts/shard/calculate-pkg-shards.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-require-imports */
 const { execSync } = require('child_process');
 
--- external/ag-shared/scripts/shard/calculate-shards.js
+++ external/ag-shared/scripts/shard/calculate-shards.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-require-imports */
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
