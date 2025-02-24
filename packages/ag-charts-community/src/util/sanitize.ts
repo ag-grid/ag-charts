f876d4c0feba5ed5190b9f70f8a1872d2742f9f6
Jacob  Parker <jacobparker1992@gmail.com>
2025-02-24 15:03:45 +0000

    AG-8167 Multi-line tooltips

--- packages/ag-charts-community/src/util/sanitize.ts
+++ packages/ag-charts-community/src/util/sanitize.ts
@@ -2,14 +2,11 @@ import { createElement } from '../core';
 
 let element: HTMLElement | null = null;
 
-export function sanitizeHtml(text: string): string;
-export function sanitizeHtml(text: string | undefined): string | undefined {
-    if (text == null) {
-        return;
-    } else if (text === '') {
+export function sanitizeHtml(text: string): string {
+    if (text === '') {
         return '';
     }
     element ??= createElement('div');
     element.textContent = String(text);
-    return element.innerHTML;
+    return element.innerHTML.replace(/\n/g, '<br>');
 }
