354982e3fc559296414275756aa102024c645886
Jacob  Parker <jacobparker1992@gmail.com>
2025-02-26 10:59:06 +0000

    fix build

--- packages/ag-charts-community/src/chart/themes/chartTheme.ts
+++ packages/ag-charts-community/src/chart/themes/chartTheme.ts
@@ -266,9 +266,6 @@ export class ChartTheme {
                 enabled: true,
                 darkTheme: IS_DARK_THEME,
                 delay: 0,
-                position: {
-                    type: 'pointer',
-                },
                 pagination: {
                     enabled: false,
                 },
