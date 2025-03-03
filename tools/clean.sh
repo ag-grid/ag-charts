6af5119e637735f1e888c11a7613519e456931a9
Tak Tran <contact@tutaktran.com>
2025-03-03 16:04:02 +0000

    AG-14158 - Remove the `.astro` folder when running `nx clean`

--- tools/clean.sh
+++ tools/clean.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-npx rimraf -g dist/ 'packages/*/dist/' 'libraries/*/dist/' 'plugins/*/dist/' 'external/*/dist/' 'packages/ag-charts-website/node_modules/.vite/'
+npx rimraf -g dist/ 'packages/*/dist/' 'libraries/*/dist/' 'plugins/*/dist/' 'external/*/dist/' 'packages/ag-charts-website/node_modules/.vite/ packages/ag-charts-website/.astro/'
 nx reset --only-cache && rimraf .nx/
 echo Cleaned
