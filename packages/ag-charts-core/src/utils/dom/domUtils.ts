c5c377ae2fe70b248b177b71e766736cebdfe5a3
Alan Treadway <alan.treadway@ag-grid.com>
2025-03-04 11:42:44 +0000

    Fix build.

--- packages/ag-charts-core/src/utils/dom/domUtils.ts
+++ packages/ag-charts-core/src/utils/dom/domUtils.ts
@@ -3,7 +3,7 @@ import { getWindow } from './globalsProxy';
 export function parseColor(color: string): string | null {
     // getWindow is required to make sure this works on our CI.
     // Using Option instead of createElement because it should be faster.
-    const OptionConstructor = Option ?? getWindow('Option');
+    const OptionConstructor = getWindow<new () => any>('Option');
     const { style } = new OptionConstructor();
     style.color = color;
     return style.color || null;
