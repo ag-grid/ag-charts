57781d23bd9d943dca16ad52fd5e3a0ca9d65060
Jacob  Parker <jacobparker1992@gmail.com>
2025-03-05 11:00:23 +0000

    Add radial gradient class

---
+++ packages/ag-charts-community/src/scene/gradient/radialGradient.ts
@@ -0,0 +1,15 @@
+import type { BBox } from '../bbox';
+import { type ColorSpace, Gradient } from './gradient';
+import type { GradientColorStop } from './stops';
+
+export class RadialGradient extends Gradient {
+    constructor(colorSpace: ColorSpace, stops: GradientColorStop[], bbox?: BBox) {
+        super(colorSpace, stops, bbox);
+    }
+
+    protected override createCanvasGradient(ctx: CanvasRenderingContext2D, bbox: BBox): CanvasGradient | undefined {
+        const cx = bbox.x + bbox.width * 0.5;
+        const cy = bbox.y + bbox.height * 0.5;
+        return ctx.createRadialGradient(cx, cy, 0, cx, cy, Math.hypot(bbox.width * 0.5, bbox.height * 0.5));
+    }
+}
