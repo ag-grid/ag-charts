efb7db8beefd8b44e7c003f37173f0e7c80747e6
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-03-07 09:41:48 +0000

    AG-13860 - fix sankey series link gradient fill

--- packages/ag-charts-enterprise/src/series/sankey/sankeySeriesProperties.ts
+++ packages/ag-charts-enterprise/src/series/sankey/sankeySeriesProperties.ts
@@ -68,8 +68,8 @@ class SankeySeriesLabelProperties extends Label<AgSankeySeriesLabelFormatterPara
 }
 
 class SankeySeriesLinkProperties extends BaseProperties<AgSankeySeriesLinkOptions<any>> {
-    @Validate(COLOR_STRING, { optional: true })
-    fill: string | undefined = undefined;
+    @Validate(OR(COLOR_GRADIENT, COLOR_STRING), { optional: true })
+    fill: AgFillType | undefined = undefined;
 
     @Validate(RATIO)
     fillOpacity = 1;
