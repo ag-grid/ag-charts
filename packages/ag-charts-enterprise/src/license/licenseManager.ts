badff00e0d111dbb88bdd517d75496fb082afcd8
seanlandsman <sean@thelandsmans.com>
2025-03-07 17:09:22 +0000

    Reset license

--- packages/ag-charts-enterprise/src/license/licenseManager.ts
+++ packages/ag-charts-enterprise/src/license/licenseManager.ts
@@ -21,7 +21,7 @@ const LICENSE_TYPES = {
 const LICENSING_HELP_URL = 'https://ag-grid.com/charts/licensing/';
 
 export class LicenseManager {
-    private static readonly RELEASE_INFORMATION: string = 'MTc0MTE4NDM2MDU0OA==';
+    private static readonly RELEASE_INFORMATION: string = 'MTczMTQ5NDc0NzMyNw==';
     private licenseKey?: string;
     private gridContext: boolean = false;
     private watermarkMessage: string | undefined = undefined;
