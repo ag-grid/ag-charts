42a3e77303014f9fc54b7dd3eaa553526919f783
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-03-18 12:34:37 +0000

    AG-13865 - Fix boxplot

--- packages/ag-charts-enterprise/src/series/box-plot/boxPlotSeries.ts
+++ packages/ag-charts-enterprise/src/series/box-plot/boxPlotSeries.ts
@@ -21,7 +21,6 @@ const {
     ContinuousScale,
     ChartAxisDirection,
     motion,
-    isGradientOrPatternFill,
 } = _ModuleSupport;
 
 class BoxPlotSeriesNodeEvent<
@@ -507,28 +506,24 @@ export class BoxPlotSeries extends _ModuleSupport.AbstractBarSeries<
 
     getFormattedStyles(nodeDatum: BoxPlotNodeDatum, scope: 'tooltip' | 'node' | 'highlight'): AgBoxPlotSeriesStyle {
         const { id: seriesId, properties } = this;
-        const { xKey, minKey, q1Key, medianKey, q3Key, maxKey, itemStyler, cornerRadius, defaultColorRange } =
-            properties;
-        const { datum, stroke, strokeWidth, strokeOpacity, lineDash, lineDashOffset, cap, whisker } = nodeDatum;
-        let fill;
-        let fillOpacity: number | undefined;
-
-        // @todo(AG-11876) Use fillOpacity to match area, range area, radar area, chord, and sankey series
-        const useFakeFill = true;
-        if (useFakeFill) {
-            fill = nodeDatum.fill;
-            fillOpacity = properties.fillOpacity;
-        } else if (isGradientOrPatternFill(nodeDatum.fill)) {
-            fill = nodeDatum.fill;
-        } else {
-            fill = nodeDatum.fill;
-            fillOpacity = properties.fillOpacity;
-        }
+        const {
+            xKey,
+            minKey,
+            q1Key,
+            medianKey,
+            q3Key,
+            maxKey,
+            itemStyler,
+            cornerRadius,
+            defaultColorRange,
+            fillOpacity,
+        } = properties;
+        const { datum, fill, stroke, strokeWidth, strokeOpacity, lineDash, lineDashOffset, cap, whisker } = nodeDatum;
 
         let styles: Required<AgBoxPlotSeriesStyle> = this.getShapeStyle(
             {
                 fill,
-                fillOpacity: fillOpacity!,
+                fillOpacity,
                 stroke,
                 strokeWidth,
                 strokeOpacity,
