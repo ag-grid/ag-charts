1fd47dddec5abf1d9db53c716c3f6c701d84ad72
iMoses <ido.moshe@ag-grid.com>
2025-03-18 11:53:04 +0000

    Fix broken type (merge leftovers)

--- packages/ag-charts-types/src/chart/legendOptions.ts
+++ packages/ag-charts-types/src/chart/legendOptions.ts
@@ -1,4 +1,4 @@
-import type { AgFillType } from '../series/cartesian/commonOptions';
+import type { AgColorType } from '../series/cartesian/commonOptions';
 import type { Formatter } from './callbackOptions';
 import type { AgPreventableEvent } from './eventOptions';
 import type { AgMarkerShape, CssColor, FontFamily, FontSize, FontStyle, FontWeight, Opacity, PixelSize } from './types';
@@ -140,7 +140,7 @@ export interface AgPaginationMarkerOptions {
 
 export interface AgPaginationMarkerStyle {
     /** The fill colour to use for the pagination button markers. */
-    fill?: AgFillType;
+    fill?: AgColorType;
     /** Opacity of the pagination buttons. */
     fillOpacity?: Opacity;
     /** The colour to use for the button strokes. */
