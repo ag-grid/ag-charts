5195aaac6edfe7fc3e0bb93dd85d547d1bd219ab
Jacob  Parker <jacobparker1992@gmail.com>
2025-03-19 10:15:11 +0000

    AG-13206 Disable animations for category axis

--- packages/ag-charts-community/src/scale/groupedCategoryScale.ts
+++ packages/ag-charts-community/src/scale/groupedCategoryScale.ts
@@ -1,6 +1,12 @@
 import { CategoryScale } from './categoryScale';
+import type { NormalizedDomain } from './scale';
 
 export class GroupedCategoryScale<D, I = number> extends CategoryScale<D, I> {
+    override normalizeDomains(...domains: D[][]): NormalizedDomain<D> {
+        const { domain } = super.normalizeDomains(...domains);
+        return { domain, animatable: false };
+    }
+
     protected override getIndex(value: D) {
         return super.getIndex(value) ?? this.getMatchIndex(value);
     }
