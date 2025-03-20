4f428948b9aaec5bad8da199498d828c59596bcb
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-03-20 15:37:30 +0000

    AG-14473 - Add series fill docs page

---
+++ packages/ag-charts-website/src/content/docs/fills/_examples/pattern-fill/data.ts
@@ -0,0 +1,9 @@
+export function getData() {
+    return [
+        { value: 26, name: 'Category A', radius: 0.35 },
+        { value: 25, name: 'Category B', radius: 0.3 },
+        { value: 15, name: 'Category C', radius: 0.25 },
+        { value: 10, name: 'Category D', radius: 0.2 },
+        { value: 5, name: 'Category E', radius: 0.15 },
+    ];
+}
