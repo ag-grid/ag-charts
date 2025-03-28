bc1100f5d83bd5a87d6549f85d24a00a4bd641c2
Alan Treadway <alan.treadway@ag-grid.com>
2025-03-28 12:57:33 +0000

    Increase e2e test parallelisation on CI.

--- packages/ag-charts-website/playwright.config.ts
+++ packages/ag-charts-website/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Limit parallel tests on CI. */
-    workers: process.env.CI ? 4 : undefined,
+    workers: process.env.CI ? 3 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
         [
