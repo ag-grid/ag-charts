087f301ee560096a9af651ef3199bc5c70549b8b
Alan Treadway <alan.treadway@ag-grid.com>
2025-04-01 16:50:18 +0100

    Improve GHA self-hosted debug runner script.

--- external/ag-shared/scripts/gha-debug/runner.sh
+++ external/ag-shared/scripts/gha-debug/runner.sh
@@ -21,12 +21,21 @@ fi
 workdir=$(mktemp -d)
 echo "Using workdir: ${workdir}"
 
-trap "docker stop github-runner && rm -rf ${workdir}" EXIT
+trap "docker stop github-runner gha-cache-server && rm -rf ${workdir}" EXIT
+
+docker run -d --rm --name gha-cache-server \
+    -e API_BASE_URL="http://localhost:3000" \
+    -p 3000:3000 \
+    -v cache-data:/app/.data \
+    ghcr.io/falcondev-oss/github-actions-cache-server:latest
 
 docker run -d --rm --name github-runner \
   -e REPO_URL="https://github.com/ag-grid/${repo}" \
-  -e RUNNER_NAME="${HOSTNAME}" \
+  -e RUNNER_NAME="$(hostname)" \
   -e RUNNER_TOKEN="${RUNNER_TOKEN}" \
+  -e RUNNER_SCOPE="repo" \
+  -e RUNNER_GROUP="default" \
+  -e ACTIONS_RESULTS_URL="http://localhost:3000" \
   -e RUNNER_WORKDIR="/tmp/github-runner-your-repo" \
   -e LABELS="ubuntu-debug,ubuntu-debug-${RUNNER_NAME}" \
   -v /var/run/docker.sock:/var/run/docker.sock \
