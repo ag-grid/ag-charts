f5f5c284365a76234746ad16fd473d3684744ec0
Oli Legat <olivier.legat@ag-grid.com>
2025-04-02 15:16:59 +0100

    AG-8876 Add placeholder `contextMenu.items[]` contract option

--- packages/ag-charts-community/src/chart/commonOptionsDefs.ts
+++ packages/ag-charts-community/src/chart/commonOptionsDefs.ts
@@ -332,6 +332,7 @@ export const commonChartOptionsDefs: OptionsDefs<Omit<AgBaseThemeableChartOption
     },
     contextMenu: {
         enabled: boolean,
+        items: unknown,
         extraActions: contextMenuActionsArray,
         extraSeriesAreaActions: contextMenuActionsArray,
         extraNodeActions: contextMenuActionsArray,
--- packages/ag-charts-types/src/chart/contextMenuOptions.ts
+++ packages/ag-charts-types/src/chart/contextMenuOptions.ts
@@ -6,19 +6,67 @@ import type {
 } from './eventOptions';
 import type { AgChartLegendContextMenuEvent } from './legendOptions';
 
+interface ContextMenuItemMixin<TType extends AgContextMenuItemType, TEvent extends  { type: string; event: Event }> {
+    /**  TODO: writeme. */
+    type: TType;
+    /**  TODO: writeme. */
+    label: string;
+    /**  TODO: writeme. */
+    iconUrl?: string;
+    /**  TODO: writeme. */
+    enable?: boolean;
+    /**  TODO: writeme. */
+    action?: (event: TEvent) => void;
+    /**  TODO: writeme. */
+    items?: AgContextMenuItem;
+}
+
+type AgContextMenuItemLiteral =
+    | 'defaults'
+    | 'download'
+    | 'zoom-to-cursor'
+    | 'pan-to-cursor'
+    | 'toggle-series-visibility'
+    | 'toggle-other-series'
+    | 'reset-zoom';
+
+type AgContextMenuItemType = 'all' | 'series-area' | 'node' | 'legend';
+
+type AgContextMenuItem =
+    | AgContextMenuItemLiteral
+    | ContextMenuItemMixin<'all', AgChartContextMenuEvent>
+    | ContextMenuItemMixin<'series-area', AgSeriesAreaContextMenuActionEvent>
+    | ContextMenuItemMixin<'node', AgNodeContextMenuActionEvent>
+    | ContextMenuItemMixin<'legend', AgChartLegendContextMenuEvent>;
+
 export interface AgContextMenuOptions {
     /**  Whether to show the context menu. */
     enabled?: boolean;
-    /**  Custom actions displayed in the context menu when right-clicking anywhere on the chart. */
+    /**  TODO: writeme. */
+    items?: AgContextMenuItem[];
+    /**
+     * Custom actions displayed in the context menu when right-clicking anywhere on the chart.
+     * @deprecated v11.3.0 use `items` instead.
+     */
     extraActions?: AgContextMenuAction<AgChartContextMenuEvent>[];
-    /**  Custom actions displayed in the context menu when right-clicking anywhere on the series area. */
+    /**
+     * Custom actions displayed in the context menu when right-clicking anywhere on the series area.
+     * @deprecated v11.3.0 use `items` instead.
+     */
     extraSeriesAreaActions?: AgContextMenuAction<AgSeriesAreaContextMenuActionEvent>[];
-    /**  Custom actions displayed in the context menu when right-clicking on a series node. */
+    /**
+     * Custom actions displayed in the context menu when right-clicking on a series node.
+     * @deprecated v11.3.0 use `items` instead.
+     */
     extraNodeActions?: AgContextMenuAction<AgNodeContextMenuActionEvent>[];
-    /**  Custom actions displayed in the context menu when right-clicking on a legend item. */
+    /**
+     * Custom actions displayed in the context menu when right-clicking on a legend item.
+     * @deprecated v11.3.0 use `items` instead.
+     */
     extraLegendItemActions?: AgContextMenuAction<AgChartLegendContextMenuEvent>[];
 }
 
+/** @deprecated v11.3.0 use `AgContextMenuItem` instead. */
 export interface AgContextMenuAction<TEvent = AgNodeContextMenuActionEvent> {
     /** The text to display in the context menu for the custom action. */
     label: string;
