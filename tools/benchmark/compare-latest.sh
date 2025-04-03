1fb15a10527dda53fb5eaee6d27f109cade2fa36
Alan Treadway <alan.treadway@ag-grid.com>
2025-04-03 13:49:55 +0100

    Fix CI PR base branch detection (don't assume latest).

--- tools/benchmark/compare-latest.sh
+++ tools/benchmark/compare-latest.sh
@@ -1,12 +1,13 @@
 
 #!/bin/bash
 
-set -eux
+set -eu
 
 export NX_DAEMON=false
 yarn nx daemon --stop
 
-base=$(git merge-base HEAD origin/latest)
+base_name=${1:-latest}
+base=$(git merge-base HEAD origin/${base_name})
 head=$(git rev-parse HEAD)
 branch=$(git rev-parse --abbrev-ref HEAD)
 tools_dir=$(dirname "$0")
@@ -14,24 +15,24 @@ root=$(git rev-parse --show-toplevel)
 
 trap 'git checkout ${branch}' INT TERM ERR EXIT
 
-if [[ ${branch} == "latest" ]] ; then
-    echo "You are on the latest branch, please switch to a different branch to run this script."
+if [[ ${branch} == "${base_name}" ]] ; then
+    echo "You are on the ${base_name} branch, please switch to a different branch to run this script."
     exit 1
 fi
 
-echo "Running benchmarks on ${branch} against latest"
-echo "${base} (latest) vs ${head} (${branch})"
+echo "Running benchmarks on ${branch} against ${base_name}"
+echo "${base} (${base_name}) vs ${head} (${branch})"
 
 git checkout ${base}
 yarn nx run-many -t benchmark --parallel 1 --exclude all
 yarn nx run-many -t benchmark --parallel 1 --exclude all
-node ${tools_dir}/collate-reports.js "latest-${base}"
+node ${tools_dir}/collate-reports.js "${base_name}-${base}"
 
 git checkout ${head}
 yarn nx run-many -t benchmark --parallel 1 --exclude all
 yarn nx run-many -t benchmark --parallel 1 --exclude all
 node ${tools_dir}/collate-reports.js "${branch}-${head}"
 
-node ${tools_dir}/compare-versions.js --report-only --base latest-${base} --compare ${branch}-${head} >${root}/reports/benchmark.log
+node ${tools_dir}/compare-versions.js --report-only --base ${base_name}-${base} --compare ${branch}-${head} >${root}/reports/benchmark.log
 cat ${root}/reports/benchmark.log
 echo "Benchmark results saved to ${root}/reports/benchmark.log"
