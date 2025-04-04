c40d724f95c0b7b529cf7cc6586a5d4399da63a9
Alan Treadway <alan.treadway@ag-grid.com>
2025-04-04 16:40:46 +0100

    Add Playwright network caching for non-local resources.

--- packages/ag-charts-website/playwright.config.ts
+++ packages/ag-charts-website/playwright.config.ts
@@ -17,8 +17,6 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
-    /* Limit parallel tests on CI. */
-    workers: process.env.CI ? 2 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
         [
