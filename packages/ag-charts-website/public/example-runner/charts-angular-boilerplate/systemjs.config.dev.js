fca29e861efc41f9f23441a813ac06f0fab14bfb
Alan Treadway <alan.treadway@ag-grid.com>
2025-04-07 12:12:29 +0100

    AG-14669 - Revert Angular example TS version to 4.4.

--- packages/ag-charts-website/public/example-runner/charts-angular-boilerplate/systemjs.config.dev.js
+++ packages/ag-charts-website/public/example-runner/charts-angular-boilerplate/systemjs.config.dev.js
@@ -50,7 +50,7 @@
 
             ts: 'npm:plugin-typescript@8.0.0/lib/plugin.js',
             tslib: 'npm:tslib@2.3.1/tslib.js',
-            typescript: 'npm:typescript@5.4.5/lib/typescript.min.js',
+            typescript: 'npm:typescript@4.4/lib/typescript.min.js',
 
             // appLocation comes from index.html
             app: appLocation,
