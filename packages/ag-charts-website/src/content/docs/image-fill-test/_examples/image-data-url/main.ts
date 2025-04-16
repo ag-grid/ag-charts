4fa66f813beac7a3e571e5361e2952b167de44df
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-04-16 17:37:16 +0100

    AG-14558 - update image test page examples

--- packages/ag-charts-website/src/content/docs/image-fill-test/_examples/image-data-url/main.ts
+++ packages/ag-charts-website/src/content/docs/image-fill-test/_examples/image-data-url/main.ts
@@ -18,6 +18,7 @@ const options: AgChartOptions = {
             yKey: 'Tate Modern',
             yName: 'Tate Modern',
             normalizedTo: 100,
+            fillOpacity: 1,
             fill: {
                 type: 'image',
                 url: ImageDataURL,
