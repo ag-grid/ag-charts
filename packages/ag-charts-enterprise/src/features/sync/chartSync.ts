bc9215da4438d1e26f9fdba91eb118633f04f5f3
Alan Treadway <alan.treadway@ag-grid.com>
2025-04-17 16:49:58 +0100

    AG-13979 - Fix interaction sync for non-key-based-cases.

--- packages/ag-charts-enterprise/src/features/sync/chartSync.ts
+++ packages/ag-charts-enterprise/src/features/sync/chartSync.ts
@@ -124,6 +124,8 @@ export class ChartSync extends BaseProperties implements _ModuleSupport.ModuleIn
     ) {
         const { syncManager } = this.moduleContext;
 
+        const { domainsByKey = {} } = syncManager.getGroupState(this.groupId) ?? {};
+        const hasKeyBasedDomains = Object.keys(domainsByKey).length > 0;
         for (const [chart, axis] of syncManager.getGroupSiblingAxes(this.groupId)) {
             if (!chart.modulesManager.getModule<ChartSync>('sync')?.nodeInteraction) continue;
             if (!CartesianAxis.is(axis) || axis.direction !== mainDirection) continue;
@@ -134,7 +136,7 @@ export class ChartSync extends BaseProperties implements _ModuleSupport.ModuleIn
                 .map(this.findMatchingNodes(axis, mainDirection, valueIsDate, eventValue))
                 .filter(isDefined);
 
-            if (this.domainMode === 'key') {
+            if (this.domainMode === 'key' && hasKeyBasedDomains) {
                 const secondaryKey = `${secondaryDirection}Key` as const;
                 const secondaryValue = _ModuleSupport.isObjectWithProperty(event.currentHighlight, secondaryKey)
                     ? event.currentHighlight?.[secondaryKey]
