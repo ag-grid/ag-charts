34a72b791c0857ca82a46a49c91ff2a00dfaf093
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-04-17 14:02:33 +0100

    AG-14558 - Image docs example

---
+++ packages/ag-charts-website/src/content/docs/fills/_examples/image-fill-customisation/main.ts
@@ -0,0 +1,95 @@
+import { AgCartesianChartOptions, AgCharts } from 'ag-charts-community';
+
+import { getData } from './data';
+
+const options: AgCartesianChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    title: {
+        text: 'The Musical Spectrum',
+    },
+    subtitle: {
+        text: '50 musical instruments by popularity, invention era, and learning curve',
+    },
+    seriesArea: {
+        padding: {
+            right: 30,
+            left: 30,
+            top: 30,
+            bottom: 30,
+        },
+    },
+    series: [
+        {
+            type: 'bubble',
+            xKey: 'year_of_invention',
+            xName: 'Year of Invention',
+            yKey: 'difficulty',
+            yName: 'Difficulty',
+            sizeKey: 'popularity',
+            sizeName: 'Popularity',
+            labelKey: 'instrument',
+            labelName: 'Instrument',
+            label: { enabled: true },
+            maxSize: 50,
+            size: 15,
+            itemStyler: ({ datum }) => {
+                return {
+                    fill: {
+                        type: 'image',
+                        url: datum.icon,
+                        backgroundFill: '#f1f0de',
+                    },
+                };
+            },
+        },
+    ],
+    axes: [
+        {
+            position: 'bottom',
+            type: 'number',
+            nice: false,
+            title: {
+                text: 'Year Of Invention',
+            },
+            gridLine: {
+                enabled: false,
+            },
+            crosshair: {
+                label: {
+                    format: `#{d} CE`,
+                },
+            },
+            label: {
+                formatter: ({ value }) => (value === -250 ? `BCE` : value === 250 ? `CE` : `${value} CE`),
+            },
+            interval: {
+                values: [-250, 250],
+            },
+            crossLines: [
+                {
+                    type: 'line',
+                    value: 0,
+                    lineDash: [1, 2],
+                },
+            ],
+        },
+        {
+            position: 'right',
+            type: 'number',
+            gridLine: {
+                enabled: false,
+            },
+            min: 0,
+            max: 10,
+            label: {
+                formatter: ({ value }) => (value === 0 ? `Easy` : value === 10 ? `Difficult` : value),
+            },
+            interval: {
+                values: [0, 10],
+            },
+        },
+    ],
+};
+
+const chart = AgCharts.create(options);
