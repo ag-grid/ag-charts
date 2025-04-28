7a012023d639bf3417e6103abebd777fefa173be
Kyler Phillips <kylerphillips@hotmail.co.uk>
2025-04-28 12:19:16 +0100

    AG-14135 Rename src refs

--- external/ag-website-shared/src/markdoc/tags/touchimage.ts
+++ external/ag-website-shared/src/markdoc/tags/touchimage.ts
@@ -3,7 +3,7 @@ import type { Render } from '@astrojs/markdoc/config';
 import type { Config, Schema } from '@markdoc/markdoc';
 
 export const touchImage: Schema<Config, Render> = {
-    render: component('../../external/ag-website-shared/src/components/touch-device/TouchImage.astro'),
+    render: component('../../external/ag-website-shared/src/components/touch-image/TouchImage.astro'),
     attributes: {
         title: { type: String },
         playlist: { type: String },
