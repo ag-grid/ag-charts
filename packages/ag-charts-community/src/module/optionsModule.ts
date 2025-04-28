0e58b68d940afea3f47e45e6250224e21a9080d8
Oli Legat <olivier.legat@ag-grid.com>
2025-04-28 09:54:23 +0100

    AG-8876 Fix build

--- packages/ag-charts-community/src/module/optionsModule.ts
+++ packages/ag-charts-community/src/module/optionsModule.ts
@@ -158,13 +158,13 @@ export class ChartOptions<T extends AgChartOptions = AgChartOptions> {
 
             this.userOptions = deepClone(merge(deltaOptions, baseChartOptions.userOptions), {
                 ...ChartOptions.OPTIONS_CLONE_OPTS,
-                seen: new Set(),
+                seen: [],
             }) as T;
         } else {
             // Full update case.
             this.userOptions = deepClone(currentUserOptions ?? newUserOptions, {
                 ...ChartOptions.OPTIONS_CLONE_OPTS,
-                seen: new Set(),
+                seen: [],
             });
             this.specialOverrides = this.specialOverridesDefaults({ ...specialOverrides });
         }
