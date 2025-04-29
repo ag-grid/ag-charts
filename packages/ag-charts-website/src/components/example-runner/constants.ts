371a23c91206a9f7021827eac76c5244a462716a
Tak Tran <contact@tutaktran.com>
2025-04-29 09:57:21 +0100

    AG-14734 - Only add e2e check script for example in new tab

--- packages/ag-charts-website/src/components/example-runner/constants.ts
+++ packages/ag-charts-website/src/components/example-runner/constants.ts
@@ -1,6 +1,9 @@
 export const POST_INIT_MESSAGE_START = '<!-- INIT MESSAGE START -->';
 export const POST_INIT_MESSAGE_END = '<!-- INIT MESSAGE END -->';
 
+export const E2E_STYLE_START = '<!-- E2E STYLE START -->';
+export const E2E_STYLE_END = '<!-- E2E STYLE END -->';
+
 /**
  * DOM selector to know that the library has loaded
  */
