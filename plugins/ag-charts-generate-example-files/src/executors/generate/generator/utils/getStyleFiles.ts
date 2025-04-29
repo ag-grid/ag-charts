49e48cdcf0ee00a65cbf2cfa0f7e1e2e9a023a16
Tak Tran <contact@tutaktran.com>
2025-04-29 15:13:44 +0100

    AG-14734 - Use shared example controls file for generating examples

--- plugins/ag-charts-generate-example-files/src/executors/generate/generator/utils/getStyleFiles.ts
+++ plugins/ag-charts-generate-example-files/src/executors/generate/generator/utils/getStyleFiles.ts
@@ -6,7 +6,7 @@ import { getFileList } from './fileUtils';
 
 // Relative to root
 const EXAMPLE_STYLES_FILE_PATH =
-    './plugins/ag-charts-generate-example-files/src/executors/generate/generator/styles/example-controls.css';
+    './external/ag-website-shared/src/components/example-runner/styles/example-controls.css';
 const EXAMPLE_STYLE_FILE_NAME = 'ag-example-styles.css';
 
 export const getStyleFiles = async ({
