ab5e30d877206012ff5a56b6634b5a4ecf9088e2
Alan Treadway <alan.treadway@ag-grid.com>
2025-04-30 11:21:46 +0100

    AG-13979 - Add e2e test for horizontal bar sync.

---
+++ packages/ag-charts-website/src/content/docs/sync-test/_examples/multi-series-horizontal-sync/main.ts
@@ -0,0 +1,75 @@
+import { AgCartesianChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { getBenchmark1Data, getBenchmark2Data } from './data';
+import { formatBytes, formatMillis, labelFormatter } from './utils';
+
+const commonOptions: AgCartesianChartOptions = {
+    sync: { axes: 'xy' },
+    axes: [
+        {
+            type: 'category',
+            position: 'left',
+        },
+        {
+            type: 'number',
+            position: 'bottom',
+            keys: [`timeMs`],
+            label: { formatter: labelFormatter(formatMillis) },
+        },
+        {
+            type: 'number',
+            position: 'top',
+            keys: [`heapUsed`, `canvasBytes`],
+            label: { formatter: labelFormatter(formatBytes) },
+        },
+    ],
+    series: [
+        {
+            type: 'bar',
+            direction: 'horizontal',
+            xKey: 'name',
+            yKey: `timeMs`,
+            yName: `Time`,
+            stackGroup: 'time',
+        },
+        {
+            type: 'bar',
+            direction: 'horizontal',
+            xKey: 'name',
+            yKey: `heapUsed`,
+            yName: `Heap`,
+            stackGroup: 'memory',
+        },
+        {
+            type: 'bar',
+            direction: 'horizontal',
+            xKey: 'name',
+            yKey: `canvasBytes`,
+            yName: `Canvas`,
+            stackGroup: 'memory',
+        },
+    ],
+    // tooltip: { mode: 'shared' },
+};
+
+const chartOptions1 = {
+    ...commonOptions,
+    container: document.getElementById('myChart1'),
+    title: {
+        text: 'Benchmark 1',
+    },
+    data: getBenchmark1Data(),
+};
+
+AgCharts.create(chartOptions1);
+
+const chartOptions2 = {
+    ...commonOptions,
+    container: document.getElementById('myChart2'),
+    title: {
+        text: 'Benchmark 2',
+    },
+    data: getBenchmark2Data(),
+};
+
+AgCharts.create(chartOptions2);
