382ed4a39eb4c136865fe63a09824855949630ea
Jacob  Parker <jacobparker1992@gmail.com>
2025-04-30 12:53:41 +0100

    AG-13561 Secondary axis improvements

--- packages/ag-charts-community/src/util/secondaryAxisTicks.test.ts
+++ packages/ag-charts-community/src/util/secondaryAxisTicks.test.ts
@@ -2,45 +2,69 @@ import { expect, test } from '@jest/globals';
 
 import { calculateNiceSecondaryAxis } from './secondaryAxisTicks';
 
-function ticks(a: number, b: number, count: number): number[] {
-    const scale = {
-        toDomain: (d: any) => d,
-    };
-    const { ticks: result } = calculateNiceSecondaryAxis(scale, [a, b], { unzoomed: count, zoomed: count }, false);
-    return result;
-}
-
-function compareTicks(actual: number[], array: number[]) {
-    expect(actual).toHaveLength(array.length);
-    actual.forEach((tick, index) => {
-        expect(tick).toBeCloseTo(array[index], 10);
-    });
-}
-
 describe('secondaryAxisTicks', () => {
-    test('ticks', () => {
-        const ticks_49_216 = [40, 80, 120, 160, 200, 240];
-        const ticks_0_216 = [0, 50, 100, 150, 200, 250];
-        const ticks_100491_135198 = [100000, 104000, 108000, 112000, 116000, 120000, 124000, 128000, 132000, 136000];
-        const ticks_1009_1018 = [1009, 1011, 1013, 1015, 1017, 1019];
-        const ticks_7_26 = [7, 11, 15, 19, 23, 27];
-        const ticks_n7_26 = [-7, -3, 1, 5, 9, 13, 17, 21, 25, 29];
-        const ticks_1000_1002 = [1000, 1000.4, 1000.8, 1001.2, 1001.6, 1002, 1002.4, 1002.8, 1003.2, 1003.6];
-        const ticks_n26_n7 = [-27, -23, -19, -15, -11, -7];
-        const ticks_f002_f004 = [0, 0.001, 0.002, 0.003, 0.004, 0.005];
-        const ticks_11_20 = [11, 13, 15, 17, 19, 21, 23, 25, 27];
-        const ticks_0_101 = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180];
-
-        compareTicks(ticks(49.9, 216.4, 6), ticks_49_216);
-        compareTicks(ticks(0, 216.4, 6), ticks_0_216);
-        compareTicks(ticks(100491, 135198, 10), ticks_100491_135198);
-        compareTicks(ticks(1009.6, 1018.2, 6), ticks_1009_1018);
-        compareTicks(ticks(7, 26.5, 6), ticks_7_26);
-        compareTicks(ticks(-7, 26.5, 10), ticks_n7_26);
-        compareTicks(ticks(1000, 1002, 10), ticks_1000_1002);
-        compareTicks(ticks(-26.5, -7, 6), ticks_n26_n7);
-        compareTicks(ticks(0.002, 0.004, 6), ticks_f002_f004);
-        compareTicks(ticks(11, 20, 9), ticks_11_20);
-        compareTicks(ticks(0, 101, 10), ticks_0_101);
+    test.each([
+        {
+            domain: [49.9, 216.4],
+            count: 6,
+            actual: [0, 50, 100, 150, 200, 250],
+        },
+        {
+            domain: [0, 216.4],
+            count: 6,
+            actual: [0, 50, 100, 150, 200, 250],
+        },
+        {
+            domain: [100491, 135198],
+            count: 10,
+            actual: [100000, 105000, 110000, 115000, 120000, 125000, 130000, 135000, 140000, 145000],
+        },
+        {
+            domain: [1009.6, 1018.2],
+            count: 6,
+            actual: [1009, 1011, 1013, 1015, 1017, 1019],
+        },
+        {
+            domain: [7, 26.5],
+            count: 6,
+            actual: [5, 10, 15, 20, 25, 30],
+        },
+        {
+            domain: [-7, 26.5],
+            count: 10,
+            actual: [-10, -5, 0, 5, 10, 15, 20, 25, 30, 35],
+        },
+        {
+            domain: [1000, 1002],
+            count: 10,
+            actual: [1000, 1000.5, 1001, 1001.5, 1002, 1002.5, 1003, 1003.5, 1004, 1004.5],
+        },
+        {
+            domain: [-26.5, -7],
+            count: 6,
+            actual: [-30, -25, -20, -15, -10, -5],
+        },
+        {
+            domain: [0.002, 0.004],
+            count: 6,
+            actual: [0, 0.001, 0.002, 0.003, 0.004, 0.005],
+        },
+        {
+            domain: [11, 20],
+            count: 9,
+            actual: [10, 12, 14, 16, 18, 20, 22, 24, 26],
+        },
+        {
+            domain: [0, 101],
+            count: 10,
+            actual: [0, 20, 40, 60, 80, 100, 120, 140, 160, 180],
+        },
+    ])('Ticks for $domain (count $count)', ({ domain, count, actual }) => {
+        const scale = {
+            toDomain: (d: any) => d,
+        };
+        const { ticks } = calculateNiceSecondaryAxis(scale, domain, { unzoomed: count, zoomed: count }, false);
+        expect(ticks).toHaveLength(actual.length);
+        expect(ticks).toMatchObject(actual.map((t) => expect.closeTo(t, 10)));
     });
 });
