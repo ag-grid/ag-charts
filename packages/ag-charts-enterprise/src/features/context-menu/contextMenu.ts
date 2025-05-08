0160c5e75655e59c0848c70904541da8b7bba1ec
Oli Legat <olivier.legat@ag-grid.com>
2025-05-08 18:53:41 +0100

    AG-8876 TC1 nx format

--- packages/ag-charts-enterprise/src/features/context-menu/contextMenu.ts
+++ packages/ag-charts-enterprise/src/features/context-menu/contextMenu.ts
@@ -217,8 +217,8 @@ export class ContextMenu extends _ModuleSupport.BaseModuleInstance implements _M
 
         const canvasBounds = this.ctx.widgets.chartWidget.getElement().getBoundingClientRect();
         const buttonClientRect = button.getBoundingClientRect();
-        const remainingSpaceOnRight = (canvasBounds.left + canvasBounds.width) - buttonClientRect.right;
-        const remainingSpaceOnLeft = buttonClientRect.left - canvasBounds.left
+        const remainingSpaceOnRight = canvasBounds.left + canvasBounds.width - buttonClientRect.right;
+        const remainingSpaceOnLeft = buttonClientRect.left - canvasBounds.left;
         const menuOffsetWidth = menu.getElement().offsetWidth;
 
         if (remainingSpaceOnRight >= menuOffsetWidth) {
