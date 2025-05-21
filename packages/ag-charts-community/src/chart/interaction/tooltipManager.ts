b162981adc0d46a727f4c594707b810301293405
Jacob  Parker <jacobparker1992@gmail.com>
2025-05-21 12:35:53 +0100

    Fix cone funnel series test

--- packages/ag-charts-community/src/chart/interaction/tooltipManager.ts
+++ packages/ag-charts-community/src/chart/interaction/tooltipManager.ts
@@ -1,6 +1,7 @@
 import type { DOMManager } from '../../dom/domManager';
 import type { LocaleManager } from '../../locale/localeManager';
 import type { BBoxValues } from '../../util/bboxinterface';
+import { objectsEqual } from '../../util/object';
 import { StateTracker } from '../../util/stateTracker';
 import type { SeriesTooltip } from '../series/seriesTooltip';
 import type { ErrorBoundSeriesNodeDatum, ISeries, SeriesNodeDatum } from '../series/seriesTypes';
@@ -82,7 +83,7 @@ export class TooltipManager {
         const canvasRect = this.domManager.getBoundingClientRect();
         const boundingRect = this.tooltip.bounds === 'extended' ? this.domManager.getOverlayClientRect() : canvasRect;
 
-        if (this.appliedState?.content === state?.content) {
+        if (objectsEqual(this.appliedState?.content, state?.content)) {
             const renderInstantly = this.tooltip.isVisible();
             this.tooltip.show(boundingRect, canvasRect, state?.meta, null, undefined, renderInstantly);
         } else {
