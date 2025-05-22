00a8c82920a6edbc269cb577d8e3842217afa343
Oli Legat <olivier.legat@ag-grid.com>
2025-05-22 12:35:29 +0100

    Revert "AG-11754 Throw error when `node == null`"
    This reverts commit 17b3dd68fb67a5007e6a17a013f53917381ec183.

--- plugins/ag-charts-generate-code-reference-files/src/doc-interfaces/types-utils.ts
+++ plugins/ag-charts-generate-code-reference-files/src/doc-interfaces/types-utils.ts
@@ -472,7 +472,7 @@ function getJsDoc(node: ts.Node & { jsDoc?: { getFullText(): string }[] }) {
 }
 
 export function printNode(node: ts.Node | undefined) {
-    if (node == null) throw new Error(`unexpected ${node} node`);
+    if (node == null) return null;
     try {
         return tsPrinter.printNode(ts.EmitHint.Unspecified, node, node.getSourceFile()).replace(/\n\s*/g, ' ');
     } catch (e) {
