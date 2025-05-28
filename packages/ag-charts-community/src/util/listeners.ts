c9624c25eed34ea6964295047cc9c079bbf6fbac
iMoses <ido.moshe@ag-grid.com>
2025-05-28 11:31:04 +0100

    revert changes where there's type collision with the widgets

---
+++ packages/ag-charts-community/src/util/listeners.ts
@@ -0,0 +1,55 @@
+import { Logger } from 'ag-charts-core';
+
+type Handler = (...args: any[]) => void;
+
+export type Listener<H extends Handler> = {
+    symbol?: symbol;
+    handler: H;
+};
+
+export class Listeners<EventType extends string, EventHandler extends Handler> {
+    protected readonly registeredListeners: Map<EventType, Listener<EventHandler>[]> = new Map();
+
+    public addListener<T extends EventType>(eventType: T, handler: EventHandler) {
+        const record = { symbol: Symbol(eventType), handler };
+
+        if (this.registeredListeners.has(eventType)) {
+            this.registeredListeners.get(eventType)!.push(record);
+        } else {
+            this.registeredListeners.set(eventType, [record]);
+        }
+
+        return () => this.removeListener(record.symbol);
+    }
+
+    private removeListener(eventSymbol: symbol) {
+        for (const [type, listeners] of this.registeredListeners.entries()) {
+            const matchIndex = listeners.findIndex((listener) => listener.symbol === eventSymbol);
+            if (matchIndex >= 0) {
+                listeners.splice(matchIndex, 1);
+                if (listeners.length === 0) {
+                    this.registeredListeners.delete(type);
+                }
+                break;
+            }
+        }
+    }
+
+    public dispatch(eventType: EventType, ...params: Parameters<EventHandler>): void {
+        for (const listener of this.getListenersByType(eventType)) {
+            try {
+                listener.handler(...params);
+            } catch (e) {
+                Logger.errorOnce(e);
+            }
+        }
+    }
+
+    protected getListenersByType(eventType: EventType): Listener<EventHandler>[] {
+        return this.registeredListeners.get(eventType) ?? [];
+    }
+
+    destroy() {
+        this.registeredListeners.clear();
+    }
+}
