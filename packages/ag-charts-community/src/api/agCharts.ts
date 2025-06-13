9b9b40d5834488b8e93b4be99e1269d5e38293ff
Oli Legat <olivier.legat@ag-grid.com>
2025-06-13 16:03:59 +0200

    AG-15046 Fix `nx run ag-charts-website:validate-examples`

--- packages/ag-charts-community/src/api/agCharts.ts
+++ packages/ag-charts-community/src/api/agCharts.ts
@@ -5,6 +5,7 @@ import type {
     AgFinancialChartOptions,
     AgGaugeOptions,
     AgSparklineOptions,
+    TDatumDefault,
 } from 'ag-charts-types';
 
 import { Chart } from '../chart/chart';
@@ -63,7 +64,7 @@ export abstract class AgCharts {
     /**
      * Create a new `AgChartInstance` based upon the given configuration options.
      */
-    public static create<O extends AgChartOptions>(
+    public static create<O extends AgChartOptions<TDatumDefault, any>>( // set TContext=any for backward-compatibility
         userOptions: O,
         optionsMetadata?: ChartInternalOptionMetadata
     ): AgChartInstance<O> {
