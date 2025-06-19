f7684bb411f317e62409534f5a1fc1f86a916084
Jacob  Parker <jacobparker1992@gmail.com>
2025-06-19 15:34:22 +0100

    AG-15169 Highlighting docs

--- packages/ag-charts-website/src/content/docs/series-highlighting/_examples/basic-column/main.ts
+++ packages/ag-charts-website/src/content/docs/series-highlighting/_examples/basic-column/main.ts
@@ -1,27 +1,10 @@
 import { AgChartOptions, AgCharts } from 'ag-charts-community';
 
 var data = [
-    {
-        beverage: 'Coffee',
-        Q1: 450,
-        Q2: 560,
-        Q3: 600,
-        Q4: 700,
-    },
-    {
-        beverage: 'Tea',
-        Q1: 270,
-        Q2: 380,
-        Q3: 450,
-        Q4: 520,
-    },
-    {
-        beverage: 'Milk',
-        Q1: 180,
-        Q2: 170,
-        Q3: 190,
-        Q4: 200,
-    },
+    { quarter: 'Q1', coffee: 450, tea: 270, milk: 180 },
+    { quarter: 'Q2', coffee: 560, tea: 380, milk: 170 },
+    { quarter: 'Q3', coffee: 600, tea: 450, milk: 190 },
+    { quarter: 'Q4', coffee: 700, tea: 520, milk: 200 },
 ];
 
 const options: AgChartOptions = {
@@ -29,15 +12,21 @@ const options: AgChartOptions = {
     container: document.getElementById('myChart'),
     theme: {
         overrides: {
-            bar: {
+            area: {
                 series: {
                     highlight: {
                         highlightedItem: {
-                            fill: 'red',
-                            stroke: 'maroon',
-                            strokeWidth: 4,
+                            fill: 'yellow',
+                            stroke: 'gold',
+                            strokeWidth: 2,
+                        },
+                        unhighlightedItem: {
+                            fill: 'maroon',
+                            strokeWidth: 0,
                         },
                         highlightedSeries: {
+                            fill: 'red',
+                            stroke: 'maroon',
                             strokeWidth: 2,
                         },
                         unhighlightedSeries: {
@@ -58,10 +47,30 @@ const options: AgChartOptions = {
         text: 'Based on a sample size of 200 respondents',
     },
     series: [
-        { type: 'bar', xKey: 'beverage', yKey: 'Q1', stacked: true },
-        { type: 'bar', xKey: 'beverage', yKey: 'Q2', stacked: true },
-        { type: 'bar', xKey: 'beverage', yKey: 'Q3', stacked: true },
-        { type: 'bar', xKey: 'beverage', yKey: 'Q4', stacked: true },
+        {
+            type: 'area',
+            xKey: 'quarter',
+            yKey: 'coffee',
+            yName: 'Coffee',
+            marker: { enabled: true, size: 10 },
+            stacked: true,
+        },
+        {
+            type: 'area',
+            xKey: 'quarter',
+            yKey: 'tea',
+            yName: 'Tea',
+            marker: { enabled: true, size: 10 },
+            stacked: true,
+        },
+        {
+            type: 'area',
+            xKey: 'quarter',
+            yKey: 'milk',
+            yName: 'Milk',
+            marker: { enabled: true, size: 10 },
+            stacked: true,
+        },
     ],
 };
 
