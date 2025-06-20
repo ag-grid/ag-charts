2d40b22a71285e71a15453aa6886ba8060e4609b
Laurence Roberts <laurence.roberts@ag-grid.com>
2025-06-20 12:12:12 +0100

    AG-15027 Update tooltip docs for new default shared

--- packages/ag-charts-website/src/content/docs/tooltips/_examples/default-tooltip/main.ts
+++ packages/ag-charts-website/src/content/docs/tooltips/_examples/default-tooltip/main.ts
@@ -14,13 +14,37 @@ const options: AgCartesianChartOptions = {
 const chart = AgCharts.create(options);
 
 function removeYNames() {
-    (options.series![0] as AgBarSeriesOptions).yName = undefined;
-    (options.series![1] as AgBarSeriesOptions).yName = undefined;
+    for (const series of options.series ?? []) {
+        (series as AgBarSeriesOptions).yName = undefined;
+    }
     chart.update(options);
 }
 
 function addYNames() {
     (options.series![0] as AgBarSeriesOptions).yName = 'Sweaters Made';
-    (options.series![1] as AgBarSeriesOptions).yName = 'Hats Made';
+    if (options.series![1]) (options.series![1] as AgBarSeriesOptions).yName = 'Hats Made';
+    if (options.series![2]) (options.series![2] as AgBarSeriesOptions).yName = 'Gloves Made';
+    if (options.series![3]) (options.series![3] as AgBarSeriesOptions).yName = 'Socks Made';
+    if (options.series![4]) (options.series![4] as AgBarSeriesOptions).yName = 'Sunglasses Made';
+    chart.update(options);
+}
+
+function showNumSeries(num: number) {
+    if (num === 1) {
+        options.series = [{ type: 'bar', xKey: 'month', stacked: true, yKey: 'value1', yName: 'Sweaters Made' }];
+    } else if (num === 2) {
+        options.series = [
+            { type: 'bar', xKey: 'month', stacked: true, yKey: 'value1', yName: 'Sweaters Made' },
+            { type: 'bar', xKey: 'month', stacked: true, yKey: 'hats_made', yName: 'Hats Made' },
+        ];
+    } else {
+        options.series = [
+            { type: 'bar', xKey: 'month', stacked: true, yKey: 'value1', yName: 'Sweaters Made' },
+            { type: 'bar', xKey: 'month', stacked: true, yKey: 'hats_made', yName: 'Hats Made' },
+            { type: 'bar', xKey: 'month', stacked: true, yKey: 'gloves_made', yName: 'Gloves Made' },
+            { type: 'bar', xKey: 'month', stacked: true, yKey: 'socks_made', yName: 'Socks Made' },
+            { type: 'bar', xKey: 'month', stacked: true, yKey: 'sunglasses_made', yName: 'Sunglasses Made' },
+        ];
+    }
     chart.update(options);
 }
