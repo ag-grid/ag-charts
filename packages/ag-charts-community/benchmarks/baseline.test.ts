24daadad08a31907f060afb7c0e04fe4f6d3a1de
Alan Treadway <alan.treadway@ag-grid.com>
2025-06-20 12:13:13 +0100

    Add baseline benchmarks.

--- packages/ag-charts-community/benchmarks/baseline.test.ts
+++ packages/ag-charts-community/benchmarks/baseline.test.ts
@@ -2,31 +2,33 @@ import { AgCartesianChartOptions } from 'ag-charts-types';
 
 import { benchmark, setupBenchmark } from './benchmark';
 
-const expectations = { expectedRelativeMB: 15, expectedCanvasCount: 0, autoSnapshot: false };
+const expectations = { expectedRelativeMB: 2, expectedCanvasCount: 1, autoSnapshot: false };
 
 describe('baseline benchmark', () => {
-    const ctx = setupBenchmark<AgCartesianChartOptions>('simple-chart').repeatCount(500);
+    const ctx = setupBenchmark<AgCartesianChartOptions>('simple-chart').repeatCount(10);
 
-    benchmark('array push and truncate', ctx, expectations, () => {
+    benchmark('array push and truncate x1M', ctx, expectations, () => {
         const arr: number[] = [];
-        for (let i = 0; i < 1000000; i++) {
+        for (let i = 0; i < 1_000_000; i++) {
             arr.push(i);
         }
         arr.splice(0, arr.length);
     });
 
-    benchmark('json parse and stringify', ctx, expectations, () => {
-        JSON.parse(JSON.stringify(ctx.options));
+    benchmark('json parse and stringify x10K', ctx, expectations, () => {
+        for (let i = 0; i < 10000; i++) {
+            JSON.parse(JSON.stringify(ctx.options));
+        }
     });
 
-    benchmark('Math.sqrt()', ctx, expectations, () => {
-        for (let i = 0; i < 1000000; i++) {
+    benchmark('Math.sqrt() x10K', ctx, expectations, () => {
+        for (let i = 0; i < 10000; i++) {
             expect(Math.sqrt(i)).toBeDefined();
         }
     });
 
-    benchmark('Math.random()', ctx, expectations, () => {
-        for (let i = 0; i < 1000000; i++) {
+    benchmark('Math.random() x10K', ctx, expectations, () => {
+        for (let i = 0; i < 10000; i++) {
             expect(Math.random()).toBeDefined();
         }
     });
