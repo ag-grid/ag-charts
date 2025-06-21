1acc694630ab3a88ee03db969414a411b77d6f34
Alan Treadway <alan.treadway@ag-grid.com>
2025-06-21 18:23:49 +0100

    Workaround ESBuild __copyProps performance bottleneck.

--- packages/ag-charts-community/src/scene/util/fill.ts
+++ packages/ag-charts-community/src/scene/util/fill.ts
@@ -1,5 +1,8 @@
-import { type InternalAgColorType, type InternalAgGradientColor, isArray, isObject } from 'ag-charts-core';
+import { type InternalAgColorType, type InternalAgGradientColor } from 'ag-charts-core';
+import * as core from 'ag-charts-core';
 import type { AgImageFill, AgPatternColor } from 'ag-charts-types';
+
+const { isArray, isObject } = core;
 
 export function isGradientFill(fill: any): fill is InternalAgGradientColor {
     return isObject(fill) && fill.type == 'gradient';
