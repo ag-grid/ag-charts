81f5b9f6ae517cc5fcac83099cbf4b435d32cf3d
Alan Treadway <alan.treadway@ag-grid.com>
2025-06-23 11:34:52 +0100

    Remove debug output from sync-test example.

--- packages/ag-charts-website/src/content/docs/sync-test/_examples/financial-charts-sync/main.ts
+++ packages/ag-charts-website/src/content/docs/sync-test/_examples/financial-charts-sync/main.ts
@@ -2,8 +2,6 @@ import { AgCharts, AgFinancialChartOptions } from 'ag-charts-enterprise';
 
 import { getData } from './data';
 
-(window as any).agChartsDebug = 'sync';
-
 const commonOptions: AgFinancialChartOptions = {
     minWidth: 0,
     minHeight: 0,
