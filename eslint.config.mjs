c6e7a4267892c644c48ae523695b60e5f03d8709
Oli Legat <olivier.legat@ag-grid.com>
2025-06-24 16:57:44 +0100

    AG-14318 Enable aglint/change-detection by default

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -125,6 +125,7 @@ export default [
             'no-console': 2,
             'no-unused-vars': 0,
             'no-case-declarations': 0,
+            'aglint/change-detection': 2,
             '@typescript-eslint/no-explicit-any': 0,
             '@typescript-eslint/consistent-type-imports': 0,
             '@typescript-eslint/no-redundant-type-constituents': 1,
