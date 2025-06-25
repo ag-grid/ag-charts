430e55706c9b40c69f5b562effe083c057ea5183
Alan Treadway <alan.treadway@ag-grid.com>
2025-06-25 10:44:16 +0100

    AG-15218 - Reduce parseColor() DOM element creations.

--- packages/ag-charts-core/src/utils/dom/domUtils.ts
+++ packages/ag-charts-core/src/utils/dom/domUtils.ts
@@ -1,10 +1,9 @@
 import { getWindow } from './globalsProxy';
 
+// getWindow is required to make sure this works on our CI.
+// Using Option instead of createElement because it should be faster.
+const { style } = new (getWindow<new () => any>('Option'))();
 export function parseColor(color: string): string | null {
-    // getWindow is required to make sure this works on our CI.
-    // Using Option instead of createElement because it should be faster.
-    const OptionConstructor = getWindow<new () => any>('Option');
-    const { style } = new OptionConstructor();
     style.color = color;
     return style.color || null;
 }
