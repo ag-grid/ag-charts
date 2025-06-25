cf21583fade69dfc38824680722738ac80fdaf31
Alan Treadway <alan.treadway@ag-grid.com>
2025-06-25 10:52:42 +0100

    AG-15218 - Reduce graph.ts memory allocation churn.

--- packages/ag-charts-core/src/utils/dom/domUtils.ts
+++ packages/ag-charts-core/src/utils/dom/domUtils.ts
@@ -2,8 +2,12 @@ import { getWindow } from './globalsProxy';
 
 // getWindow is required to make sure this works on our CI.
 // Using Option instead of createElement because it should be faster.
-const { style } = new (getWindow<new () => any>('Option'))();
+let style: CSSStyleDeclaration;
 export function parseColor(color: string): string | null {
+    if (style == null) {
+        const OptionConstructor = getWindow<new () => any>('Option');
+        style = new OptionConstructor().style;
+    }
     style.color = color;
     return style.color || null;
 }
