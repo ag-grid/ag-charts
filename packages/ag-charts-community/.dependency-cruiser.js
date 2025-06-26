2a5b822932403bf501e02b58de31ac9afebee2bf
Oli Legat <olivier.legat@ag-grid.com>
2025-06-26 11:14:11 +0100

    Revert "AG-14318 Ignore test *.mjs files in ag-charts-community:lint:depcruise"
    This reverts commit 1ebc0400f9d4505efdac4c20cdfe2e48be67d17d.

--- packages/ag-charts-community/.dependency-cruiser.js
+++ packages/ag-charts-community/.dependency-cruiser.js
@@ -85,12 +85,7 @@ module.exports = {
                 "That's problematic as the package either (1) won't be available on live (2 - worse) will be " +
                 'available on live with an non-guaranteed version. Fix it by adding the package to the dependencies ' +
                 'in your package.json.',
-            from: {
-                pathNot: [
-                    'src/chart/test/lint-change-detection-eslint-config.mjs',
-                    '../../eslint-rules/change-detection.mjs',
-                ],
-            },
+            from: {},
             to: {
                 dependencyTypes: ['npm-no-pkg', 'npm-unknown'],
             },
