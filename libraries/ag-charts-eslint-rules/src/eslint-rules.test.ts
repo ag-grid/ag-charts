48fc0ff0d6d32257452ae2fa02687a800cf972d4
Oli Legat <olivier.legat@ag-grid.com>
2025-06-26 16:23:36 +0100

    AG-15303 Add tests for aglint/require-explicit-generic

---
+++ libraries/ag-charts-eslint-rules/src/eslint-rules.test.ts
@@ -0,0 +1,34 @@
+import { execSync } from 'child_process';
+import path from 'path';
+
+function runRuleTest(ruleNameSuffix: string) {
+    const lintDataFile = path.resolve(__dirname, `lint-${ruleNameSuffix}.data.ts`);
+    const eslintConfigFile = path.resolve(__dirname, `lint-${ruleNameSuffix}-eslint-config.mjs`);
+    let stdout: string = '';
+    let stderr: string = '';
+    const env: any = { ...process.env, NO_COLOR: '1' };
+    delete env['FORCE_COLOR'];
+
+    try {
+        // eslint-disable-next-line sonarjs/os-command
+        stdout = execSync(`npx eslint -c ${eslintConfigFile} ${lintDataFile}`, {
+            env,
+            encoding: 'utf-8',
+            stdio: 'pipe',
+        });
+    } catch (error: any) {
+        stdout = error.stdout;
+        stderr = error.stderr;
+    }
+    // The `__dirname` variable depends on the current buildhost. So substitute it to make the test consistent.
+    stdout = stdout.replaceAll(__dirname, '<dirname>');
+    stderr = stderr.replaceAll(__dirname, '<dirname>');
+    expect({ stdout, stderr }).toMatchSnapshot();
+}
+
+function testRule(ruleNameSuffix: string) {
+    test(`aglint/${ruleNameSuffix}`, () => runRuleTest(ruleNameSuffix));
+}
+
+testRule('change-detection');
+testRule('require-explicit-generic');
