e0dbfcb2707e6861693d0930266a2cf7a4ceb001
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-07-04 10:42:57 +0100

    AG-15250 - clean up

--- packages/ag-charts-community/src/chart/interaction/highlightManager.ts
+++ packages/ag-charts-community/src/chart/interaction/highlightManager.ts
@@ -9,14 +9,14 @@ import type { SeriesNodeDatum } from '../series/seriesTypes';
 export class HighlightManager {
     private readonly highlightStates = new StateTracker<{
         highlightedDatum?: HighlightNodeDatum;
-        previousHighlight?: HighlightNodeDatum;
+        previousHighlightedDatum?: HighlightNodeDatum;
     }>();
 
     constructor(private readonly eventsHub: EventsHub) {}
 
     public updateHighlight(callerId: string, highlightedDatum?: HighlightNodeDatum): void {
         const previousHighlight = this.getActiveHighlight();
-        this.highlightStates.set(callerId, { highlightedDatum, previousHighlight });
+        this.highlightStates.set(callerId, { highlightedDatum, previousHighlightedDatum: previousHighlight });
         const currentHighlight = this.getActiveHighlight();
         if (!this.isEqual(currentHighlight, previousHighlight)) {
             this.eventsHub.emit('highlight:change', { callerId, currentHighlight, previousHighlight });
@@ -28,7 +28,7 @@ export class HighlightManager {
     }
 
     public getPreviousHighlight(): HighlightNodeDatum | undefined {
-        return this.highlightStates.stateValue()?.previousHighlight;
+        return this.highlightStates.stateValue()?.previousHighlightedDatum;
     }
 
     private isEqual(a?: SeriesNodeDatum<unknown>, b?: SeriesNodeDatum<unknown>): boolean {
