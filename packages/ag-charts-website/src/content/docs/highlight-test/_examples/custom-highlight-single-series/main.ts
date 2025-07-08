e79ada4b88ec32b0a8040531404ea0d295175cd0
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-07-08 15:12:22 +0100

    AG-15250 - Highlight test page

---
+++ packages/ag-charts-website/src/content/docs/highlight-test/_examples/custom-highlight-single-series/main.ts
@@ -0,0 +1,56 @@
+// @ag-skip-fws
+import { AgChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    title: {
+        text: 'Custom Highlight Single Series',
+    },
+    data: getData(),
+    series: [
+        {
+            type: 'bar',
+            xKey: 'year',
+            yKey: 'visitors',
+            label: {
+                enabled: true,
+            },
+            stroke: 'lime',
+            strokeWidth: 4,
+            highlight: {
+                highlightedItem: {
+                    stroke: 'red',
+                    strokeWidth: 2,
+                    lineDash: [4, 5],
+                },
+            },
+        },
+    ],
+    axes: [
+        {
+            type: 'category',
+            position: 'bottom',
+            title: {
+                text: 'Year',
+            },
+        },
+        {
+            type: 'number',
+            position: 'left',
+            title: {
+                text: 'Total Visitors',
+            },
+        },
+    ],
+    formatter: {
+        y(params) {
+            let value = params.value as number;
+            value /= 1000_000;
+            return `${Math.floor(value)}M`;
+        },
+    },
+};
+
+AgCharts.create(options);
