fbdd7ccf369ccdb61f307b2f94cd5eec3d027243
Oli Legat <olivier.legat@ag-grid.com>
2025-07-15 10:50:23 +0100

    AG-8290 Implement label options for chord series

--- packages/ag-charts-enterprise/src/series/chord/chordSeries.test.ts
+++ packages/ag-charts-enterprise/src/series/chord/chordSeries.test.ts
@@ -436,4 +436,47 @@ describe('ChordSeries', () => {
             await compare();
         });
     });
+
+    test('AG-8290 label boxing', async () => {
+        const options = prepareEnterpriseTestOptions({
+            data: [
+                { from: 'Asia', to: 'Europe', size: 20 },
+                { from: 'Asia', to: 'Americas', size: 19 },
+                { from: 'Asia', to: 'Africa', size: 4 },
+                { from: 'Asia', to: 'Oceania', size: 8 },
+                { from: 'Europe', to: 'Asia', size: 5 },
+                { from: 'Europe', to: 'Americas', size: 5 },
+                { from: 'Europe', to: 'Africa', size: 3 },
+                { from: 'Europe', to: 'Oceania', size: 2 },
+                { from: 'Americas', to: 'Asia', size: 4 },
+                { from: 'Americas', to: 'Europe', size: 5 },
+                { from: 'Americas', to: 'Africa', size: 11 },
+                { from: 'Americas', to: 'Oceania', size: 6 },
+                { from: 'Africa', to: 'Asia', size: 3 },
+                { from: 'Africa', to: 'Europe', size: 9 },
+                { from: 'Africa', to: 'Americas', size: 3 },
+                { from: 'Oceania', to: 'Asia', size: 1 },
+                { from: 'Oceania', to: 'Europe', size: 1 },
+                { from: 'Oceania', to: 'Americas', size: 1 },
+            ],
+            series: [
+                {
+                    type: 'chord',
+                    fromKey: 'from',
+                    toKey: 'to',
+                    sizeKey: 'size',
+                    label: {
+                        color: 'green',
+                        fill: 'pink',
+                        padding: 20,
+                        cornerRadius: 15,
+                        border: { stroke: 'red', strokeWidth: 3 },
+                    },
+                },
+            ],
+        });
+
+        chart = deproxy(AgCharts.create(options));
+        await compare();
+    });
 });
