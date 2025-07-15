c3ccedb300c20636edb0071748f287b6317e25d9
Oli Legat <olivier.legat@ag-grid.com>
2025-07-15 15:41:29 +0100

    AG-14639 Add unit test for grouped-category axes

--- packages/ag-charts-community/src/chart/axis/groupedCategoryAxis.test.ts
+++ packages/ag-charts-community/src/chart/axis/groupedCategoryAxis.test.ts
@@ -275,4 +275,48 @@ describe('Grouped Category Axis Examples', () => {
             await compare();
         });
     });
+
+    test('AG-14639 label boxing', async () => {
+        const options = prepareTestOptions({
+            data: [
+                { location: ['Europe', 'United Kingdom', 'London'], gold: 27, silver: 23, bronze: 17 },
+                { location: ['Europe', 'United Kingdom', 'Manchester'], gold: 12, silver: 8, bronze: 10 },
+                { location: ['Europe', 'Germany', 'Berlin'], gold: 17, silver: 10, bronze: 15 },
+                { location: ['Asia', 'China', 'Beijing'], gold: 38, silver: 32, bronze: 18 },
+                { location: ['Asia', 'China', 'Shanghai'], gold: 20, silver: 15, bronze: 12 },
+                { location: ['Asia', 'Japan', 'Tokyo'], gold: 27, silver: 14, bronze: 17 },
+                { location: ['North America', 'United States', 'Los Angeles'], gold: 46, silver: 37, bronze: 38 },
+                { location: ['North America', 'United States', 'New York'], gold: 30, silver: 28, bronze: 25 },
+                { location: ['North America', 'Canada', 'Toronto'], gold: 8, silver: 6, bronze: 10 },
+                { location: ['South America', 'Brazil', 'Rio de Janeiro'], gold: 7, silver: 6, bronze: 6 },
+                { location: ['Africa', 'South Africa', 'Cape Town'], gold: 4, silver: 4, bronze: 6 },
+                { location: ['Oceania', 'Australia', 'Sydney'], gold: 17, silver: 7, bronze: 22 },
+                { location: ['Oceania', 'New Zealand', 'Auckland'], gold: 10, silver: 5, bronze: 8 },
+            ],
+            axes: [
+                {
+                    type: 'grouped-category',
+                    position: 'bottom',
+                    label: {
+                        color: 'purple',
+                        border: { strokeWidth: 2, stroke: 'red' },
+                        fill: 'pink',
+                    },
+                    depthOptions: [
+                        {},
+                        { label: { fontWeight: 'bold', fill: 'cyan' } },
+                        { label: { fontSize: 10, fill: 'lime' } },
+                    ],
+                },
+                { type: 'number', position: 'left' },
+            ],
+            series: [
+                { type: 'bar', xKey: 'location', yKey: 'gold' },
+                { type: 'bar', xKey: 'location', yKey: 'silver' },
+                { type: 'bar', xKey: 'location', yKey: 'bronze' },
+            ],
+        });
+        chart = AgCharts.create(options);
+        await compare();
+    });
 });
