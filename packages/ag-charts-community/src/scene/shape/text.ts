1897df008a7a1ae8c274a4c9006ac1214fcaafc0
Oli Legat <olivier.legat@ag-grid.com>
2025-07-23 13:08:21 +0100

    AG-14639 Fix failing snapshot test

--- packages/ag-charts-community/src/scene/shape/text.ts
+++ packages/ag-charts-community/src/scene/shape/text.ts
@@ -415,8 +415,8 @@ export class Text<D = any> extends Shape<D> {
     }
 
     setBoxing(props: TextBoxingProperties) {
-        if (props.fill != null || props.border?.stroke != null) {
-            const stroke = props.border?.enabled ? props.border?.stroke : undefined;
+        const stroke = props.border?.enabled ? props.border?.stroke : undefined;
+        if (props.fill != null || stroke != null) {
             this.boxing ??= new Rect();
             this.boxing.fill = props.fill;
             this.boxing.fillOpacity = props.fillOpacity ?? 1;
