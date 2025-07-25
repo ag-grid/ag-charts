3f4e911731378ff4b16b3bd313a26b3d9dee5302
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-07-25 11:26:25 +0100

    AG-15349 - Fix boxplot series styles

--- packages/ag-charts-enterprise/src/series/box-plot/boxPlotTypes.ts
+++ packages/ag-charts-enterprise/src/series/box-plot/boxPlotTypes.ts
@@ -1,8 +1,6 @@
-import type { FillOptions, LineDashOptions, Ratio, StrokeOptions, _ModuleSupport } from 'ag-charts-community';
+import type { _ModuleSupport } from 'ag-charts-community';
 
-export interface BoxPlotNodeDatum
-    extends Readonly<Required<FillOptions & StrokeOptions & LineDashOptions>>,
-        Omit<_ModuleSupport.CartesianSeriesNodeDatum, 'xValue' | 'yKey' | 'yValue'> {
+export interface BoxPlotNodeDatum extends Omit<_ModuleSupport.CartesianSeriesNodeDatum, 'xValue' | 'yKey' | 'yValue'> {
     readonly bandwidth: number;
 
     readonly scaledValues: {
@@ -13,11 +11,6 @@ export interface BoxPlotNodeDatum
         readonly q3Value: number;
         readonly maxValue: number;
     };
-
-    readonly cap: {
-        readonly lengthRatio: Ratio;
-    };
-    readonly whisker: Readonly<StrokeOptions & LineDashOptions>;
 
     readonly focusRect: { x: number; y: number; width: number; height: number };
 }
