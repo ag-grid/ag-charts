92c395ddd18fab2f60810551466ca10d28bcf93f
iMoses <ido.moshe@ag-grid.com>
2025-07-28 12:40:56 +0100

    cleanup

--- plugins/ag-charts-generate-code-reference-files/src/doc-interfaces/type-resolver.ts
+++ plugins/ag-charts-generate-code-reference-files/src/doc-interfaces/type-resolver.ts
@@ -87,7 +87,7 @@ export class TypeResolver {
         }
     }
 
-    protected resolveUnion(unionKey: TypeNode, debug?: boolean) {
+    protected resolveUnion(unionKey: TypeNode) {
         if (typeof unionKey === 'string') {
             if (unionKey.startsWith("'") && unionKey.endsWith("'")) {
                 return unionKey;
