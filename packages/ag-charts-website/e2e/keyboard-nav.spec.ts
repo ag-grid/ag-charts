4620ffc03f5248bb67286f3627981ff953fa70f2
Oli Legat <olivier.legat@ag-grid.com>
2025-07-31 12:18:12 +0100

    AG-15607 Add e2e test for `bar.thickness < thickness`

--- packages/ag-charts-website/e2e/keyboard-nav.spec.ts
+++ packages/ag-charts-website/e2e/keyboard-nav.spec.ts
@@ -453,6 +453,21 @@ test.describe('keyboard-nav', () => {
         });
     });
 
+    test.describe('AG-15607 linear gauge bar thickness', () => {
+        test('horizontal', async ({ page }) => {
+            await gotoExample(page, toExamplePageUrl('linear-gauge-test', 'bar-thickness-horizontal', 'vanilla').url);
+            await page.mouse.click(400, 300);
+            await page.keyboard.press('ArrowLeft');
+            await expect(page).toHaveScreenshot('AG-15607-bar-thickness-horizontal.png');
+        });
+        test('vertical', async ({ page }) => {
+            await gotoExample(page, toExamplePageUrl('linear-gauge-test', 'bar-thickness-vertical', 'vanilla').url);
+            await page.mouse.click(400, 300);
+            await page.keyboard.press('ArrowLeft');
+            await expect(page).toHaveScreenshot('AG-15607-bar-thickness-vertical.png');
+        });
+    });
+
     test('AG-13891 pagination buttons aria-disabled', async ({ page }) => {
         await gotoExample(page, toExamplePageUrl('accessibility', 'keyboard-navigation', 'vanilla').url);
 
