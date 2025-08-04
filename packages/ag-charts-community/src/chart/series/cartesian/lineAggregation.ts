9984baf4855b49cb7fbc47df40c8846533fcad95
Jacob  Parker <jacobparker1992@gmail.com>
2025-08-04 11:43:11 +0100

    Line perf 2

--- packages/ag-charts-community/src/chart/series/cartesian/lineAggregation.ts
+++ packages/ag-charts-community/src/chart/series/cartesian/lineAggregation.ts
@@ -21,6 +21,62 @@ export interface LineSeriesDataAggregationFilter {
     maxRange: number;
 }
 
+function calculateSmallestInterval(
+    xValues: any[],
+    d0: number,
+    d1: number,
+    startDatumIndex: number,
+    endDatumIndex: number,
+    currentSmallestInterval: number,
+    depth: number
+): number {
+    if (startDatumIndex >= endDatumIndex) return currentSmallestInterval;
+
+    const ratio = Number.isFinite(d0)
+        ? aggregationXRatioForXValue(xValues[endDatumIndex], d0, d1) -
+          aggregationXRatioForXValue(xValues[startDatumIndex], d0, d1)
+        : aggregationXRatioForDatumIndex(endDatumIndex, xValues.length) -
+          aggregationXRatioForDatumIndex(startDatumIndex, xValues.length);
+
+    if (ratio === 0) return currentSmallestInterval;
+
+    const currentInterval = Math.abs(ratio) / (endDatumIndex - startDatumIndex);
+
+    let recurse: boolean;
+    if (depth < 3) {
+        recurse = true;
+    } else if (depth > 20) {
+        recurse = false;
+    } else {
+        recurse = currentInterval <= currentSmallestInterval;
+    }
+
+    currentSmallestInterval = Math.min(currentSmallestInterval, currentInterval);
+
+    if (!recurse) return currentSmallestInterval;
+
+    const midIndex = Math.floor((startDatumIndex + endDatumIndex) / 2);
+    const leadingInterval = calculateSmallestInterval(
+        xValues,
+        d0,
+        d1,
+        startDatumIndex,
+        midIndex,
+        currentSmallestInterval,
+        depth + 1
+    );
+    const trailingInterval = calculateSmallestInterval(
+        xValues,
+        d0,
+        d1,
+        midIndex + 1,
+        endDatumIndex,
+        currentSmallestInterval,
+        depth + 1
+    );
+    return Math.min(leadingInterval, trailingInterval, currentSmallestInterval);
+}
+
 function aggregationContainsIndex(
     xValues: any[],
     d0: number,
@@ -55,7 +111,16 @@ export function aggregateLineData(
 
     const [d0, d1] = aggregationDomain(scale, domain);
 
-    let maxRange = aggregationRangeFittingPoints(xValues);
+    const smallestInterval = calculateSmallestInterval(
+        xValues,
+        d0,
+        d1,
+        0,
+        xValues.length - 1,
+        1 / (xValues.length - 1),
+        0
+    );
+    let maxRange = aggregationRangeFittingPoints(xValues, { smallestInterval });
 
     const { indexData, valueData } = createAggregationIndices(xValues, yValues, yValues, d0, d1, maxRange);
 
