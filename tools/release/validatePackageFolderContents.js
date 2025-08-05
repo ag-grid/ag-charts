e38424ed038b8edf624d66ac32f0d056ef5e6e90
seanlandsman <sean@thelandsmans.com>
2025-08-05 09:15:51 +0100

    Fix dist validation scripts (#4908)

--- tools/release/validatePackageFolderContents.js
+++ tools/release/validatePackageFolderContents.js
@@ -3,9 +3,6 @@ const path = require('path');
 
 const EXPECTED_FOLDER_CONTENTS = {
     'dist/packages': [
-        'ag-charts-types',
-        'ag-charts-website',
-        'ag-charts-angular.tgz',
         'ag-charts-angular.tgz',
         'ag-charts-community.tgz',
         'ag-charts-core.tgz',
