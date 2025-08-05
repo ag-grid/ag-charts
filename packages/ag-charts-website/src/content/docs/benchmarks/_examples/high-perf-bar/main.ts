38ecb3b5618a2eb0e8f04fa4818639936a092548
Jacob  Parker <jacobparker1992@gmail.com>
2025-08-05 15:51:56 +0100

    High perf series benchmarks

---
+++ packages/ag-charts-website/src/content/docs/benchmarks/_examples/high-perf-bar/main.ts
@@ -0,0 +1,40 @@
+/* @ag-options-extract */
+import { AgChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+(window as any).agChartsDebug = 'scene:stats:verbose';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    animation: { enabled: false },
+    zoom: {
+        enabled: true,
+        anchorPointX: 'pointer',
+        anchorPointY: 'pointer',
+        minVisibleItems: 0,
+    },
+    navigator: {
+        enabled: true,
+    },
+    series: [
+        {
+            type: 'bar',
+            xKey: 'timestamp',
+            yKey: 'price',
+        },
+    ],
+    axes: [
+        { type: 'number', position: 'left' },
+        { type: 'unit-time', position: 'bottom' },
+    ],
+};
+/* @ag-options-end */
+
+const start = performance.now();
+const chart = AgCharts.create(options);
+
+chart.waitForUpdate().then(() => {
+    console.log('Total update time: ', performance.now() - start);
+});
