92f5f9de68944f71dca9109fc62514b9c1295126
AgGitDeployment <deployments.team@ag-grid.com>
2025-08-06 15:11:44 +0100

    BRANCH prep for 10.3.9-beta.20250806

--- packages/ag-charts-enterprise/src/license/licenseManager.ts
+++ packages/ag-charts-enterprise/src/license/licenseManager.ts
@@ -21,7 +21,7 @@ const LICENSE_TYPES = {
 const LICENSING_HELP_URL = 'https://ag-grid.com/charts/licensing/';
 
 export class LicenseManager {
-    private static readonly RELEASE_INFORMATION: string = 'MTc0OTAzMzk4MzQyMA==';
+    private static readonly RELEASE_INFORMATION: string = 'MTc1NDQ4OTUwMjY0NQ==';
     private licenseKey?: string;
     private gridContext: boolean = false;
     private watermarkMessage: string | undefined = undefined;
