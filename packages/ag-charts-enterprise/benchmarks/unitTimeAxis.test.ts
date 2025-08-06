b81daf13e558460a8e3d23af732a75be6e1d4351
Jacob  Parker <jacobparker1992@gmail.com>
2025-08-06 14:42:04 +0100

    Update benchmark numbers

--- packages/ag-charts-enterprise/benchmarks/unitTimeAxis.test.ts
+++ packages/ag-charts-enterprise/benchmarks/unitTimeAxis.test.ts
@@ -29,7 +29,7 @@ describe('unit time axis benchmark', () => {
         benchmark(
             '20x zoom',
             ctx,
-            { expectedRelativeMB: 5, expectedCanvasCount: 4, autoSnapshot: false },
+            { expectedRelativeMB: 5.5, expectedCanvasCount: 4, autoSnapshot: false },
             async () => {
                 for (let i = 0; i < 20; i++) {
                     await ctx.scroll(ctx.options.width! / 2, ctx.options.height! / 2, -1, 0);
