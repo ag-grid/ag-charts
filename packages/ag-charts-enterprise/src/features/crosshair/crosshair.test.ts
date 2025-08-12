411d173bcf9da302a4f60055ed9bf8a69b8229f3
Jacob  Parker <jacobparker1992@gmail.com>
2025-08-12 14:21:48 +0100

    AG-15565 Add crosshair test

--- packages/ag-charts-enterprise/src/features/crosshair/crosshair.test.ts
+++ packages/ag-charts-enterprise/src/features/crosshair/crosshair.test.ts
@@ -330,4 +330,55 @@ describe('Crosshair', () => {
         await hoverAction(300, 300)(chart);
         await compare();
     });
+
+    it('AG-15565', async () => {
+        const data = [
+            { month: 'Jan', subscriptions: 222, services: 250, products: 200 },
+            { month: 'Feb', subscriptions: 240, services: 255, products: 210 },
+            { month: 'Mar', subscriptions: 280, services: 245, products: null },
+            { month: 'Apr', subscriptions: 300, services: 260, products: 205 },
+            { month: 'May', subscriptions: 350, services: 235, products: 215 },
+            { month: 'Jun', subscriptions: 420, services: Infinity, products: 200 },
+            { month: 'Jul', subscriptions: 300, services: 255, products: 100 },
+            { month: 'Aug', subscriptions: 270, services: 305, products: 210 },
+            { month: 'Sep', subscriptions: 260, services: 280, products: 250 },
+            { month: 'Oct', subscriptions: 385, services: 250, products: NaN },
+            { month: 'Nov', subscriptions: 320, services: 265, products: 215 },
+            { month: 'Dec', subscriptions: 330, services: 255, products: 220 },
+        ];
+
+        const options: AgChartOptions = {
+            data,
+            series: [
+                {
+                    type: 'area',
+                    xKey: 'month',
+                    yKey: 'subscriptions',
+                    yName: 'Subscriptions',
+                    stacked: true,
+                },
+                {
+                    type: 'area',
+                    xKey: 'month',
+                    yKey: 'services',
+                    yName: 'Services',
+                    stacked: true,
+                },
+                {
+                    type: 'area',
+                    xKey: 'month',
+                    yKey: 'products',
+                    yName: 'Products',
+                    stacked: true,
+                },
+            ],
+        };
+
+        prepareEnterpriseTestOptions(options);
+
+        chart = AgCharts.create(options);
+        await waitForChartStability(chart);
+        await hoverAction(260, 140)(chart);
+        await compare();
+    });
 });
