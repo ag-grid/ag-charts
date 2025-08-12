7aaba46c86b46b221375ae84530cb362dae0e0b0
Tak Tran <contact@tutaktran.com>
2025-08-12 15:03:24 +0100

    AG-14510 - Minify examples js if not dev

--- packages/ag-charts-website/src/pages/homepage/examples.js.ts
+++ packages/ag-charts-website/src/pages/homepage/examples.js.ts
@@ -1,8 +1,22 @@
 import { getGalleryExamplesJs } from '@components/homepage/components/gallery-examples/galleryExamplesData';
+import { getIsDev } from '@utils/env';
 import { type CollectionEntry, getEntry } from 'astro:content';
+import { transform } from 'esbuild';
 
 const { data: galleryData } = (await getEntry('homepageGallery', 'examples')) as CollectionEntry<'homepageGallery'>;
 const { data: allGalleryData } = (await getEntry('gallery', 'data')) as CollectionEntry<'gallery'>;
+
+async function minifyJs(js: string): Promise<string> {
+    const res = await transform(js, {
+        minify: true,
+        loader: 'js',
+        format: 'esm', // your script tag uses type="module"
+        target: 'es2019',
+        legalComments: 'none',
+        sourcemap: false,
+    });
+    return res.code;
+}
 
 export async function GET() {
     const js = await getGalleryExamplesJs({
@@ -10,7 +24,9 @@ export async function GET() {
         allGalleryData,
     });
 
-    return new Response(js, {
+    const output = getIsDev() ? js : await minifyJs(js);
+
+    return new Response(output, {
         status: 200,
         headers: {
             'Content-Type': 'application/javascript',
