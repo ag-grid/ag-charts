c628ec3cd346eb8fd1309f14d0fe304bfdf59c9f
Jacob  Parker <jacobparker1992@gmail.com>
2025-08-20 10:41:14 +0100

    AG-15780 Candlestick, ohlc alignment changes

--- packages/ag-charts-enterprise/src/series/candlestick/candlestickNode.ts
+++ packages/ag-charts-enterprise/src/series/candlestick/candlestickNode.ts
@@ -79,7 +79,7 @@ export class CandlestickNode extends OhlcBaseNode {
         wickPath.moveTo(centerX, y1);
         wickPath.lineTo(centerX, boxBottom - strokeWidth / 2);
 
-        const boxStrokeAdjustment = strokeAlignment + strokeWidth / 2;
+        const boxStrokeAdjustment = strokeWidth / 2 - strokeAlignment;
         const rectHeight = boxBottom - boxTop - 2 * boxStrokeAdjustment;
         if (rectHeight > 0) {
             path.rect(
