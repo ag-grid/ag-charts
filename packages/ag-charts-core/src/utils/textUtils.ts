711bb23c84256715d558e3e4ea1c52f09e45de33
iMoses <ido.moshe@ag-grid.com>
2025-09-02 11:29:00 +0100

    revert appendEllipsis blacklist

--- packages/ag-charts-core/src/utils/textUtils.ts
+++ packages/ag-charts-core/src/utils/textUtils.ts
@@ -4,14 +4,6 @@ export const EllipsisChar = '\u2026';
 export const LineSplitter = /\r?\n/g;
 export const TrimEdgeGuard = '\u200B'; // zero-width space, not trimmed, zero width
 
-export function guardTextEdges(str: string) {
-    return TrimEdgeGuard + str + TrimEdgeGuard;
-}
-
-export function unguardTextEdges(str: string) {
-    return str.replaceAll(TrimEdgeGuard, '');
-}
-
 export interface FontOptions {
     fontSize: FontSize;
     fontStyle?: FontStyle;
@@ -35,6 +27,15 @@ export function calcLineHeight(fontSize: number, lineHeightRatio = 1.15) {
     return Math.round(fontSize * lineHeightRatio);
 }
 
+const TrimCharsRegex = /[.,;:-]{1,5}$/;
 export function appendEllipsis(text: string) {
-    return text.replace(/[^\p{L}\p{Nd}]{1,5}$/u, '') + EllipsisChar;
+    return text.replace(TrimCharsRegex, '') + EllipsisChar;
+}
+
+export function guardTextEdges(str: string) {
+    return TrimEdgeGuard + str + TrimEdgeGuard;
+}
+
+export function unguardTextEdges(str: string) {
+    return str.replaceAll(TrimEdgeGuard, '');
 }
