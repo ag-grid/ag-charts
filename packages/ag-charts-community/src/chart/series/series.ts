033add7415c3bc08bed1b5814a97a5f93a5e67cf
Oli Legat <olivier.legat@ag-grid.com>
2025-09-02 10:23:36 +0100

    AG-15830 Revert debugging code

--- packages/ag-charts-community/src/chart/series/series.ts
+++ packages/ag-charts-community/src/chart/series/series.ts
@@ -739,7 +739,7 @@ export abstract class Series<
 
         const { x, y } = point;
         const key = JSON.stringify({ x, y, maxDistance, selectedPickModes });
-        if (false && this._pickNodeCache.has(key)) {
+        if (this._pickNodeCache.has(key)) {
             return this._pickNodeCache.get(key);
         }
 
