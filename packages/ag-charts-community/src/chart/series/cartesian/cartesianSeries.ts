d694949648692f3bdbdb86214a9722ce6b003175
Laurence Roberts <laurence.roberts@ag-grid.com>
2025-09-03 14:38:48 +0100

    AG-15765 Fix autoscale to use cross axis values

--- packages/ag-charts-community/src/chart/series/cartesian/cartesianSeries.ts
+++ packages/ag-charts-community/src/chart/series/cartesian/cartesianSeries.ts
@@ -980,12 +980,12 @@ export abstract class CartesianSeries<
         const crossAxis = shouldFlipXY ? this.axes[ChartAxisDirection.Y]! : this.axes[ChartAxisDirection.X]!;
         const axis = shouldFlipXY ? this.axes[ChartAxisDirection.X]! : this.axes[ChartAxisDirection.Y]!;
 
+        const crossVisibleRange = shouldFlipXY ? yVisibleRange ?? [0, 1] : xVisibleRange;
+        const axisVisibleRange = shouldFlipXY ? xVisibleRange : yVisibleRange ?? [0, 1];
+
         if (yVisibleRange == null) {
             const sortOrder = this.sortOrder(crossAxisKey);
-
-            if (sortOrder == null) {
-                yVisibleRange = [0, 1];
-            } else {
+            if (sortOrder != null) {
                 // Fast path if we only need to look at the x axis
                 const crossScale = crossAxis.scale;
                 const crossScaleRange = crossScale.range;
@@ -993,15 +993,15 @@ export abstract class CartesianSeries<
 
                 const xValues = this.keysOrValues(crossAxisKey);
 
-                let [r0, r1] = this.visibleRangeIndices(crossAxisKey, xVisibleRange, undefined, { sortOrder });
+                let [r0, r1] = this.visibleRangeIndices(crossAxisKey, crossVisibleRange, undefined, { sortOrder });
                 r1 -= 1;
 
                 // @todo(AG-7083) - figure out how to determine this
                 const pixelSize = 0;
-                if (this.xCoordinateRange(xValues[r0], pixelSize, r0)[0] < xVisibleRange[0]) {
+                if (this.xCoordinateRange(xValues[r0], pixelSize, r0)[0] < crossVisibleRange[0]) {
                     r0 += 1;
                 }
-                if (this.xCoordinateRange(xValues[r1], pixelSize, r1)[1] > xVisibleRange[1]) {
+                if (this.xCoordinateRange(xValues[r1], pixelSize, r1)[1] > crossVisibleRange[1]) {
                     r1 -= 1;
                 }
 
@@ -1016,9 +1016,6 @@ export abstract class CartesianSeries<
         const convert = (d: number[], r: number[], v: number) => {
             return d[0] + ((v - r[0]) / (r[1] - r[0])) * (d[1] - d[0]);
         };
-
-        const crossVisibleRange = shouldFlipXY ? yVisibleRange : xVisibleRange;
-        const axisVisibleRange = shouldFlipXY ? xVisibleRange : yVisibleRange;
 
         const crossAxisRange = crossAxis.range;
         const axisRange = axis.range;
