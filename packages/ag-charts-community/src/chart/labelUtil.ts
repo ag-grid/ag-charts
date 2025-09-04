edca56c0d4078c79d2ddd6abed5a1aaff69c2bed
Oli Legat <olivier.legat@ag-grid.com>
2025-09-04 11:49:21 +0100

    Revert "AG-8917 TC7 Add `quickAutoEnable` to address benchmark time-perf failure"
    This reverts commit 81549fb27963b66c2175115d372d4828e7c301d1.

--- packages/ag-charts-community/src/chart/labelUtil.ts
+++ packages/ag-charts-community/src/chart/labelUtil.ts
@@ -59,12 +59,11 @@ export function getLabelStyles<TParams>(
             highlighted,
             highlightState,
         };
-        // AG-8917 TC7 The correct call here should be `resolvePartial(..)` with the `pick: false` option. However, that
-        // has too much of an impact on time-performance. For just optimise the `enabled` property.
         const stylerResult =
-            series.ctx.optionsGraphService.quickAutoEnable(
-                ['series', `${series.declarationOrder}`, 'label', 'border'],
-                series.callWithContext(label.itemStyler, { ...params, ...styleParams })
+            series.ctx.optionsGraphService.resolvePartial(
+                ['series', `${series.declarationOrder}`, 'label'],
+                series.callWithContext(label.itemStyler, { ...params, ...styleParams }),
+                { pick: false }
             ) ?? {};
 
         return mergeDefaults(stylerResult, styleParams);
