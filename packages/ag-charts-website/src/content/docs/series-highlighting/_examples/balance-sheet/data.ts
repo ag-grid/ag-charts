4d1b999938135a0dba6181c23d8e998fd0233d11
Oli Legat <olivier.legat@ag-grid.com>
2025-09-04 15:54:59 +0100

    AG-15866 Docs: Add series-highlighting label.itemStyler example

---
+++ packages/ag-charts-website/src/content/docs/series-highlighting/_examples/balance-sheet/data.ts
@@ -0,0 +1,39 @@
+export function getData() {
+    return [
+        {
+            year: 2018,
+            cash: 50,
+            networth: 120,
+            assets: 200,
+            liabilities: 80,
+        },
+        {
+            year: 2019,
+            cash: 65,
+            networth: 150,
+            assets: 240,
+            liabilities: 90,
+        },
+        {
+            year: 2020,
+            cash: 40,
+            networth: 130,
+            assets: 210,
+            liabilities: 80,
+        },
+        {
+            year: 2021,
+            cash: 70,
+            networth: 180,
+            assets: 280,
+            liabilities: 100,
+        },
+        {
+            year: 2022,
+            cash: 90,
+            networth: 220,
+            assets: 320,
+            liabilities: 100,
+        },
+    ];
+}
