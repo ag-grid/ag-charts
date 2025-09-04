b8a3230d49f2f28267b61afc0d626681876522cd
Oli Legat <olivier.legat@ag-grid.com>
2025-09-04 10:28:04 +0100

    AG-8917 TC8 Add test for sunburst label boxing styles

--- packages/ag-charts-enterprise/src/series/sunburst/sunburstSeries.test.ts
+++ packages/ag-charts-enterprise/src/series/sunburst/sunburstSeries.test.ts
@@ -542,4 +542,61 @@ describe('SunburstSeries', () => {
             await compare();
         });
     });
+
+    describe('AG-8917', () => {
+        const data = [
+            {
+                name: 'Americas',
+                children: [
+                    { name: 'United States', gdp: 26.949, gdpChange: 0.06 },
+                    { name: 'Canada', gdp: 2.117, gdpChange: 0 },
+                    { name: 'Brazil', gdp: 2.126, gdpChange: 0.11 },
+                ],
+                gdpChange: 0.09,
+            },
+            {
+                name: 'Asia',
+                children: [
+                    { name: 'China', gdp: 17.7, gdpChange: 0 },
+                    { name: 'Japan', gdp: 4.23, gdpChange: 0 },
+                    { name: 'India', gdp: 4.0, gdpChange: 0.2 },
+                ],
+                gdpChange: 0.05,
+            },
+            {
+                name: 'Europe',
+                children: [
+                    {
+                        name: 'EU',
+                        children: [
+                            { name: 'Germany', gdp: 4.429, gdpChange: 0.09 },
+                            { name: 'France', gdp: 3.049, gdpChange: 0.1 },
+                            { name: 'Italy', gdp: 2.186, gdpChange: 0.09 },
+                        ],
+                        gdpChange: 0.08,
+                    },
+                    { name: 'United Kingdom', gdp: 3.332, gdpChange: 0.09 },
+                ],
+                gdpChange: 0.08,
+            },
+        ];
+
+        test('static label styles', async () => {
+            const options = prepareEnterpriseTestOptions({
+                data,
+                series: [
+                    {
+                        type: 'sunburst',
+                        labelKey: 'name',
+                        sizeKey: 'gdp',
+                        secondaryLabelKey: 'gdpChange',
+                        label: { fill: 'red', border: { stroke: 'lime' } },
+                        secondaryLabel: { fill: 'blue' },
+                    },
+                ],
+            });
+            chart = AgCharts.create(options);
+            await compare();
+        });
+    });
 });
