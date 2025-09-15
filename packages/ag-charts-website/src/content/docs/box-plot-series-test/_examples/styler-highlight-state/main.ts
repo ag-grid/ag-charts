d5a648249bf236339b1bdd6d0c7d8db7a7817e6f
Oli Legat <olivier.legat@ag-grid.com>
2025-09-15 14:28:12 +0100

    AG-15782 Remove unused `styler` variable/function

--- packages/ag-charts-website/src/content/docs/box-plot-series-test/_examples/styler-highlight-state/main.ts
+++ packages/ag-charts-website/src/content/docs/box-plot-series-test/_examples/styler-highlight-state/main.ts
@@ -1,33 +1,6 @@
-import { AgBoxPlotSeriesStyle, AgBoxPlotSeriesStylerParams, AgChartOptions, AgCharts } from 'ag-charts-enterprise';
+import { AgBoxPlotSeriesStylerParams, AgChartOptions, AgCharts } from 'ag-charts-enterprise';
 
 import { getData } from './data';
-
-const styler = (params: AgBoxPlotSeriesStylerParams<unknown, unknown>): AgBoxPlotSeriesStyle | undefined => {
-    if (params.yName === 'Company 1')
-        return {
-            fill: 'cyan',
-            lineDash: [7, 2],
-            lineDashOffset: 5,
-            stroke: 'blue',
-            strokeWidth: 7,
-            strokeOpacity: 0.5,
-            whisker: {
-                lineDash: [3, 3],
-                lineDashOffset: 5,
-                stroke: 'navy',
-                strokeWidth: 3,
-                strokeOpacity: 1,
-            },
-        };
-    else if (params.yName === 'Company 2')
-        return {
-            fill: 'magenta',
-            fillOpacity: 0.5,
-            cornerRadius: 15,
-            cap: { lengthRatio: 0.5 },
-        };
-    return {};
-};
 
 const options: AgChartOptions = {
     container: document.getElementById('myChart'),
@@ -43,7 +16,7 @@ const options: AgChartOptions = {
             medianKey: 's1_median',
             q3Key: 's1_q3',
             maxKey: 's1_max',
-            styler: (params) => {
+            styler: (params: AgBoxPlotSeriesStylerParams<unknown, unknown>) => {
                 switch (params.highlightState) {
                     case 'highlighted-item':
                         return { fill: 'yellow', strokeWidth: 4 };
@@ -68,7 +41,7 @@ const options: AgChartOptions = {
             medianKey: 's2_median',
             q3Key: 's2_q3',
             maxKey: 's2_max',
-            styler: (params) => {
+            styler: (params: AgBoxPlotSeriesStylerParams<unknown, unknown>) => {
                 switch (params.highlightState) {
                     case 'highlighted-item':
                         return { fill: 'lime', strokeWidth: 4 };
