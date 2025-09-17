fca57ea24cc8426c108f47d60c757189782b9de7
Oli Legat <olivier.legat@ag-grid.com>
2025-09-17 15:48:00 +0100

    AG-15782 Add radial-column-series-test docs page

---
+++ packages/ag-charts-website/src/content/docs/radial-column-series-test/_examples/styler/main.ts
@@ -0,0 +1,50 @@
+import { AgChartOptions, AgCharts, AgRadialSeriesStyle, AgRadialSeriesStylerParams } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+const styler = (params: AgRadialSeriesStylerParams<unknown, unknown>): AgRadialSeriesStyle | undefined => {
+    if (params.radiusKey === 'sw') {
+        return {
+            fill: 'cyan',
+            lineDash: [7, 2],
+            lineDashOffset: 5,
+            stroke: 'blue',
+            strokeWidth: 7,
+            strokeOpacity: 0.5,
+        };
+    }
+    if (params.radiusKey === 'hw')
+        return {
+            fill: 'hotpink',
+            stroke: 'darkmagenta',
+            strokeWidth: 2,
+        };
+    return {};
+};
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    series: [
+        {
+            type: 'radial-column',
+            angleKey: 'quarter',
+            radiusKey: 'sw',
+            radiusName: 'Software',
+            fill: 'lime', // ignored
+            fillOpacity: 0.5, // not ignored
+            styler,
+        },
+        {
+            type: 'radial-column',
+            angleKey: 'quarter',
+            radiusKey: 'hw',
+            stroke: 'CornflowerBlue', // ignored
+            strokeOpacity: 0.5, // not ignored
+            strokeWidth: 15, // ignored
+            styler,
+        },
+    ],
+};
+
+AgCharts.create(options);
