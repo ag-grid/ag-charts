e81df2c6e6608d97379db75ec26fcd525d4489a2
Oli Legat <olivier.legat@ag-grid.com>
2025-09-17 16:05:53 +0100

    AG-15782 Implement `series[type='radial-column'].styler`

--- packages/ag-charts-enterprise/src/series/radial-column/radialColumnSeries.ts
+++ packages/ag-charts-enterprise/src/series/radial-column/radialColumnSeries.ts
@@ -65,6 +65,10 @@ export class RadialColumnSeries extends RadialColumnSeriesBase<_ModuleSupport.Ra
     }
 
     protected override hasItemStylers(): boolean {
-        return this.properties.itemStyler != null || this.properties.label.itemStyler != null;
+        return (
+            this.properties.itemStyler != null ||
+            this.properties.styler != null ||
+            this.properties.label.itemStyler != null
+        );
     }
 }
