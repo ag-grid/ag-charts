464f21896a7039166974abdc338136ec602c1a8f
iMoses <ido.moshe@ag-grid.com>
2025-10-01 16:12:34 +0100

    fix lint

--- packages/ag-charts-types/src/chart/themeMap.ts
+++ packages/ag-charts-types/src/chart/themeMap.ts
@@ -4,7 +4,6 @@ import type { WithThemeParams } from './operationOptions';
 import type { AgChartThemeOverrides } from './themeOptions';
 import type { ContextDefault, DatumDefault } from './types';
 
-
 type ThemesMap<TDatum = DatumDefault, TContext = ContextDefault> = AgChartThemeOverrides<TDatum, TContext> & {
     'linear-gauge'?: { series: AgLinearGaugeOptions<TDatum, TContext> };
     'radial-gauge'?: { series: AgRadialGaugeOptions<TDatum, TContext> };
