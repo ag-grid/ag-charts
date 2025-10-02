e8bdffa0aa69789c4d23ee77387536da64e4a9ff
Oli Legat <olivier.legat@ag-grid.com>
2025-10-02 16:11:47 +0100

    AG-15773 Fix compilation

--- packages/ag-charts-community/src/chart/legend/legendManager.ts
+++ packages/ag-charts-community/src/chart/legend/legendManager.ts
@@ -1,11 +1,19 @@
-import { Logger, isArray } from 'ag-charts-core';
-import type { AgInitialStateLegendOptions } from 'ag-charts-types';
+import { type AreExact, Logger, isArray } from 'ag-charts-core';
+import type { AgInitialStateLegendOptions as DefectAgInitialStateLegendOptions } from 'ag-charts-types';
 
 import type { MementoOriginator } from '../../api/state/memento';
 import type { EventsHub } from '../../core/eventsHub';
 import type { CategoryLegendDatum } from './legendDatum';
 
+// FIXME: AG-16068 locally patch API AgInitialStateLegendOptions, but force compilation error if&when
+// AgInitialStateLegendOptions is publicly patched.
+type AgInitialStateLegendOptions =
+    AreExact<DefectAgInitialStateLegendOptions['itemId'], string | undefined> extends false
+        ? never
+        : Omit<DefectAgInitialStateLegendOptions, 'itemId'> & { itemId?: string | number };
+
 type LegendDataMap = Map<string, CategoryLegendDatum[]>;
+
 type LegendDataMemento = AgInitialStateLegendOptions[];
 
 export class LegendManager implements MementoOriginator<LegendDataMemento> {
