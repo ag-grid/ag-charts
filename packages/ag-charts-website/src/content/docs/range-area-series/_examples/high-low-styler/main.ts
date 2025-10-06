c7f46b37ed16b4e0d9bd71521be4081b53eae1fc
Oli Legat <olivier.legat@ag-grid.com>
2025-10-06 18:07:00 +0100

    AG-9002 Initial docs example

---
+++ packages/ag-charts-website/src/content/docs/range-area-series/_examples/high-low-styler/main.ts
@@ -0,0 +1,54 @@
+import { AgCartesianChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { DataType, getData } from './data';
+
+const options: AgCartesianChartOptions<DataType, never> = {
+    container: document.getElementById('myChart'),
+    title: {
+        text: 'AA-Rated Corporate Bond Yield Range (2024)',
+    },
+    data: getData(),
+    series: [
+        {
+            type: 'range-area',
+            xKey: 'date',
+            yLowKey: 'low',
+            yHighKey: 'high',
+            fill: {
+                type: 'gradient',
+                colorStops: [{ color: '#cccccc00', stop: 0 }, { color: '#cccccc80' }],
+            },
+            item: {
+                high: {
+                    stroke: '#39ac39',
+                    strokeWidth: 2,
+                    marker: {
+                        fill: '#39ac39',
+                        fillOpacity: 1,
+                    },
+                },
+                low: {
+                    stroke: '#e60000',
+                    strokeWidth: 2,
+                    marker: {
+                        fill: '#e60000',
+                        fillOpacity: 1,
+                    },
+                },
+            },
+        },
+    ],
+    axes: [
+        {
+            type: 'time',
+            position: 'bottom',
+        },
+        {
+            type: 'number',
+            position: 'left',
+            label: { format: '#{0.1%}' },
+        },
+    ],
+};
+
+AgCharts.create(options);
