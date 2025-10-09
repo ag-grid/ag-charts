03ec9fcbfc64d1b378ded56fc48205225ae7e1a9
iMoses <ido.moshe@ag-grid.com>
2025-10-09 15:34:55 +0100

    Remove old module registries WIP

--- packages/ag-charts-community/src/module/optionsModule.test.ts
+++ packages/ag-charts-community/src/module/optionsModule.test.ts
@@ -1631,6 +1631,7 @@ describe('ChartOptions', () => {
                         type: 'series',
                         name: seriesType,
                         chartType: 'cartesian',
+                        options: {},
                         stackable,
                         groupable,
                         stackedByDefault,
