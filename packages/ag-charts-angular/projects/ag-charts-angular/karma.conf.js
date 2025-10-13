d281ffb3c16e534dd1654688ddb74e1c5c96b854
Alan Treadway <alan.treadway@ag-grid.com>
2025-10-13 07:57:36 +0100

    AG-16097 - Fix SonarCloud S7772 issue (Prefer node:path over path)
    Updated karma.conf.js to use the modern 'node:path' protocol prefix instead of
    'path' when requiring built-in Node.js modules. This is a best practice in
    Node.js v16+ to explicitly identify built-in modules.

--- packages/ag-charts-angular/projects/ag-charts-angular/karma.conf.js
+++ packages/ag-charts-angular/projects/ag-charts-angular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
             suppressAll: true, // removes the duplicated traces
         },
         coverageReporter: {
-            dir: require('path').join(__dirname, '../../coverage/ag-charts-angular'),
+            dir: require('node:path').join(__dirname, '../../coverage/ag-charts-angular'),
             subdir: '.',
             reporters: [{ type: 'html' }, { type: 'text-summary' }],
         },
