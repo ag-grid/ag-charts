c726f5de2e46461f5c025c258e759cfd0eb804b6
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-10-15 12:18:30 +0100

    AG-5877 - e2e tests

---
+++ packages/ag-charts-website/src/content/docs/zoom-test/_examples/e2e-zoom-axis-overlap/main.ts
@@ -0,0 +1,49 @@
+import { AgCartesianChartOptions, AgCharts } from 'ag-charts-enterprise';
+
+import { getData } from './data';
+
+const options: AgCartesianChartOptions = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    zoom: {
+        enabled: true,
+        enableAxisDragging: true,
+        enableAxisScrolling: true,
+    },
+    padding: {
+        top: 10,
+        left: 0,
+        right: 40,
+        bottom: 0,
+    },
+    axes: [
+        {
+            type: 'category',
+            position: 'bottom',
+            crossAt: { value: 35 },
+            title: { text: 'Index' },
+            line: {
+                stroke: 'black',
+            },
+            paddingOuter: 0,
+        },
+        {
+            type: 'number',
+            position: 'left',
+            crossAt: { value: 5 },
+            title: { text: 'Value' },
+            line: {
+                stroke: 'black',
+            },
+        },
+    ],
+    series: [
+        {
+            type: 'bar',
+            xKey: 'x',
+            yKey: 'y',
+        },
+    ],
+};
+
+AgCharts.create(options);
