70fab35396816f1574e0de83d659d3a232abef25
Oli Legat <olivier.legat@ag-grid.com>
2025-10-20 13:13:44 +0100

    CRT-960 Use Date objects instead of strings for X axis

--- packages/ag-charts-website/src/content/docs/localisation/_examples/installing-locale/main.ts
+++ packages/ag-charts-website/src/content/docs/localisation/_examples/installing-locale/main.ts
@@ -10,12 +10,12 @@ const options: AgChartOptions = {
         text: 'Hover zoom buttons for tooltips, right click to show context menu',
     },
     data: [
-        { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
-        { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
-        { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
-        { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
-        { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
-        { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
+        { month: new Date(2025, 0, 1), avgTemp: 2.3, iceCreamSales: 162000 },
+        { month: new Date(2025, 2, 1), avgTemp: 6.3, iceCreamSales: 302000 },
+        { month: new Date(2025, 4, 1), avgTemp: 16.2, iceCreamSales: 800000 },
+        { month: new Date(2025, 6, 1), avgTemp: 22.8, iceCreamSales: 1254000 },
+        { month: new Date(2025, 8, 1), avgTemp: 14.5, iceCreamSales: 950000 },
+        { month: new Date(2025, 10, 1), avgTemp: 8.9, iceCreamSales: 200000 },
     ],
     series: [
         {
