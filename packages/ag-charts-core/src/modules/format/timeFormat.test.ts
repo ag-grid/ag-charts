cd767eecbaf02d9cdb693ee647f8236421016dc2
Oli Legat <olivier.legat@ag-grid.com>
2025-10-20 16:15:20 +0100

    CRT-960 Add Spanish & Chinese timeFormet tests

--- packages/ag-charts-core/src/modules/format/timeFormat.test.ts
+++ packages/ag-charts-core/src/modules/format/timeFormat.test.ts
@@ -11,175 +11,308 @@ describe('Date/Time Formatting', () => {
     // Falls on a Sunday.
     const FIRST_DAY_OF_2017 = new Date(Date.UTC(2017, 0, 1, 11, 12, 13, 100));
 
-    type Case = { name: string; format: string; expected: { 'en-US': string }; date?: Date };
+    type Case = {
+        name: string;
+        format: string;
+        expected: {
+            'en-US': string;
+            'es-ES': string;
+            'zh-TW': string;
+        };
+        date?: Date;
+    };
     const cases: Case[] = [
         {
             name: 'short text',
             format: '%a %-d %b, %y',
-            expected: { 'en-US': 'Tue 3 Sep, 19' },
+            expected: {
+                'en-US': 'Tue 3 Sep, 19',
+                'es-ES': 'mar 3 sept, 19',
+                'zh-TW': '週二 3 9月, 19',
+            },
         },
         {
             name: 'long text',
             format: '%A %d %B, %Y',
-            expected: { 'en-US': 'Tuesday 03 September, 2019' },
+            expected: {
+                'en-US': 'Tuesday 03 September, 2019',
+                'es-ES': 'martes 03 septiembre, 2019',
+                'zh-TW': '星期二 03 9月, 2019',
+            },
         },
         {
             name: 'locale date + time',
             format: '%c',
-            expected: { 'en-US': '9/3/2019, 3:50:17 PM' },
+            expected: {
+                'en-US': '9/3/2019, 3:50:17 PM',
+                'es-ES': '3/9/2019, 3:50:17 p. m.',
+                'zh-TW': '2019/9/3 下午3:50:17',
+            },
         },
         {
             name: 'precise time',
             format: '%H:%M:%S.%L',
-            expected: { 'en-US': '15:50:17.300' },
+            expected: {
+                'en-US': '15:50:17.300',
+                'es-ES': '15:50:17.300',
+                'zh-TW': '15:50:17.300',
+            },
         },
         {
             name: 'super-precise time',
             format: '%H:%M:%S.%f',
-            expected: { 'en-US': '15:50:17.300000' },
+            expected: {
+                'en-US': '15:50:17.300000',
+                'es-ES': '15:50:17.300000',
+                'zh-TW': '15:50:17.300000',
+            },
         },
         {
             name: 'am/pm',
             format: '%I:%M %p',
-            expected: { 'en-US': '03:50 PM' },
+            expected: {
+                'en-US': '03:50 PM',
+                'es-ES': '03:50 PM',
+                'zh-TW': '03:50 PM',
+            },
         },
         {
             name: 'UNIX milliseconds',
             format: '%Q',
-            expected: { 'en-US': '1567522217300' },
+            expected: {
+                'en-US': '1567522217300',
+                'es-ES': '1567522217300',
+                'zh-TW': '1567522217300',
+            },
         },
         {
             name: 'UNIX seconds',
             format: '%s',
-            expected: { 'en-US': '1567522217' },
+            expected: {
+                'en-US': '1567522217',
+                'es-ES': '1567522217',
+                'zh-TW': '1567522217',
+            },
         },
         {
             name: 'days of year',
             format: '%j',
-            expected: { 'en-US': '246' },
+            expected: {
+                'en-US': '246',
+                'es-ES': '246',
+                'zh-TW': '246',
+            },
         },
         {
             name: 'days of year (1st day)',
             format: '%j',
-            expected: { 'en-US': '001' },
+            expected: {
+                'en-US': '001',
+                'es-ES': '001',
+                'zh-TW': '001',
+            },
             date: FIRST_DAY_OF_2019,
         },
         {
             name: 'weeks of year (Sunday-based)',
             format: '%U',
-            expected: { 'en-US': '35' },
+            expected: {
+                'en-US': '35',
+                'es-ES': '35',
+                'zh-TW': '35',
+            },
         },
         {
             name: 'weeks of year (Sunday-based) 1st Jan 2019',
             format: '%U',
-            expected: { 'en-US': '00' },
+            expected: {
+                'en-US': '00',
+                'es-ES': '00',
+                'zh-TW': '00',
+            },
             date: FIRST_DAY_OF_2019,
         },
         {
             name: 'weeks of year (Sunday-based) 1st Jan 2018',
             format: '%U',
-            expected: { 'en-US': '00' },
+            expected: {
+                'en-US': '00',
+                'es-ES': '00',
+                'zh-TW': '00',
+            },
             date: FIRST_DAY_OF_2018,
         },
         {
             name: 'weeks of year (Sunday-based) 1st Jan 2017',
             format: '%U',
-            expected: { 'en-US': '01' },
+            expected: {
+                'en-US': '01',
+                'es-ES': '01',
+                'zh-TW': '01',
+            },
             date: FIRST_DAY_OF_2017,
         },
         {
             name: 'weeks of year (Monday-based)',
             format: '%W',
-            expected: { 'en-US': '35' },
+            expected: {
+                'en-US': '35',
+                'es-ES': '35',
+                'zh-TW': '35',
+            },
         },
         {
             name: 'weeks of year (Monday-based) 1st Jan 2019',
             format: '%W',
-            expected: { 'en-US': '00' },
+            expected: {
+                'en-US': '00',
+                'es-ES': '00',
+                'zh-TW': '00',
+            },
             date: FIRST_DAY_OF_2019,
         },
         {
             name: 'weeks of year (Monday-based) 1st Jan 2018',
             format: '%W',
-            expected: { 'en-US': '01' },
+            expected: {
+                'en-US': '01',
+                'es-ES': '01',
+                'zh-TW': '01',
+            },
             date: FIRST_DAY_OF_2018,
         },
         {
             name: 'weeks of year (Monday-based) 1st Jan 2017',
             format: '%W',
-            expected: { 'en-US': '00' },
+            expected: {
+                'en-US': '00',
+                'es-ES': '00',
+                'zh-TW': '00',
+            },
             date: FIRST_DAY_OF_2017,
         },
         {
             name: 'weeks of year (ISO-based)',
             format: '%V',
-            expected: { 'en-US': '36' },
+            expected: {
+                'en-US': '36',
+                'es-ES': '36',
+                'zh-TW': '36',
+            },
         },
         {
             name: 'weeks of year (ISO-based) 1st Jan 2019',
             format: '%V',
-            expected: { 'en-US': '01' },
+            expected: {
+                'en-US': '01',
+                'es-ES': '01',
+                'zh-TW': '01',
+            },
             date: FIRST_DAY_OF_2019,
         },
         {
             name: 'weeks of year (ISO-based) 1st Jan 2018',
             format: '%V',
-            expected: { 'en-US': '01' },
+            expected: {
+                'en-US': '01',
+                'es-ES': '01',
+                'zh-TW': '01',
+            },
             date: FIRST_DAY_OF_2018,
         },
         {
             name: 'weeks of year (ISO-based) 1st Jan 2017',
             format: '%V',
-            expected: { 'en-US': '52' },
+            expected: {
+                'en-US': '52',
+                'es-ES': '52',
+                'zh-TW': '52',
+            },
             date: FIRST_DAY_OF_2017,
         },
         {
             name: 'timezone offset',
             format: '%Z',
-            expected: { 'en-US': '+0100' },
+            expected: {
+                'en-US': '+0100',
+                'es-ES': '+0100',
+                'zh-TW': '+0100',
+            },
         },
         {
             name: 'locale date',
             format: '%x',
-            expected: { 'en-US': '9/3/2019' },
+            expected: {
+                'en-US': '9/3/2019',
+                'es-ES': '3/9/2019',
+                'zh-TW': '2019/9/3',
+            },
         },
         {
             name: 'locale time',
             format: '%X',
-            expected: { 'en-US': '3:50:17 PM' },
+            expected: {
+                'en-US': '3:50:17 PM',
+                'es-ES': '15:50:17',
+                'zh-TW': '下午3:50:17',
+            },
         },
         {
             name: '% escape',
             format: '100%%',
-            expected: { 'en-US': '100%' },
+            expected: {
+                'en-US': '100%',
+                'es-ES': '100%',
+                'zh-TW': '100%',
+            },
         },
         {
             name: 'default padding',
             format: '%A, %d %B, %Y - %H:%M:%S.%L',
-            expected: { 'en-US': 'Tuesday, 03 September, 2019 - 15:50:17.300' },
+            expected: {
+                'en-US': 'Tuesday, 03 September, 2019 - 15:50:17.300',
+                'es-ES': 'martes, 03 septiembre, 2019 - 15:50:17.300',
+                'zh-TW': '星期二, 03 9月, 2019 - 15:50:17.300',
+            },
         },
         {
             name: '0 padding',
             format: '%A, %0d %B, %Y - %H:%M:%S.%L',
-            expected: { 'en-US': 'Tuesday, 03 September, 2019 - 15:50:17.300' },
+            expected: {
+                'en-US': 'Tuesday, 03 September, 2019 - 15:50:17.300',
+                'es-ES': 'martes, 03 septiembre, 2019 - 15:50:17.300',
+                'zh-TW': '星期二, 03 9月, 2019 - 15:50:17.300',
+            },
         },
         {
             name: 'space padding',
             format: '%A, %_d %B, %Y - %H:%M:%S.%L',
-            expected: { 'en-US': 'Tuesday,  3 September, 2019 - 15:50:17.300' },
+            expected: {
+                'en-US': 'Tuesday,  3 September, 2019 - 15:50:17.300',
+                'es-ES': 'martes,  3 septiembre, 2019 - 15:50:17.300',
+                'zh-TW': '星期二,  3 9月, 2019 - 15:50:17.300',
+            },
         },
         {
             name: 'null padding',
             format: '%A, %-d %B, %Y - %H:%M:%S.%L',
-            expected: { 'en-US': 'Tuesday, 3 September, 2019 - 15:50:17.300' },
+            expected: {
+                'en-US': 'Tuesday, 3 September, 2019 - 15:50:17.300',
+                'es-ES': 'martes, 3 septiembre, 2019 - 15:50:17.300',
+                'zh-TW': '星期二, 3 9月, 2019 - 15:50:17.300',
+            },
         },
         {
             name: 'invalid formatters',
             format: '%T%n',
-            expected: { 'en-US': 'Tn' },
+            expected: {
+                'en-US': 'Tn',
+                'es-ES': 'Tn',
+                'zh-TW': 'Tn',
+            },
         },
     ];
-    const langs = [['en-US']] as const;
+    const langs: (keyof Case['expected'])[][] = [['en-US'], ['es-ES'], ['zh-TW']];
 
     it('should be using Europe/London timezone', () => {
         // If this test fails, check that process.env.TZ is set to Europe/London in jest.setup.js.
