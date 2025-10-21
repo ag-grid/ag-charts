d733e53b40eb94c8e3783819f1e0c83bb6ddcc1c
Oli Legat <olivier.legat@ag-grid.com>
2025-10-21 16:48:33 +0100

    CRT-960 Fix legend item button text when localeText changes
    Conflicts:
    	packages/ag-charts-community/src/chart/chart.ts

--- packages/ag-charts-community/src/chart/chart.ts
+++ packages/ag-charts-community/src/chart/chart.ts
@@ -37,6 +37,7 @@ import type {
     AgColorType,
     AgDataTransaction,
     AgInitialStateLegendOptions,
+    AgLocaleOptions,
     AgMiniChartSeriesOptions,
     FormatterConfiguration,
     SeriesOptionsTypes,
@@ -623,6 +624,7 @@ export abstract class Chart extends Observable implements ModuleInstance, ChartS
     }
 
     private apiUpdate = false;
+    private pendingLocaleText?: AgLocaleOptions['localeText'];
     private _pendingFactoryUpdatesCount = 0;
     private _performUpdateSkipAnimations: boolean = false;
     private readonly _performUpdateNotify = new AsyncAwaitQueue();
@@ -758,6 +760,11 @@ export abstract class Chart extends Observable implements ModuleInstance, ChartS
 
                 await this.processData();
                 this.seriesAreaManager.dataChanged();
+                if (this.pendingLocaleText) {
+                    this.modulesManager.getModule<any>('locale').localeText = this.pendingLocaleText;
+                    this.pendingLocaleText = undefined;
+                }
+
                 this.updateSplits('📊');
             // fallthrough
 
@@ -1424,8 +1431,7 @@ export abstract class Chart extends Observable implements ModuleInstance, ChartS
             }
         }
         if (deltaOptions.locale?.localeText) {
-            const localeModule: any = this.modulesManager.getModule('locale');
-            localeModule.localeText = deltaOptions.locale?.localeText;
+            this.pendingLocaleText = deltaOptions.locale?.localeText;
         }
 
         this.chartOptions = newChartOptions;
