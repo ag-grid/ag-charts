9a3fd3d78693296034d5b1d1e8ce6cfddd055af9
Alan Treadway <alan.treadway@ag-grid.com>
2025-11-04 17:46:53 +0000

    AG-16258 - Add regression tests for axis issues.

---
+++ packages/ag-charts-website/src/content/docs/sparklines/_examples/sparkline-data-updates/data.ts
@@ -0,0 +1,11 @@
+export const data = [
+    { date: new Date('2025-01-01'), price: 150 },
+    { date: new Date('2025-01-02'), price: 152 },
+    { date: new Date('2025-01-03'), price: 148 },
+    { date: new Date('2025-01-04'), price: 155 },
+    { date: new Date('2025-01-05'), price: 153 },
+    { date: new Date('2025-01-06'), price: 158 },
+    { date: new Date('2025-01-07'), price: 156 },
+    { date: new Date('2025-01-08'), price: 160 },
+];
+
