90708ea24fc98f5e8783a67df7a78a7304af3aa6
seanlandsman <sean@thelandsmans.com>
2025-11-05 09:41:33 +0000

    Correct form return path

--- external/ag-website-shared/src/components/contact-form/constants.ts
+++ external/ag-website-shared/src/components/contact-form/constants.ts
@@ -1,13 +1,8 @@
-import { GRID_URL, SITE_URL } from '@constants';
+import { GRID_URL } from '@constants';
 import { pathJoin } from '@utils/pathJoin';
-import { urlWithBaseUrl } from '@utils/urlWithBaseUrl';
 
 export type ResultType = 'success' | 'failure';
 
-console.log(GRID_URL);
-console.log(GRID_URL);
-console.log(GRID_URL);
-console.log(GRID_URL);
 export const RETURN_URLS: Record<ResultType, string> = {
     // NOTE: Need to add trailing slash to avoid 302 redirect on S3
     success: pathJoin(GRID_URL, '/contact/success') + '/',
