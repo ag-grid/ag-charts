59bcff58c207bfaed58ce79c4a7f43273f645602
seanlandsman <sean@thelandsmans.com>
2025-11-06 10:56:14 +0000

    Backport new npm release process for LTS

---
+++ tools/release/performSigningActions.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+PACKAGE_DIRECTORY="./dist/packages"
+
+if [ "$#" -ne 1 ]
+then
+    echo "You must the GPG public key as the first argument"
+    exit 1
+fi
+
+if [ -z "$1" ];
+then
+    echo "The GPG public key supplied is empty"
+    exit 1
+fi
+
+for package in `ls $PACKAGE_DIRECTORY/*.tgz`;
+do
+  gpg --sign --detach-sign --interactive --verbose --digest-algo sha512 -o $package.sig $package
+done
+
+echo "$1"  > "$PACKAGE_DIRECTORY/gpg-pub.asc"
