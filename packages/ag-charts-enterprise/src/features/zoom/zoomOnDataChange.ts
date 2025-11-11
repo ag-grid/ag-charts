b72b05b307ec3ac790a249bad67c8f370da69042
Oli Legat <olivier.legat@ag-grid.com>
2025-11-11 10:42:47 +0000

    AG-8627 Remove onChange callback (it's not clear if we need this now)

--- packages/ag-charts-enterprise/src/features/zoom/zoomOnDataChange.ts
+++ packages/ag-charts-enterprise/src/features/zoom/zoomOnDataChange.ts
@@ -1,24 +1,14 @@
 import { _ModuleSupport } from 'ag-charts-community';
-import { ActionOnSet, BaseProperties, type DeepRequired, Property } from 'ag-charts-core';
+import { BaseProperties, type DeepRequired, Property } from 'ag-charts-core';
 import type { AgZoomOnDataChange, AgZoomOnDataChangeStrategy } from 'ag-charts-types';
 
 // `chart.zoom.onDataChange` options
 export class ZoomOnDataChangeProperties extends BaseProperties implements DeepRequired<AgZoomOnDataChange> {
-    public onChange?: () => void;
-
     @Property
-    @ActionOnSet<ZoomOnDataChangeProperties>({
-        changeValue() {
-            this.onChange?.();
-        },
-    })
     strategy: AgZoomOnDataChangeStrategy = 'preserveDomain';
 }
 
 export class ZoomOnDataChange {
     constructor(private readonly properties: ZoomOnDataChangeProperties) {
-        properties.onChange = () => this.onPropertiesChange();
     }
-
-    onPropertiesChange(): void {}
 }
