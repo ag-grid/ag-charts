fa32cabecdecb5d7a6ad1d4fe1f82ef1f3b41fe7
Oli Legat <olivier.legat@ag-grid.com>
2025-11-24 16:53:07 +0000

    AG-16259 Move node.js test to playwright

--- packages/ag-charts-website/e2e/context-menu.spec.ts
+++ packages/ag-charts-website/e2e/context-menu.spec.ts
@@ -116,4 +116,42 @@ test.describe('context-menu', () => {
         await sayHello.hover();
         await expect(page).toHaveScreenshot('AG-16178-say-hello-hovered.png');
     });
+
+    test.describe('AG-16259 showsOn', () => {
+        test.beforeEach(async ({ page }) => {
+            const { url } = toExamplePageUrl('context-menu-test', 'ag-16259-showOn', 'vanilla');
+            await gotoExample(page, url);
+        });
+
+        const cases: [string, number, number, string][] = [
+            ['chart', 212, 50, 'always,'],
+            ['title', 405, 47, 'always,'],
+            ['subtitle', 399, 76, 'always,'],
+            ['footnote', 403, 555, 'always,'],
+            ['xAxisLabel', 432, 476, 'always,'],
+            ['yAxisLabel', 46, 275, 'always,'],
+            ['seriesNode1', 400, 300, 'always,series-area,series-node,'],
+            ['seriesNode2', 710, 350, 'always,series-area,series-node,'],
+            ['seriesArea', 292, 165, 'always,series-area,'],
+            ['legendItem1', 356, 521, 'always,legend-item,'],
+            ['legendItem2', 460, 521, 'always,legend-item,'],
+        ];
+
+        for (const [name, x, y, expectedHtmlText] of cases) {
+            test(name, async ({ page }) => {
+                const outerHTML = await page.evaluate(
+                    ({ x, y }) => {
+                        const el = document.elementFromPoint(x, y);
+                        return el?.outerHTML ?? '';
+                    },
+                    { x, y }
+                );
+                expect(outerHTML).toMatchSnapshot();
+
+                await page.mouse.click(x, y, { button: 'right' });
+                const actualHtmlText = await page.textContent('.ag-charts-context-menu');
+                expect(actualHtmlText).toEqual(expectedHtmlText);
+            });
+        }
+    });
 });
