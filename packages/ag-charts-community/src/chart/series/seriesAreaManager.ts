46340a990fac1ffb35b8aeefe377629fb06ea3d0
Oli Legat <olivier.legat@ag-grid.com>
2025-11-25 10:00:52 +0000

    AG-16259 Fix context-menu `showOn` bug when right-clicking a caption

--- packages/ag-charts-community/src/chart/series/seriesAreaManager.ts
+++ packages/ag-charts-community/src/chart/series/seriesAreaManager.ts
@@ -333,7 +333,7 @@ export class SeriesAreaManager extends BaseManager {
         const { sourceEvent } = event;
         if (sourceEvent.currentTarget != current.getElement()) return;
 
-        if (sourceEvent.target == this.chart.ctx.widgets.containerWidget.getElement()) {
+        if (current !== this.chart.ctx.widgets.seriesWidget) {
             if (this.isState(InteractionState.ContextMenuable)) {
                 const { currentX: canvasX, currentY: canvasY } = event;
                 this.chart.ctx.contextMenuRegistry.dispatchContext(
