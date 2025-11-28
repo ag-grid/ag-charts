d2c54edf25344596d596ddf3dce8d41ef831207c
Oli Legat <olivier.legat@ag-grid.com>
2025-11-28 09:17:58 +0000

    AG-8627 Rename default 'resize' to 'preserveRatios'

--- packages/ag-charts-enterprise/src/features/zoom/zoomModule.ts
+++ packages/ag-charts-enterprise/src/features/zoom/zoomModule.ts
@@ -89,7 +89,7 @@ export const ZoomModule: PluginModuleDefinition<AgZoomOptions> = {
             padding: 0.05,
         },
         onDataChange: {
-            strategy: 'resize', // TODO(olegat): change default to 'preserveDomain'
+            strategy: 'preserveRatios', // TODO(olegat): change default to 'preserveDomain'
         },
         anchorPointX: 'end',
         anchorPointY: 'middle',
