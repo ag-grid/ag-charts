77472fe9e24ab2b5ab2a592612986816512fc2ae
Oli Legat <olivier.legat@ag-grid.com>
2025-11-28 13:39:02 +0000

    AG-15406 nx format

--- packages/ag-charts-types/src/chart/flashOnUpdateOptions.ts
+++ packages/ag-charts-types/src/chart/flashOnUpdateOptions.ts
@@ -1,4 +1,4 @@
-import type { CssColor, DurationMs, Opacity } from "./types";
+import type { CssColor, DurationMs, Opacity } from './types';
 
 export type AgFlashOnUpdateItem = 'chart' | 'category';
 
