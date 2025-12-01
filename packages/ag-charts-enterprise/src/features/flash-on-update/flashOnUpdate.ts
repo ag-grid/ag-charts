0bbb55b274175ef3afea028b5ee7ae4d2eec5fb8
Oli Legat <olivier.legat@ag-grid.com>
2025-12-01 13:25:20 +0000

    AG-15406 Add placeholder flashCategoryBands implementation

--- packages/ag-charts-enterprise/src/features/flash-on-update/flashOnUpdate.ts
+++ packages/ag-charts-enterprise/src/features/flash-on-update/flashOnUpdate.ts
@@ -1,7 +1,9 @@
 import { _ModuleSupport } from 'ag-charts-community';
-import { BaseProperties, CleanupRegistry, Property } from 'ag-charts-core';
-import type { ModuleInstance } from 'ag-charts-core';
+import { BaseProperties, CleanupRegistry, Property, createElement, setAttribute, setElementBBox } from 'ag-charts-core';
+import type { BoxBounds, ModuleInstance } from 'ag-charts-core';
 import type { AgFlashOnUpdateItem, AgFlashOnUpdateOptions, CssColor, DurationMs, Opacity } from 'ag-charts-types';
+
+type DataSet<T> = _ModuleSupport.DataSet<T>;
 
 export class FlashOnUpdate extends BaseProperties implements ModuleInstance, AgFlashOnUpdateOptions {
     static readonly className = 'FlashOnUpdate';
@@ -25,28 +27,36 @@ export class FlashOnUpdate extends BaseProperties implements ModuleInstance, AgF
     fadeDuration: DurationMs = 900;
 
     private readonly cleanup = new CleanupRegistry();
+    private readonly element: Element;
+    private data: unknown[] = [];
+    private animationTimeout?: ReturnType<typeof setTimeout>;
 
     constructor(protected ctx: _ModuleSupport.ModuleContext) {
         super();
-        const onFirstDraw = () => {
-            ctx.eventsHub.off('layout:complete', onFirstDraw);
-            this.cleanup.register(ctx.eventsHub.on('data:update', () => this.onDataChange()));
+        this.element = this.ctx.domManager.addChild('canvas-background', 'flashOnUpdate');
+        this.element.role = 'presentation';
+
+        let firstUpdate = true;
+        const onFirstDataUpdate = (e: DataSet<unknown> | undefined): void => {
+            if (firstUpdate && e?.data) {
+                this.data = e?.data;
+                firstUpdate = false;
+            } else {
+                this.onDataChange(e);
+            }
         };
-        this.cleanup.register(ctx.eventsHub.on('layout:complete', onFirstDraw));
+        this.cleanup.register(ctx.eventsHub.on('data:update', onFirstDataUpdate));
     }
 
     destroy() {
+        this.ctx.domManager.removeChild('canvas-background', 'flashOnUpdate');
         this.cleanup.flush();
     }
 
-    private onDataChange(): void {
-        if (!this.enabled) return;
-
-        const containerEl = this.ctx.widgets.containerWidget.getElement();
-
+    private flashElem(el: Animatable): void {
         const { flashDuration, fadeDuration } = this;
         const duration = flashDuration + fadeDuration;
-        containerEl.animate(
+        el.animate(
             [
                 { background: this.color, offset: 0 },
                 { background: this.color, offset: flashDuration / duration },
@@ -54,5 +64,41 @@ export class FlashOnUpdate extends BaseProperties implements ModuleInstance, AgF
             ],
             { duration, easing: 'ease-out' }
         );
+    }
+
+    private flashCategoryBands(e: DataSet<unknown> | undefined): void {
+        clearTimeout(this.animationTimeout);
+
+        const flashBounds: BoxBounds[] = this.computeCategoryFlashBounds(e);
+        for (const bounds of flashBounds) {
+            const e = createElement('div');
+            setAttribute(e, 'role', 'presentation');
+            setElementBBox(e, bounds);
+            this.element.appendChild(e);
+            this.flashElem(e);
+        }
+
+        const duration = this.flashDuration + this.fadeDuration;
+        this.animationTimeout = setTimeout(() => (this.element.innerHTML = ''), duration);
+    }
+
+    private computeCategoryFlashBounds(e: DataSet<unknown> | undefined): BoxBounds[] {
+        e satisfies any;
+        this.data satisfies any;
+        return [{ x: 20, y: 30, height: 100, width: 140 }];
+    }
+
+    private onDataChange(e: DataSet<unknown> | undefined): void {
+        if (!this.enabled) return;
+
+        switch (this.item) {
+            case 'chart':
+                return this.flashElem(this.ctx.widgets.containerWidget.getElement());
+            case 'category':
+                return this.flashCategoryBands(e);
+            default:
+                const unreachable = (a: never): never => a;
+                return unreachable(this.item);
+        }
     }
 }
