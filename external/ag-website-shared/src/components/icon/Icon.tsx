a115604c36fa8eaef2a848521fe90d679b979100
Tak Tran <contact@tutaktran.com>
2025-12-04 15:26:50 +0000

    AG-3390 - Fix trial licence anchor link (#5755)
    * AG-3390 - Fix trial licence anchor link
    
    * git subrepo push external/ag-website-shared
    
    subrepo:
      subdir:   "external/ag-website-shared"
      merged:   "61540c2b3f"
    upstream:
      origin:   "git@github.com:ag-grid/ag-website-shared.git"
      branch:   "latest"
      commit:   "61540c2b3f"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "30db3b8"
    
    * Ag-3390 - Remove unused cursor icon
    
    * git subrepo push external/ag-website-shared
    
    subrepo:
      subdir:   "external/ag-website-shared"
      merged:   "e2916ec7f5"
    upstream:
      origin:   "git@github.com:ag-grid/ag-website-shared.git"
      branch:   "latest"
      commit:   "e2916ec7f5"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "30db3b8"

--- external/ag-website-shared/src/components/icon/Icon.tsx
+++ external/ag-website-shared/src/components/icon/Icon.tsx
@@ -153,7 +153,6 @@ export const ICON_MAP = {
     alarm: CarbonIcon.Alarm,
     terminal: CarbonIcon.Terminal,
     support: CarbonIcon.Chat,
-    cursor: CarbonIcon.Cursor_1,
     ...SOCIALS_ICON_MAP,
     ...CHARTS_ICON_MAP,
 };
