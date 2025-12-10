33d5ff4e36ef618835ae1e2625e99825e4820a47
Oli Legat <olivier.legat@ag-grid.com>
2025-12-10 17:05:45 +0000

    AG-16426 TC2 Add a test-variant for when data changes too

--- packages/ag-charts-enterprise/src/features/zoom/zoomAutoScale.test.ts
+++ packages/ag-charts-enterprise/src/features/zoom/zoomAutoScale.test.ts
@@ -217,4 +217,73 @@ describe('ZoomAutoScale', () => {
         expect(zoomListener.mock.mock.calls[0][0]).not.toMatchObject({ ratioY: { start: 0, end: 1 } });
         expect(zoomListener.mock.mock.calls[0][0].autoScaledAxes).toMatchObject(['y']);
     });
+
+    test('updateDelta should respond to autoScaling and data changes', async () => {
+        type D = { input: number; output: number };
+        let state: AgChartState;
+        const zoomListener = newFreezableMockInferred<MockZoomListener<D, unknown>>();
+        const options: AgCartesianChartOptions<D> = {
+            data: [
+                { input: 0, output: -20 },
+                { input: 1, output: 50 },
+                { input: 2, output: 25 },
+                { input: 3, output: 60 },
+                { input: 4, output: 50 },
+                { input: 5, output: 22 },
+                { input: 6, output: 50 },
+                { input: 7, output: 175 },
+            ],
+            series: [{ type: 'line', xKey: 'input', yKey: 'output' }],
+            zoom: {
+                enabled: true,
+                autoScaling: {
+                    enabled: false,
+                },
+            },
+            listeners: {
+                zoom: zoomListener.frozen,
+            },
+        };
+
+        // Init chart; Expect no zoom at all.
+        await createChart<D>(options);
+        state = chart.getState();
+        expect(state.zoom?.autoScaledAxes).toBeUndefined();
+        expect(state.zoom?.ratioX).toMatchObject({ start: 0, end: 1 });
+        expect(state.zoom?.ratioY).toMatchObject({ start: 0, end: 1 });
+        expect(zoomListener.mock).toBeCalledTimes(0);
+
+        // Zoom In; Expect zoom on X-axis only (no autoScaling).
+        await wheelZoomIn();
+        state = chart.getState();
+        expect(state.zoom?.autoScaledAxes).toBeUndefined();
+        expect(state.zoom?.ratioX).not.toMatchObject({ start: 0, end: 1 });
+        expect(state.zoom?.ratioY).toMatchObject({ start: 0, end: 1 });
+        expect(zoomListener.mock).toBeCalledTimes(1);
+        expect(zoomListener.mock.mock.calls[0][0]).not.toMatchObject({ ratioX: { start: 0, end: 1 } });
+        expect(zoomListener.mock.mock.calls[0][0]).toMatchObject({ ratioY: { start: 0, end: 1 } });
+        expect(zoomListener.mock.mock.calls[0][0]?.autoScaledAxes).toBeUndefined();
+        zoomListener.mock.mockClear();
+
+        // Enable autoScaling;
+        // (1)  Expect zoom on both X and Y axes.
+        // (2)  Expect the Y ranges to have been multiplied by 100.
+        await chart.updateDelta({
+            data: options.data!.map(({ input, output }) => ({ input, output: output * 100 })),
+            zoom: { autoScaling: { enabled: true } },
+        });
+        state = chart.getState();
+        // (1)
+        expect(state.zoom?.autoScaledAxes).toMatchObject(['y']);
+        expect(state.zoom?.ratioX).not.toMatchObject({ start: 0, end: 1 });
+        expect(state.zoom?.ratioY).not.toMatchObject({ start: 0, end: 1 });
+        expect(zoomListener.mock).toBeCalledTimes(1);
+        expect(zoomListener.mock.mock.calls[0][0]).not.toMatchObject({ ratioX: { start: 0, end: 1 } });
+        expect(zoomListener.mock.mock.calls[0][0]).not.toMatchObject({ ratioY: { start: 0, end: 1 } });
+        expect(zoomListener.mock.mock.calls[0][0].autoScaledAxes).toMatchObject(['y']);
+        // (2)
+        const expectedYRange = { start: 1434.9999999999964, end: 18265.000000000004 };
+        expect(state.zoom?.rangeY).toMatchObject(expectedYRange);
+        expect(zoomListener.mock.mock.calls[0][0]).toMatchObject({ rangeY: expectedYRange });
+    });
 });
