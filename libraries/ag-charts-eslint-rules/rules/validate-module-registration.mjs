682b6391bd89115c3a472a3467465a030b6d7219
Oli Legat <olivier.legat@ag-grid.com>
2025-12-11 16:12:36 +0000

    AG-15406 Add `requirements` parameter to `requireModule`

--- libraries/ag-charts-eslint-rules/rules/validate-module-registration.mjs
+++ libraries/ag-charts-eslint-rules/rules/validate-module-registration.mjs
@@ -91,9 +91,9 @@ export default {
         /**
          * Add a required module with reason
          */
-        function requireModule(moduleId, reason, node) {
-            if (!requiredModules.has(moduleId)) {
-                requiredModules.set(moduleId, { reason, node });
+        function requireModule(requirements, moduleId, reason, node) {
+            if (!requirements.has(moduleId)) {
+                requirements.set(moduleId, { reason, node });
             }
         }
 
@@ -171,7 +171,7 @@ export default {
         /**
          * Process a single series object to find required modules
          */
-        function processSeriesObject(element, parentNode) {
+        function processSeriesObject(requirements, element, parentNode) {
             element = unwrapExpressions(element);
             if (!element || element.type !== 'ObjectExpression') return;
 
@@ -187,7 +187,7 @@ export default {
                         seriesTypes.push(seriesType);
                         const moduleId = seriesTypeToModule.get(seriesType);
                         if (moduleId) {
-                            requireModule(moduleId, `series type '${seriesType}'`, prop);
+                            requireModule(requirements, moduleId, `series type '${seriesType}'`, prop);
                         }
                     }
                 }
@@ -195,7 +195,7 @@ export default {
                 // Check for series plugins (errorBar)
                 if (keyName && seriesPluginToModule.has(keyName)) {
                     const moduleId = seriesPluginToModule.get(keyName);
-                    requireModule(moduleId, `series option '${keyName}'`, prop);
+                    requireModule(requirements, moduleId, `series option '${keyName}'`, prop);
                 }
             }
         }
@@ -227,12 +227,12 @@ export default {
         /**
          * Process a CallExpression that may contain series (e.g., data.map())
          */
-        function processSeriesCallExpression(callExpr, parentNode) {
+        function processSeriesCallExpression(requirements, callExpr, parentNode) {
             const callback = callExpr.arguments[0];
             if (callback && (callback.type === 'ArrowFunctionExpression' || callback.type === 'FunctionExpression')) {
                 const returnValue = extractReturnValue(callback.body);
                 if (returnValue && returnValue.type === 'ObjectExpression') {
-                    processSeriesObject(returnValue, parentNode);
+                    processSeriesObject(requirements, returnValue, parentNode);
                 }
             }
         }
@@ -240,7 +240,7 @@ export default {
         /**
          * Process series array to find required modules
          */
-        function processSeriesArray(seriesArray, parentNode) {
+        function processSeriesArray(requirements, seriesArray, parentNode) {
             seriesArray = unwrapExpressions(seriesArray);
 
             if (seriesArray.type === 'ArrayExpression') {
@@ -251,23 +251,23 @@ export default {
                     if (element.type === 'SpreadElement') {
                         const spreadArg = unwrapExpressions(element.argument);
                         if (spreadArg.type === 'CallExpression') {
-                            processSeriesCallExpression(spreadArg, parentNode);
+                            processSeriesCallExpression(requirements, spreadArg, parentNode);
                         }
                         continue;
                     }
 
-                    processSeriesObject(element, parentNode);
+                    processSeriesObject(requirements, element, parentNode);
                 }
             } else if (seriesArray.type === 'CallExpression') {
                 // Handle computed series like: series: data.map(d => ({ type: 'bar', ... }))
-                processSeriesCallExpression(seriesArray, parentNode);
+                processSeriesCallExpression(requirements, seriesArray, parentNode);
             }
         }
 
         /**
          * Process axes object to find required modules
          */
-        function processAxes(axesNode, parentNode) {
+        function processAxes(requirements, axesNode, parentNode) {
             if (axesNode.type !== 'ObjectExpression') return;
 
             // Object format: axes: { x: { type: '...' }, y: { type: '...' } }
@@ -281,14 +281,14 @@ export default {
                     explicitAxes.add(axisKey);
                 }
 
-                processAxisObject(prop.value, prop);
+                processAxisObject(requirements, prop.value, prop);
             }
         }
 
         /**
          * Process a single axis object
          */
-        function processAxisObject(axisObj, parentNode) {
+        function processAxisObject(requirements, axisObj, parentNode) {
             for (const prop of axisObj.properties) {
                 if (prop.type !== 'Property') continue;
                 const keyName = prop.key.type === 'Identifier' ? prop.key.name : getStringValue(prop.key);
@@ -298,7 +298,7 @@ export default {
                     if (axisType) {
                         const moduleId = axisTypeToModule.get(axisType);
                         if (moduleId) {
-                            requireModule(moduleId, `axis type '${axisType}'`, prop);
+                            requireModule(requirements, moduleId, `axis type '${axisType}'`, prop);
                         }
                     }
                 }
@@ -310,7 +310,7 @@ export default {
                         continue;
                     }
                     const moduleId = axisPluginToModule.get(keyName);
-                    requireModule(moduleId, `axis option '${keyName}'`, prop);
+                    requireModule(requirements, moduleId, `axis option '${keyName}'`, prop);
                 }
             }
         }
@@ -318,14 +318,14 @@ export default {
         /**
          * Process top-level plugin options
          */
-        function processPluginOption(keyName, valueNode, propNode) {
+        function processPluginOption(requirements, keyName, valueNode, propNode) {
             if (pluginOptionToModule.has(keyName)) {
                 // Skip if feature is explicitly disabled (e.g., legend: { enabled: false })
                 if (isFeatureDisabled(valueNode)) {
                     return;
                 }
                 const moduleId = pluginOptionToModule.get(keyName);
-                requireModule(moduleId, `option '${keyName}'`, propNode);
+                requireModule(requirements, moduleId, `option '${keyName}'`, propNode);
             }
 
             // Check for nested options under annotations
@@ -336,7 +336,7 @@ export default {
                         nestedProp.key.type === 'Identifier' ? nestedProp.key.name : getStringValue(nestedProp.key);
                     if (nestedKey && annotationsPluginToModule.has(nestedKey)) {
                         const nestedModuleId = annotationsPluginToModule.get(nestedKey);
-                        requireModule(nestedModuleId, `annotations.${nestedKey} option`, nestedProp);
+                        requireModule(requirements, nestedModuleId, `annotations.${nestedKey} option`, nestedProp);
                     }
                 }
             }
@@ -566,7 +566,7 @@ export default {
         /**
          * Apply default axes based on series types
          */
-        function applyDefaultAxes() {
+        function applyDefaultAxes(requirements) {
             for (const seriesType of seriesTypes) {
                 const defaults = seriesDefaultAxes.get(seriesType);
                 if (defaults) {
@@ -574,26 +574,26 @@ export default {
                     if (defaults.x && !explicitAxes.has('x')) {
                         const moduleId = axisTypeToModule.get(defaults.x);
                         if (moduleId) {
-                            requireModule(moduleId, `default axis for '${seriesType}' series`, null);
+                            requireModule(requirements, moduleId, `default axis for '${seriesType}' series`, null);
                         }
                     }
                     if (defaults.y && !explicitAxes.has('y')) {
                         const moduleId = axisTypeToModule.get(defaults.y);
                         if (moduleId) {
-                            requireModule(moduleId, `default axis for '${seriesType}' series`, null);
+                            requireModule(requirements, moduleId, `default axis for '${seriesType}' series`, null);
                         }
                     }
                     // Polar axes (angle, radius) - only apply if not explicitly defined
                     if (defaults.angle && !explicitAxes.has('angle')) {
                         const moduleId = axisTypeToModule.get(defaults.angle);
                         if (moduleId) {
-                            requireModule(moduleId, `default axis for '${seriesType}' series`, null);
+                            requireModule(requirements, moduleId, `default axis for '${seriesType}' series`, null);
                         }
                     }
                     if (defaults.radius && !explicitAxes.has('radius')) {
                         const moduleId = axisTypeToModule.get(defaults.radius);
                         if (moduleId) {
-                            requireModule(moduleId, `default axis for '${seriesType}' series`, null);
+                            requireModule(requirements, moduleId, `default axis for '${seriesType}' series`, null);
                         }
                     }
                 }
@@ -633,15 +633,16 @@ export default {
 
             // Find options object properties
             Property(node) {
+                const requirements = requiredModules;
                 // Only process top-level properties of objects that look like chart options
                 const keyName = node.key.type === 'Identifier' ? node.key.name : getStringValue(node.key);
                 if (!keyName) return;
 
                 if (keyName === 'series') {
                     // Handle series: [...], series: variable, series: data.map(...)
-                    processSeriesArray(node.value, node);
+                    processSeriesArray(requirements, node.value, node);
                 } else if (keyName === 'axes') {
-                    processAxes(node.value, node);
+                    processAxes(requirements, node.value, node);
                 } else if (keyName === 'axis') {
                     // Sparklines use singular 'axis' - mark all axes as explicit to skip default axis application
                     explicitAxes.add('x');
@@ -650,10 +651,10 @@ export default {
                     explicitAxes.add('radius');
                     // Process the axis type if it's an object with type property
                     if (node.value.type === 'ObjectExpression') {
-                        processAxisObject(node.value, node);
+                        processAxisObject(requirements, node.value, node);
                     }
                 } else if (pluginOptionToModule.has(keyName)) {
-                    processPluginOption(keyName, node.value, node);
+                    processPluginOption(requirements, keyName, node.value, node);
                 } else if (keyName === 'type') {
                     // Handle type properties anywhere in the file
                     const typeValue = getStringValue(node.value);
@@ -673,13 +674,13 @@ export default {
                     if (seriesTypeToModule.has(typeValue)) {
                         seriesTypes.push(typeValue);
                         const moduleId = seriesTypeToModule.get(typeValue);
-                        requireModule(moduleId, `series type '${typeValue}'`, node);
+                        requireModule(requirements, moduleId, `series type '${typeValue}'`, node);
                     }
 
                     // Check if it's an axis type
                     if (axisTypeToModule.has(typeValue)) {
                         const moduleId = axisTypeToModule.get(typeValue);
-                        requireModule(moduleId, `axis type '${typeValue}'`, node);
+                        requireModule(requirements, moduleId, `axis type '${typeValue}'`, node);
                     }
                 }
             },
@@ -687,6 +688,7 @@ export default {
             // Scan ALL string literals in the file for known series/axis types
             // This catches dynamic patterns like: case 'bar':, type === 'bar' ? ...
             Literal(node) {
+                const requirements = requiredModules;
                 if (typeof node.value !== 'string') return;
                 const value = node.value;
 
@@ -697,13 +699,13 @@ export default {
                 if (seriesTypeToModule.has(value)) {
                     seriesTypes.push(value);
                     const moduleId = seriesTypeToModule.get(value);
-                    requireModule(moduleId, `series type '${value}'`, node);
+                    requireModule(requirements, moduleId, `series type '${value}'`, node);
                 }
 
                 // Check if it's a known axis type and require the module
                 if (axisTypeToModule.has(value)) {
                     const moduleId = axisTypeToModule.get(value);
-                    requireModule(moduleId, `axis type '${value}'`, node);
+                    requireModule(requirements, moduleId, `axis type '${value}'`, node);
                 }
             },
 
@@ -715,7 +717,7 @@ export default {
                 }
 
                 // Apply default axes based on series
-                applyDefaultAxes();
+                applyDefaultAxes(requiredModules);
 
                 // Expand bundles in registered modules
                 const expandedRegistered = expandBundles(registeredModules);
