c5cf695707467db1ba586ee23ab27ce62b10b477
James Swinton-Bland <james_swinton@hotmail.co.uk>
2025-12-16 10:56:21 +0000

    Add landing page template

---
+++ packages/ag-charts-website/src/content/docs/bubble-series/_examples/bubble-no-labels/main.ts
@@ -0,0 +1,57 @@
+import { AgChartOptions, AgCharts, LegendModule } from 'ag-charts-community';
+import { BubbleSeriesModule, ModuleRegistry, NumberAxisModule } from 'ag-charts-community';
+
+import { femaleData, maleData } from './data';
+
+ModuleRegistry.registerModules([BubbleSeriesModule, LegendModule, NumberAxisModule]);
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    series: [
+        {
+            type: 'bubble',
+            data: maleData,
+            xKey: 'height',
+            yKey: 'weight',
+            sizeKey: 'age',
+        },
+        {
+            type: 'bubble',
+            data: femaleData,
+            xKey: 'height',
+            yKey: 'weight',
+            sizeKey: 'age',
+        },
+    ],
+    axes: {
+        x: {
+            type: 'number',
+            label: {
+                enabled: false,
+            },
+            gridLine: {
+                enabled: false,
+            },
+            line: {
+                enabled: false,
+            },
+        },
+        y: {
+            type: 'number',
+            label: {
+                enabled: false,
+            },
+            gridLine: {
+                enabled: false,
+            },
+            line: {
+                enabled: false,
+            },
+        },
+    },
+    legend: {
+        enabled: false,
+    },
+};
+
+AgCharts.create(options);
---
+++ packages/ag-charts-website/src/content/docs/line-series/_examples/simple-line-no-labels/main.ts
@@ -0,0 +1,113 @@
+import { AgChartOptions, AgCharts, LegendModule } from 'ag-charts-community';
+import { CategoryAxisModule, LineSeriesModule, ModuleRegistry, NumberAxisModule } from 'ag-charts-community';
+
+import { getData } from './data';
+
+const timeDurationFormatter = (time) => {
+    const hours = Math.floor(time / 60);
+    const minutes = Math.floor(time % 60);
+    return `${hours}h ${minutes < 10 ? '0' : ''}${minutes}m`;
+};
+
+const options = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    theme: {
+        overrides: {
+            line: {
+                series: {
+                    interpolation: { type: 'smooth' },
+                    marker: {
+                        enabled: false,
+                    },
+                    tooltip: {
+                        renderer: ({ datum, yKey, yName, xKey }) => {
+                            return {
+                                heading: `${datum[xKey]} years`,
+                                data: [{ label: yName, value: timeDurationFormatter(datum[yKey]) }],
+                            };
+                        },
+                    },
+                },
+            },
+        },
+    },
+    series: [
+        {
+            type: 'line',
+            xKey: 'age',
+            xName: 'Age',
+            yKey: 'timeSpentWithFriends',
+            yName: 'With Friends',
+        },
+        {
+            type: 'line',
+            xKey: 'age',
+            xName: 'Age',
+            yKey: 'timeSpentWithChildren',
+            yName: 'With Children',
+        },
+        {
+            type: 'line',
+            xKey: 'age',
+            xName: 'Age',
+            yKey: 'timeSpentWithFamily',
+            yName: 'With Family',
+        },
+        {
+            type: 'line',
+            xKey: 'age',
+            xName: 'Age',
+            yKey: 'timeSpentWithPartner',
+            yName: 'With Partner',
+        },
+        {
+            type: 'line',
+            xKey: 'age',
+            xName: 'Age',
+            yKey: 'timeSpentWithCoworkers',
+            yName: 'With Coworkers',
+        },
+    ],
+    axes: {
+        x: {
+            type: 'category',
+            position: 'bottom',
+            bandHighlight: {
+                enabled: true,
+            },
+            label: {
+                enabled: false,
+            },
+            gridLine: {
+                enabled: false,
+            },
+            line: {
+                enabled: false,
+            },
+        },
+        y: {
+            position: 'left',
+            type: 'number',
+            label: {
+                enabled: false,
+            },
+            nice: false,
+            interval: { values: [0, 180, 360, 540] },
+            gridLine: {
+                enabled: false,
+            },
+            line: {
+                enabled: false,
+            },
+        },
+    },
+    tooltip: {
+        mode: 'shared',
+    },
+    legend: {
+        enabled: false,
+    },
+};
+
+AgCharts.create(options);
