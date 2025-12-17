c6b3a805f58db1e485fc209160661045ba285561
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2025-12-17 14:02:13 +0000

    AG-16449 - Bump canvas count

--- packages/ag-charts-community/benchmarks/simpleChart.test.ts
+++ packages/ag-charts-community/benchmarks/simpleChart.test.ts
@@ -23,24 +23,24 @@ describe('simple-chart benchmark', () => {
             await ctx.blur();
         });
 
-        benchmark('1x legend toggle', ctx, { expectedRelativeMB: 3.8, expectedCanvasCount: 4 }, async () => {
+        benchmark('1x legend toggle', ctx, { expectedRelativeMB: 3.8, expectedCanvasCount: 6 }, async () => {
             await ctx.legendToggle();
             await ctx.legendToggle();
         });
 
-        benchmark('10x legend toggle', ctx, { expectedRelativeMB: 1.9, expectedCanvasCount: 4 }, async () => {
+        benchmark('10x legend toggle', ctx, { expectedRelativeMB: 1.9, expectedCanvasCount: 6 }, async () => {
             for (let i = 0; i < 5; i++) {
                 await ctx.legendToggle(i);
                 await ctx.legendToggle(i);
             }
         });
 
-        benchmark('1x datum highlight', ctx, { expectedRelativeMB: 1.9, expectedCanvasCount: 4 }, async () => {
+        benchmark('1x datum highlight', ctx, { expectedRelativeMB: 1.9, expectedCanvasCount: 6 }, async () => {
             const point = ctx.nodePositions[0][1];
             await ctx.hover(point.x, point.y);
         });
 
-        benchmark('15x datum highlight', ctx, { expectedRelativeMB: 1.9, expectedCanvasCount: 4 }, async () => {
+        benchmark('15x datum highlight', ctx, { expectedRelativeMB: 1.9, expectedCanvasCount: 6 }, async () => {
             for (let nodeIdx = 0; nodeIdx < 5; nodeIdx++) {
                 for (let seriesIdx = 0; seriesIdx < 3; seriesIdx++) {
                     const point = ctx.nodePositions[seriesIdx][nodeIdx];
