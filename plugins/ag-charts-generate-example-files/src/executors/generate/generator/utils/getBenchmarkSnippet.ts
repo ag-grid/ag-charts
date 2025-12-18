df0d6f380c2ac6c9c53441f1103ce6f331b87e79
Alan Treadway <alan.treadway@ag-grid.com>
2025-12-18 11:54:00 +0000

    AG-16452 - Break out benchmark.js.

--- plugins/ag-charts-generate-example-files/src/executors/generate/generator/utils/getBenchmarkSnippet.ts
+++ plugins/ag-charts-generate-example-files/src/executors/generate/generator/utils/getBenchmarkSnippet.ts
@@ -4,9 +4,12 @@ export const BENCHMARK_END = '/** BENCHMARK END **/';
 export const getBenchmarkSnippet = () =>
     `${BENCHMARK_START}
 // Benchmark loader - imports and initializes the benchmark harness
-import('./benchmarkHarness.js').then(({ initBenchmark }) => {
-    if (typeof getBenchmarkConfig === 'function') {
-        initBenchmark(getBenchmarkConfig());
-    }
-});
+// Skip in E2E tests to avoid networkidle timeout from dynamic import
+if (!window.location.hash.includes('e2e=true')) {
+    import('./benchmarkHarness.js').then(({ initBenchmark }) => {
+        if (typeof getBenchmarkConfig === 'function') {
+            initBenchmark(getBenchmarkConfig());
+        }
+    });
+}
 ${BENCHMARK_END}`;
