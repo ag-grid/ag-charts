ea53bc5dfd7d8657a00abc945d13e6fdc7f15e4a
Oli Legat <olivier.legat@ag-grid.com>
2026-01-05 15:55:03 +0000

    AG-15850 Add Chart-State types

--- packages/ag-charts-types/src/api/initialStateOptions.ts
+++ packages/ag-charts-types/src/api/initialStateOptions.ts
@@ -13,6 +13,8 @@ export interface AgInitialStateThemeableOptions {
 
 // Options
 export interface AgInitialStateOptions {
+    /** The initial picked item. */
+    picked?: AgPickedState;
     /** The initial set of annotations to display on the chart. */
     annotations?: AgAnnotation[];
     /* The initial chart type. */
@@ -45,4 +47,22 @@ export interface AgInitialStateZoomRange {
 export interface AgInitialStateZoomRatio {
     start?: Ratio;
     end?: Ratio;
+}
+
+export interface AgPickedItemsState {
+    /** The unique identifier of the picked items. */
+    ids: string[];
+    /**
+     * The active picked series datum shapes.
+     *
+     * This is usually `0`, but can be another positive number when tooltip pagination is enabled. The active item corresponds to the item that is currently shown in the tooltip pagination.
+     */
+    activeIndex: number;
+}
+
+export interface AgPickedState {
+    /** The unique identifier of the picked series. */
+    seriesId: string;
+    /** The picked items in this series. If `undefined`, then the entire series is picked. */
+    items?: AgPickedItemsState;
 }
