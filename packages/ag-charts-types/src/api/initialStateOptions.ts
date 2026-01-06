30112c78072fef4af70ebd65274dc142b5136792
Oli Legat <olivier.legat@ag-grid.com>
2026-01-06 12:07:57 +0000

    AG-15850 Update `AgPickedState` to include a `series` array prop
    This will make the state compatible with multi-donuts with shared legends.

--- packages/ag-charts-types/src/api/initialStateOptions.ts
+++ packages/ag-charts-types/src/api/initialStateOptions.ts
@@ -49,22 +49,29 @@ export interface AgInitialStateZoomRatio {
     end?: Ratio;
 }
 
+export interface AgPickedSeriesState {
+    /** The unique identifier of the series that is picked in its entirety. */
+    seriesId: string;
+}
+
 export interface AgPickedItemsState {
-    /** The unique identifier of the picked items. */
-    ids: string[];
+    /** The unique identifier of the series that this picked datum belongs to. */
+    seriesId: string;
+    /** The unique identifier of the picked datum. */
+    itemId: string;
+}
+
+export interface AgPickedState {
     /**
      * The active picked series datum shapes.
      *
      * This is usually `0`, but can be another positive number when tooltip pagination is enabled. The active item corresponds to the item that is currently shown in the tooltip pagination.
      */
-    activeIndex: number;
-}
-
-export interface AgPickedState {
-    /** The unique identifier of the picked series. */
-    seriesId: string;
-    /** The picked items in this series. If `undefined`, then the entire series is picked. */
-    items?: AgPickedItemsState;
+    activeIndex?: number;
+    /** List of series that are picked in their entirety. */
+    series?: AgPickedSeriesState[];
+    /** List of items that are picked individually. If an entire series is picked, then that series will be included in the `series` property. */
+    items?: AgPickedItemsState[];
     /** The frozen state. When the picked item is frozen, user interactions with the chart will be ignored and not updated the currently picked item. */
     frozen: boolean;
 }
