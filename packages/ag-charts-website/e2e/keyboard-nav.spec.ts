f8a8438663497bc653054d8b4bfa25f8d6077334
Oli Legat <olivier.legat@ag-grid.com>
2026-01-08 17:01:25 +0000

    AG-16523 Add `type` to import

--- packages/ag-charts-website/e2e/keyboard-nav.spec.ts
+++ packages/ag-charts-website/e2e/keyboard-nav.spec.ts
@@ -1,4 +1,4 @@
-import { Locator } from '@playwright/test';
+import type { Locator } from '@playwright/test';
 
 import { expect, test } from './fixture';
 import { SELECTORS, gotoExample, repeat, setupIntrinsicAssertions, toExamplePageUrl, toExamplePageUrls } from './util';
