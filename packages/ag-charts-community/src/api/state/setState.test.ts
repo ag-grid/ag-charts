ca9855773c414dbcd148b03e0a82739c6723bb6b
Oli Legat <olivier.legat@ag-grid.com>
2026-01-12 13:21:30 +0000

    AG-15850 Add Jest test

---
+++ packages/ag-charts-community/src/api/state/setState.test.ts
@@ -0,0 +1,61 @@
+import { afterEach, describe, expect } from '@jest/globals';
+
+import type { AgCartesianChartOptions, AgChartInstance } from 'ag-charts-types';
+
+import {
+    IMAGE_SNAPSHOT_DEFAULTS,
+    extractImageData,
+    setupMockCanvas,
+    setupMockConsole,
+    waitForChartStability,
+} from '../../chart/test/utils';
+
+describe('setState', () => {
+    setupMockConsole();
+    const ctx = setupMockCanvas();
+    let chart: AgChartInstance;
+
+    async function compare(): Promise<void> {
+        await waitForChartStability(chart);
+        const imageData = extractImageData(ctx);
+        expect(imageData).toMatchImageSnapshot(IMAGE_SNAPSHOT_DEFAULTS);
+    }
+
+    afterEach(() => {
+        if (chart) {
+            chart.destroy();
+            (chart as unknown) = undefined;
+        }
+    });
+
+    describe('active', () => {
+        function makeOptions(): AgCartesianChartOptions<unknown, unknown> {
+            return {
+                data: [
+                    { Year: '2010', Spain: -50000, UK: 245000, Ireland: -30000, France: 70000, Germany: 128000 },
+                    { Year: '2011', Spain: -120000, UK: 215000, Ireland: -35000, France: 80000, Germany: 279000 },
+                    { Year: '2012', Spain: -150000, UK: 165000, Ireland: -25000, France: 90000, Germany: 369000 },
+                    { Year: '2013', Spain: -90000, UK: 210000, Ireland: -10000, France: 100000, Germany: 429000 },
+                    { Year: '2014', Spain: -30000, UK: 260000, Ireland: 5000, France: 110000, Germany: 550000 },
+                    { Year: '2015', Spain: 10000, UK: 330000, Ireland: 20000, France: 120000, Germany: 1139000 },
+                    { Year: '2016', Spain: 50000, UK: 270000, Ireland: 35000, France: 90000, Germany: 500000 },
+                    { Year: '2017', Spain: 150000, UK: 230000, Ireland: 45000, France: 85000, Germany: 385000 },
+                    { Year: '2018', Spain: 330000, UK: 250000, Ireland: 34000, France: 60000, Germany: 400000 },
+                    { Year: '2019', Spain: 420000, UK: 280000, Ireland: 33000, France: 140000, Germany: 327000 },
+                    { Year: '2020', Spain: -60000, UK: -94000, Ireland: -15000, France: -40000, Germany: 220000 },
+                    { Year: '2021', Spain: 70000, UK: 50000, Ireland: 11000, France: 100000, Germany: 330000 },
+                    { Year: '2022', Spain: 727000, UK: 606000, Ireland: 38000, France: 160000, Germany: 1462000 },
+                    { Year: '2023', Spain: 640000, UK: 672000, Ireland: 45000, France: 180000, Germany: 1000000 },
+                    { Year: '2024', Spain: 550000, UK: 500000, Ireland: 40000, France: 150000, Germany: 750000 },
+                ],
+                series: [
+                    { type: 'line', xKey: 'Year', yKey: 'Spain' },
+                    { type: 'line', xKey: 'Year', yKey: 'UK' },
+                    { type: 'line', xKey: 'Year', yKey: 'Ireland' },
+                    { type: 'line', xKey: 'Year', yKey: 'France' },
+                    { type: 'line', xKey: 'Year', yKey: 'Germany' },
+                ],
+            };
+        }
+    });
+});
