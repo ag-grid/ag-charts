4bf5be5fe1704c2cc57883165762a855f162bdff
Alan Treadway <alan.treadway@ag-grid.com>
2026-01-15 17:42:45 +0000

    AG-13929 - Straighten out ag-charts-test dependencies.

--- packages/ag-charts-core/jest.jsdom-env.cjs
+++ packages/ag-charts-core/jest.jsdom-env.cjs
@@ -2,6 +2,8 @@ const { TestEnvironment } = require('jest-environment-jsdom');
 const { DOMMatrix, Image, Canvas } = require('skia-canvas');
 const timezoneMock = require('timezone-mock');
 
+const { ConfiguredCanvasMixin } = require('ag-charts-core');
+
 /**
  * Timezone-aware jsdom Jest environment. Supports `@timezone` JSDoc
  * pragma within test suites to set timezone.
