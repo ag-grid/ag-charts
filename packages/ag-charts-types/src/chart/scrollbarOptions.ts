db57b849331dc01eb5e8668f8effd19d78403ce9
Mana Peirov <mana.jalali.15@ucl.ac.uk>
2026-01-22 16:55:06 +0000

    AG-7918 - Add comments and defaults to API options

--- packages/ag-charts-types/src/chart/scrollbarOptions.ts
+++ packages/ag-charts-types/src/chart/scrollbarOptions.ts
@@ -7,6 +7,7 @@ export type AgScrollbarPlacement = 'inner' | 'outer';
 export interface AgScrollbarStyle extends FillOptions, StrokeOptions, LineDashOptions {
     /** Apply rounded corners. */
     cornerRadius?: PixelSize;
+    /** The opacity for the scrollbar element. */
     opacity?: number;
 }
 
@@ -20,31 +21,45 @@ export interface AgScrollbarThumbHoverStyle {
 }
 
 export interface AgScrollbarThumbStyle extends AgScrollbarStyle {
+    /** Minimum size of the thumb in pixels */
     minSize?: number;
     /** Styling applied to the thumb on hover. */
     hoverStyle?: AgScrollbarThumbHoverStyle;
 }
 
 export interface AgScrollbarBaseOptions extends Toggleable {
+    /**  Set to `true` to enable the scrollbar. */
+    enabled?: boolean;
+    /**  Thickness of the scrollbar in pixels (height for horizontal, width for vertical). */
     thickness?: number;
+    /**  Spacing in pixels between the scrollbar and the series area or axis labels, depending on placement. */
     spacing?: number;
+    /**  Spacing in pixels between ticks on the scrollbar (if applicable). */
     tickSpacing?: number;
+    /** Styling options for the scrollbar track. */
     track?: AgScrollbarTrackStyle;
+    /** Styling options for the scrollbar thumb. */
     thumb?: AgScrollbarThumbStyle;
+    /** Controls when the scrollbar is shown. */
     visible?: AgScrollbarVisibility;
+    /** Controls whether the scrollbar is placed inside the axis label and ticks or outside. */
     placement?: AgScrollbarPlacement;
 }
 
 export interface AgScrollbarHorizontalOrientationOptions extends AgScrollbarBaseOptions {
+    /** Which horizontal axis position the scrollbar should use. */
     position?: 'top' | 'bottom';
 }
 
 export interface AgScrollbarVerticalOrientationOptions extends AgScrollbarBaseOptions {
+    /** Which vertical axis position the scrollbar should use. */
     position?: 'left' | 'right';
 }
 
 export interface AgScrollbarOptions extends AgScrollbarBaseOptions {
+    /** Options applied to the horizontal scrollbar. */
     horizontal?: AgScrollbarHorizontalOrientationOptions;
+    /** Options applied to the vertical scrollbar. */
     vertical?: AgScrollbarVerticalOrientationOptions;
 }
 
