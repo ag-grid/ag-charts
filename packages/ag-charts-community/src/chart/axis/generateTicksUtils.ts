7907a24779e91aa5347546f086cf308238bb58a9
iMoses <ido.moshe@ag-grid.com>
2026-02-10 09:55:47 +0000

    revert changes & snapshots

--- packages/ag-charts-community/src/chart/axis/generateTicksUtils.ts
+++ packages/ag-charts-community/src/chart/axis/generateTicksUtils.ts
@@ -102,14 +102,11 @@ export function axisLabelsOverlap(data: readonly BoxBounds[], padding: number = 
     const result: BoxBounds[] = [];
 
     for (const datum of data) {
-        const { x, y } = datum;
-        let { width, height } = datum;
-        width += padding;
-        height += padding;
-        if (result.some((l) => boxCollides(l, x, y, width, height))) {
+        const { x, y, width, height } = datum;
+        if (result.some((l) => boxCollides(l, x, y, width + padding, height + padding))) {
             return true;
         }
-        result.push({ x, y, width, height });
+        result.push(datum);
     }
 
     return false;
