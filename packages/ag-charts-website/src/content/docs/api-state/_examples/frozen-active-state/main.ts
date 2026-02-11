2c57b1be896f0f0d8381531282f4f77663493515
Oli Legat <olivier.legat@ag-grid.com>
2026-02-11 17:04:00 +0000

    AG-16732 Add custom tooltip

--- packages/ag-charts-website/src/content/docs/api-state/_examples/frozen-active-state/main.ts
+++ packages/ag-charts-website/src/content/docs/api-state/_examples/frozen-active-state/main.ts
@@ -1,4 +1,5 @@
 import {
+    AgCandlestickSeriesTooltipRendererParams,
     AgCartesianChartOptions,
     AgCharts,
     AnimationModule,
@@ -23,6 +24,45 @@ ModuleRegistry.registerModules([
     TimeAxisModule,
 ]);
 
+let isFrozen = false;
+
+function tooltipRenderer({ datum }: AgCandlestickSeriesTooltipRendererParams<TradeDatum>): string {
+    const date = new Date(datum.date);
+    const heading = date.toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+    });
+
+    const format = (v: number) => `$${v.toFixed(2)}`;
+
+    return `
+        <div class="custom-tooltip">
+          <span>${heading}</span>
+          <div>
+            <span>open</span>
+            <span>${format(datum.open)}</span>
+          </div>
+          <div>
+            <span>high</span>
+            <span>${format(datum.high)}</span>
+          </div>
+          <div>
+            <span>low</span>
+            <span>${format(datum.low)}</span>
+          </div>
+          <div>
+            <span>close</span>
+            <span>${format(datum.close)}</span>
+          </div>
+          <div ${isFrozen ? '' : 'style="display: none"'}>
+            <button type="button" onclick="unfreeze()">Unfreeze</button>
+          </div>
+        </div>`;
+}
+
 const options: AgCartesianChartOptions<TradeDatum> = {
     container: document.getElementById('myChart'),
     title: {
@@ -37,6 +77,31 @@ const options: AgCartesianChartOptions<TradeDatum> = {
             highKey: 'high',
             lowKey: 'low',
             closeKey: 'close',
+            listeners: {
+                seriesNodeClick: (event) => {
+                    isFrozen = true;
+                    chart.setState({
+                        version,
+                        active: {
+                            frozen: true,
+                            activeItem: {
+                                type: 'series-node',
+                                // TODO: should event include `itemId`?
+                                itemId: chart.getState().active.activeItem.itemId,
+                                seriesId: event.seriesId,
+                            },
+                        },
+                    });
+                },
+            },
+            tooltip: {
+                interaction: { enabled: true },
+                position: {
+                    anchorTo: 'chart',
+                    placement: 'top-right',
+                },
+                renderer: tooltipRenderer,
+            },
         },
     ],
     axes: {
@@ -53,3 +118,9 @@ const options: AgCartesianChartOptions<TradeDatum> = {
 };
 
 const chart = AgCharts.create(options);
+const version = chart.getState().version;
+
+function unfreeze() {
+    isFrozen = false;
+    chart.setState({ version, active: { activeItem: chart.getState().active.activeItem, frozen: false } });
+}
