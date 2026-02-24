4460b1ad1019673e0d138f4c7acb11e45e29e60f
Tak Tran <contact@tutaktran.com>
2026-02-24 12:38:47 +0000

    AG-3390 - Prettier formatting

--- packages/ag-charts-website/src/content/docs/benchmarks/_examples/large-scale-multi-series/main.ts
+++ packages/ag-charts-website/src/content/docs/benchmarks/_examples/large-scale-multi-series/main.ts
@@ -1,5 +1,4 @@
 // @ag-skip-fws
-
 /* @ag-options-extract */
 import { AgCartesianChartOptions, AgCharts, AgLineSeriesOptions, VERSION } from 'ag-charts-community';
 
--- packages/ag-charts-website/src/content/docs/highlight-test/_examples/large-scale/main.ts
+++ packages/ag-charts-website/src/content/docs/highlight-test/_examples/large-scale/main.ts
@@ -1,5 +1,4 @@
 // @ag-skip-fws
-
 /* @ag-options-extract */
 import { AgCartesianChartOptions, AgCharts } from 'ag-charts-community';
 
