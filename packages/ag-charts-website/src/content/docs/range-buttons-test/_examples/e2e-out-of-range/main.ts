030c6a888a65e2ae89b0abcd77d2160516df24af
Laurence Roberts <laurence.roberts@ag-grid.com>
2026-03-05 14:46:50 +0000

    AG-16838 Fix out of range example

--- packages/ag-charts-website/src/content/docs/range-buttons-test/_examples/e2e-out-of-range/main.ts
+++ packages/ag-charts-website/src/content/docs/range-buttons-test/_examples/e2e-out-of-range/main.ts
@@ -32,7 +32,7 @@ const options: AgFinancialChartOptions = {
                         { label: 'February 2025 (out)', value: [new Date(2025, 1, 0), new Date(2025, 1, 27)] },
                         {
                             label: 'Window +1 Second (out)',
-                            value: (start, end, windowStart, windowEnd) => {
+                            value: (_start, _end, windowStart, windowEnd) => {
                                 return [windowStart, Number(windowEnd) + 1];
                             },
                         },
