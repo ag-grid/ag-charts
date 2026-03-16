7dd2de37dba7bd2d2cd9934fd0fc284a8f1f2575
Alan Treadway <alan.treadway@ag-grid.com>
2026-03-16 15:23:51 +0000

    AG-16900 Preserve skipInitialRead for queued SizeMonitor observations
    Forward the opts parameter through the pre-document-ready queue so
    sparklines created before document load still skip the synchronous
    getBoundingClientRect.

--- packages/ag-charts-community/src/util/sizeMonitor.ts
+++ packages/ag-charts-community/src/util/sizeMonitor.ts
@@ -20,7 +20,7 @@ export class SizeMonitor {
     private resizeObserver: ResizeObserver | undefined;
     private pixelRatioObserver: PixelRatioObserver | undefined;
     private documentReady = false;
-    private queuedObserveRequests: [HTMLElement, OnSizeChange][] = [];
+    private queuedObserveRequests: [HTMLElement, OnSizeChange, { skipInitialRead?: boolean }?][] = [];
     private removeLoadListener: (() => void) | undefined;
 
     constructor(agDocument: AgDocument) {
@@ -60,8 +60,8 @@ export class SizeMonitor {
 
     onLoad: EventListener = () => {
         this.documentReady = true;
-        for (const [el, cb] of this.queuedObserveRequests) {
-            this.observe(el, cb);
+        for (const [el, cb, opts] of this.queuedObserveRequests) {
+            this.observe(el, cb, opts);
         }
         this.queuedObserveRequests = [];
         this.observeWindow();
@@ -104,7 +104,7 @@ export class SizeMonitor {
     // Only a single callback is supported.
     observe(element: HTMLElement, cb: OnSizeChange, opts?: { skipInitialRead?: boolean }) {
         if (!this.documentReady) {
-            this.queuedObserveRequests.push([element, cb]);
+            this.queuedObserveRequests.push([element, cb, opts]);
             return;
         }
 
