d3a6520dd69e085b2dcc277fbb52722d02b5bac7
Alan Treadway <alan.treadway@ag-grid.com>
2026-03-23 15:48:20 +0000

    CRT-1082 Use Number.NaN to satisfy unicorn/prefer-number-properties

--- packages/ag-charts-enterprise/src/series/range-bar/rangeBarSeries.ts
+++ packages/ag-charts-enterprise/src/series/range-bar/rangeBarSeries.ts
@@ -254,7 +254,7 @@ export class RangeBarSeries extends _ModuleSupport.AbstractBarSeries<RangeBarSer
             extraProps.push(animationValidation());
         }
 
-        const visibleProps = this.visible ? {} : { forceValue: NaN };
+        const visibleProps = this.visible ? {} : { forceValue: Number.NaN };
         const allowNullKey = this.properties.allowNullKeys ?? false;
         const { dataModel, processedData } = await this.requestDataModel(dataController, this.data, {
             props: [
