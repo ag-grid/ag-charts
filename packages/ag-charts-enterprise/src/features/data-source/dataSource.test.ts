2fd11cdb21c2ffb4fcd6127163fc07c96245cc9a
Laurence Roberts <laurence.roberts@ag-grid.com>
2026-03-27 12:33:32 +0000

    AG-11729 Update dataSource and dataWindowProcessor to allow numeric

--- packages/ag-charts-enterprise/src/features/data-source/dataSource.test.ts
+++ packages/ag-charts-enterprise/src/features/data-source/dataSource.test.ts
@@ -15,8 +15,55 @@ import {
     setupMockConsole,
     waitForChartStability,
 } from 'ag-charts-community-test';
+import { isDate } from 'ag-charts-core';
 
 import { prepareEnterpriseTestOptions } from '../../test/utils';
+
+// Note: We set crosshair: { enabled: false } and tooltip: { range: 'exact'} to avoid the highlight
+// styling from being rendered styling because there is a race condition with the clickAction and
+// data-update handling, which sometimes triggers the highlight rendering, and sometimes doesn't. We're
+// not explicitly testing highlight rendering, so this allows us to treat highlighted & unhighlighted
+// charts as equal.
+const BASE_OPTIONS: AgCartesianChartOptions = {
+    tooltip: { range: 'exact' },
+    dataSource: {
+        // @ts-expect-error Set undocumented options to instantly resolve for tests
+        requestThrottle: 0,
+        updateThrottle: 0,
+        updateDuringInteraction: true,
+    },
+    navigator: {
+        enabled: true,
+    },
+    zoom: {
+        scrollingStep: 0.5, // Make sure we zoom enough in a single step so we can detect it
+        minVisibleItems: 1,
+    },
+};
+
+const DATE_OPTIONS: AgCartesianChartOptions = {
+    ...BASE_OPTIONS,
+    axes: {
+        y: { type: 'number', position: 'left', crosshair: { enabled: false } },
+        x: {
+            type: 'time',
+            position: 'bottom',
+            min: new Date('2024-01-01 00:00:00'),
+            max: new Date('2024-01-07 00:00:00'),
+            crosshair: { enabled: false },
+        },
+    },
+    series: [{ type: 'line', xKey: 'time', yKey: 'price' }],
+};
+
+const NUMERIC_OPTIONS: AgCartesianChartOptions = {
+    ...BASE_OPTIONS,
+    axes: {
+        x: { type: 'number', position: 'bottom', crosshair: { enabled: false } },
+        y: { type: 'number', position: 'left', crosshair: { enabled: false } },
+    },
+    series: [{ type: 'line', xKey: 'x', yKey: 'y' }],
+};
 
 describe('DataSource', () => {
     setupMockConsole();
@@ -24,55 +71,12 @@ describe('DataSource', () => {
     let chart: any;
     const ctx = setupMockCanvas();
 
-    // Note: We set crosshair: { enabled: false } and tooltip: { range: 'exact'} to avoid the highlight
-    // styling from being rendered styling because there is a race condition with the clickAction and
-    // data-update handling, which sometimes triggers the highlight rendering, and sometimes doesn't. We're
-    // not explicitly testing highlight rendering, so this allows us to treat highlighted & unhighlighted
-    // charts as equal.
-    const EXAMPLE_OPTIONS: AgCartesianChartOptions = {
-        tooltip: { range: 'exact' },
-        dataSource: {
-            // @ts-expect-error Set undocumented options to instantly resolve for tests
-            requestThrottle: 0,
-            updateThrottle: 0,
-            updateDuringInteraction: true,
-        },
-        axes: {
-            y: {
-                type: 'number',
-                position: 'left',
-                crosshair: { enabled: false },
-            },
-            x: {
-                type: 'time',
-                position: 'bottom',
-                min: new Date('2024-01-01 00:00:00'),
-                max: new Date('2024-01-07 00:00:00'),
-                crosshair: { enabled: false },
-            },
-        },
-        series: [
-            {
-                type: 'line',
-                xKey: 'time',
-                yKey: 'price',
-            },
-        ],
-        navigator: {
-            enabled: true,
-        },
-        zoom: {
-            scrollingStep: 0.5, // Make sure we zoom enough in a single step so we can detect it
-            minVisibleItems: 1,
-        },
-    };
-
     let cx: number = 0;
     let cy: number = 0;
 
     async function prepareChart(
         dataSourceOptions?: AgChartOptions['dataSource'],
-        baseOptions: AgCartesianChartOptions = EXAMPLE_OPTIONS
+        baseOptions: AgCartesianChartOptions = DATE_OPTIONS
     ) {
         const options: AgChartOptions = {
             ...baseOptions,
@@ -100,7 +104,6 @@ describe('DataSource', () => {
 
     const compare = async () => {
         await waitForChartStability(chart);
-        await delay((EXAMPLE_OPTIONS.dataSource as any).requestThrottle);
         await waitForChartStability(chart);
 
         const imageData = extractImageData(ctx);
@@ -139,11 +142,11 @@ describe('DataSource', () => {
                 getData: () => response,
             },
             {
-                ...EXAMPLE_OPTIONS,
+                ...DATE_OPTIONS,
                 axes: {
-                    ...EXAMPLE_OPTIONS.axes!,
+                    ...DATE_OPTIONS.axes!,
                     y: {
-                        ...(EXAMPLE_OPTIONS.axes!.y! as AgNumberAxisOptions),
+                        ...(DATE_OPTIONS.axes!.y! as AgNumberAxisOptions),
                         type: 'number',
                         min: 40,
                         max: 100,
@@ -176,27 +179,26 @@ describe('DataSource', () => {
                 { time: new Date('2024-01-07 00:00:00'), price: 50 },
             ]);
             dataSource = {
-                getData: ({ windowStart, windowEnd }) => {
+                getData: async ({ windowStart, windowEnd }) => {
                     const day = 1000 * 60 * 60 * 24;
-                    return response.then((data) =>
-                        data.filter((d) => {
-                            const time = d.time.getTime();
-                            const isDay = time % day === 0;
-                            const hasWindow =
-                                windowStart != null &&
-                                windowEnd != null &&
-                                windowEnd.getTime() - windowStart.getTime() < day * 4;
-                            const isWindow = hasWindow && time >= windowStart.getTime() && time <= windowEnd.getTime();
-                            return isDay || isWindow;
-                        })
-                    );
+                    const data = await response;
+                    return data.filter((d) => {
+                        const time = d.time.getTime();
+                        const isDay = time % day === 0;
+                        const hasWindow =
+                            isDate(windowStart) &&
+                            isDate(windowEnd) &&
+                            windowEnd.getTime() - windowStart.getTime() < day * 4;
+                        const isWindow = hasWindow && time >= windowStart.getTime() && time <= windowEnd.getTime();
+                        return isDay || isWindow;
+                    });
                 },
             };
         });
 
         it('should load a window at the end', async () => {
             await prepareChart(dataSource, {
-                ...EXAMPLE_OPTIONS,
+                ...DATE_OPTIONS,
                 initialState: { zoom: { ratioX: { start: 0.5, end: 1 } } },
             });
             await response;
@@ -205,7 +207,7 @@ describe('DataSource', () => {
 
         it('should load a window in the middle', async () => {
             await prepareChart(dataSource, {
-                ...EXAMPLE_OPTIONS,
+                ...DATE_OPTIONS,
                 initialState: { zoom: { ratioX: { start: 0.25, end: 0.75 } } },
             });
             await response;
@@ -213,12 +215,27 @@ describe('DataSource', () => {
         });
 
         it('should change the window after a change in zoom', async () => {
-            await prepareChart(dataSource, {
-                ...EXAMPLE_OPTIONS,
-            });
+            await prepareChart(dataSource);
             await response;
             await compare();
             await scrollAction(cx, cy, -1)(chart);
+            await compare();
+        });
+    });
+
+    describe('numeric data', () => {
+        it('should load numeric data', async () => {
+            const response = delay(1).then(() => [
+                { x: 1, y: 0 },
+                { x: 2, y: 50 },
+                { x: 3, y: 25 },
+                { x: 4, y: 75 },
+                { x: 5, y: 50 },
+                { x: 6, y: 25 },
+                { x: 7, y: 50 },
+            ]);
+            await prepareChart({ getData: () => response }, NUMERIC_OPTIONS);
+            await response;
             await compare();
         });
     });
