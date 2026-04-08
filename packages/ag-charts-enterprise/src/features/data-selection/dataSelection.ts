69f17825fef4d91e0debde2860ea605ad4c63633
Oli Legat <olivier.legat@ag-grid.com>
2026-04-08 16:03:50 +0100

    AG-17015 Add properties to module

--- packages/ag-charts-enterprise/src/features/data-selection/dataSelection.ts
+++ packages/ag-charts-enterprise/src/features/data-selection/dataSelection.ts
@@ -1,7 +1,13 @@
 import type { _ModuleSupport } from 'ag-charts-community';
-import { AbstractModuleInstance } from 'ag-charts-core';
+import { AbstractModuleInstance, Property } from 'ag-charts-core';
 
 export class DataSelection extends AbstractModuleInstance {
+    @Property
+    enabled: boolean = false;
+
+    @Property
+    enableClick: boolean = true;
+
     constructor(private readonly ctx: _ModuleSupport.ModuleContext) {
         super();
         this.ctx; // TODO - unused
