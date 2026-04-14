66481ad70f05ce7ec1ffc246b059e3c354f8c431
Oli Legat <olivier.legat@ag-grid.com>
2026-04-14 16:21:02 +0100

    AG-17015 Migrate dataSelection.ts to use chartState.ts

--- packages/ag-charts-community/src/chart/chartState.ts
+++ packages/ag-charts-community/src/chart/chartState.ts
@@ -1,9 +1,12 @@
-import type { NormalisedLegendOptions } from 'ag-charts-core';
+import type { NormalisedLegendOptions, NormalisedSelectionOptions } from 'ag-charts-core';
 import type { AgChartOptions } from 'ag-charts-types';
 
 import type { CategoryLegendDatum } from './legend/legendDatum';
 
-export type ResolvedChartOptions = Omit<AgChartOptions, 'legend'> & { legend: NormalisedLegendOptions };
+export type ResolvedChartOptions = Omit<AgChartOptions, 'legend' | 'selection'> & {
+    legend: NormalisedLegendOptions;
+    selection: NormalisedSelectionOptions;
+};
 
 export interface ChartState {
     options: ResolvedChartOptions;
