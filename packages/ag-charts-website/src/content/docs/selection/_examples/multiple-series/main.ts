34ee038229b04b68567dfc641c29da394a5e1c19
Oli Legat <olivier.legat@ag-grid.com>
2026-04-21 12:59:21 +0100

    AG-17015 Fix `nx validate-examples`

--- packages/ag-charts-website/src/content/docs/selection/_examples/multiple-series/main.ts
+++ packages/ag-charts-website/src/content/docs/selection/_examples/multiple-series/main.ts
@@ -14,6 +14,7 @@ import {
     RangeAreaSeriesModule,
     RangeBarSeriesModule,
     SelectionModule,
+    ZoomModule,
 } from 'ag-charts-enterprise';
 import {
     AgAreaSeriesOptions,
@@ -43,6 +44,7 @@ ModuleRegistry.registerModules([
     RangeAreaSeriesModule,
     RangeBarSeriesModule,
     SelectionModule,
+    ZoomModule,
 ]);
 
 const fullData = getData();
@@ -104,6 +106,7 @@ const options: AgCartesianChartOptions<unknown> = {
             },
         },
     },
+    selection: undefined,
     navigator: {
         enabled: true,
     },
