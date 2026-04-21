1ba8d907544041104bd30582380fe5700b580a3f
Oli Legat <olivier.legat@ag-grid.com>
2026-04-21 15:25:02 +0100

    AG-17082 Remove obsolete assertion

--- packages/ag-charts-enterprise/src/features/data-selection/dataSelection.ts
+++ packages/ag-charts-enterprise/src/features/data-selection/dataSelection.ts
@@ -50,7 +50,7 @@ export class DataSelection extends AbstractModuleInstance {
         if (type !== 'click') return;
 
         const bufferMap: BufferMap | undefined = copySelectionBuffers(this.ctx.chartService);
-        if (clickedNode === undefined || !(clickedNode.series.properties.selection.enabled as boolean)) {
+        if (clickedNode === undefined || !clickedNode.series.properties.selection.enabled) {
             this.clearAllSelections();
         } else {
             const { data } = clickedNode.series;
