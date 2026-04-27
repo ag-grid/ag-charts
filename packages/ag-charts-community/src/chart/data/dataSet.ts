1ad6d71c3b0e843d482b423e91620a31b9d77dcf
Oli Legat <olivier.legat@ag-grid.com>
2026-04-27 14:48:50 +0100

    AG-17004 Fix eslint error unicorn/prefer-number-properties

--- packages/ag-charts-community/src/chart/data/dataSet.ts
+++ packages/ag-charts-community/src/chart/data/dataSet.ts
@@ -178,7 +178,7 @@ export class DataSet<T = unknown> {
 
     getIndexFromItemId(itemId: string | number): number | undefined {
         if (typeof itemId === 'number') {
-            if (isNaN(itemId) || itemId < 0 || itemId >= this.data.length) {
+            if (Number.isNaN(itemId) || itemId < 0 || itemId >= this.data.length) {
                 return undefined;
             } else {
                 return itemId;
