69a179cfc153a3317f1d6b7da69ffbd10fb3c6e7
David Skewis <davidskewis@LX4X6R3K6X-AG-Grid.local>
2026-05-06 10:46:50 +0100

    AG-17139 remove production specific code

--- packages/ag-charts-website/src/pages/robots.txt.ts
+++ packages/ag-charts-website/src/pages/robots.txt.ts
@@ -1,26 +1,6 @@
-import { SITE_URL } from '@constants';
 import { getIsArchive, getIsProduction } from '@utils/env';
-import { pathJoin } from '@utils/pathJoin';
-import { getSitemapIgnorePaths } from '@utils/sitemapPages';
-import { urlWithBaseUrl } from '@utils/urlWithBaseUrl';
 
-const disallowAllRobotsTxt = () => 'User-agent: * Disallow: /';
-
-const productionRobotsTxt = (allowPaths: string[] = [], disallowPaths: string[] = []) => `User-agent: *
-Allow: ${urlWithBaseUrl('/')}
-${allowPaths
-    .map((path) => {
-        return `Allow: ${path}`;
-    })
-    .join('\n')}
-${disallowPaths
-    .map((path) => {
-        return `Disallow: ${path}`;
-    })
-    .join('\n')}
-
-Sitemap: ${pathJoin(SITE_URL, urlWithBaseUrl('/sitemap-0.xml'))}
-`;
+const disallowAllRobotsTxt = () => 'User-agent: *\nDisallow: /';
 
 export async function GET() {
     // Only generate robots.txt in staging environments
@@ -28,13 +8,7 @@ export async function GET() {
         return new Response('Not Found', { status: 404 });
     }
 
-    let output;
-    if (!getIsProduction()) {
-        output = disallowAllRobotsTxt();
-    } else {
-        const ignorePaths = await getSitemapIgnorePaths();
-        output = productionRobotsTxt([], ignorePaths);
-    }
+    const output = disallowAllRobotsTxt();
 
     return new Response(output, {
         status: 200,
