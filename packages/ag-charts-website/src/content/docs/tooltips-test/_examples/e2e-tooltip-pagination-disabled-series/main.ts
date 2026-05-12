bf3e3650bedd3f15e4f842482639f041f8ab7a01
Oli Legat <olivier.legat@ag-grid.com>
2026-05-12 13:53:14 +0100

    AG-16619 Add e2e example for this bug

---
+++ packages/ag-charts-website/src/content/docs/tooltips-test/_examples/e2e-tooltip-pagination-disabled-series/main.ts
@@ -0,0 +1,50 @@
+import type { AgCartesianChartOptions } from 'ag-charts-community';
+import {
+    AgCharts,
+    BubbleSeriesModule,
+    CategoryAxisModule,
+    LegendModule,
+    ModuleRegistry,
+    NumberAxisModule,
+} from 'ag-charts-community';
+
+ModuleRegistry.registerModules([BubbleSeriesModule, CategoryAxisModule, LegendModule, NumberAxisModule]);
+const options: AgCartesianChartOptions = {
+    container: document.getElementById('myChart'),
+    tooltip: { pagination: true },
+    data: [{ x: 1, y: 1, z1: 3, z2: 2, z3: 1 }],
+    series: [
+        {
+            type: 'bubble',
+            xKey: 'x',
+            yKey: 'y',
+            sizeKey: 'z1',
+            domain: [1, 3],
+            size: 30,
+            maxSize: 130,
+            highlight: { enabled: false },
+        },
+        {
+            type: 'bubble',
+            xKey: 'x',
+            yKey: 'y',
+            sizeKey: 'z2',
+            domain: [1, 3],
+            size: 30,
+            maxSize: 130,
+            highlight: { enabled: false },
+            tooltip: { enabled: false },
+        },
+        {
+            type: 'bubble',
+            xKey: 'x',
+            yKey: 'y',
+            sizeKey: 'z3',
+            domain: [1, 3],
+            size: 30,
+            maxSize: 130,
+            highlight: { enabled: false },
+        },
+    ],
+};
+AgCharts.create(options);
