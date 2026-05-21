0b98a579aa4f69135507bd8df7e3dc4541757dca
Oli Legat <olivier.legat@ag-grid.com>
2026-05-21 10:56:21 +0100

    AG-17107 Remove debug logging

--- packages/ag-charts-enterprise/src/features/data-selection/dataSelectionServiceImp.ts
+++ packages/ag-charts-enterprise/src/features/data-selection/dataSelectionServiceImp.ts
@@ -3,9 +3,6 @@ import type { _ModuleSupport } from 'ag-charts-community';
 type SeriesLike = { id: string; data: _ModuleSupport.DataSet<unknown> | undefined };
 
 export class DataSelectionServiceImp implements _ModuleSupport.DataSelectionService {
-    constructor() {
-        console.log('debug init');
-    }
     getSeriesSelectedCount(series: SeriesLike): number {
         return series.data?.selections?.get(series.id)?.getSelectedCount() ?? 0;
     }
