02b34e75abdddb9c11a1f7beb88434122dc66282
Tak Tran <contact@tutaktran.com>
2026-05-22 14:54:40 +0100

    AG-3390 - Update staging paths for charts

--- tools/staging/updateChartsStagingRemote.sh
+++ tools/staging/updateChartsStagingRemote.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Cleaning current charts staging"
-rm -rf @WWW_ROOT_DIR@/html/*
-mv @FILENAME@ @WWW_ROOT_DIR@/html/
+rm -rf @WWW_ROOT_DIR@/charts/*
+mv @FILENAME@ @WWW_ROOT_DIR@/charts/
 
 echo "Unzipping new charts staging"
-unzip -q @WWW_ROOT_DIR@/html/@FILENAME@ -d @WWW_ROOT_DIR@/html/
+unzip -q @WWW_ROOT_DIR@/charts/@FILENAME@ -d @WWW_ROOT_DIR@/charts/
