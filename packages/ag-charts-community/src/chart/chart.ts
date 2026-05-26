3937b8f4f840a763ab06ba3208004d399057b18b
Oli Legat <olivier.legat@ag-grid.com>
2026-05-26 12:33:21 +0100

    AG-17107 Fix declaration-order depend z-ordering

--- packages/ag-charts-community/src/chart/chart.ts
+++ packages/ag-charts-community/src/chart/chart.ts
@@ -1989,7 +1989,8 @@ export abstract class Chart extends Observable implements ModuleInstance, ChartS
     private initSeriesDeclarationOrder(series: UnknownSeries[]) {
         // Ensure declaration order is set, this is used for correct z-index behaviour for combo charts.
         for (let idx = 0; idx < series.length; idx++) {
-            series[idx].setSeriesIndex(idx);
+            const declaractionOrder = series[idx].seriesGrouping?.declarationOrder ?? idx;
+            series[idx].setSeriesIndex(declaractionOrder);
         }
     }
 
