baa7d1fe2c6e3faceef889486af2145b82f801a4
Oli Legat <olivier.legat@ag-grid.com>
2026-05-26 12:28:19 +0100

    AG-17107 Iterate getSelection() items by series declaraction order

--- packages/ag-charts-enterprise/src/features/data-selection/dataSelectionService.ts
+++ packages/ag-charts-enterprise/src/features/data-selection/dataSelectionService.ts
@@ -4,7 +4,7 @@ import type { DynamicContext } from 'ag-charts-core';
 import type { DataSetSelectionsIterator } from './dataSelectionUtil';
 import { DataSetSelection } from './dataSetSelection';
 
-const { SelectionState } = _ModuleSupport;
+const { SelectionState, iterateSeriesByFocusOrder } = _ModuleSupport;
 
 type ChartRegistry = _ModuleSupport.ChartRegistry;
 type DataChangeDescription = _ModuleSupport.DataChangeDescription;
@@ -113,7 +113,7 @@ export class DataSelectionService implements IDataSelectionService {
     *iterateDataSetSelections(): Generator<DataSetSelectionsIterator> {
         const it: DataSetSelectionsIterator = {} as any;
 
-        for (const series of this.ctx.chartService.series) {
+        for (const series of iterateSeriesByFocusOrder(this.ctx.chartService.series)) {
             const seriesId = series.id;
             const dataSet = series.data;
             const selection = this.selections.get(series.id);
