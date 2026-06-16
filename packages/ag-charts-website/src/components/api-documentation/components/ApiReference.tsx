d07c70e26fd5f9a74fb5221c4a13f1a69a32d763
David Skewis <david.skewis@ag-grid.com>
2026-06-16 11:43:56 +0100

    AG-17568 add support for code block links

--- packages/ag-charts-website/src/components/api-documentation/components/ApiReference.tsx
+++ packages/ag-charts-website/src/components/api-documentation/components/ApiReference.tsx
@@ -14,7 +14,7 @@ import { fetchInterfacesReference } from '@utils/client/fetchInterfacesReference
 import { useToggle } from '@utils/hooks/useToggle';
 import { urlWithBaseUrl } from '@utils/urlWithBaseUrl';
 import classnames from 'classnames';
-import { type AllHTMLAttributes, type CSSProperties, createContext, useContext, useEffect } from 'react';
+import { Children, isValidElement, type AllHTMLAttributes, type CSSProperties, createContext, useContext, useEffect } from 'react';
 import Markdown from 'react-markdown';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import remarkBreaks from 'remark-breaks';
@@ -278,7 +278,22 @@ function ApiReferenceRow({
             </div>
             <div className={styles.rightColumn}>
                 <div role="presentation" className={styles.description}>
-                    <Markdown remarkPlugins={[remarkBreaks]} urlTransform={(url: string) => urlWithBaseUrl(url)}>
+                    <Markdown
+                        remarkPlugins={[remarkBreaks]}
+                        urlTransform={(url: string) => urlWithBaseUrl(url)}
+                        components={{
+                            a({ children, href, ...props }) {
+                                const hasCode = Children.toArray(children).some(
+                                    (child) => isValidElement(child) && child.type === 'code'
+                                );
+                                return (
+                                    <a href={href} className={hasCode ? 'meta-link' : undefined} {...props}>
+                                        {children}
+                                    </a>
+                                );
+                            },
+                        }}
+                    >
                         {parseJsDocs(member.docs)}
                     </Markdown>
                     {hasChildProps && (
