feff365eec594200383ad4a2fa8af448c866e6b0
Oli Legat <olivier.legat@ag-grid.com>
2026-09-10 15:19:58 +0100

    AG-18361 Make MouseWidgetEvent native-only not synthetics

--- packages/ag-charts-community/src/module-support.ts
+++ packages/ag-charts-community/src/module-support.ts
@@ -486,7 +486,6 @@ export { SliderWidget } from './widget/sliderWidget';
 export { ToolbarWidget } from './widget/toolbarWidget';
 export type {
     DragWidgetEvent,
-    HoverLikeEvent,
     KeyboardWidgetEvent,
     MouseWidgetEvent,
     WheelWidgetEvent,
