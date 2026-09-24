0b7393be2194bf538d22f1c5be0377da51d5ea8d
Oli Legat <olivier.legat@ag-grid.com>
2026-09-24 10:35:36 +0100

    AG-16824 Temporary node-index / datum-index workaround
    The implementation of `BarSeries.computeFocusBounds` looks correct. However the
    DataModelSeries base class is feeding in node-indices instead of datum-indices.
    
    Temporarily convert these node-indices into datum-indices.

--- packages/ag-charts-community/src/chart/series/cartesian/barSeries.ts
+++ packages/ag-charts-community/src/chart/series/cartesian/barSeries.ts
@@ -2195,7 +2195,10 @@ export class BarSeries extends AbstractBarSeries<BarSeriesTypes> {
         return this.options.label.enabled;
     }
 
-    protected computeFocusBounds({ datumIndex }: PickFocusInputs): BBox | undefined {
+    protected computeFocusBounds(opts: PickFocusInputs): BBox | undefined {
+        const datumIndex = this.contextNodeData?.nodeData[opts.datumIndex].datumIndex;
+        if (datumIndex == null) return undefined;
+
         const ctx = this.nodeDatumContext;
         if (ctx == null || this.processedData?.invalidData?.get(this.id)?.[datumIndex] === true) return undefined;
 
